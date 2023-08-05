bee7eee0826e023ce3e477aae1f34e8b6aface08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:57 +0000

    Add package badger-opt/0.2/badger-opt-0.2.tar.gz

---
+++ packages/badger-opt/badger-opt-0.2.tar.gz/badger-opt-0.2/src/badger/gui/__init__.py
@@ -0,0 +1,10 @@
+from PyQt6.QtWidgets import QApplication
+import sys
+from .windows.main_window import BadgerMainWindow
+
+def launch_gui():
+    app = QApplication(sys.argv)
+
+    window = BadgerMainWindow()
+    window.show()
+    sys.exit(app.exec())
