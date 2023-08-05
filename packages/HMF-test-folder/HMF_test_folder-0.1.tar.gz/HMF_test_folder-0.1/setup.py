d4ac58a6a2b76689694b829bdfd15262c0764d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:46 +0000

    Add package HMF-test-folder/0.1/HMF_test_folder-0.1.tar.gz

---
+++ packages/HMF-test-folder/HMF_test_folder-0.1.tar.gz/HMF_test_folder-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='HMF_test_folder',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['HMF_test_folder'],
+      zip_safe=False)
