95ccd216c2931f8b8e60a95354514451d5b6e867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:55 +0000

    Add package pptb/0.1.4a1/pptb-0.1.4a1-py3-none-any.whl

---
+++ packages/pptb/pptb-0.1.4a1-py3-none-any.whl/pptb/vision/models/__init__.py
@@ -0,0 +1,34 @@
+# GoogLeNet
+from .googlenet import GoogLeNet
+from .googlenet import googlenet
+
+# InceptionV3
+from .inceptionv3 import InceptionV3
+from .inceptionv3 import inception_v3
+
+# ResNeXt
+from .resnext import ResNeXt
+from .resnext import resnext50_32x4d
+from .resnext import resnext50_64x4d
+from .resnext import resnext101_32x4d
+from .resnext import resnext101_64x4d
+from .resnext import resnext152_32x4d
+from .resnext import resnext152_64x4d
+
+# ShuffleNetV2
+from .shufflenetv2 import ShuffleNetV2
+from .shufflenetv2 import shufflenet_v2_x0_25
+from .shufflenetv2 import shufflenet_v2_x0_33
+from .shufflenetv2 import shufflenet_v2_x0_5
+from .shufflenetv2 import shufflenet_v2_x1_0
+from .shufflenetv2 import shufflenet_v2_x1_5
+from .shufflenetv2 import shufflenet_v2_x2_0
+from .shufflenetv2 import shufflenet_v2_swish
+
+# ConvMixer
+from .convmixer import convmixer_768_32
+from .convmixer import convmixer_1024_20_ks9_p14
+from .convmixer import convmixer_1536_20
+
+# DenseNet
+from .densenet import *
