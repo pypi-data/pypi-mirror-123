2b3c79694cd0d6c8a4e8995c581498a349c151a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:03 +0000

    Add package distributions-shenderovv/0.1/distributions_shenderovv-0.1.tar.gz

---
+++ packages/distributions-shenderovv/distributions_shenderovv-0.1.tar.gz/distributions_shenderovv-0.1/README.md
@@ -0,0 +1,4 @@
+# This is the Readme file
+
+# It should contain the information about distributions package, but it doesn't
+# Really sorry
---
+++ packages/distributions-shenderovv/distributions_shenderovv-0.1.tar.gz/distributions_shenderovv-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_shenderovv',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_shenderovv'],
+      zip_safe=False)
