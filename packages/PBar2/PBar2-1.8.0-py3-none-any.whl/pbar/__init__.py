79658cdbcc05152a2dd929ca66b47f56deb309aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package PBar2/1.8.0/PBar2-1.8.0-py3-none-any.whl

---
+++ packages/PBar2/PBar2-1.8.0-py3-none-any.whl/pbar/__init__.py
@@ -0,0 +1,12 @@
+"""
+![logo_small](https://user-images.githubusercontent.com/48654552/134991429-1109ad1d-92fa-423f-8ce1-3b777471cb3d.png)
+
+### PBar module for displaying custom progress bars for Python 3.9+
+
+[GitHub Repository](https://github.com/DarviL82/PBar)
+"""
+
+__version__ = "1.8.0"
+
+from . bar import PBar, animate, taskWrapper, barHelper
+from . sets import CharSet, FormatSet, ColorSet
