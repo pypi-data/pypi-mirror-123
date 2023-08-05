3b1021ae4e1d5f062a06fdb6bffcca627c1457ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:45 +0000

    Add package skyhub/1.1.11/skyhub-1.1.11.tar.gz

---
+++ packages/skyhub/skyhub-1.1.11.tar.gz/skyhub-1.1.11/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['skyhub', 'skyhub.controllers', 'skyhub.models', 'skyhub.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dynaconf>=3.1.7,<4.0.0', 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'skyhub',
+    'version': '1.1.11',
+    'description': '',
+    'long_description': None,
+    'author': 'Carmo-sousa',
+    'author_email': 'carmosousa20@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
