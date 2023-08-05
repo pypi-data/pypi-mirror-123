59cca974754cea66e3fbe494e63982d32d9188fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package probability-distribution-functions/0.1/probability_distribution_functions-0.1.tar.gz

---
+++ packages/probability-distribution-functions/probability_distribution_functions-0.1.tar.gz/probability_distribution_functions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distribution_functions',
+      version='0.1',
+      description='Data Distributions: calculates and plots Gaussian & Binomial Distributions from a data file.',
+      packages=['probability_distribution_functions'],
+	  author = 'Sheryl Hohman',
+	  author_email = 'sherylhohman@yahoo.com',
+      zip_safe=False)
