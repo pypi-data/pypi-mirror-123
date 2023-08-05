5743b32e6f7dae32686645aa27a85bd42f4ca34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:33 +0000

    Add package google-api-python-client-stubs/1.7.1/google-api-python-client-stubs-1.7.1.tar.gz

---
+++ packages/google-api-python-client-stubs/google-api-python-client-stubs-1.7.1.tar.gz/google-api-python-client-stubs-1.7.1/googleapiclient-stubs/_apis/gkehub/v1alpha/schemas.pyi
@@ -0,0 +1,451 @@
+import typing
+
+import typing_extensions
+
+_list = list
+
+@typing.type_check_only
+class AuditConfig(typing_extensions.TypedDict, total=False):
+    auditLogConfigs: _list[AuditLogConfig]
+    service: str
+
+@typing.type_check_only
+class AuditLogConfig(typing_extensions.TypedDict, total=False):
+    exemptedMembers: _list[str]
+    logType: typing_extensions.Literal[
+        "LOG_TYPE_UNSPECIFIED", "ADMIN_READ", "DATA_WRITE", "DATA_READ"
+    ]
+
+@typing.type_check_only
+class Binding(typing_extensions.TypedDict, total=False):
+    condition: Expr
+    members: _list[str]
+    role: str
+
+@typing.type_check_only
+class CancelOperationRequest(typing_extensions.TypedDict, total=False): ...
+
+@typing.type_check_only
+class CloudAuditLoggingFeatureSpec(typing_extensions.TypedDict, total=False):
+    allowlistedServiceAccounts: _list[str]
+
+@typing.type_check_only
+class CommonFeatureSpec(typing_extensions.TypedDict, total=False):
+    cloudauditlogging: CloudAuditLoggingFeatureSpec
+    multiclusteringress: MultiClusterIngressFeatureSpec
+
+@typing.type_check_only
+class CommonFeatureState(typing_extensions.TypedDict, total=False):
+    servicemesh: ServiceMeshFeatureState
+    state: FeatureState
+
+@typing.type_check_only
+class ConfigManagementBinauthzConfig(typing_extensions.TypedDict, total=False):
+    enabled: bool
+
+@typing.type_check_only
+class ConfigManagementBinauthzState(typing_extensions.TypedDict, total=False):
+    version: ConfigManagementBinauthzVersion
+    webhook: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+
+@typing.type_check_only
+class ConfigManagementBinauthzVersion(typing_extensions.TypedDict, total=False):
+    webhookVersion: str
+
+@typing.type_check_only
+class ConfigManagementConfigSync(typing_extensions.TypedDict, total=False):
+    git: ConfigManagementGitConfig
+    resourceRequirements: dict[str, typing.Any]
+    sourceFormat: str
+
+@typing.type_check_only
+class ConfigManagementConfigSyncDeploymentState(
+    typing_extensions.TypedDict, total=False
+):
+    admissionWebhook: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    gitSync: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    importer: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    monitor: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    reconcilerManager: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    rootReconciler: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    syncer: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+
+@typing.type_check_only
+class ConfigManagementConfigSyncState(typing_extensions.TypedDict, total=False):
+    deploymentState: ConfigManagementConfigSyncDeploymentState
+    syncState: ConfigManagementSyncState
+    version: ConfigManagementConfigSyncVersion
+
+@typing.type_check_only
+class ConfigManagementConfigSyncVersion(typing_extensions.TypedDict, total=False):
+    admissionWebhook: str
+    gitSync: str
+    importer: str
+    monitor: str
+    reconcilerManager: str
+    rootReconciler: str
+    syncer: str
+
+@typing.type_check_only
+class ConfigManagementContainerResourceRequirements(
+    typing_extensions.TypedDict, total=False
+):
+    containerName: str
+    cpuLimit: ConfigManagementQuantity
+    memoryLimit: ConfigManagementQuantity
+
+@typing.type_check_only
+class ConfigManagementErrorResource(typing_extensions.TypedDict, total=False):
+    resourceGvk: ConfigManagementGroupVersionKind
+    resourceName: str
+    resourceNamespace: str
+    sourcePath: str
+
+@typing.type_check_only
+class ConfigManagementGatekeeperDeploymentState(
+    typing_extensions.TypedDict, total=False
+):
+    gatekeeperAudit: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    gatekeeperControllerManagerState: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    gatekeeperMutation: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+
+@typing.type_check_only
+class ConfigManagementGitConfig(typing_extensions.TypedDict, total=False):
+    gcpServiceAccountEmail: str
+    httpsProxy: str
+    noSslVerify: bool
+    policyDir: str
+    secretType: str
+    syncBranch: str
+    syncDepth: str
+    syncRepo: str
+    syncRev: str
+    syncWaitSecs: str
+
+@typing.type_check_only
+class ConfigManagementGroupVersionKind(typing_extensions.TypedDict, total=False):
+    group: str
+    kind: str
+    version: str
+
+@typing.type_check_only
+class ConfigManagementHierarchyControllerConfig(
+    typing_extensions.TypedDict, total=False
+):
+    enableHierarchicalResourceQuota: bool
+    enablePodTreeLabels: bool
+    enabled: bool
+
+@typing.type_check_only
+class ConfigManagementHierarchyControllerDeploymentState(
+    typing_extensions.TypedDict, total=False
+):
+    extension: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    hnc: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+
+@typing.type_check_only
+class ConfigManagementHierarchyControllerState(
+    typing_extensions.TypedDict, total=False
+):
+    state: ConfigManagementHierarchyControllerDeploymentState
+    version: ConfigManagementHierarchyControllerVersion
+
+@typing.type_check_only
+class ConfigManagementHierarchyControllerVersion(
+    typing_extensions.TypedDict, total=False
+):
+    extension: str
+    hnc: str
+
+@typing.type_check_only
+class ConfigManagementInstallError(typing_extensions.TypedDict, total=False):
+    errorMessage: str
+
+@typing.type_check_only
+class ConfigManagementMembershipSpec(typing_extensions.TypedDict, total=False):
+    binauthz: ConfigManagementBinauthzConfig
+    configSync: ConfigManagementConfigSync
+    hierarchyController: ConfigManagementHierarchyControllerConfig
+    policyController: ConfigManagementPolicyController
+    version: str
+
+@typing.type_check_only
+class ConfigManagementMembershipState(typing_extensions.TypedDict, total=False):
+    binauthzState: ConfigManagementBinauthzState
+    clusterName: str
+    configSyncState: ConfigManagementConfigSyncState
+    hierarchyControllerState: ConfigManagementHierarchyControllerState
+    membershipSpec: ConfigManagementMembershipSpec
+    operatorState: ConfigManagementOperatorState
+    policyControllerState: ConfigManagementPolicyControllerState
+
+@typing.type_check_only
+class ConfigManagementOperatorState(typing_extensions.TypedDict, total=False):
+    deploymentState: typing_extensions.Literal[
+        "DEPLOYMENT_STATE_UNSPECIFIED", "NOT_INSTALLED", "INSTALLED", "ERROR"
+    ]
+    errors: _list[ConfigManagementInstallError]
+    version: str
+
+@typing.type_check_only
+class ConfigManagementPolicyController(typing_extensions.TypedDict, total=False):
+    auditIntervalSeconds: str
+    enabled: bool
+    exemptableNamespaces: _list[str]
+    logDeniesEnabled: bool
+    mutationEnabled: bool
+    referentialRulesEnabled: bool
+    templateLibraryInstalled: bool
+
+@typing.type_check_only
+class ConfigManagementPolicyControllerState(typing_extensions.TypedDict, total=False):
+    deploymentState: ConfigManagementGatekeeperDeploymentState
+    version: ConfigManagementPolicyControllerVersion
+
+@typing.type_check_only
+class ConfigManagementPolicyControllerVersion(typing_extensions.TypedDict, total=False):
+    version: str
+
+@typing.type_check_only
+class ConfigManagementQuantity(typing_extensions.TypedDict, total=False):
+    string: str
+
+@typing.type_check_only
+class ConfigManagementSyncError(typing_extensions.TypedDict, total=False):
+    code: str
+    errorMessage: str
+    errorResources: _list[ConfigManagementErrorResource]
+
+@typing.type_check_only
+class ConfigManagementSyncState(typing_extensions.TypedDict, total=False):
+    code: typing_extensions.Literal[
+        "SYNC_CODE_UNSPECIFIED",
+        "SYNCED",
+        "PENDING",
+        "ERROR",
+        "NOT_CONFIGURED",
+        "NOT_INSTALLED",
+        "UNAUTHORIZED",
+        "UNREACHABLE",
+    ]
+    errors: _list[ConfigManagementSyncError]
+    importToken: str
+    lastSync: str
+    lastSyncTime: str
+    sourceToken: str
+    syncToken: str
+
+@typing.type_check_only
+class Empty(typing_extensions.TypedDict, total=False): ...
+
+@typing.type_check_only
+class Expr(typing_extensions.TypedDict, total=False):
+    description: str
+    expression: str
+    location: str
+    title: str
+
+@typing.type_check_only
+class Feature(typing_extensions.TypedDict, total=False):
+    createTime: str
+    deleteTime: str
+    labels: dict[str, typing.Any]
+    membershipSpecs: dict[str, typing.Any]
+    membershipStates: dict[str, typing.Any]
+    name: str
+    resourceState: FeatureResourceState
+    spec: CommonFeatureSpec
+    state: CommonFeatureState
+    updateTime: str
+
+@typing.type_check_only
+class FeatureResourceState(typing_extensions.TypedDict, total=False):
+    state: typing_extensions.Literal[
+        "STATE_UNSPECIFIED",
+        "ENABLING",
+        "ACTIVE",
+        "DISABLING",
+        "UPDATING",
+        "SERVICE_UPDATING",
+    ]
+
+@typing.type_check_only
+class FeatureState(typing_extensions.TypedDict, total=False):
+    code: typing_extensions.Literal["CODE_UNSPECIFIED", "OK", "WARNING", "ERROR"]
+    description: str
+    updateTime: str
+
+@typing.type_check_only
+class GoogleRpcStatus(typing_extensions.TypedDict, total=False):
+    code: int
+    details: _list[dict[str, typing.Any]]
+    message: str
+
+@typing.type_check_only
+class IdentityServiceAuthMethod(typing_extensions.TypedDict, total=False):
+    name: str
+    oidcConfig: IdentityServiceOidcConfig
+    proxy: str
+
+@typing.type_check_only
+class IdentityServiceMembershipSpec(typing_extensions.TypedDict, total=False):
+    authMethods: _list[IdentityServiceAuthMethod]
+
+@typing.type_check_only
+class IdentityServiceMembershipState(typing_extensions.TypedDict, total=False):
+    failureReason: str
+    installedVersion: str
+    memberConfig: IdentityServiceMembershipSpec
+    state: typing_extensions.Literal["DEPLOYMENT_STATE_UNSPECIFIED", "OK", "ERROR"]
+
+@typing.type_check_only
+class IdentityServiceOidcConfig(typing_extensions.TypedDict, total=False):
+    certificateAuthorityData: str
+    clientId: str
+    deployCloudConsoleProxy: bool
+    extraParams: str
+    groupPrefix: str
+    groupsClaim: str
+    issuerUri: str
+    kubectlRedirectUri: str
+    scopes: str
+    userClaim: str
+    userPrefix: str
+
+@typing.type_check_only
+class ListFeaturesResponse(typing_extensions.TypedDict, total=False):
+    nextPageToken: str
+    resources: _list[Feature]
+
+@typing.type_check_only
+class ListLocationsResponse(typing_extensions.TypedDict, total=False):
+    locations: _list[Location]
+    nextPageToken: str
+
+@typing.type_check_only
+class ListOperationsResponse(typing_extensions.TypedDict, total=False):
+    nextPageToken: str
+    operations: _list[Operation]
+
+@typing.type_check_only
+class Location(typing_extensions.TypedDict, total=False):
+    displayName: str
+    labels: dict[str, typing.Any]
+    locationId: str
+    metadata: dict[str, typing.Any]
+    name: str
+
+@typing.type_check_only
+class MembershipFeatureSpec(typing_extensions.TypedDict, total=False):
+    configmanagement: ConfigManagementMembershipSpec
+    identityservice: IdentityServiceMembershipSpec
+
+@typing.type_check_only
+class MembershipFeatureState(typing_extensions.TypedDict, total=False):
+    configmanagement: ConfigManagementMembershipState
+    identityservice: IdentityServiceMembershipState
+    metering: MeteringMembershipState
+    servicemesh: ServiceMeshMembershipState
+    state: FeatureState
+
+@typing.type_check_only
+class MeteringMembershipState(typing_extensions.TypedDict, total=False):
+    lastMeasurementTime: str
+    preciseLastMeasuredClusterVcpuCapacity: float
+
+@typing.type_check_only
+class MultiClusterIngressFeatureSpec(typing_extensions.TypedDict, total=False):
+    billing: typing_extensions.Literal[
+        "BILLING_UNSPECIFIED", "PAY_AS_YOU_GO", "ANTHOS_LICENSE"
+    ]
+    configMembership: str
+
+@typing.type_check_only
+class Operation(typing_extensions.TypedDict, total=False):
+    done: bool
+    error: GoogleRpcStatus
+    metadata: dict[str, typing.Any]
+    name: str
+    response: dict[str, typing.Any]
+
+@typing.type_check_only
+class OperationMetadata(typing_extensions.TypedDict, total=False):
+    apiVersion: str
+    cancelRequested: bool
+    createTime: str
+    endTime: str
+    statusDetail: str
+    target: str
+    verb: str
+
+@typing.type_check_only
+class Policy(typing_extensions.TypedDict, total=False):
+    auditConfigs: _list[AuditConfig]
+    bindings: _list[Binding]
+    etag: str
+    version: int
+
+@typing.type_check_only
+class ServiceMeshAnalysisMessage(typing_extensions.TypedDict, total=False):
+    args: dict[str, typing.Any]
+    description: str
+    messageBase: ServiceMeshAnalysisMessageBase
+    resourcePaths: _list[str]
+
+@typing.type_check_only
+class ServiceMeshAnalysisMessageBase(typing_extensions.TypedDict, total=False):
+    documentationUrl: str
+    level: typing_extensions.Literal["LEVEL_UNSPECIFIED", "ERROR", "WARNING", "INFO"]
+    type: ServiceMeshType
+
+@typing.type_check_only
+class ServiceMeshFeatureState(typing_extensions.TypedDict, total=False):
+    analysisMessages: _list[ServiceMeshAnalysisMessage]
+
+@typing.type_check_only
+class ServiceMeshMembershipState(typing_extensions.TypedDict, total=False):
+    analysisMessages: _list[ServiceMeshAnalysisMessage]
+
+@typing.type_check_only
+class ServiceMeshType(typing_extensions.TypedDict, total=False):
+    code: str
+    displayName: str
+
+@typing.type_check_only
+class SetIamPolicyRequest(typing_extensions.TypedDict, total=False):
+    policy: Policy
+    updateMask: str
+
+@typing.type_check_only
+class TestIamPermissionsRequest(typing_extensions.TypedDict, total=False):
+    permissions: _list[str]
+
+@typing.type_check_only
+class TestIamPermissionsResponse(typing_extensions.TypedDict, total=False):
+    permissions: _list[str]
