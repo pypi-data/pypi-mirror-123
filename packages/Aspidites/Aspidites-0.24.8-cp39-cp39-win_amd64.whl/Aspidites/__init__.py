a3adc6af620b7d75e1c102fe84eeeec93dbac58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:19 +0000

    Add package Aspidites/0.24.8/Aspidites-0.24.8-cp39-cp39-win_amd64.whl

---
+++ packages/Aspidites/Aspidites-0.24.8-cp39-cp39-win_amd64.whl/Aspidites/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.24.8'
+__title__ = 'Aspidites'
+__author__ = 'Ross J. Duff'
+__license__ = "GPL"
+__description__ = (
+    f"""
+    {__title__} v{__version__} is the reference Woma programming language compiler.
+    Copyright (C) 2021  {__author__}
+    This program comes with ABSOLUTELY NO WARRANTY; 
+    This is free software, and you are welcome to redistribute it
+    under the conditions of the {__license__}v3 found in the LICENSE file.
+    """
+)
+__mimetype__ = 'text/woma'
