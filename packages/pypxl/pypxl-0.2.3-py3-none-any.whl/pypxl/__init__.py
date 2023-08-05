33b27e7bfbffc6282f097b0bb0be297cd218b0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package pypxl/0.2.3/pypxl-0.2.3-py3-none-any.whl

---
+++ packages/pypxl/pypxl-0.2.3-py3-none-any.whl/pypxl/__init__.py
@@ -0,0 +1,14 @@
+"""
+pypxl - an asyncronous wrapper for pxlapi (pxlapi.dev)
+
+:copyright: (c) 2021 Kile
+:license: MIT, see LICENSE for more details
+"""
+
+__title__ = "pypxl"
+__author__ = "Kile"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 Kile"
+__version__ = "0.2.3"
+
+from .client import PxlClient
