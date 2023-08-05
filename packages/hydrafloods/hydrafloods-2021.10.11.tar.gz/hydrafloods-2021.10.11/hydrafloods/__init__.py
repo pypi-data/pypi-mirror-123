26dd01786b4811eb0323a6c04738fc09db9eaddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:32 +0000

    Add package hydrafloods/2021.10.11/hydrafloods-2021.10.11.tar.gz

---
+++ packages/hydrafloods/hydrafloods-2021.10.11.tar.gz/hydrafloods-2021.10.11/hydrafloods/__init__.py
@@ -0,0 +1,14 @@
+from hydrafloods.datasets import *
+from hydrafloods.geeutils import *
+from hydrafloods.thresholding import *
+from hydrafloods.filtering import *
+from hydrafloods.corrections import *
+from hydrafloods.indices import *
+from hydrafloods.fuzzy import *
+from hydrafloods.depths import *
+from hydrafloods.floods import *
+from hydrafloods import fetch, utils
+
+# from hydrafloods import *
+
+__version__ = "2021.10.11"
