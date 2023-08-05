0a73749004f8683f2eb640a1449f7ee0a06d94c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:42 +0000

    Add package koaeda/0.0.0.3/koaeda-0.0.0.3.tar.gz

---
+++ packages/koaeda/koaeda-0.0.0.3.tar.gz/koaeda-0.0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="koaeda",
+    version="0.0.0.3",
+    license="MIT",
+    author="decyma",
+    author_email="soos3121@gmail.com",
+    description="Using Korean Language A Easier Data Augmentation",
+    long_description=open("README.md").read(),
+    url="https://github.com/Kangsukmin/KoAEDA",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
