aeaddedf0ed575000182a4e2668a5acd6449ddf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:37 +0000

    Add package godaddy-reseller-client/0.9.0/godaddy_reseller_client-0.9.0-py3-none-any.whl

---
+++ packages/godaddy-reseller-client/godaddy_reseller_client-0.9.0-py3-none-any.whl/godaddy_reseller_client/certificates.py
@@ -0,0 +1,11 @@
+class certificate:
+    def __init__(self, base_url, key, secret):
+        self.base_url = base_url
+        self.key = key
+        self.secret = secret
+        self.headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Auhthorization': 'sso-key {}:{}'.format(key,secret)
+        }
+    
