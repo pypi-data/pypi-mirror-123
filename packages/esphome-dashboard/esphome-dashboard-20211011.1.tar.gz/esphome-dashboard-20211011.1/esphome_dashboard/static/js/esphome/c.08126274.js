aae15a2d9189bd257857b57bafa4fc1855ff13aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:35 +0000

    Add package esphome-dashboard/20211011.1/esphome-dashboard-20211011.1.tar.gz

---
+++ packages/esphome-dashboard/esphome-dashboard-20211011.1.tar.gz/esphome-dashboard-20211011.1/esphome_dashboard/static/js/esphome/c.08126274.js
@@ -0,0 +1,7 @@
+import{_ as e,n as s,a,y as d}from"./index-d602274d.js";import"./c.1acc301b.js";let l=class extends a{render(){return d`
+      <esphome-process-dialog
+        heading="Update All"
+        .type=${"update-all"}
+        @closed=${this._handleClose}
+      ></esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};l=e([s("esphome-update-all-dialog")],l);
---
+++ packages/esphome-dashboard/esphome-dashboard-20211011.1.tar.gz/esphome-dashboard-20211011.1/esphome_dashboard/static/js/esphome/c.135d6514.js
@@ -0,0 +1,24 @@
+import{_ as o,e as t,t as e,n as s,a as i,y as r,K as n}from"./index-d602274d.js";import"./c.1acc301b.js";import{o as a}from"./c.078f4f57.js";import"./c.ae36766d.js";import"./c.c5739bcc.js";let c=class extends i{render(){return r`
+      <esphome-process-dialog
+        .heading=${`Logs ${this.configuration}`}
+        .type=${"logs"}
+        .spawnParams=${{configuration:this.configuration,port:this.target}}
+        @closed=${this._handleClose}
+        @process-done=${this._handleProcessDone}
+      >
+        <mwc-button
+          slot="secondaryAction"
+          dialogAction="close"
+          label="Edit"
+          @click=${this._openEdit}
+        ></mwc-button>
+        ${void 0===this._result||0===this._result?"":r`
+              <mwc-button
+                slot="secondaryAction"
+                dialogAction="close"
+                label="Retry"
+                @click=${this._handleRetry}
+              ></mwc-button>
+            `}
+      </esphome-process-dialog>
+    `}_openEdit(){n(this.configuration)}_handleProcessDone(o){this._result=o.detail}_handleRetry(){a(this.configuration,this.target)}_handleClose(){this.parentNode.removeChild(this)}};o([t()],c.prototype,"configuration",void 0),o([t()],c.prototype,"target",void 0),o([e()],c.prototype,"_result",void 0),c=o([s("esphome-logs-dialog")],c);
