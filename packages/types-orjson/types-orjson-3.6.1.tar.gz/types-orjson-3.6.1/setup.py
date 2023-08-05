a99be8d34cd7ac08ea5156dbca58ef5eb59eba1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:52 +0000

    Add package types-orjson/3.6.1/types-orjson-3.6.1.tar.gz

---
+++ packages/types-orjson/types-orjson-3.6.1.tar.gz/types-orjson-3.6.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+name = "types-orjson"
+description = "Typing stubs for orjson"
+long_description = '''
+## Typing stubs for orjson
+
+This is a PEP 561 type stub package for the `orjson` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `orjson`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/orjson. All fixes for
+types and metadata should be contributed there.
+
+*Note:* The `orjson` package includes type annotations or type stubs
+since version 3.6.1. Please uninstall the `types-orjson`
+package if you use this or a newer version.
+
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `9f869723509ac027bae6b6f567b921d6a229e8ec`.
+'''.lstrip()
+
+setup(name=name,
+      version="3.6.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['orjson-stubs'],
+      package_data={'orjson-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
