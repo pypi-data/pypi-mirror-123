df1eb3c45cf5f461537e827d4862afac4c95e2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:37 +0000

    Add package sqla-wrapper/5.0.0.dev5/sqla-wrapper-5.0.0.dev5.tar.gz

---
+++ packages/sqla-wrapper/sqla-wrapper-5.0.0.dev5.tar.gz/sqla-wrapper-5.0.0.dev5/README.md
@@ -0,0 +1,17 @@
+![SQLA-Wrapper](header.png)
+
+A friendly wrapper for [modern SQLAlchemy](https://docs.sqlalchemy.org/en/14/glossary.html#term-2.0-style) (v1.4 or later) and Alembic.
+
+**Documentation:** https://jpsca.github.io/sqla-wrapper/
+
+Includes:
+
+- A `SQLAlchemy` wrapper, that does all the SQLAlchemy setup and gives you:
+    - A preconfigured scoped session.
+    - A model baseclass including some helper methods.
+    - A helper for performant testing with a real database
+
+- An `Alembic` wrapper that loads the config from your application instead of an ini file.
+
+- A `sa` helper module, that imports all the functions and classes from `sqlalchemy`and `sqlalchemy.orm`,
+so you don't need to repeat those imports everywhere.
---
+++ packages/sqla-wrapper/sqla-wrapper-5.0.0.dev5.tar.gz/sqla-wrapper-5.0.0.dev5/sqla_wrapper/session.py
@@ -0,0 +1,68 @@
+from typing import Any, List
+
+import sqlalchemy.orm
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import scoped_session
+
+
+class Session(sqlalchemy.orm.Session):
+    def all(self, Model: Any, **attrs) -> List[Any]:
+        """Returns all the object found with these attributes."""
+        return self.execute(
+            select(Model).filter_by(**attrs)
+        ).scalars().all()
+
+    def create(self, Model: Any, **attrs) -> Any:
+        """Creates a new object and adds it to the session."""
+        obj = Model(**attrs)
+        self.add(obj)
+        self.flush()
+        return obj
+
+    def first(self, Model: Any, **attrs) -> Any:
+        """Returns the first object found with these attributes."""
+        return self.execute(
+            select(Model).filter_by(**attrs)
+        ).scalars().first()
+
+    def first_or_create(self, Model: Any, **attrs) -> Any:
+        """Tries to find an object, and if none exists
+        it tries to creates a new one."""
+        obj = self.first(Model, **attrs)
+        if obj:
+            return obj
+        return self.create_or_first(Model, **attrs)
+
+    def create_or_first(self, Model: Any, **attrs) -> Any:
+        """Tries to create a new object, and if it fails
+        because already exists, return the first it founds."""
+        try:
+            return self.create(Model, **attrs)
+        except IntegrityError:
+            self.rollback()
+        return self.first(Model, **attrs)
+
+
+class PatchedScopedSession(scoped_session):
+    def all(self, Model: Any, **attrs) -> List[Any]:
+        """Returns all the object found with these attributes."""
+        return self.registry().all(Model, **attrs)
+
+    def create(self, Model: Any, **attrs) -> Any:
+        """Creates a new object and adds it to the session."""
+        return self.registry().create(Model, **attrs)
+
+    def first(self, Model: Any, **attrs) -> Any:
+        """Returns the first object found with these attributes."""
+        return self.registry().first(Model, **attrs)
+
+    def first_or_create(self, Model: Any, **attrs) -> Any:
+        """Tries to find an object, and if none exists
+        it tries to creates a new one."""
+        return self.registry().first_or_create(Model, **attrs)
+
+    def create_or_first(self, Model: Any, **attrs) -> Any:
+        """Tries to create a new object, and if it fails
+        because already exists, return the first it founds."""
+        return self.registry().create_or_first(Model, **attrs)
