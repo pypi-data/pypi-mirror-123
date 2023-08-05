5d9ba7f51244a8f41864b042a46cf43ac1f61e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package BinomialdistributionsAN/1.6/BinomialdistributionsAN-1.6.tar.gz

---
+++ packages/BinomialdistributionsAN/BinomialdistributionsAN-1.6.tar.gz/BinomialdistributionsAN-1.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BinomialdistributionsAN',
+      version='1.6',
+      description='Gaussian Binomial distributions',
+      packages=['BinomialdistributionsAN'],
+      author=' Aymar A. NAHUM ',
+      author_email='postmaster@jecorde.net',
+      zip_safe=False)
