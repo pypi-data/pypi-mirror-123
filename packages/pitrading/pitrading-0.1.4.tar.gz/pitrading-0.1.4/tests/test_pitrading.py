4755e3fa51a9eabde5219a61c849432183640d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:08 +0000

    Add package pitrading/0.1.4/pitrading-0.1.4.tar.gz

---
+++ packages/pitrading/pitrading-0.1.4.tar.gz/pitrading-0.1.4/tests/test_pitrading.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""Tests for `pitrading` package."""
+
+
+from pitrading.holidays import Holidays
+import unittest
+
+from pitrading import pitrading
+
+
+class TestPitrading(unittest.TestCase):
+    """Tests for `pitrading` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
