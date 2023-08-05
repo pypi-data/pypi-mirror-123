d9bedacb2ad862276f2af599b9333fbbca4cb6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:52 +0000

    Add package teby-probability/1.0/teby_probability-1.0.tar.gz

---
+++ packages/teby-probability/teby_probability-1.0.tar.gz/teby_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='teby_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['teby_probability'],
+      author='Motebang Lebusa',
+      author_email='lebusam@gmail.com',
+      zip_safe=False)
