7bb29f04585b0134869c09f13c96a86c017228ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:14 +0000

    Add package DistributionsInheritanceAN/1.2/DistributionsInheritanceAN-1.2.tar.gz

---
+++ packages/DistributionsInheritanceAN/DistributionsInheritanceAN-1.2.tar.gz/DistributionsInheritanceAN-1.2/DistributionsInheritanceAN/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+from .Binomialdistribution import Binomial
+from .Gaussiandistribution import Gaussian
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+# TODO: import the Binomial class from the Binomialdistribution module
---
+++ packages/DistributionsInheritanceAN/DistributionsInheritanceAN-1.2.tar.gz/DistributionsInheritanceAN-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DistributionsInheritanceAN',
+      version='1.2',
+      description='Gaussian Binomial distributions',
+      packages=['DistributionsInheritanceAN'],
+      author=' Aymar A. NAHUM ',
+      author_email='postmaster@jecorde.net',
+      zip_safe=False)
