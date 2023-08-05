242a6b4e5598bb7de0ff8cb2cdb32b56d83fc3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:46 +0000

    Add package Flask-Collect-Invenio/1.4.0/Flask_Collect_Invenio-1.4.0-py2.py3-none-any.whl

---
+++ packages/Flask-Collect-Invenio/Flask_Collect_Invenio-1.4.0-py2.py3-none-any.whl/flask_collect/version.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Flask-Collect-Invenio.
+# Copyright (C) 2021 TU Wien.
+#
+# Flask-Collect-Invenio is free software; you can redistribute it and/or modify
+# it under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""
+Version information for Flask-Collect-Invenio.
+
+This file is imported by ``flask_collect.__init__``, and parsed by
+``setup.py`` as well as ``docs/conf.py``.
+"""
+
+# Do not change the format of this next line. Doing so risks breaking
+# setup.py and docs/conf.py
+
+__version__ = "1.4.0"
