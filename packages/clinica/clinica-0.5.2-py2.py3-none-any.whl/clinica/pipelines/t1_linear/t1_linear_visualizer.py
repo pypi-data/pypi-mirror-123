6b6a30ba1f2210d23624e0f4b3be875c83cfc5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package clinica/0.5.2/clinica-0.5.2-py2.py3-none-any.whl

---
+++ packages/clinica/clinica-0.5.2-py2.py3-none-any.whl/clinica/pipelines/t1_linear/t1_linear_visualizer.py
@@ -0,0 +1,7 @@
+# coding: utf8
+
+"""T1 Linear - Clinica Visualizer.
+This file has been generated automatically by the `clinica generate template`
+command line tool. See here for more details:
+https://aramislab.paris.inria.fr/clinica/docs/public/latest/InteractingWithClinica/
+"""
