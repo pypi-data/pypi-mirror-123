b82816c75e3602b67bf474c9259424cc51b8e72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package mollie-api-python/2.11.0/mollie-api-python-2.11.0.tar.gz

---
+++ packages/mollie-api-python/mollie-api-python-2.11.0.tar.gz/mollie-api-python-2.11.0/mollie/api/objects/chargeback.py
@@ -0,0 +1,33 @@
+from .base import ObjectBase
+
+
+class Chargeback(ObjectBase):
+    @classmethod
+    def get_resource_class(cls, client):
+        from ..resources.chargebacks import Chargebacks
+
+        return Chargebacks(client)
+
+    @property
+    def id(self):
+        return self._get_property("id")
+
+    @property
+    def amount(self):
+        return self._get_property("amount")
+
+    @property
+    def settlement_amount(self):
+        return self._get_property("settlementAmount")
+
+    @property
+    def created_at(self):
+        return self._get_property("createdAt")
+
+    @property
+    def reversed_at(self):
+        return self._get_property("reversedAt")
+
+    @property
+    def payment_id(self):
+        return self._get_property("paymentId")
