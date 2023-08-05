9b6169de717838c289b67335d3c9565bb21ae86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:17 +0000

    Add package pl-lib-mercury/100.100.100/pl-lib-mercury-100.100.100.tar.gz

---
+++ packages/pl-lib-mercury/pl-lib-mercury-100.100.100.tar.gz/pl-lib-mercury-100.100.100/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pl-lib-mercury',
+    version='100.100.100',    
+    description='For questions reach out to security@plaid.com',
+    url='https://www.plaid.com',
+    author='Plaid',
+    author_email='agupta@plaid.com',
+    license='BSD 2-clause',
+    packages=['pl-lib-mercury'],
+    install_requires=[],
+
+    classifiers=[
+    ],
+)
