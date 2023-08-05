c5e7334f42ca9f883932a147317c9e792cdb6a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:18 +0000

    Add package plaid-python-monorepo/100.100.100/plaid-python-monorepo-100.100.100.tar.gz

---
+++ packages/plaid-python-monorepo/plaid-python-monorepo-100.100.100.tar.gz/plaid-python-monorepo-100.100.100/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='plaid-python-monorepo',
+    version='100.100.100',    
+    description='For questions reach out to security@plaid.com',
+    url='https://www.plaid.com',
+    author='Plaid',
+    author_email='agupta@plaid.com',
+    license='BSD 2-clause',
+    packages=['plaid-python-monorepo'],
+    install_requires=[],
+
+    classifiers=[
+    ],
+)
