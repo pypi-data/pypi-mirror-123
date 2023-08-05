07c57215e8a80239d8af0694059df70a19e69c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:00 +0000

    Add package types-redis/3.5.12/types-redis-3.5.12.tar.gz

---
+++ packages/types-redis/types-redis-3.5.12.tar.gz/types-redis-3.5.12/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-redis"
+description = "Typing stubs for redis"
+long_description = '''
+## Typing stubs for redis
+
+This is a PEP 561 type stub package for the `redis` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `redis`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/redis. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `9f869723509ac027bae6b6f567b921d6a229e8ec`.
+'''.lstrip()
+
+setup(name=name,
+      version="3.5.12",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['redis-stubs'],
+      package_data={'redis-stubs': ['__init__.pyi', 'client.pyi', 'connection.pyi', 'exceptions.pyi', 'lock.pyi', 'utils.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
