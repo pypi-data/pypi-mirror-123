d7953f7a166a9093b01b23f483153848a7975fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:20 +0000

    Add package halloumi-ami-pipelines/0.0.32/halloumi-ami-pipelines-0.0.32.tar.gz

---
+++ packages/halloumi-ami-pipelines/halloumi-ami-pipelines-0.0.32.tar.gz/halloumi-ami-pipelines-0.0.32/setup.py
@@ -0,0 +1,81 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "halloumi-ami-pipelines",
+    "version": "0.0.32",
+    "description": "halloumi-ami-pipelines",
+    "license": "Apache-2.0",
+    "url": "https://github.com/sentiampc/halloumi-ami-pipelines.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Sentia<support.mpc@sentia.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/sentiampc/halloumi-ami-pipelines.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "ami_pipelines",
+        "ami_pipelines._jsii"
+    ],
+    "package_data": {
+        "ami_pipelines._jsii": [
+            "halloumi-ami-pipelines@0.0.32.jsii.tgz"
+        ],
+        "ami_pipelines": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.aws-codebuild>=1.125.0, <2.0.0",
+        "aws-cdk.aws-codecommit>=1.125.0, <2.0.0",
+        "aws-cdk.aws-codepipeline-actions>=1.125.0, <2.0.0",
+        "aws-cdk.aws-codepipeline>=1.125.0, <2.0.0",
+        "aws-cdk.aws-ec2>=1.125.0, <2.0.0",
+        "aws-cdk.aws-events-targets>=1.125.0, <2.0.0",
+        "aws-cdk.aws-events>=1.125.0, <2.0.0",
+        "aws-cdk.aws-iam>=1.125.0, <2.0.0",
+        "aws-cdk.aws-imagebuilder>=1.125.0, <2.0.0",
+        "aws-cdk.aws-kms>=1.125.0, <2.0.0",
+        "aws-cdk.aws-lambda>=1.125.0, <2.0.0",
+        "aws-cdk.aws-logs-destinations>=1.125.0, <2.0.0",
+        "aws-cdk.aws-logs>=1.125.0, <2.0.0",
+        "aws-cdk.aws-s3>=1.125.0, <2.0.0",
+        "aws-cdk.aws-sns-subscriptions>=1.125.0, <2.0.0",
+        "aws-cdk.aws-sns>=1.125.0, <2.0.0",
+        "aws-cdk.core>=1.125.0, <2.0.0",
+        "aws-cdk.custom-resources>=1.125.0, <2.0.0",
+        "constructs>=3.2.27, <4.0.0",
+        "jsii>=1.34.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
