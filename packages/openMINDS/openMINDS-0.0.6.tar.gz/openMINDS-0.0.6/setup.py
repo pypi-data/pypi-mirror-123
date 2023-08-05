43cde88bd3a2bc34794f39ed71dba36eb5ce9376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:30 +0000

    Add package openMINDS/0.0.6/openMINDS-0.0.6.tar.gz

---
+++ packages/openMINDS/openMINDS-0.0.6.tar.gz/openMINDS-0.0.6/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openMINDS",
+    version="0.0.6",
+    author="Stefan Köhnen",
+    author_email="s.koehnen@fz-juelich.de",
+    description="Python library for interacting with openMINDS metadata schemas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HumanBrainProject/openMINDS_generator",
+    project_urls={
+        "Bug Tracker": "https://github.com/HumanBrainProject/openMINDS_generator/issues",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "click==8.0.1",
+        "jsonschema==4.1.0",
+        "requests==2.26.0",
+        "GitPython==3.1.24"
+    ],
+    python_requires=">=3.6",
+)
