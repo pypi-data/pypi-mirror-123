75f10bf40da9308d31df21d2a1b0f03ccd251c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:16 +0000

    Add package Disecon/0.1.10/Disecon-0.1.10.tar.gz

---
+++ packages/Disecon/Disecon-0.1.10.tar.gz/Disecon-0.1.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+readme = ""
+with open("README.md") as f:
+  readme = f.read()
+
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Disecon',
+  version='0.1.10',
+  description='Disecon',
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  url='',  
+  author='LegosAndStuff',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='economy', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
