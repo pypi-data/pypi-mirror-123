5958be23f69b99f117d8fef0ba3fd7e3a36af794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:28 +0000

    Add package website-sitemap-parser/0.0.11/website-sitemap-parser-0.0.11.tar.gz

---
+++ packages/website-sitemap-parser/website-sitemap-parser-0.0.11.tar.gz/website-sitemap-parser-0.0.11/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='website-sitemap-parser',
+    version='0.0.11',
+    author="Bart Machielsen",
+    author_email="bartmachielsen@gmail.com",
+    description="Website Sitemap Parser",
+    install_requires=[
+        'requests',
+        'stopit'
+    ],
+    # long_description='',
+    # long_description_content_type="text/markdown",
+    url="https://github.com/bartmachielsen/website-sitemap-parser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
