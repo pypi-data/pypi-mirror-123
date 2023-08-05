4ad8bd844fd383d174e71509634b0d87abb3155a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:45 +0000

    Add package docutools/2021.10.9/docutools-2021.10.9-py3-none-any.whl

---
+++ packages/docutools/docutools-2021.10.9-py3-none-any.whl/lcdoc/mkdocs/lp/plugs/python/pyplugs/__init__.py
@@ -0,0 +1,12 @@
+from . import (
+    call_flow_logging,
+    comments,
+    cov_report,
+    data_table,
+    diag_diagram,
+    git_changelog,
+    lprunner,
+    mpl_pyplot,
+    project_dependencies,
+    screenshot,
+)
