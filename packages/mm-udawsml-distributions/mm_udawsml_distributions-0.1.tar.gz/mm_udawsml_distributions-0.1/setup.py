b19e51cac7a45d62ae5db86139808f2fa851be1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:55 +0000

    Add package mm-udawsml-distributions/0.1/mm_udawsml_distributions-0.1.tar.gz

---
+++ packages/mm-udawsml-distributions/mm_udawsml_distributions-0.1.tar.gz/mm_udawsml_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mm_udawsml_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mm_udawsml_distributions'],
+      author='margit',
+      zip_safe=False)
