bb7d8f6b7b6cc00893d6caec32f71301bae1c8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:49 +0000

    Add package azureml-automl-runtime/1.35.0/azureml_automl_runtime-1.35.0-py3-none-any.whl

---
+++ packages/azureml-automl-runtime/azureml_automl_runtime-1.35.0-py3-none-any.whl/azureml/automl/runtime/shared/_parqueter.py
@@ -0,0 +1,137 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""Class for parquet based serialization and deserialization."""
+from typing import cast
+
+import errno
+import logging
+
+import numpy as np
+import pandas as pd
+
+from azureml._common._error_definition import AzureMLError
+from azureml.automl.core.shared import logging_utilities
+from azureml.automl.core.shared._diagnostics.automl_error_definitions import DiskFull, InsufficientMemory
+from azureml.automl.core.shared._diagnostics.contract import Contract
+from azureml.automl.core.shared.exceptions import ResourceException
+
+logger = logging.getLogger(__name__)
+
+
+class Parqueter:
+    """Default Parqueter based on Pandas implementation."""
+
+    _compression = 'gzip'
+
+    @staticmethod
+    def dump_numpy_array(obj: np.ndarray, path: str) -> str:
+        """
+        Dump a numpy array to a parquet file. This will convert a single dimensional object to
+        two dimensions.
+
+        :param obj: The numpy array to serialize.
+        :param path: Path to dump the file to.
+        :return: The path.
+        """
+        Contract.assert_type(value=obj, name='obj', expected_types=np.ndarray, log_safe=True)
+        Contract.assert_value(value=path, name='path')
+        num_rows = obj.shape[0]
+        # Convert `obj` to 2-dimensions.
+        if obj.ndim == 1:
+            obj = obj.reshape(num_rows, 1)
+
+        df_wrapper = pd.DataFrame(obj)
+        return Parqueter.dump_pandas_dataframe(df_wrapper, path)
+
+    @staticmethod
+    def dump_pandas_dataframe(obj: pd.DataFrame, path: str) -> str:
+        """
+        Dump a pandas dataframe object to a parquet file. This will write a 2-dimensional
+        array even if the input object is one-dimensional.
+
+        :param obj: The pandas dataframe to serialize.
+        :param path: Path to dump the file to.
+        :return: The path.
+        """
+        Contract.assert_type(value=obj, name='obj', expected_types=pd.DataFrame, log_safe=True)
+        Contract.assert_value(value=path, name='path')
+        try:
+            num_rows = obj.shape[0]
+            # Convert `obj` to 2-dimensions.
+            if obj.ndim == 1:
+                obj = obj.reshape(num_rows, 1)
+
+            df = pd.DataFrame(obj)
+            # `to_parquet` only supports string column names.
+            df.columns = [str(c) for c in df.columns]
+            df.to_parquet(path, compression=Parqueter._compression)
+        except MemoryError as me:
+            raise ResourceException._with_error(AzureMLError.create(InsufficientMemory), inner_exception=me) from me
+        except OSError as oe:
+            if oe.errno == errno.ENOSPC:
+                raise ResourceException._with_error(
+                    AzureMLError.create(DiskFull, target="pandas.DataFrame.to_parquet",
+                                        operation_name="pandas.DataFrame.to_parquet"),
+                    inner_exception=oe
+                ) from oe
+            else:
+                logger.error("Failed to store as a parquet file to disk. OS Error Code: {}".format(str(oe.errno)))
+                raise
+        except Exception as e:
+            logging_utilities.log_traceback(e, None)
+            raise
+
+        return path
+
+    @staticmethod
+    def load_pandas_dataframe(path: str) -> pd.DataFrame:
+        """
+        Deserialize parquet file into a pandas dataframe.
+
+        :param path: The file path used by the parqueter.
+        :return: The deserialized pandas dataframe.
+        """
+        try:
+            df = pd.read_parquet(path)
+            logger.debug('Deserialized parquet file into a pandas dataframe of shape: {}'.format(df.shape))
+            return df
+        except MemoryError as me:
+            raise ResourceException._with_error(AzureMLError.create(InsufficientMemory), inner_exception=me) from me
+        except Exception as e:
+            if isinstance(e, OSError):
+                logger.error("Failed to load the parquet file from disk. OS Error Code: {}".format(str(e.errno)))
+            logging_utilities.log_traceback(e, None)
+            raise
+
+    @staticmethod
+    def load_numpy_array(path: str) -> np.ndarray:
+        """
+        Deserialize parquet file into a numpy array.
+
+        :param path: The file path where the serialized file is available.
+        :return: The deserialized object.
+        """
+        df = Parqueter.load_pandas_dataframe(path)
+        array = cast(np.ndarray, df.to_numpy())
+        logger.debug('Deserialized parquet file into a numpy array of shape: {}'.format(array.shape))
+        return array
+
+    @staticmethod
+    def load_single_dim_numpy_array(path: str) -> np.ndarray:
+        """
+        Deserialize parquet file into a single dimensional numpy array.
+
+        :param path: The file path where the serialized file is available.
+        :return: The deserialized object.
+        """
+        array = Parqueter.load_numpy_array(path)
+        Contract.assert_true(array.ndim == 2 and array.shape[1] == 1,
+                             message='Expected an array with a single column but received one '
+                                     'with shape: {}'.format(array.shape))
+        single_dim_array = array.ravel()
+        logger.debug('Deserialized parquet file into a single dimensional array of shape: {}'.format(
+            single_dim_array.shape
+        ))
+
+        return single_dim_array
