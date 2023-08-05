9c941a96a432e4d13b82f14ac3a0a84975a699db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package quovo-sync/100.100.100/quovo-sync-100.100.100.tar.gz

---
+++ packages/quovo-sync/quovo-sync-100.100.100.tar.gz/quovo-sync-100.100.100/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='quovo-sync',
+    version='100.100.100',    
+    description='For questions reach out to security@plaid.com',
+    url='https://www.plaid.com',
+    author='Plaid',
+    author_email='agupta@plaid.com',
+    license='BSD 2-clause',
+    packages=['quovo-sync'],
+    install_requires=[],
+
+    classifiers=[
+    ],
+)
