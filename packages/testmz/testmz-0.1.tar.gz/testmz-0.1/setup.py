a88cdfda380c4559ff3f2b9f2385b8f8cd27076a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:59 +0000

    Add package testmz/0.1/testmz-0.1.tar.gz

---
+++ packages/testmz/testmz-0.1.tar.gz/testmz-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='testmz',
+      version='0.1',
+      description='The first python test package',
+      url='http://github.com/mudassir21/server',
+      author='Mudassir Zakaria',
+      author_email='mudassir21bd@gmail.com',
+      license='MIT',
+      packages=['testmz'],
+      zip_safe=False)
