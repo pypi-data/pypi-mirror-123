5fcd864b2c88420acd37fb63a4c86d303d5a5898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:21 +0000

    Add package zermeloapi/2021.10.12.10.57.3/zermeloapi-2021.10.12.10.57.3.tar.gz

---
+++ packages/zermeloapi/zermeloapi-2021.10.12.10.57.3.tar.gz/zermeloapi-2021.10.12.10.57.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from datetime import datetime
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="zermeloapi",
+    version=str(datetime.today()).split('.')[0].replace('-','.').replace(':','.').replace(' ','.'),
+    author="b.vill",
+    author_email="villeriusborro@gmail.com",
+    description="zermelo api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
