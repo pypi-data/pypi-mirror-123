19970650198ee1177c113384fa3f8ef2f0d8918d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:34 +0000

    Add package types-Send2Trash/1.8.1/types-Send2Trash-1.8.1.tar.gz

---
+++ packages/types-Send2Trash/types-Send2Trash-1.8.1.tar.gz/types-Send2Trash-1.8.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-Send2Trash"
+description = "Typing stubs for Send2Trash"
+long_description = '''
+## Typing stubs for Send2Trash
+
+This is a PEP 561 type stub package for the `Send2Trash` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `Send2Trash`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/Send2Trash. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `9f869723509ac027bae6b6f567b921d6a229e8ec`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.8.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['send2trash-stubs'],
+      package_data={'send2trash-stubs': ['__init__.pyi', 'compat.pyi', 'exceptions.pyi', 'util.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
