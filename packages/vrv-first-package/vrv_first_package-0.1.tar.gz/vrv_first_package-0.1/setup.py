30de28cb7fb7c9a6b116ad384b42c228467ab7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package vrv-first-package/0.1/vrv_first_package-0.1.tar.gz

---
+++ packages/vrv-first-package/vrv_first_package-0.1.tar.gz/vrv_first_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vrv_first_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vrv_first_package'],
+      zip_safe=False)
