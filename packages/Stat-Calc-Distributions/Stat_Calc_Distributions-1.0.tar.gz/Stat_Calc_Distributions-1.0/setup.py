47f33e85d53af8591251d9c3ad718fcc0a8a1711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:04 +0000

    Add package Stat-Calc-Distributions/1.0/Stat_Calc_Distributions-1.0.tar.gz

---
+++ packages/Stat-Calc-Distributions/Stat_Calc_Distributions-1.0.tar.gz/Stat_Calc_Distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Stat_Calc_Distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['Stat_Calc_Distributions'],
+      zip_safe=False)
