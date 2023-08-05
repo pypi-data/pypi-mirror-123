afc914cef152fb44716de636a103fef43e8a60c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package rock-finance/0.0.1/rock_finance-0.0.1-py3-none-any.whl

---
+++ packages/rock-finance/rock_finance-0.0.1-py3-none-any.whl/rock_finance/utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests as _requests
+
+
+user_agent_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+}
+
+
+def get_html(url, proxy=None, session=None):
+    session = session or _requests
+    html = session.get(url=url, proxies=proxy, headers=user_agent_headers).text
+    return html
