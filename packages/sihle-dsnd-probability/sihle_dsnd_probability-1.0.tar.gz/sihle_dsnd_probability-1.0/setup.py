ffcdc73a21178fe9af99708caf34ca53c4870f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:35 +0000

    Add package sihle-dsnd-probability/1.0/sihle_dsnd_probability-1.0.tar.gz

---
+++ packages/sihle-dsnd-probability/sihle_dsnd_probability-1.0.tar.gz/sihle_dsnd_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sihle_dsnd_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['sihle_dsnd_probability'],
+      author= 'Sihle Wilson',
+      author_email= 'sihle.wilson@gmail.com',
+      zip_safe=False)
