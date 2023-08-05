14b27a2e001be379d39964f1f8cb806172af3713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:00 +0000

    Add package tox-core-python/0.0.dev1/tox-core-python-0.0.dev1.tar.gz

---
+++ packages/tox-core-python/tox-core-python-0.0.dev1.tar.gz/tox-core-python-0.0.dev1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='tox-core-python',
+   version='0.0.dev1',
+   description='',
+   author='Tanium Inc.',
+   author_email='product-security@tanium.com',
+   packages=['tox-core-python'],
+)
