e61f095e84c8a69a71b65910c291d104fe11629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:02 +0000

    Add package alarmer/0.1.3/alarmer-0.1.3.tar.gz

---
+++ packages/alarmer/alarmer-0.1.3.tar.gz/alarmer-0.1.3/alarmer/provider/wecom.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+import requests
+
+from alarmer import Provider
+
+
+class WeComProvider(Provider):
+    """
+    https://work.weixin.qq.com/api/doc/90000/90136/91770
+    """
+
+    def __init__(self, webhook_url: str):
+        super().__init__()
+        self.webhook_url = webhook_url
+
+    def send(
+        self, message: str, exc: Optional[BaseException] = None, context: Optional[dict] = None
+    ):
+        data = {"msgtype": "text", "content": {"text": message}}
+        return requests.post(
+            self.webhook_url,
+            json=data,
+        )
