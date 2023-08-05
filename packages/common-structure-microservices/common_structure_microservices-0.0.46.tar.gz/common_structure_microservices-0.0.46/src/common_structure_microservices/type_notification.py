8b4fd50e0c4258312ea1a270cdf82f6d28cd012f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:26 +0000

    Add package common-structure-microservices/0.0.46/common_structure_microservices-0.0.46.tar.gz

---
+++ packages/common-structure-microservices/common_structure_microservices-0.0.46.tar.gz/common_structure_microservices-0.0.46/src/common_structure_microservices/type_notification.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class TypeNotification(Enum):
+    COMMUNITY = "community"
+    REQUEST_COMMUNITY = "request-community"
+    INVITATION_COMMUNITY = "invitation"
+    PUBLICATION = "publication"
+    FULL_TEXT = "full_text"
+    USER = "user"
