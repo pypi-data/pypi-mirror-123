892d2b6fa1a8722799ae08f66292c308d1c97017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:29 +0000

    Add package silverbots/0.1.1/silverbots-0.1.1-py3-none-any.whl

---
+++ packages/silverbots/silverbots-0.1.1-py3-none-any.whl/silverbots/telegram.py
@@ -0,0 +1,32 @@
+import requests
+
+
+class Telegram:
+    def __init__(self, token):
+        self.token = token
+        self.web = f"https://api.telegram.org/bot{token}/"
+
+    def send_message(self, chat, text):
+        msg = {
+            "chat_id": chat,
+            "text": text
+        }
+        requests.post(self.web + "sendMessage", json=msg)
+
+    def run(self, debug=False, handle_function=None):
+        print("* Starting bot and waiting for updates")
+        if debug:
+            print("* Debug mod: ENABLED")
+        else:
+            print("* Debug mod: DISABLED")
+        print()
+
+        offset = 0
+        while True:
+            r = requests.post(self.web + f"getUpdates?offset={offset}").json()["result"]
+            for x in r:
+                if debug:
+                    print(x["message"])
+                handle_function(x["message"])
+            if len(r) != 0:
+                offset = r[-1]["update_id"] + 1
