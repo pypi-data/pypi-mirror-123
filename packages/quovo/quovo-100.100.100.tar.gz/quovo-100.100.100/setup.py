58da868602f0193ea33cf684b527f5f3dddd34a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package quovo/100.100.100/quovo-100.100.100.tar.gz

---
+++ packages/quovo/quovo-100.100.100.tar.gz/quovo-100.100.100/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='quovo',
+    version='100.100.100',    
+    description='For questions reach out to security@plaid.com',
+    url='https://www.plaid.com',
+    author='Plaid',
+    author_email='agupta@plaid.com',
+    license='BSD 2-clause',
+    packages=['quovo'],
+    install_requires=[],
+
+    classifiers=[
+    ],
+)
