ab2990f68c2974b01100e7fbc08c9e88c95e2553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:16 +0000

    Add package meilisearch-fastapi/0.9.2/meilisearch_fastapi-0.9.2-py3-none-any.whl

---
+++ packages/meilisearch-fastapi/meilisearch_fastapi-0.9.2-py3-none-any.whl/meilisearch_fastapi/models/__init__.py
@@ -0,0 +1,27 @@
+from meilisearch_fastapi.models.document_info import (  # noqa: F401
+    DocumentDelete,
+    DocumentInfo,
+    DocumentInfoAutoBatch,
+    DocumentInfoBatches,
+)
+from meilisearch_fastapi.models.index import (  # noqa: F401
+    DisplayedAttributes,
+    DisplayedAttributesUID,
+    DistinctAttribute,
+    DistinctAttributeWithUID,
+    FilterableAttributes,
+    FilterableAttributesWithUID,
+    IndexUpdate,
+    PrimaryKey,
+    RankingRules,
+    RankingRulesWithUID,
+    SearchableAttributes,
+    SearchableAttributesWithUID,
+    StopWords,
+    StopWordsWithUID,
+    Synonyms,
+    SynonymsWithUID,
+)
+from meilisearch_fastapi.models.meili_message import MeiliSearchMessage  # noqa: F401
+from meilisearch_fastapi.models.search_parameters import SearchParameters  # noqa: F401
+from meilisearch_fastapi.models.settings import MeiliSearchIndexSettings  # noqa: F401
