f0c7d8291d12583e243cd7a0276753deed7c07ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:29 +0000

    Add package ansiscape/1.0.1/ansiscape-1.0.1-py3-none-any.whl

---
+++ packages/ansiscape/ansiscape-1.0.1-py3-none-any.whl/ansiscape/__main__.py
@@ -0,0 +1,46 @@
+from argparse import ArgumentParser
+from sys import argv, stdin, stdout
+from typing import List, TextIO
+
+from ansiscape import Sequence, get_version
+from ansiscape.example import make_example
+
+
+def cli_entry(
+    cli_args: List[str] = argv,
+    in_pipe: TextIO = stdin,
+    out_pipe: TextIO = stdout,
+) -> None:
+    """
+    CLI entrypoint.
+
+    Arguments:
+        args:     Command line arguments
+        in_pipe:  "stdin" or other pipe to read from
+        out_pipe: "stdout" or other pipe to write to
+    """
+
+    parser = ArgumentParser(
+        description="Describes ANSI escape codes read from stdin.",
+        epilog="Made with love by Cariad Eccleston: https://github.com/cariad/ansiscape",
+    )
+
+    parser.add_argument("--example", action="store_true", help="print an example")
+    parser.add_argument("--version", action="store_true", help="print the version")
+
+    args = parser.parse_args(cli_args[1:])
+
+    if args.example:
+        out_pipe.write(str(make_example()))
+        return
+
+    if args.version:
+        out_pipe.write(str(get_version()) + "\n")
+        return
+
+    Sequence(in_pipe.read().strip()).write_json(out_pipe)
+    out_pipe.flush()
+
+
+if __name__ == "__main__":
+    cli_entry()
