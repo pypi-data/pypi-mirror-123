9b06f92891683047592186eb29c45bbc645a3a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package Simyan/0.6.0/Simyan-0.6.0.tar.gz

---
+++ packages/Simyan/Simyan-0.6.0.tar.gz/Simyan-0.6.0/tests/test_init.py
@@ -0,0 +1,37 @@
+"""
+The Test Init module.
+
+This module contains tests for project init.
+"""
+import pytest
+from deprecation import fail_if_not_removed
+
+from simyan import api, create_session
+from simyan.exceptions import AuthenticationError
+from simyan.session import Session
+
+
+@fail_if_not_removed
+def test_api():
+    """Test for api()."""
+    with pytest.raises(AuthenticationError):
+        api()
+
+    m = None
+    try:
+        m = api(api_key="Something")
+    except Exception as exc:
+        print(f"simyan.api() raised {exc} unexpectedly!")
+
+    assert m.__class__.__name__ == Session.__name__
+
+
+def test_create_session():
+    """Test for create_session()."""
+    m = None
+    try:
+        m = create_session(api_key="Something")
+    except Exception as exc:
+        print(f"simyan.create_session() raised {exc} unexpectedly!")
+
+    assert m.__class__.__name__ == Session.__name__
