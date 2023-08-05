2e4d5d7b12817cb0b73348bb06aa45fb51b16c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:28 +0000

    Add package thin-ws-server/0.0.5/thin_ws_server-0.0.5.tar.gz

---
+++ packages/thin-ws-server/thin_ws_server-0.0.5.tar.gz/thin_ws_server-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+options = {
+    "name": "thin_ws_server",
+    "version": "0.0.5",
+    "author": "atoy322",
+    "description": "",
+    "long_description": "",
+    "packages": find_packages()
+}
+
+setup(**options)
+
