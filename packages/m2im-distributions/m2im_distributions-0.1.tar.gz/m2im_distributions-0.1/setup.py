fa12f528411ad11de01bf456a051010647d9ee17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:51 +0000

    Add package m2im-distributions/0.1/m2im_distributions-0.1.tar.gz

---
+++ packages/m2im-distributions/m2im_distributions-0.1.tar.gz/m2im_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='m2im_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['m2im_distributions'],
+      zip_safe=False)
