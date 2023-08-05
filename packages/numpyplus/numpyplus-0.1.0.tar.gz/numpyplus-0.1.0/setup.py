4d460111d337fb1692179d147692e4c56e56553d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:51 +0000

    Add package numpyplus/0.1.0/numpyplus-0.1.0.tar.gz

---
+++ packages/numpyplus/numpyplus-0.1.0.tar.gz/numpyplus-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='numpyplus',
+    version='0.1.0',    
+    description='An upgraded version of numpy',
+    url='https://github.com/Shashulovskiy/numpyplus',
+    author='Artem Shashulovskiy',
+    author_email='shashulovskiy@gmail.com',
+    license='BSD 2-clause',
+    packages=['numpyplus'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy<=1.0',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
