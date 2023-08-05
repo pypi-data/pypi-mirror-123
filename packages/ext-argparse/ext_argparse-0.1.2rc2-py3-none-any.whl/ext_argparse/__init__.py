cfebf615b2bc679e5ec138967a99851da61c6783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:14 +0000

    Add package ext-argparse/0.1.2rc2/ext_argparse-0.1.2rc2-py3-none-any.whl

---
+++ packages/ext-argparse/ext_argparse-0.1.2rc2-py3-none-any.whl/ext_argparse/__init__.py
@@ -0,0 +1,24 @@
+#  ================================================================
+#  Created by Gregory Kramida on 3/21/19.
+#  Copyright (c) 2019 Gregory Kramida
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ================================================================
+__title__ = "ext_argparse"
+__author__ = "Gregory Kramida"
+__license__ = "Apache License Version 2.0"
+__js__ = None
+__js_url__ = None
+
+from ext_argparse.argproc import process_arguments, save_defaults, dump, add_comments_from_help
+from ext_argparse.param_enum import ParameterEnum
+from ext_argparse.parameter import Parameter
---
+++ packages/ext-argparse/ext_argparse-0.1.2rc2-py3-none-any.whl/ext_argparse/param_enum.py
@@ -0,0 +1,67 @@
+from enum import Enum, EnumMeta, _EnumDict
+
+
+class NestedEnumMeta(EnumMeta):
+    @classmethod
+    def __prepare__(metacls, cls, bases):
+        # check that previous enum members do not exist
+        EnumMeta._check_for_existing_members(cls, bases)
+        # create the namespace dict
+        enum_dict = _EnumDict()
+        enum_dict._cls_name = cls
+        # inherit previous flags and _generate_next_value_ function
+        member_type, first_enum = EnumMeta._get_mixins_(cls, bases)
+        if first_enum is not None:
+            enum_dict['_generate_next_value_'] = getattr(
+                first_enum, '_generate_next_value_', None,
+            )
+
+        return enum_dict
+
+    def __new__(metacls, cls, bases, classdict):
+        enum_class = super().__new__(metacls, cls, bases, classdict)
+        member_type, first_enum = metacls._get_mixins_(cls, bases)
+        __new__, save_new, use_args = metacls._find_new_(
+            classdict, member_type, first_enum,
+        )
+        nested_enum_members = []
+        setattr(enum_class, "_full_member_map_", {})
+
+        for key, member in enum_class._member_map_.items():
+            if isinstance(member.parameter, NestedEnumMeta):
+                nested_enum_members.append((key, member))
+            else:
+                member.argument = None
+            enum_class._full_member_map_[key] = member
+
+        for key, member in nested_enum_members:
+            del enum_class._member_map_[key]
+            delattr(enum_class, key)
+            setattr(enum_class, key, member.parameter)
+            enum_class._member_map_[key] = member.parameter
+
+        return enum_class
+
+    def __iter__(cls):
+        """
+        Returns members in definition order.
+        """
+        return (cls._full_member_map_[name] for name in cls._member_names_)
+
+    @property
+    def parameter(self):
+        return self
+
+    @property
+    def type(self):
+        return 'parameter_enum'
+
+
+class ParameterEnum(Enum, metaclass=NestedEnumMeta):
+    @property
+    def parameter(self):
+        return super().value
+
+    @property
+    def value(self):
+        return self.argument
