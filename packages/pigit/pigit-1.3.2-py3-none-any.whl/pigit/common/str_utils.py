69904b970a82c32a4ef39af066c52e804acdb5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:48 +0000

    Add package pigit/1.3.2/pigit-1.3.2-py3-none-any.whl

---
+++ packages/pigit/pigit-1.3.2-py3-none-any.whl/pigit/common/str_utils.py
@@ -0,0 +1,235 @@
+# -*- coding:utf-8 -*-
+
+"""This file support some str util method."""
+
+# from typing import Final # python3.8
+
+# yapf: disable
+WIDTHS: list[tuple[int,int]] = [
+    (126, 1), (159, 0), (687, 1), (710, 0), (711, 1), (727, 0), (733, 1), (879, 0),
+    (1154, 1), (1161, 0), (4347, 1), (4447, 2), (7467, 1),
+    (7521, 0), (8369, 1), (8426, 0), (9000, 1), (9002, 2),
+    (11021, 1), (12350, 2), (12351, 1), (12438, 2), (12442, 0), (19893, 2),
+    (19967, 1), (55203, 2), (63743, 1), (64106, 2), (65039, 1), (65059, 0),
+    (65131, 2), (65279, 1), (65376, 2), (65500, 1), (65510, 2), (120831, 1),
+    (262141, 2), (1114109, 1),
+]
+# yapf: enable
+
+
+def get_width(r: int) -> int:
+    """Gets the width occupied by characters on the command line.
+
+    >>> get_width(ord('a'))
+    1
+    >>> get_width(ord('中'))
+    2
+    >>> get_width(ord('Ç'))
+    1
+    """
+    if r == 0xE or r == 0xF:
+        return 0
+    for num, wid in WIDTHS:
+        if r <= num:
+            return wid
+    return 1
+
+
+# Mark the type corresponding to the file suffix.
+# abcdefg hijklmn opq rst uvw xyz
+SUFFIX_TYPE: dict[str, str] = {
+    "": "",
+    "bat": "Batch",
+    "c": "C",
+    "cfg": "Properties",
+    "conf": "Properties",
+    "cpp": "C++",
+    "cs": "C#",
+    "css": "CSS",
+    "dart": "Dart",
+    "dea": "XML",
+    "go": "Go",
+    "gradle": "Groovy",
+    "h": "C",
+    "hpp": "C++",
+    "htm": "HTML",
+    "html": "HTML",
+    "ini": "Ini",
+    "java": "Java",
+    "js": "Java Script",
+    "json": "Json",
+    "jsx": "React",
+    "kt": "Kotlin",
+    "launch": "XML",
+    "less": "CSS",
+    "lua": "Lua",
+    "log": "Log",
+    "m": "Object-C",
+    "mm": "Object-C++",
+    "markdown": "Markdown",
+    "md": "Markdown",
+    "msg": "ROS Message",
+    "php": "PHP",
+    "plist": "XML",
+    "properties": "Propertie",
+    "py": "Python",
+    "r": "R",
+    "rb": "Ruby",
+    "rc": "Properties",
+    "rs": "Rust",
+    "rst": "reStructuredText",
+    "rviz": "YAML",
+    "ts": "Type Script",
+    "tsx": "React",
+    "sass": "CSS",
+    "scss": "CSS",
+    "sh": "Shell",
+    "sql": "SQL",
+    "srdf": "YAML",
+    "srv": "ROS Message",
+    "swift": "Swift",
+    "toml": "Properties",
+    "urdf": "XML",
+    "vb": "Visual Basic",
+    "vim": "Vim Scirpt",
+    "vue": "Vue",
+    "xhtml": "HTML",
+    "xml": "XML",
+    "yaml": "YAML",
+    "yml": "YAML",
+    "zsh": "Shell",
+}
+
+# Mark the type of some special files.
+SPECIAL_NAMES: dict[str, str] = {
+    "license": "LICENSE",
+    "requirements.txt": "Pip requirement",
+    "vimrc": "Vim Scirpt",
+    "dockerfile": "Docker",
+}
+
+
+def adjudgment_type(file: str, original: bool = False) -> str:
+    """Get file type.
+
+    First, judge whether the file name is special, and then query the
+    file suffix. Otherwise, the suffix or name will be returned as is.
+
+    Args:
+        file (str): file name string.
+        original (bool, option): whether return origin when match failed.
+
+    Returns:
+        (str): file type.
+
+    >>> adjudgment_type('test.py')
+    'Python'
+    >>> adjudgment_type('xxxxx')
+    'unknown'
+    >>> adjudgment_type('xxxxx', True)
+    'xxxxx'
+    """
+
+    pre_type = SPECIAL_NAMES.get(file.lower(), None)
+    if pre_type:
+        return pre_type
+
+    suffix = file.split(".")[-1]
+    suffix_type = SUFFIX_TYPE.get(suffix.lower(), None)
+    if suffix_type:
+        return suffix_type
+    else:
+        return suffix if original else "unknown"
+
+
+FILE_ICONS: dict[str, str] = {
+    "": "",
+    "Batch": "",
+    "C": "",
+    "C#": "",
+    "C++": "",
+    "CSS": "",
+    "Dart": "",
+    "Groovy": "",
+    "Go": "",
+    "HTML": "",
+    "Java": "",
+    "Java Scirpt": "",
+    "Lua": "",
+    "Kotlin": "",
+    "Markdown": "",
+    "PHP": "",
+    "Propertie": "",
+    "Python": "",
+    "R": "ﳒ",
+    "React": "",
+    "Ruby": "",
+    "Rust": "",
+    "ROS Message": "",
+    "reStructuredText": "",
+    "Shell": "",
+    "Swift": "",
+    "SQL": "",
+    "snippets": "",
+    "Type Scirpt": "",
+    "Vim Scirpt": "",
+    "Vue": "﵂",
+    "YAML": "",
+    "XML": "",
+}
+
+
+def get_file_icon(file_type: str) -> str:
+    """According file type return icon.
+
+    Args:
+        file_type (str): type string.
+
+    Returns:
+        str: icon.
+
+    >>> get_file_icon('Python')
+    ''
+    >>> get_file_icon('Lua')
+    ''
+    >>> get_file_icon('xxxxxxxxxx')
+    ''
+    """
+
+    #     
+    return FILE_ICONS.get(file_type, "")
+
+
+def shorten(
+    text: str, width: int, placeholder: str = "...", front: bool = False
+) -> str:
+    """Truncate exceeded characters.
+
+    Args:
+        text (str): Target string.
+        width (int): Limit length.
+        placeholder (str): Placeholder string. Defaults to "..."
+        front (bool): Head hidden or tail hidden. Defaults to False.
+
+    Returns:
+        (str): shorten string.
+
+    >>> shorten('Hello world!', 9, placeholder='^-^')
+    'Hello ^-^'
+    >>> shorten('Hello world!', 9, placeholder='^-^', front=True)
+    '^-^world!'
+    """
+
+    if len(text) > width:
+        if front:
+            text = placeholder + text[-width + len(placeholder) :]
+        else:
+            text = text[: width - len(placeholder)] + placeholder
+
+    return text
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
---
+++ packages/pigit/pigit-1.3.2-py3-none-any.whl/pigit/common/utils.py
@@ -0,0 +1,231 @@
+# -*- coding:utf-8 -*-
+
+import re
+import signal
+import subprocess
+import logging
+from math import sqrt
+from collections import Counter
+from typing import Union, Iterable, Tuple
+
+
+Log = logging.getLogger(__name__)
+
+# Exit code.
+EXIT_NORMAL: int = 0
+EXIT_ERROR: int = 1
+
+
+def leave(code: int, *args) -> None:
+    """Exit program.
+
+    Receive error code, error message. If the error code matches, print the
+    error information to the log. Then the command line output prompt, and
+    finally exit.
+
+    Args:
+        code: Exit code.
+        *args: Other messages.
+    """
+
+    if code == EXIT_ERROR:
+        Log.error(args)
+
+    raise SystemExit(0)
+
+
+def run_cmd(*args) -> bool:
+    """Run system command.
+
+    Returns:
+        (bool): Whether run successful.
+
+    >>> run_cmd('pwd')
+    True
+    >>> run_cmd('which', 'python')
+    True
+    """
+
+    try:
+        # ? In python2, `subprocess` not support `with` sentence.
+        proc = subprocess.Popen(" ".join(args), shell=True)
+        proc.wait()
+    except Exception as e:
+        Log.error(str(e) + str(e.__traceback__))
+        return False
+    else:
+        return True
+
+
+def exec_cmd(*args) -> tuple[str, str]:
+    """Run system command and get result.
+
+    Returns:
+        (str, str): Error string and result string.
+    """
+
+    try:
+        # Take over the input stream and get the return information.
+        proc = subprocess.Popen(
+            " ".join(args),
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            shell=True,
+        )
+
+        # Get normal output and error output.
+        res = proc.stdout.read().decode()
+        err = proc.stderr.read().decode()
+        proc.kill()
+    except Exception as e:
+        Log.error(str(e) + str(e.__traceback__))
+        return str(e), ""
+    else:
+        return err, res
+
+
+def confirm(text: str = "", default: bool = True) -> bool:
+    """Obtain confirmation results.
+    Args:
+        text (str): Confirmation prompt.
+        default (bool): Result returned when unexpected input.
+
+    Returns:
+        (bool): Confirm result.
+
+    >>> confirm()
+    True
+    >>> confirm(default=False)
+    False
+    """
+    input_command: str = input(text).strip().lower()
+    if input_command in ["n", "no", "N", "No"]:
+        return False
+    elif input_command in ["y", "yes", "Y", "Yes"]:
+        return True
+    else:
+        return default
+
+
+def similar_command(command: str, all_commands: Iterable) -> str:
+    """Get the most similar command with K-NearestNeighbor.
+
+    Args:
+        command (str): command string.
+        all_commands (list): The list of all command.
+
+    Returns:
+        (str): most similar command string.
+
+    >>> commands = ['branch', 'working tree', 'index', 'log', 'push', 'pull', 'tag','commit','conflict']
+    >>> similar_command('br', commands)
+    'branch'
+    >>> similar_command('wo', commands)
+    'working tree'
+    >>> similar_command('com', commands)
+    'commit'
+    """
+
+    #  The dictionary of letter frequency of all commands.
+    words: dict = {word: dict(Counter(word)) for word in all_commands}
+    # Letter frequency of command.
+    fre = dict(Counter(command))
+    # The distance between the frequency of each letter in the command
+    # to be tested and all candidate commands, that is the difference
+    # between the frequency of letters.
+    frequency_difference: dict[str, list[int]] = {
+        word: [fre[ch] - words[word].get(ch, 0) for ch in command]
+        + [words[word][ch] - fre.get(ch, 0) for ch in word]
+        for word in words
+    }
+    # Square of sum of squares of word frequency difference.
+    frequency_sum_square: list[Tuple[str, int]] = list(
+        map(
+            lambda item: (item[0], int(sqrt(sum(map(lambda i: i ** 2, item[1]))))),
+            frequency_difference.items(),
+        )
+    )
+
+    # The value of `frequency_sum_square` is multiplied by the weight to find
+    # the minimum.
+    # Distance weight: compensate for the effect of length difference.
+    # Compare Weight: The more similar the beginning, the higher the weight.
+    min_frequency_command: str = min(
+        frequency_sum_square,
+        key=lambda item: item[1]
+        * (
+            len(command) / len(item[0])
+            if len(command) / len(item[0])
+            else len(item[0]) / len(command)
+        )
+        # Returns how many identical letters are compared from the head. sigmod to 0 ~ 1.
+        * (1 / (len(list(filter(lambda i: i[0] == i[1], zip(command, item[0])))) + 1)),
+    )[0]
+    return min_frequency_command
+
+
+def color_print(value: str, *styles, **options) -> None:
+    """Print to terminal.
+
+    Print special information with color and style according to the
+    incoming parameters.
+
+    Args:
+        msg: A special message.
+        style: Message style, like: [bold, underline].
+    """
+
+    value = "{0}{1}\033[0m".format("".join(styles), value)
+    end = options.get("end", "\n")
+    print(value, end=end)
+
+
+# color hexa string reg.
+_color_re = re.compile(r"^#[0-9A-Fa-f]{6}")
+
+
+def is_color(var_: Union[str, list, tuple, None]) -> bool:
+    """Adjust `s` whether is color. Like: '#FF0000', [255, 0, 0], (0, 255, 0)
+
+    >>> is_color('#FF0000')
+    True
+    >>> is_color([255, 0, 0])
+    True
+    >>> is_color((0, 255, 0))
+    True
+    >>> is_color(None)
+    False
+    >>> is_color(12345)
+    False
+    """
+    if not var_:
+        return False
+    elif type(var_) == str:
+        return True if _color_re.match(str(var_)) else False
+    elif isinstance(var_, list) or isinstance(var_, tuple):
+        if len(var_) != 3:
+            return False
+        else:
+            for i in var_:
+                if i < 0 or i > 255:
+                    return False
+            else:
+                return True
+    else:
+        return False
+
+
+def init_hook():
+    """Take over some system signals, which are used at
+    the beginning of PIGIT.
+    """
+    try:
+        signal.signal(signal.SIGINT, leave)
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
