f0ec8e594276fe31e79fda1aa736244c9cada6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:19 +0000

    Add package sqltest/0.0.2/sqltest-0.0.2.tar.gz

---
+++ packages/sqltest/sqltest-0.0.2.tar.gz/sqltest-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setup(name="sqltest",
+          version="0.0.2",
+          author="stayrascal",
+          author_email="stayrascal@gmail.com",
+          description="sqltest: easy testing ETL SQLs",
+          long_description=long_description,
+          long_description_content_type="text/markdown",
+          keywords=["test", "ETL", "sql"])
