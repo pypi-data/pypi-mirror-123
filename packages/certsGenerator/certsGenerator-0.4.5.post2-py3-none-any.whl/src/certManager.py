dae25f752e2c8d98e11ae16589530b036bf773a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:45 +0000

    Add package certsGenerator/0.4.5.post2/certsGenerator-0.4.5.post2-py3-none-any.whl

---
+++ packages/certsGenerator/certsGenerator-0.4.5.post2-py3-none-any.whl/src/certManager.py
@@ -0,0 +1,324 @@
+import logging
+import os
+import stat
+import sys
+from typing import Tuple, Union, Dict, Optional
+
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import ed25519
+
+from src.conf import Conf
+from src.helpers import loadFile
+from src.certBuilder import CertBuilder
+
+
+class CertManager:
+    def __init__(self, confFile: str):
+        self.conf: Conf = Conf(confFile=confFile)
+
+    def storePrivateKey(
+        self,
+        certName: str,
+        private_key: Union[
+            ec.EllipticCurvePrivateKey, rsa.RSAPrivateKey, ed25519.Ed25519PrivateKey
+        ],
+    ) -> None:
+        certConf = self.conf.getCert(certName=certName)
+        # get passphrase
+        passphrase = self.conf.getPassphrase(certName=certName)
+        # set encryption algorithm
+        encryption_algorithm = self._getEncryptionAlgorithm(passphrase=passphrase)
+        # get other params from conf
+        encoding, key_format = self._getParamsForPrivateBytes(certConf=certConf)
+        # get path and save the key file
+        path = self.conf.getCertPath(certName=certName, ext="private_key")
+
+        content: bytes = None
+        try:
+            if certConf.get("private_key").get("serialization").lower() == "openssh":
+                content = serialization.ssh.serialize_ssh_private_key(
+                    private_key, password=passphrase
+                )
+            else:
+                content = private_key.private_bytes(
+                    encoding=encoding,
+                    format=key_format,
+                    encryption_algorithm=encryption_algorithm,
+                )
+
+            with open(path, mode="wb") as f:
+                f.write(content)
+
+            # set proper file permission for security
+            try:
+                os.chmod(path, mode=0o600)
+            except OSError as e:
+                logging.error(f"can't set permission {stat.S_IRUSR} on {path}: {e}")
+                sys.exit()
+        except OSError as e:
+            logging.error(f"failed to write file {path}: {e}")
+            sys.exit()
+
+    def storePublicKey(self, certName: str, cert: x509.Certificate) -> None:
+        path = self.conf.getCertPath(certName=certName, ext="signed_certificate")
+        certConf = self.conf.getCert(certName=certName)
+        format = certConf.get("public_key").get("format", None)
+
+        try:
+            if type(format) == str and format.lower() == "openssh":
+                content = serialization.ssh.serialize_ssh_public_key(cert.public_key())
+                with open(path, "wb") as f:
+                    f.write(content)
+            # DER or PEM encodings
+            else:
+                encoding, _ = self._getParamsForPublicBytes(certConf=certConf)
+                content = cert.public_bytes(encoding)
+                with open(path, "wb") as f:
+                    f.write(content)
+
+        except OSError:
+            logging.error(f"can't save public key in {path}")
+            sys.exit()
+
+    # info: this method may be used for signing purpose
+    def getPrivateKey(
+        self, certName: str
+    ) -> Union[ec.EllipticCurvePrivateKey, rsa.RSAPrivateKey]:
+        certConf = self.conf.getCert(certName=certName)
+
+        if "storage" not in certConf.keys():
+            raise ValueError(f"key not found in {certConf}")
+            sys.exit()
+        path = certConf["storage"]["path"]
+        certName = certConf["subject_name"]
+
+        keyFile = self.conf.getCertPath(certName=certName, ext="private_key")
+
+        private_key = None
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        if os.path.exists(keyFile):
+            logging.info(f"Private key of {certName} file already exist")
+            private_key_bytes = loadFile(fileName=keyFile)
+            password = self.conf.getPassphrase(certName=certName)
+            try:
+                enc = certConf.get("private_key").get("encoding")
+                if enc == "PEM":
+                    private_key = serialization.load_pem_private_key(
+                        private_key_bytes, password=password
+                    )
+                elif enc == "DER":
+                    private_key = serialization.load_der_private_key(
+                        private_key_bytes, password=password
+                    )
+                # this use case is not supposed to be happen as ssh private key is only written
+                # when created once (no further use, as it is the case for leaf certs)
+                # elif enc.to_lower() == "openssh":
+                #    private_key = serialization.load_ssh_private_key(
+                #        private_key_bytes, password=password
+                #    )
+            except ValueError as e:
+                logging.error(
+                    f"Error while loading the private key {keyFile}. Please make sure the key is properly generated and the file is not empty. {e}"
+                )
+                sys.exit()
+
+        else:
+            logging.info(f"Creating private key of {certName}")
+            key_type: str = (
+                certConf.get("private_key").get("algorithm").get("type").upper()
+            )
+
+            params = certConf.get("private_key").get("algorithm").get("params", None)
+
+            if key_type == "EC":
+                private_key = ec.generate_private_key(
+                    curve=self.conf.curveMapping.get(params.get("curve"))
+                )
+            elif key_type == "RSA":
+                private_key = rsa.generate_private_key(
+                    public_exponent=65537, key_size=int(params.get("key_size"))
+                )
+            elif key_type == "ED25519":
+                private_key = ed25519.Ed25519PrivateKey.generate()
+            else:
+                logging.error(f"Key type not found or implemented {key_type}")
+                raise ValueError()
+
+            self.storePrivateKey(certName=certName, private_key=private_key)
+        return private_key
+
+    def createCerts(self, certName: str) -> None:
+        certConf = self.conf.getCert(certName=certName)
+        # get the private key (created if it doesn't exist yet)
+        private_key = self.getPrivateKey(certName=certName)
+
+        # regarding cert
+        certFile = self.conf.getCertPath(certName=certName, ext="signed_certificate")
+        issuer_name = certConf["issuer_name"]
+        # check if cert exists, otherwise create it
+        if not os.path.exists(certFile):
+            logging.info(f"Creating cert file for {certName}...")
+            # but to create the cert, we need issuer key to sign it
+            issuer_private_key = self.getPrivateKey(certName=issuer_name)
+            # ok then we build the cert
+            subject_name = certConf["subject_name"]
+            ski = x509.SubjectKeyIdentifier.from_public_key(private_key.public_key())
+            cert = CertBuilder(certName=subject_name, conf=self.conf, ski=ski).builder
+            cert = cert.public_key(private_key.public_key())
+            # now sign the cert with the issuer private key
+            hashAlg = self.conf.hashMapping.get(
+                certConf.get("private_key").get("sign_with_alg")
+            )
+            cert = cert.sign(private_key=issuer_private_key, algorithm=hashAlg)
+            # now store the cert
+            self.storePublicKey(certName=certName, cert=cert)
+            logging.info(
+                f"Cert created for {certName} with crt signed by {issuer_name}"
+            )
+        else:
+            logging.info(f"Cert file already exists for {certName}")
+
+        self._checkSignature(subjectName=certName, issuerName=issuer_name)
+
+    def _checkSignature(self, subjectName: str, issuerName: str) -> None:
+        logging.info(f"checking signature of {subjectName} from issuer {issuerName}")
+
+        # set to true as default
+        verify_cert_signature: bool = True
+
+        # in case verify_all_chain = false and can't find cert of issuer
+        if (self.conf.general.get("options", None) is not None) and (
+            self.conf.general.get("options").get("verify_all_chain", None) == "false"
+        ):
+            if (
+                self.conf.getCertPath(certName=issuerName, ext="signed_certificate")
+                is None
+            ):
+                verify_cert_signature = False
+
+        if verify_cert_signature is False:
+            logging.info("Skipping signature check as verify_all_chain = false in conf")
+        else:
+            issuerCrtFile = self.conf.getCertPath(
+                certName=issuerName, ext="signed_certificate"
+            )
+            subjectCrtFile = self.conf.getCertPath(
+                certName=subjectName, ext="signed_certificate"
+            )
+            # load certificates files
+            issuer_public_key = b""
+            subject_public_key = b""
+            if issuerCrtFile != subjectCrtFile:
+                issuer_public_key = loadFile(fileName=issuerCrtFile)
+                subject_public_key = loadFile(fileName=subjectCrtFile)
+            else:
+                subject_public_key = loadFile(fileName=subjectCrtFile)
+                issuer_public_key = subject_public_key
+
+            # extract issuer and subject public keys
+            issuer_cert_conf = self.conf.getCert(certName=issuerName)
+            subject_cert_conf = self.conf.getCert(certName=subjectName)
+            issuer_enc = issuer_cert_conf.get("public_key").get("encoding")
+            subj_enc = subject_cert_conf.get("public_key").get("encoding")
+            cert_to_check = ""
+
+            if issuer_enc.lower() == "openssh" or subj_enc.lower() == "openssh":
+                logging.info("OpenSSH public keys signatures are not checked")
+                pass
+            else:
+                if issuer_enc == "PEM":
+                    issuer_public_key = x509.load_pem_x509_certificate(
+                        issuer_public_key
+                    ).public_key()
+                elif issuer_enc == "DER":
+                    issuer_public_key = x509.load_der_x509_certificate(
+                        issuer_public_key
+                    ).public_key()
+
+                if subj_enc == "PEM":
+                    cert_to_check = x509.load_pem_x509_certificate(subject_public_key)
+                elif subj_enc == "DER":
+                    cert_to_check = x509.load_der_x509_certificate(subject_public_key)
+
+                try:
+                    # checking now
+                    if isinstance(issuer_public_key, rsa.RSAPublicKey):
+                        issuer_public_key.verify(
+                            cert_to_check.signature,
+                            cert_to_check.tbs_certificate_bytes,
+                            self.conf.RSApaddingMapping["PKCS1v15"](),
+                            cert_to_check.signature_hash_algorithm,
+                        )
+                    elif isinstance(issuer_public_key, ec.EllipticCurvePublicKey):
+                        issuer_public_key.verify(
+                            cert_to_check.signature,
+                            cert_to_check.tbs_certificate_bytes,
+                            ec.ECDSA(cert_to_check.signature_hash_algorithm),
+                        )
+                    elif isinstance(issuer_public_key, ed25519.Ed25519PublicKey):
+                        issuer_public_key.verify(
+                            cert_to_check.signature,
+                            cert_to_check.tbs_certificate_bytes,
+                        )
+                    else:
+                        logging.error(
+                            f"Failed to verify due to unsupported algorythm {type(cert_to_check.public_key())}"
+                        )
+                        raise ValueError()
+                        sys.exit()
+                except ValueError as e:
+                    logging.error(f"Signature verification failed: {e}")
+                    sys.exit()
+
+                logging.info(f"Signature validation success for {subjectName}")
+
+    def _getParamsForPrivateBytes(
+        self, certConf: Dict
+    ) -> Tuple[serialization.Encoding, serialization.PrivateFormat]:
+        encoding = self.conf.encodingMapping.get(
+            certConf.get("private_key").get("encoding")
+        )
+        key_format = self.conf.privateSerializationMapping[
+            certConf.get("private_key").get("serialization")
+        ]
+
+        return encoding, key_format
+
+    def _getParamsForPublicBytes(
+        self, certConf: Dict
+    ) -> Tuple[serialization.Encoding, serialization.PublicFormat]:
+        encoding = self.conf.encodingMapping.get(
+            certConf.get("public_key").get("encoding")
+        )
+        key_format: serialization.PublicFormat = None
+
+        try:
+            # default key format is SubjectPublicKeyInfo if not specified
+            if certConf.get("public_key").get("serialization", None) is None:
+                key_format = self.conf.publicSerializationMapping[
+                    "SubjectPublicKeyInfo"
+                ]
+            else:
+                key_format = self.conf.publicSerializationMapping[
+                    certConf.get("public_key").get("serialization")
+                ]
+        except ValueError as e:
+            logging.error(f"Enable to set key format due to error {e}")
+            sys.exit()
+
+        return encoding, key_format
+
+    def _getEncryptionAlgorithm(
+        self, passphrase: Optional[str] = None
+    ) -> Union[serialization.BestAvailableEncryption, serialization.NoEncryption]:
+        if passphrase:
+            encryption_algorithm = serialization.BestAvailableEncryption(passphrase)
+        else:
+            encryption_algorithm = serialization.NoEncryption()
+
+        return encryption_algorithm
---
+++ packages/certsGenerator/certsGenerator-0.4.5.post2-py3-none-any.whl/src/conf.py
@@ -0,0 +1,285 @@
+import datetime
+import sys
+import logging
+
+from orjson import loads, JSONEncodeError
+from typing import Union, Dict, Any
+
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from src.helpers import loadFile
+
+
+class Conf:
+    def __init__(self, confFile: str):
+        self.general = self._load(confFile)
+        self.fileExtenstions = self._getFileExt()
+
+        # run checks
+        self._checkCertName()
+        self._checkEncoding()
+        self._checkExtendedKeyUsage()
+        self._checkKeyUsage()
+        self._checkNotValidDate()
+        self._checkPubKeyEncodingPresent()
+        self._checkRedundantNames()
+        self._checkSerialization()
+
+    def getCert(self, certName: str) -> Union[Dict[Any, Any], None]:
+        certs = self.general["certs"]
+        conf = {}
+        for cert in certs:
+            if cert["name"] == certName:
+                conf = cert
+                break
+
+        if not (len(conf) > 0):
+            return None
+
+        return conf.get("conf")
+
+    def get(self, certName: str, field: str, isExt: bool = True) -> dict:
+        certConf = self.getCert(certName)
+        if isExt is True:
+            return certConf.get("extensions").get(field)  # type: ignore
+        elif certConf.get(field):
+            return certConf.get(field)
+        else:
+            logging.error(f"{field} not found in conf")
+            raise ValueError()
+
+    def getCertPath(self, certName: str, ext: str) -> Union[str, None]:
+        certConf = self.getCert(certName)
+        if certConf is not None:
+            path = certConf.get("storage").get("path")  # type: ignore
+            fileName = certConf.get("storage").get("fileName")  # type: ignore
+            ext = self.fileExtenstions.get(ext)  # type: ignore
+            certpath = f"{path}/{fileName}.{ext}"
+
+            return certpath
+        else:
+            print("cant fint cert path")
+            return None
+
+    def getPassphrase(self, certName: str) -> Union[bytes, None]:
+        # get passphrase
+        certConf = self.getCert(certName)
+        if certConf.get("private_key") and certConf.get("private_key").get(
+            "passphrase"
+        ):
+            p = certConf.get("private_key").get("passphrase").get("path")  # type: ignore
+            n = certConf.get("private_key").get("passphrase").get("fileName")  # type: ignore
+            passFile = f"{p}/{n}"
+            passphrase = loadFile(passFile)
+
+            if not type(passphrase) == bytes:
+                logging.error("passphrase should be of bytes type")
+                raise ValueError()
+                sys.exit()
+            return passphrase
+        else:
+            passphrase = None
+            return passphrase
+
+    def _load(self, fileName: str) -> dict:
+        generalConf = {}
+        try:
+            with open(fileName, mode="r", encoding="utf-8") as file:
+                try:
+                    generalConf = loads(file.read())
+                except JSONEncodeError as e:
+                    sys.exit(f"enable to load string from {fileName}: {e}")
+        except OSError as e:
+            logging.error(f"failed to open {fileName}: {e}")
+        return generalConf
+
+    def _getFileExt(self) -> dict:
+        return self.general.get("defaults").get("file_extentions")
+
+    def _checkPubKeyEncodingPresent(self) -> None:
+        # check if the public key encoding is persent
+        certs = self.general["certs"]
+        for cert in certs:
+            try:
+                cert.get("conf").get("public_key").get("encoding")
+            except KeyError as e:
+                certName = cert.get("name")
+                logging.error(
+                    f"Configuration file error: can't find public key encoding for {certName}, {e}"
+                )
+                raise ValueError()
+
+    def _checkRedundantNames(self) -> None:
+        # check if no redundant certs names
+        certs = self.general.get("certs")
+        names: dict = {}
+        for cert in certs:
+            if cert["name"] in names.keys():
+                names[cert["name"]] += 1
+            else:
+                names[cert["name"]] = 1
+        for k, v in names.items():
+            if v > 1:
+                logging.error(f"Configuration file error: {k} appears {v} times")
+                raise ValueError()
+
+    def _checkNotValidDate(self) -> None:
+        # check not_valid_before not_valid_after
+        certs = self.general.get("certs")
+        for certConf in certs:
+            certConf = certConf.get("conf")
+            if certConf.get("not_valid_before") == "now":
+                nvb = datetime.datetime.utcnow()
+            elif isinstance(int(), certConf.get("not_valid_before")) or isinstance(
+                int, certConf.get("not_valid_after")
+            ):
+                nvb = datetime.datetime.utcnow() + datetime.timedelta(
+                    days=certConf.get("not_valid_before")
+                )
+            else:
+                logging.error(f'invalid value from {nvb}, should be of int or "now"')
+                raise ValueError()
+
+    def _checkCertName(self) -> None:
+        certs = self.general.get("certs")
+        for cert in certs:
+            subjectName = cert.get("conf").get("subject_name")
+            certName = cert.get("name")
+            if certName != subjectName:
+                logging.error(
+                    f"certname {certName} has to be the same than the subject name, found {subjectName}"
+                )
+                raise ValueError()
+
+    def _checkKeyUsage(self) -> None:
+        certs = self.general.get("certs")
+        for cert in certs:
+            certName = cert.get("name")
+            for ku in cert.get("conf").get("extensions").get("KeyUsage").get("items"):
+                if ku.lower() not in self.keyUsage:
+                    logging.error(f"{ku} not found in allowed keyUsage for {certName}")
+                    raise ValueError()
+
+    def _checkExtendedKeyUsage(self) -> None:
+        certs = self.general.get("certs")
+        for cert in certs:
+            if cert.get("conf").get("extensions").get("ExtendedKeyUsage"):
+                for ku in (
+                    cert.get("conf")
+                    .get("extensions")
+                    .get("ExtendedKeyUsage")
+                    .get("items")
+                ):
+                    if ku.upper() not in self.extendedKeyUsageMapping.keys():
+                        logging.error(f"{ku} not found in allowed ExtendedKeyUsage")
+                        raise ValueError()
+
+    def _checkEncoding(self) -> None:
+        certs = self.general.get("certs")
+        for cert in certs:
+            certName = cert.get("name")
+            enc_priv = cert.get("conf").get("private_key").get("encoding")
+            enc_pub = cert.get("conf").get("public_key").get("encoding")
+            if (enc_priv not in self.encodingMapping.keys()) or (
+                enc_pub not in self.encodingMapping.keys()
+            ):
+                logging.error(
+                    f"encoding not found for {certName} in allowed encoding formats"
+                )
+                raise ValueError()
+
+    def _checkSerialization(self) -> None:
+        certs = self.general.get("certs")
+        for cert in certs:
+            certName = cert.get("name")
+            serialization = cert.get("conf").get("private_key").get("serialization")
+            if serialization not in self.privateSerializationMapping.keys():
+                logging.error(
+                    f"serialization not found for private key of {certName} in allowed serialization formats"
+                )
+                raise ValueError()
+
+    nameAttributesMapping = {
+        "COUNTRY_NAME": NameOID.COUNTRY_NAME,
+        "STATE_OR_PROVINCE_NAME": NameOID.STATE_OR_PROVINCE_NAME,
+        "LOCALITY_NAME": NameOID.LOCALITY_NAME,
+        "ORGANIZATION_NAME": NameOID.ORGANIZATION_NAME,
+        "COMMON_NAME": NameOID.COMMON_NAME,
+    }
+
+    extensionMapping = {
+        "SubjectAlternativeName": x509.SubjectAlternativeName,
+        "KeyUsage": x509.KeyUsage,
+        "BasicConstraints": x509.BasicConstraints,
+        "NameConstraints": x509.NameConstraints,
+        "NameAttribute": x509.NameAttribute,
+        "AuthorityKeyIdentifier": x509.AuthorityKeyIdentifier,
+        "DNSName": x509.DNSName,
+        "IPAddress": x509.IPAddress,
+    }
+
+    curveMapping = {
+        "SECP256R1": ec.SECP256R1(),
+        "SECP384R1": ec.SECP384R1(),
+        "SECP521R1": ec.SECP521R1(),
+    }
+
+    encodingMapping = {
+        "PEM": serialization.Encoding.PEM,
+        "DER": serialization.Encoding.DER,
+        "Raw": serialization.Encoding.Raw,
+        "OpenSSH": serialization.Encoding.OpenSSH,
+    }
+
+    privateSerializationMapping = {
+        "PKCS8": serialization.PrivateFormat.PKCS8,
+        "TraditionalOpenSSL": serialization.PrivateFormat.TraditionalOpenSSL,
+        "Raw": serialization.PrivateFormat.Raw,
+        "OpenSSH": serialization.PrivateFormat.OpenSSH,
+    }
+
+    publicSerializationMapping = {
+        "SubjectPublicKeyInfo": serialization.PublicFormat.SubjectPublicKeyInfo,
+        "PKCS1": serialization.PublicFormat.PKCS1,
+        "Raw": serialization.PublicFormat.Raw,
+        "OpenSSH": serialization.PublicFormat.OpenSSH,
+    }
+
+    RSApaddingMapping = {
+        "PSS": padding.PSS,
+        "OAEP": padding.OAEP,
+        "PKCS1v15": padding.PKCS1v15,
+    }
+
+    hashMapping = {
+        "sha256": hashes.SHA256(),
+        "sha384": hashes.SHA384(),
+        "sha512": hashes.SHA512(),
+    }
+
+    keyUsage = [
+        "digital_signature",
+        "content_commitment",
+        "key_encipherment",
+        "data_encipherment",
+        "key_agreement",
+        "key_cert_sign",
+        "crl_sign",
+        "encipher_only",
+        "decipher_only",
+    ]
+
+    extendedKeyUsageMapping = {
+        "SERVER_AUTH": x509.oid.ExtendedKeyUsageOID.SERVER_AUTH,
+        "CLIENT_AUTH": x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH,
+        "CODE_SIGNING": x509.oid.ExtendedKeyUsageOID.CODE_SIGNING,
+        "EMAIL_PROTECTION": x509.oid.ExtendedKeyUsageOID.EMAIL_PROTECTION,
+        "TIME_STAMPING": x509.oid.ExtendedKeyUsageOID.TIME_STAMPING,
+        "OCSP_SIGNING": x509.oid.ExtendedKeyUsageOID.OCSP_SIGNING,
+        "ANY_EXTENDED_KEY_USAGE": x509.oid.ExtendedKeyUsageOID.ANY_EXTENDED_KEY_USAGE,
+    }
