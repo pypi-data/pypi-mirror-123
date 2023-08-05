84c9b6aa8c9155f4dd70090f1c449954bedeed2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package mlprodict/0.7.1625/mlprodict-0.7.1625-cp36-cp36m-win_amd64.whl

---
+++ packages/mlprodict/mlprodict-0.7.1625-cp36-cp36m-win_amd64.whl/mlprodict/onnx_tools/onnx_export_templates.py
@@ -0,0 +1,49 @@
+"""
+@file
+@brief Templates to export an ONNX graph in a way it can we created again
+with a python script.
+
+.. versionadded:: 0.7
+"""
+import os
+from textwrap import dedent
+try:
+    from functools import cache
+except ImportError:
+    # python 3.8
+    from functools import lru_cache as cache
+
+
+@cache
+def _get_file(name):
+    """
+    Retrieves one template.
+    """
+    this = os.path.abspath(os.path.dirname(__file__))
+    filename = os.path.join(this, "_onnx_export_templates_%s.tmpl" % name)
+    if not os.path.exists(filename):
+        raise FileNotFoundError(  # pragma: no cover
+            "Unable to find template %r in folder %r." % (name, this))
+    with open(filename, "r", encoding="utf-8") as f:
+        return dedent(f.read())
+
+
+def get_onnx_template():
+    """
+    Template to export :epkg:`ONNX` into :epkg:`onnx` code.
+    """
+    return _get_file('onnx')
+
+
+def get_tf2onnx_template():
+    """
+    Template to export :epkg:`ONNX` into :epkg:`tensorflow-onnx` code.
+    """
+    return _get_file('tf2onnx')
+
+
+def get_numpy_template():
+    """
+    Template to export :epkg:`ONNX` into :epkg:`numpy` code.
+    """
+    return _get_file('numpy')
