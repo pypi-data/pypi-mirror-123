b9f61ee249f63fcef325f5e1f0aeb54e6e62b850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:52 +0000

    Add package authx/0.0.6/AuthX-0.0.6.tar.gz

---
+++ packages/authx/AuthX-0.0.6.tar.gz/AuthX-0.0.6/AuthX/__init__.py
@@ -0,0 +1,8 @@
+"""Ready to use and customizable Authentications and Oauth2 management for FastAPI"""
+
+__version__ = "0.0.6"
+
+
+from AuthX.main import Authentication, AuthX, User
+
+__all__ = ["AuthX", "Authentication", "User"]
---
+++ packages/authx/AuthX-0.0.6.tar.gz/AuthX-0.0.6/AuthX/utils/captcha.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from httpx import AsyncClient
+
+
+async def validate_captcha(captcha: Optional[str], recaptcha_secret: str):
+    """
+    Validate a captcha.
+
+    Args:
+        captcha (Optional[str]): The captcha to validate.
+        recaptcha_secret (str): The recaptcha secret.
+
+    Returns:
+        bool: Whether the captcha is valid.
+    """
+    if captcha is None:
+        return False
+    async with AsyncClient(base_url="https://www.google.com") as client:
+        payload = {
+            "secret": recaptcha_secret,
+            "response": captcha,
+        }
+        response = await client.post("/recaptcha/api/siteverify", data=payload)
+        response_obj = response.json()
+        return response_obj.get("success")
