08232a811b5d5f2a282e1cebe359084332c33824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package sub-prob/1.0/sub-prob-1.0.tar.gz

---
+++ packages/sub-prob/sub-prob-1.0.tar.gz/sub-prob-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sub-prob',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['sub-prob'],
+      author='Subham Sourav Brahma',
+      author_email='subhamsourav175@gmail.com',
+      zip_safe=False)
