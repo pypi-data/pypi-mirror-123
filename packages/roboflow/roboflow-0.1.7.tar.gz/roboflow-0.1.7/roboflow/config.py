0dc4b2a95e6f08fd633540d468df734b16c0bd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package roboflow/0.1.7/roboflow-0.1.7.tar.gz

---
+++ packages/roboflow/roboflow-0.1.7.tar.gz/roboflow-0.1.7/roboflow/config.py
@@ -0,0 +1,9 @@
+OBJECT_DETECTION_MODEL = "ObjectDetectionModel"
+CLASSIFICATION_MODEL = "ClassificationModel"
+PREDICTION_OBJECT = "Prediction"
+API_URL = "https://api.roboflow.com"
+APP_URL = "https://app.roboflow.com"
+
+from dotenv import load_dotenv
+
+load_dotenv()
