6bb1770ca28cab80d0fef9358d97aa1b6e37d69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:20 +0000

    Add package moto/2.2.10.dev2/moto-2.2.10.dev2-py2.py3-none-any.whl

---
+++ packages/moto/moto-2.2.10.dev2-py2.py3-none-any.whl/moto/emr/urls.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from .responses import ElasticMapReduceResponse
+
+url_bases = [
+    r"https?://(.+)\.elasticmapreduce\.amazonaws.com",
+    r"https?://elasticmapreduce\.(.+)\.amazonaws.com",
+]
+
+url_paths = {"{0}/$": ElasticMapReduceResponse.dispatch}
