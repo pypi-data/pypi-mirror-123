f1823880838695aaa8bbd17451b7e6ffa6139e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:54 +0000

    Add package cernest-probability/0.1/cernest_probability-0.1.tar.gz

---
+++ packages/cernest-probability/cernest_probability-0.1.tar.gz/cernest_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cernest_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['cernest_probability'],
+      author = 'Ernest Chukwunta',
+      author_email = 'ernestchukwunta@yahoo.com',
+      zip_safe=False)
