6b08abd619c98ba627a31a10ee584306e9199a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package Bi-Gau-distributions/0.1/Bi_Gau_distributions-0.1.tar.gz

---
+++ packages/Bi-Gau-distributions/Bi_Gau_distributions-0.1.tar.gz/Bi_Gau_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Bi_Gau_distributions',
+      version='0.1',
+      description='discrete variable probability distribution',
+      packages=['Bi_Gau_distributions'],
+      zip_safe=False)
