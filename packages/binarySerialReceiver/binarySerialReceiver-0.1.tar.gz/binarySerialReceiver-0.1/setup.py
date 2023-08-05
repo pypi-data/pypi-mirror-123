ec5d6de0e4c9ab7bd57129719f79370057d136a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package binarySerialReceiver/0.1/binarySerialReceiver-0.1.tar.gz

---
+++ packages/binarySerialReceiver/binarySerialReceiver-0.1.tar.gz/binarySerialReceiver-0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'binarySerialReceiver',
+  packages = ['binarySerialReceiver'],
+  version = '0.1',
+  license='MIT',
+  description = 'Simple module for receiving binary data from a serial port',
+  author = 'Francisco Liebl',
+  author_email = 'chicolliebl@gmail.com',
+  url = 'https://github.com/ChicoLiebl/python-binarySerialReceiver.git',
+  download_url = 'https://github.com/ChicoLiebl/python-binarySerialReceiver/archive/refs/tags/V1.0.tar.gz',
+  keywords = ['SERIAL', 'PORT', 'RECEIVER', 'BINARY', 'THREADED', 'BACKGROUND'],
+  install_requires=[
+          'numpy',
+          'pyserial',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
