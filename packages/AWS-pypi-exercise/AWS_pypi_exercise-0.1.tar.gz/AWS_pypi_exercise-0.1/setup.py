bf9528809e4355c1198f604da310d3ae107c9c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package AWS-pypi-exercise/0.1/AWS_pypi_exercise-0.1.tar.gz

---
+++ packages/AWS-pypi-exercise/AWS_pypi_exercise-0.1.tar.gz/AWS_pypi_exercise-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AWS_pypi_exercise',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['AWS_pypi_exercise'],
+      author='Kelvin Mwaka',
+      author_email='kevmwa@gmail.com',
+      zip_safe=False)
