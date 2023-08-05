133bf007b3c74061513641076c2c57a78da51ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package extractreq/0.0.7/extractreq-0.0.7.tar.gz

---
+++ packages/extractreq/extractreq-0.0.7.tar.gz/extractreq-0.0.7/src/extractreq/__init__.py
@@ -0,0 +1,8 @@
+from extractreq.usecase_modul1 import xmlParser
+from extractreq.usecase_modul2 import parsingRequirement
+from extractreq.usecase_modul3 import ucdReq
+
+from extractreq.modul_ekspart import partOf
+from extractreq.modul_spacySent import spacyClause
+from extractreq.modul_stanfordSent import stanford_clause
+from extractreq.modul_triplet import extractNlp
