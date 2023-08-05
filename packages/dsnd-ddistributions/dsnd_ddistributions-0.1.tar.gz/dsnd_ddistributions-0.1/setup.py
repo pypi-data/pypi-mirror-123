40c40178e602e277de3af7c6aee229bce3f14aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:37 +0000

    Add package dsnd-ddistributions/0.1/dsnd_ddistributions-0.1.tar.gz

---
+++ packages/dsnd-ddistributions/dsnd_ddistributions-0.1.tar.gz/dsnd_ddistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_ddistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_ddistributions'],
+      zip_safe=False)
