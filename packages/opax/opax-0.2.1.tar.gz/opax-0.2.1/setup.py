aacb2fc29664c0457a1a505f1e5ce8160cbd68ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:34 +0000

    Add package opax/0.2.1/opax-0.2.1.tar.gz

---
+++ packages/opax/opax-0.2.1.tar.gz/opax-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.2.1"
+url = "https://github.com/ntt123/opax"
+
+install_requires = ["pax3 >= 0.4.0rc2"]
+setup_requires = []
+tests_require = []
+
+setup(
+    name="opax",
+    version=__version__,
+    description="A stateful optimizer library for Jax",
+    author="Thông Nguyễn",
+    url=url,
+    keywords=["deep-learning", "jax", "pax", "optimizer", "adam", "sgd"],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    packages=find_packages(exclude=["examples", "tests"]),
+    extras_require={"test": tests_require},
+    python_requires=">=3.7",
+)
