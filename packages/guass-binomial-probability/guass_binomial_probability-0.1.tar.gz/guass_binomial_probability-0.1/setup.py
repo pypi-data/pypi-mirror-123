7d6131285b747f78084f2981f1a4d03392116ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:30 +0000

    Add package guass-binomial-probability/0.1/guass_binomial_probability-0.1.tar.gz

---
+++ packages/guass-binomial-probability/guass_binomial_probability-0.1.tar.gz/guass_binomial_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+      name='guass_binomial_probability',
+      version='0.1',
+      author= 'Jeremiah Binyiako',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+zip_safe=False)
