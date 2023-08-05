ee3a3c16efe078e21a509f12ac0bdc89beac4664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package Disecon/0.1.8/Disecon-0.1.8.tar.gz

---
+++ packages/Disecon/Disecon-0.1.8.tar.gz/Disecon-0.1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Disecon',
+  version='0.1.8',
+  description='Disecon',
+  long_description='Disecon is a python library that is too build off any discord python library.',
+  url='',  
+  author='LegosAndStuff',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='economy', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
