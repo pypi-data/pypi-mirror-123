57808ecc33341ad46ac28a4c8238bbcdf9f41ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:38 +0000

    Add package stats-distro/0.1/stats_distro-0.1.tar.gz

---
+++ packages/stats-distro/stats_distro-0.1.tar.gz/stats_distro-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats_distro',
+      version='0.1',
+      description='Binomial and Gaussian distributions and some functionalitites',
+      packages=['stats_distro'],
+      author = 'Kunal Sharma',
+      author_email = 'kushkunal077@gmail.com',
+      zip_safe=False)
