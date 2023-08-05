f82aa693c7fc9859b5f36aa0d6e6f7da8d0c0f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:35 +0000

    Add package types-annoy/1.17.4/types-annoy-1.17.4.tar.gz

---
+++ packages/types-annoy/types-annoy-1.17.4.tar.gz/types-annoy-1.17.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-annoy"
+description = "Typing stubs for annoy"
+long_description = '''
+## Typing stubs for annoy
+
+This is a PEP 561 type stub package for the `annoy` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `annoy`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/annoy. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `9f869723509ac027bae6b6f567b921d6a229e8ec`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.17.4",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['annoy-stubs'],
+      package_data={'annoy-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
