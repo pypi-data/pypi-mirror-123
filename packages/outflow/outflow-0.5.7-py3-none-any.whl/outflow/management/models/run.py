dbc600ee6cf60d20a8b4d1720503885f15338aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:56 +0000

    Add package outflow/0.5.7/outflow-0.5.7-py3-none-any.whl

---
+++ packages/outflow/outflow-0.5.7-py3-none-any.whl/outflow/management/models/run.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from outflow.core.db import Model
+from outflow.management.models.configuration import Configuration
+from outflow.management.models.mixins import Executable
+
+
+class Run(Model, Executable):
+    """
+    Stores a run
+    """
+
+    id = Column(Integer, primary_key=True)
+    configuration_id = Column(Integer, ForeignKey(Configuration.id), nullable=False)
+    configuration = relationship("Configuration", back_populates="runs")
