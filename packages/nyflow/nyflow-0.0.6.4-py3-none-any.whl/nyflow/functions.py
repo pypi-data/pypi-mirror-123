40078c71cb8e6c076c226c173c1661075ed3cbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:49 +0000

    Add package nyflow/0.0.6.4/nyflow-0.0.6.4-py3-none-any.whl

---
+++ packages/nyflow/nyflow-0.0.6.4-py3-none-any.whl/nyflow/functions.py
@@ -0,0 +1,83 @@
+import numpy as np
+import nyflow
+# from nyflow import cuda, utils
+from nyflow.core import Function, Variable, as_variable, as_array
+
+
+# =============================================================================
+# Basic functions: sin / cos / tanh / exp / log
+# =============================================================================
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * cos(x)    # 1.
+        return gx
+
+
+def sin(x):
+    return Sin()(x)
+
+
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+
+def cos(x):
+    return Cos()(x)
+
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        gx = gy * (1 - y * y)
+        return gx
+
+
+def tanh(x):
+    return Tanh()(x)
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        gx = gy * y
+        return gx
+
+
+def exp(x):
+    return Exp()(x)
+
+
+class Log(Function):
+    def forward(self, x):
+        y = np.log(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy / x
+        return gx
+
+
+def log(x):
+    return Log()(x)
