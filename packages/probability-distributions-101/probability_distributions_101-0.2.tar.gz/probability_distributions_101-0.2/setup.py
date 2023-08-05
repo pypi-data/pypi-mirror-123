ea76d3592fd7cb6d7b313b9ddbbfc51926e66181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:41 +0000

    Add package probability-distributions-101/0.2/probability_distributions_101-0.2.tar.gz

---
+++ packages/probability-distributions-101/probability_distributions_101-0.2.tar.gz/probability_distributions_101-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distributions_101',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_101'],
+      author = 'Wided Ahlem Touhami',
+      author_email = 'w.touhami@esi-sba.dz',
+      zip_safe=False)
