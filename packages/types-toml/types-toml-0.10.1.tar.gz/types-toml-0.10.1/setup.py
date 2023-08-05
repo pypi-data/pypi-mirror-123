2bb8700026cf862ed9f9101525f131ee2b8bcd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:05 +0000

    Add package types-toml/0.10.1/types-toml-0.10.1.tar.gz

---
+++ packages/types-toml/types-toml-0.10.1.tar.gz/types-toml-0.10.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-toml"
+description = "Typing stubs for toml"
+long_description = '''
+## Typing stubs for toml
+
+This is a PEP 561 type stub package for the `toml` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `toml`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/toml. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `9f869723509ac027bae6b6f567b921d6a229e8ec`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.10.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['toml-stubs'],
+      package_data={'toml-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
