3bc472e41863eab79e4751d4d3ca543bd2b32a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:41 +0000

    Add package rdbms-op/0.0.1/rdbms-op-0.0.1.tar.gz

---
+++ packages/rdbms-op/rdbms-op-0.0.1.tar.gz/rdbms-op-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rdbms-op",
+    version="0.0.1",
+    author="Yejin Jo",
+    author_email="ojjy90@gmail.com",
+    description="Operational Helper for Relational Database Managemant System",
+    long_description="ojjy90 let it go",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ojjy/rdbms-op",
+    project_urls={
+        "Bug Tracker": "https://github.com/ojjy/rdbms-op/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
