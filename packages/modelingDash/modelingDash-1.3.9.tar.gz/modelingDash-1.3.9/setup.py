5c28b1e806e4d08a057c889aabfef5e6db85c77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:34 +0000

    Add package modelingDash/1.3.9/modelingDash-1.3.9.tar.gz

---
+++ packages/modelingDash/modelingDash-1.3.9.tar.gz/modelingDash-1.3.9/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+dirParent = "modelingDash"
+
+setuptools.setup(
+name="modelingDash", # Replace with your own username
+version="1.3.9",
+author="Dorian Drevon",
+author_email="drevondorian@gmail.com",
+description="Utilities package",
+long_description=long_description,
+long_description_content_type="text/markdown",
+# url="https://github.com/pypa/sampleproject",
+# project_urls={
+#     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+# },
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+],
+# package_dir={"": dirParent},
+# packages=['dorianUtils'],
+packages=setuptools.find_packages(),
+package_data={'': ['confFiles/*']},
+include_package_data=True,
+install_requires=['dorianUtilsModulaire==3.9.2'],
+python_requires=">=3.8"
+)
