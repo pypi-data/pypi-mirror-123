dba9cc39c51eaa8d77f8c4a8f61482c39cdf85db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:20 +0000

    Add package dynamicremotely/0.0.1/dynamicremotely-0.0.1.tar.gz

---
+++ packages/dynamicremotely/dynamicremotely-0.0.1.tar.gz/dynamicremotely-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is the beginning of what will hopefully be a library of electromagnetic equations.
---
+++ packages/dynamicremotely/dynamicremotely-0.0.1.tar.gz/dynamicremotely-0.0.1/__init__.py
@@ -0,0 +1,8 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+def add_numbers(num1, num2):
+    return num1 - num2
+def add_numbers(num1, num2):
+    return num1 * num2
+def add_numbers(num1, num2):
+    return num1 / num2
