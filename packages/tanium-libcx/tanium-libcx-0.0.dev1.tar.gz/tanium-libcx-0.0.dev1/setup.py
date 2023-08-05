abf8e8e06224eff6b61ae37e87c584ed9bcdc651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:54 +0000

    Add package tanium-libcx/0.0.dev1/tanium-libcx-0.0.dev1.tar.gz

---
+++ packages/tanium-libcx/tanium-libcx-0.0.dev1.tar.gz/tanium-libcx-0.0.dev1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='tanium-libcx',
+   version='0.0.dev1',
+   description='',
+   author='Tanium Inc.',
+   author_email='product-security@tanium.com',
+   packages=['tanium-libcx'],
+)
