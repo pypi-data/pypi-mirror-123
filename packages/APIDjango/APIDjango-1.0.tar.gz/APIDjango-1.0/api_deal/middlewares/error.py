2d00428975f813f5c234a63b8816eb802a372d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:02 +0000

    Add package APIDjango/1.0/APIDjango-1.0.tar.gz

---
+++ packages/APIDjango/APIDjango-1.0.tar.gz/APIDjango-1.0/api_deal/middlewares/error.py
@@ -0,0 +1,18 @@
+from django.utils.deprecation import MiddlewareMixin
+
+from api_deal.error import SERVER_ERROR
+from api_deal.res import ErrorReturn
+
+
+class MyErrorMiddleware(MiddlewareMixin):
+
+    def process_exception(self, request, exception):
+        if not hasattr(exception, "errmsg"):
+            exception = SERVER_ERROR
+        if hasattr(exception, "args") and isinstance(exception.args, tuple) and exception.args:
+            errmsg_list = []
+            if exception.errmsg:
+                errmsg_list.append(exception.errmsg)
+            errmsg_list.append(";".join(exception.args))
+            exception.errmsg = ":".join(errmsg_list)
+        return ErrorReturn(exception)
