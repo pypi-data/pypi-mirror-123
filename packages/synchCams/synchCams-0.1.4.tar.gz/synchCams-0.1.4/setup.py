646ec8afa27f85e3514e4c57165315684f58c9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:33 +0000

    Add package synchCams/0.1.4/synchCams-0.1.4.tar.gz

---
+++ packages/synchCams/synchCams-0.1.4.tar.gz/synchCams-0.1.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+#from distutils.core import setup
+import codecs
+import os
+import sys
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+README_FILE = os.path.join(PROJECT_ROOT, "README.md")
+
+def get_long_description():
+    with codecs.open(README_FILE, "rt") as buff:
+        return buff.read()
+
+if len(set(("test", "easy_install")).intersection(sys.argv)) > 0:
+    import setuptools
+
+extra_setuptools_args = {}
+setup(
+    name='synchCams',
+    version='0.1.4',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/ateshkoul/SynchCams',
+    license='MIT',
+    description = '',
+    author="Atesh Koul",
+    author_email='atesh.koul@gmail.com',
+    packages=['synchCams'],
+    download_url = 'https://github.com/ateshkoul/SynchCams/archive/v_01.tar.gz',    
+    keywords=['synch cams','dual','cams'],
+    install_requires=[
+          'opencv-python',
+          'opencv-contrib-python',
+          'pyserial',
+          ''
+      ],
+    **extra_setuptools_args,
+
+)
