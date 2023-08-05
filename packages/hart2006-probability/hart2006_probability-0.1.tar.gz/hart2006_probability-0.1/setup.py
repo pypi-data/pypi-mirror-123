5b54c87be34a9924dd82bc8189b80dec5cddd92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package hart2006-probability/0.1/hart2006_probability-0.1.tar.gz

---
+++ packages/hart2006-probability/hart2006_probability-0.1.tar.gz/hart2006_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hart2006_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['hart2006_probability'],
+      author = 'Michael Hart',
+      zip_safe=False)
