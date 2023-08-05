a71cb3941f6352eacb639bf03ccc2d0f2785ee89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:01 +0000

    Add package ABBA-QuPath-RegistrationExporter/0.3.1/ABBA-QuPath-RegistrationExporter-0.3.1.tar.gz

---
+++ packages/ABBA-QuPath-RegistrationExporter/ABBA-QuPath-RegistrationExporter-0.3.1.tar.gz/ABBA-QuPath-RegistrationExporter-0.3.1/regexport/main.py
@@ -0,0 +1,17 @@
+import sys
+
+from PySide2.QtWidgets import QApplication
+#
+from regexport.app import App
+#
+
+def main():
+    qapp = QApplication(sys.argv)
+    app = App()
+    win = app.create_gui()
+    win.show()
+    sys.exit(qapp.exec_())
+
+
+if __name__ == '__main__':
+    main()
