19c1417f56d48a64a0f60d715069b8f343ae6590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:12 +0000

    Add package opax/0.2.2/opax-0.2.2-py3-none-any.whl

---
+++ packages/opax/opax-0.2.2-py3-none-any.whl/opax/__init__.py
@@ -0,0 +1,17 @@
+"""Pax optimizer library."""
+
+from . import optimizer, schedule
+from .optimizer import adam, adamw, rmsprop, sgd
+from .transform import (
+    GradientTransformation,
+    chain,
+    clip,
+    clip_by_global_norm,
+    scale,
+    scale_by_adam,
+    scale_by_rms,
+    scale_by_schedule,
+    scale_by_stddev,
+    trace,
+)
+from .utils import apply_gradients, apply_updates, transform_gradients
