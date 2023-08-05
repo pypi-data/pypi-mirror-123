c1488cf1430a9697137a98acd7567d22dfe41078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package di/0.14.2/di-0.14.2.tar.gz

---
+++ packages/di/di-0.14.2.tar.gz/di-0.14.2/di/params.py
@@ -0,0 +1,64 @@
+"""Convenience functions, mainly for the purpose of providing proper type annotations for default arguments.
+"""
+
+from typing import Optional, overload
+
+from di.dependant import Dependant
+from di.types.providers import (
+    AsyncGeneratorProvider,
+    CallableProvider,
+    CoroutineProvider,
+    DependencyProviderType,
+    DependencyType,
+    GeneratorProvider,
+)
+from di.types.scopes import Scope
+
+
+@overload
+def Depends(
+    call: Optional[AsyncGeneratorProvider[DependencyType]] = None,
+    *,
+    scope: Scope = None,
+    share: bool = True
+) -> DependencyType:
+    ...
+
+
+@overload
+def Depends(
+    call: Optional[CoroutineProvider[DependencyType]] = None,
+    *,
+    scope: Scope = None,
+    share: bool = True
+) -> DependencyType:
+    ...
+
+
+@overload
+def Depends(
+    call: Optional[GeneratorProvider[DependencyType]] = None,
+    *,
+    scope: Scope = None,
+    share: bool = True
+) -> DependencyType:
+    ...
+
+
+@overload
+def Depends(
+    call: Optional[CallableProvider[DependencyType]] = None,
+    *,
+    scope: Scope = None,
+    share: bool = True
+) -> DependencyType:
+    ...
+
+
+def Depends(
+    call: Optional[DependencyProviderType[DependencyType]] = None,
+    *,
+    scope: Scope = None,
+    share: bool = True
+) -> DependencyType:
+    return Dependant(call=call, scope=scope, share=share)  # type: ignore
