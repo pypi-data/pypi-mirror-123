c1637a710d9e0700552fb4a789c92f7b4e5682fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:49 +0000

    Add package dsnd-probability-testing/0.2/dsnd_probability_testing-0.2.tar.gz

---
+++ packages/dsnd-probability-testing/dsnd_probability_testing-0.2.tar.gz/dsnd_probability_testing-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_testing',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['dsnd_probability_testing'],
+      author = 'Yasmin Cortez',
+      author_email = 'yasmincrtz@gmail.com',
+      zip_safe=False)
