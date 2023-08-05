1f787211217c2ccdb5d8db456a53d435f448e9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package OnePuzzle/0.2.1/OnePuzzle-0.2.1-py3-none-any.whl

---
+++ packages/OnePuzzle/OnePuzzle-0.2.1-py3-none-any.whl/one_puzzle/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .__version__ import __version__
+from .__name__ import __name__
+
+__all__ = [
+    "__version__",
+    "__name__",
+]
---
+++ packages/OnePuzzle/OnePuzzle-0.2.1-py3-none-any.whl/one_puzzle/__version__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+version = (0, 2, 1)
+
+__version__ = ".".join(map(str, version))
