4e9e4109367af84cc46d3fd345c71d4ac0040cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:53 +0000

    Add package Facebook-Chat-Analysis/0.2.3/Facebook_Chat_Analysis-0.2.3.tar.gz

---
+++ packages/Facebook-Chat-Analysis/Facebook_Chat_Analysis-0.2.3.tar.gz/Facebook_Chat_Analysis-0.2.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='Facebook_Chat_Analysis',
+      version='0.2.3',
+      description='Pre-Process and Analyse Facebook Messenger Chats',
+      url='https://github.com/mario-likes-camels/Facebook_Chat_Analysis',
+      author='Patrick',
+      author_email='hi_in@hotmail.com',
+      license='MIT',
+      packages=['Facebook_Chat_Analysis'],
+      zip_safe=False)
+      
+     
