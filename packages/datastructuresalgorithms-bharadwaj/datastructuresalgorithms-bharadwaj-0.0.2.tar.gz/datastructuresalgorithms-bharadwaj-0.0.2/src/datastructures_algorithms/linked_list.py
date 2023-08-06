51af0d90ba19a84e51f72622908a64612bfabdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package datastructuresalgorithms-bharadwaj/0.0.2/datastructuresalgorithms-bharadwaj-0.0.2.tar.gz

---
+++ packages/datastructuresalgorithms-bharadwaj/datastructuresalgorithms-bharadwaj-0.0.2.tar.gz/datastructuresalgorithms-bharadwaj-0.0.2/src/datastructures_algorithms/linked_list.py
@@ -0,0 +1,16 @@
+class LinkedNode:
+    def __init__(self):
+        self.data = 0
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        return
+    def append(self):
+        return
+    def insert_element(self):
+        return
+    def remove_element(self):
+        return
+    def print_list(self):
+        return
---
+++ packages/datastructuresalgorithms-bharadwaj/datastructuresalgorithms-bharadwaj-0.0.2.tar.gz/datastructuresalgorithms-bharadwaj-0.0.2/src/datastructures_algorithms/queue.py
@@ -0,0 +1,14 @@
+class QueueNode:
+    def __init__(self):
+        self.data = 0
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        return
+    def enqueue(self):
+        return
+    def dequeue(self):
+        return
+    def print_queue(self):
+        return
