1fc34b6bb26d06197bd4a6bd923bf14bbb6a23ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:49 +0000

    Add package nmodels/1.0/nmodels-1.0.tar.gz

---
+++ packages/nmodels/nmodels-1.0.tar.gz/nmodels-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nmodels',
+    version='1.0',
+    packages=['nmodels'],
+    url='https://github.com/christopher-chandler',
+    license='MIT',
+    author='Chris',
+    author_email='christopher.chandler@outlook.de',
+    description='This is a basic bigram model'
+)
