52a73084abb53846ff09c6f70f287658cfbbecf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:44 +0000

    Add package dobbi/0.01/dobbi-0.01.tar.gz

---
+++ packages/dobbi/dobbi-0.01.tar.gz/dobbi-0.01/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='dobbi',
+    py_modules=['dobbi'],
+    version='0.01',
+    license='Apache License 2.0',
+    description='An open-source library for fast NLP text cleaning and preprocessing.',
+    author='Iaroslav Amerkhanov',
+    author_email='amerkhanov.y@gmail.com',
+    url='https://github.com/iaramer/dobbi',
+    download_url='https://github.com/iaramer/dobbi/archive/refs/tags/v0_01.tar.gz',
+    keywords=['nlp', 'text', 'string', 'preprocessing', 'cleaning', 'regexp'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
