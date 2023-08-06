d8c5de2b798de535ec221a77768e85cabb34a6a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:39 +0000

    Add package AdvEMDpy/0.0.2/AdvEMDpy-0.0.2.tar.gz

---
+++ packages/AdvEMDpy/AdvEMDpy-0.0.2.tar.gz/AdvEMDpy-0.0.2/AdvEMDpy/__init__.py
@@ -0,0 +1,13 @@
+
+#     ________
+#            /
+#      \    /
+#       \  /
+#        \/
+
+from AdvEMDpy.AdvEMDpy import EMD
+from AdvEMDpy.emd_basis import Basis
+from AdvEMDpy.emd_hilbert import Hilbert, hilbert_spectrum
+from AdvEMDpy.emd_mean import Fluctuation
+from AdvEMDpy.emd_preprocess import Preprocess
+from AdvEMDpy.emd_utils import Utility
