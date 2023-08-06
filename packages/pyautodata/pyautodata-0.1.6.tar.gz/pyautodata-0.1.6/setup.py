26beb2ac400f51e9a14f1c874a136adcfa011104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:00 +0000

    Add package pyautodata/0.1.6/pyautodata-0.1.6.tar.gz

---
+++ packages/pyautodata/pyautodata-0.1.6.tar.gz/pyautodata-0.1.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('docs/pypi.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyautodata',
+    version='0.1.6',
+    url='https://github.com/christianhelle/pyautodata',
+    license='MIT License',
+    license_files=["LICENSE"],
+    author='Christian Helle',
+    author_email='christian.helle@outlook.com',
+    description='Python library designed to minimize the setup/arrange phase of your unit tests',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.7',
+    install_requires=['pandas', 'pyspark', 'faker'],
+    entry_points={
+        'console_scripts': [
+            'python3=pyautodata.alias:python3',
+        ],
+    },
+)
