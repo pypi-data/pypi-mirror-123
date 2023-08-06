4882c4a819bb3b1f99035e23299cd2ee980617e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:10 +0000

    Add package pbs-calculator/0.1/pbs_calculator-0.1.tar.gz

---
+++ packages/pbs-calculator/pbs_calculator-0.1.tar.gz/pbs_calculator-0.1/pbs_calculator/Calculator.py
@@ -0,0 +1,27 @@
+class Calculator:
+        """
+        Calculator having basic methods like addition, subtraction, multiplication and division.
+        """
+        def add(self, a, b):
+            """
+            Adds two numbers
+            """
+            return a + b
+
+        def sub(self, a, b):
+            """
+            Subtracts two numbers
+            """
+            return a - b
+
+        def mul(self, a, b):
+            """
+            Multiplies two numbers
+            """
+            return a*b
+
+        def div(self, a, b):
+            """
+            Divides two numbers
+            """
+            return a/b
---
+++ packages/pbs-calculator/pbs_calculator-0.1.tar.gz/pbs_calculator-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pbs_calculator',
+      version='0.1',
+      description='Simple calculator',
+      packages=['pbs_calculator'],
+      author='Prema S',
+      zip_safe=False)
