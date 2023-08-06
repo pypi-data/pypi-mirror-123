9172efae2f52b631eee5f273723f2fb12fed7907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:14 +0000

    Add package emanpdf/1.1/emanpdf-1.1.tar.gz

---
+++ packages/emanpdf/emanpdf-1.1.tar.gz/emanpdf-1.1/emanpdf/pdf2txt.py
@@ -0,0 +1,18 @@
+"""This module provides functions that convert PDF to TXT"""
+
+
+class Converter:
+    """ As imple converter to convert PDFs to TXTs"""
+    def convert(path):
+        """
+        convert the given PDF to TXT
+
+        Parameters:
+        path(str): The path to the PDF  file
+
+        Returns:
+        str:The content of the PDF file as TXT
+
+  """
+
+    print("pdf2txt")
---
+++ packages/emanpdf/emanpdf-1.1.tar.gz/emanpdf-1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="emanpdf",
+    version=1.1,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["data", "tests"])
+
+
+
+)
