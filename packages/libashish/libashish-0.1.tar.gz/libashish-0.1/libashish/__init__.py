25f7f78a3bdc257d78fa087e3a550171c25ff778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:59 +0000

    Add package libashish/0.1/libashish-0.1.tar.gz

---
+++ packages/libashish/libashish-0.1.tar.gz/libashish-0.1/libashish/__init__.py
@@ -0,0 +1,2 @@
+from libashish.tuttusa_datasets.models import DataConfig, Dataset
+from libashish.tuttusa_datasets.synthetic_data import GenerateData
---
+++ packages/libashish/libashish-0.1.tar.gz/libashish-0.1/setup.py
@@ -0,0 +1,53 @@
+from distutils.core import setup
+setup(
+  name = 'libashish',         # How you named your package folder (MyLib)
+  packages = ['libashish'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'first pypi deploy',   # Give a short description about your library
+  author = 'Ashish',                   # Type in your name
+  author_email = 'agarwal.ashish.singhal@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/CRLannister/pypitest',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/CRLannister/pypitest/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['first', 'pypi', 'package'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pydantic',
+          'numpy',
+          'io',
+          'random',
+          're',
+          'pandas',
+          'tensorflow',
+          'unidecode',
+          'transliterate',
+          'pkgutil',
+          'sklearn',
+          'copy',
+          'scipy',
+          'hashlib',
+          'inspect',
+          'math',
+          'collections',
+          'matplotlib',
+          'os',
+          'pathlib',
+          'pickle',
+          'shutil',
+          'tempfile',
+          'zipfile',
+          'dload',
+          'requests',
+          'google',
+          'tqdm'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
