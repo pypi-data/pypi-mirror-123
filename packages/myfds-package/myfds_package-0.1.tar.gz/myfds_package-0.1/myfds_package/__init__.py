67bb63c49248a684248ba137a227a4846532b96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:52 +0000

    Add package myfds-package/0.1/myfds_package-0.1.tar.gz

---
+++ packages/myfds-package/myfds_package-0.1.tar.gz/myfds_package-0.1/myfds_package/__init__.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def read(csv_name: str):
+    pd.read_csv(csv_name)
---
+++ packages/myfds-package/myfds_package-0.1.tar.gz/myfds_package-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myfds_package',  # package name
+    version='0.1',  # version
+    description='My first package',  # short description
+    url='https://github.com/Ditmarscehen/myfds_package',  # package URL
+    install_requires=['pandas'],  # list of packages this package depends
+    # on.
+    packages=['myfds_package'],  # List of module names that installing
+    # this package will provide.
+)
