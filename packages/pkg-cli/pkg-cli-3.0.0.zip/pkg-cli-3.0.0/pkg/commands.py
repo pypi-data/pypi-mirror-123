d5cbefbe2d92b83a6372ec10e41c8e9029055d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:21 +0000

    Add package pkg-cli/3.0.0/pkg-cli-3.0.0.zip

---
+++ packages/pkg-cli/pkg-cli-3.0.0.zip/pkg-cli-3.0.0/pkg/commands.py
@@ -0,0 +1,29 @@
+from .decorators import setup_file_required
+from configparser import ConfigParser
+from .utils import python
+from . import PROJECT_DIR
+
+
+def init(args):
+    config = ConfigParser()
+    config['metadata'] = {
+        'name': '<package_name>',
+        'version': '0.1.0',
+    }
+    config['options'] = {
+        'packages': 'find:'
+    }
+
+    file = PROJECT_DIR / 'setup.cfg'
+    if file.exists() and file.is_file():
+        print('Warning: The "setup.cfg" file exists!!')
+        return
+
+    with open(str(file), 'w') as outfile:
+        config.write(outfile)
+
+
+@setup_file_required
+def publish(args):
+    python('-m', 'pkg.setup', 'sdist')
+    python('-m', 'twine', 'upload', 'dist/*')
