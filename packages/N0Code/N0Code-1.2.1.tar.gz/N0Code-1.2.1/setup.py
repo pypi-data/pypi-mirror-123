d0ced1bbf7230f19cc1bb98029f3dc5f947db643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:08 +0000

    Add package N0Code/1.2.1/N0Code-1.2.1.tar.gz

---
+++ packages/N0Code/N0Code-1.2.1.tar.gz/N0Code-1.2.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='N0Code',
+    version='1.2.1',
+    py_modules=["N0Code","leetcode_api"],
+    author='N0P3',
+    author_email='n0p3@qq.com',
+    description='For leetcode For Dream'
+)
