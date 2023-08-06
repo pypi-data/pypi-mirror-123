59091610ed07d1bb881ddd3e690c03043843a407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:43 +0000

    Add package trakos/0.0.3/trakos-0.0.3.tar.gz

---
+++ packages/trakos/trakos-0.0.3.tar.gz/trakos-0.0.3/src/trakos/my_class.py
@@ -0,0 +1,8 @@
+from user_agent import generate_user_agent
+class iq:
+    def run():
+        print("Hello Python :)")
+        return " Dont Forgot, Install Library :)"
+class fake:
+        def get():
+            return (generate_user_agent())
