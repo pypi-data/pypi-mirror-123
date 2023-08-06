fc69e9323fb9a7770919e064940e5274d9e5de7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:02 +0000

    Add package django-request-proxy/1.0.3/django-request-proxy-1.0.3.tar.gz

---
+++ packages/django-request-proxy/django-request-proxy-1.0.3.tar.gz/django-request-proxy-1.0.3/request_proxy/views.py
@@ -0,0 +1,22 @@
+import os
+
+import requests
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
+# TODO: properly configure for custom permissions
+@api_view(['GET'])
+def proxy(request):
+    try:
+        full_path = request.get_full_path()
+        proxied_request = requests.get(full_path[11:18] +  full_path[18:])
+        try:
+            data = proxied_request.json()
+        except:
+            data = proxied_request.content
+        return Response(status=proxied_request.status_code, data=data)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
