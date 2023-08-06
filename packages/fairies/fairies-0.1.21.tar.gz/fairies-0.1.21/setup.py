6eccaf9b4a29c924e9c9c4bf53ac83489af47859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:57 +0000

    Add package fairies/0.1.21/fairies-0.1.21.tar.gz

---
+++ packages/fairies/fairies-0.1.21.tar.gz/fairies-0.1.21/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fairies',
+    version='0.1.21',
+    description=(
+        'utils for nlp'
+    ),
+    author='liyuhao',
+    author_email='1241225413@qq.com',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    url='https://github.com/hgliyuhao',
+    install_requires=[
+        'pdfminer3k',
+        'requests',
+        'xlrd==1.2.0',
+        'jieba',
+        'xlwt',
+        'xlutils',
+        'beautifulsoup4',
+        'matplotlib'
+    ],
+)
