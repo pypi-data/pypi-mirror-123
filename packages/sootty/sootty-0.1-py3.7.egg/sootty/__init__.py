47f05e3f95b4148f11e35b334fef57522b72c59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package sootty/0.1/sootty-0.1-py3.7.egg

---
+++ packages/sootty/sootty-0.1-py3.7.egg/sootty/__init__.py
@@ -0,0 +1,8 @@
+from .main import main
+
+from .display import display
+
+from .wiretrace import Wire
+from .wiretrace import WireGroup
+from .wiretrace import WireTrace
+from .wiretrace import TraceError
