013ba888a52c3d119478fb76e2b3f77f62a345b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:57 +0000

    Add package bokehgraph/0.1.2/bokehgraph-0.1.2.tar.gz

---
+++ packages/bokehgraph/bokehgraph-0.1.2.tar.gz/bokehgraph-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bokehgraph']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bokeh>=2.4.1,<3.0.0', 'networkx>=2,<3']
+
+setup_kwargs = {
+    'name': 'bokehgraph',
+    'version': '0.1.2',
+    'description': 'Interactive Graph visualization for networkX Graphs',
+    'long_description': None,
+    'author': 'Lukas Erhard',
+    'author_email': 'luerhard@googlemail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
