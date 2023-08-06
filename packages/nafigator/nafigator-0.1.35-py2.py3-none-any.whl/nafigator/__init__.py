b57d6ae66f51a800fa0d1c2bff85473db6723f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:35 +0000

    Add package nafigator/0.1.35/nafigator-0.1.35-py2.py3-none-any.whl

---
+++ packages/nafigator/nafigator-0.1.35-py2.py3-none-any.whl/nafigator/__init__.py
@@ -0,0 +1,15 @@
+"""Top-level package for nafigator."""
+
+__version__ = "0.1.35"
+
+from .cli import *
+from .const import *
+from .utils import *
+
+from .parse2naf import *
+from .parse2folia import *
+
+from .convert2rdf import *
+
+from .linguisticprocessor import *
+from .preprocessprocessor import *
