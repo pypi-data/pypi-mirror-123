47126946610fa7288b0d3100acaf8b59f09cba22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:48 +0000

    Add package ssbh-data-py/0.4.0/ssbh_data_py-0.4.0-cp36-none-win_amd64.whl

---
+++ packages/ssbh-data-py/ssbh_data_py-0.4.0-cp36-none-win_amd64.whl/ssbh_data_py/anim_data.pyi
@@ -0,0 +1,92 @@
+from typing import List, Tuple, Any, Optional, Union
+
+
+def read_anim(path: str) -> AnimData: ...
+
+
+class AnimData:
+    major_version: int
+    minor_version: int
+    groups: list[GroupData]
+    final_frame_index: float
+
+    def __init__(
+        self,
+        major_version: int = ...,
+        minor_version: int = ...,
+    ) -> None: ...
+
+    def save(self, path: str) -> None: ...
+
+
+class GroupData:
+    group_type: GroupType
+    nodes: list[NodeData]
+
+    def __init__(
+        self,
+        group_type: GroupType,
+    ) -> None: ...
+
+
+class GroupType:
+    name: str
+    value: int
+
+    Transform: GroupType = ...
+    Visibility: GroupType = ...
+    Material: GroupType = ...
+    Camera: GroupType = ...
+
+
+class NodeData:
+    name: str
+    tracks: list[TrackData]
+
+    def __init__(
+        self,
+        name: str,
+    ) -> None: ...
+
+
+class TrackData:
+    name: str
+    values: Union[list[UvTransform], list[Transform],
+                  list[float], list[bool], list[int], list[list[float]]]
+
+    def __init__(
+        self,
+        name: str,
+    ) -> None: ...
+
+
+class Transform:
+    scale: list[float]
+    rotation: list[float]
+    translation: list[float]
+    compensate_scale: int
+
+    def __init__(
+        self,
+        scale: list[float],
+        rotation: list[float],
+        translation: list[float],
+        compensate_scale: int
+    ) -> None: ...
+
+
+class UvTransform:
+    scale_u: float
+    scale_v: float
+    rotation: float
+    translate_u: float
+    translate_v: float
+
+    def __init__(
+        self,
+        scale_u: float,
+        scale_v: float,
+        rotation: float,
+        translate_u: float,
+        translate_v: float
+    ) -> None: ...
