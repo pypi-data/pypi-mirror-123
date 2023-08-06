87875a145673a8c7b09547a098f31e78a9c1079a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:16 +0000

    Add package promptdb/0.0.1/promptdb-0.0.1.tar.gz

---
+++ packages/promptdb/promptdb-0.0.1.tar.gz/promptdb-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='promptdb',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://promptdb.io',
+    author='denis.bilenko',
+    author_email='denis.bilenko@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
