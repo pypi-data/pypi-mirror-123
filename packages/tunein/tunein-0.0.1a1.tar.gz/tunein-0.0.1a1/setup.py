fe9b0db73e57664e86bb8ea691efd85ce7486c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:30 +0000

    Add package tunein/0.0.1a1/tunein-0.0.1a1.tar.gz

---
+++ packages/tunein/tunein-0.0.1a1.tar.gz/tunein-0.0.1a1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="tunein",
+    version="0.0.1a1",
+    description="TuneIn radio api",
+    license="Apache2",
+    author="JarbasAi",
+    url="https://github.com/OpenJarbas/tunein",
+    packages=["tunein"],
+    install_requires=["ovos_utils"]
+)
