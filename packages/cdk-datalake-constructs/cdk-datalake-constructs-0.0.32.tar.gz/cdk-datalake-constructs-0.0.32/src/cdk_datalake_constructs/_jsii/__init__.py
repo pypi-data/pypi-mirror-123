d707cb8b5fe5cfcc8f4a55aed7eb3d902ace87ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:48 +0000

    Add package cdk-datalake-constructs/0.0.32/cdk-datalake-constructs-0.0.32.tar.gz

---
+++ packages/cdk-datalake-constructs/cdk-datalake-constructs-0.0.32.tar.gz/cdk-datalake-constructs-0.0.32/src/cdk_datalake_constructs/_jsii/__init__.py
@@ -0,0 +1,59 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.assets._jsii
+import aws_cdk.aws_athena._jsii
+import aws_cdk.aws_cloudformation._jsii
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_eks._jsii
+import aws_cdk.aws_emr._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_glue._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_kinesis._jsii
+import aws_cdk.aws_kinesisanalytics._jsii
+import aws_cdk.aws_kinesisfirehose._jsii
+import aws_cdk.aws_kms._jsii
+import aws_cdk.aws_lakeformation._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_lambda_python._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_assets._jsii
+import aws_cdk.aws_s3_deployment._jsii
+import aws_cdk.aws_s3_notifications._jsii
+import aws_cdk.aws_sagemaker._jsii
+import aws_cdk.aws_sam._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.aws_sqs._jsii
+import aws_cdk.aws_stepfunctions._jsii
+import aws_cdk.aws_stepfunctions_tasks._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import aws_cdk.region_info._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@randyridgley/cdk-datalake-constructs",
+    "0.0.32",
+    __name__[0:-6],
+    "cdk-datalake-constructs@0.0.32.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
