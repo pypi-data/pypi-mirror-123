e364f851ed990db91b9f9f17baa0c378bf52bbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:31 +0000

    Add package export-lib/0.4/export_lib-0.4-py3-none-any.whl

---
+++ packages/export-lib/export_lib-0.4-py3-none-any.whl/export_util/test/test.py
@@ -0,0 +1,11 @@
+import os
+import unittest
+
+
+class TestExport(unittest.TestCase):
+    def test_test(self):
+         self.assertEqual("l", "l") 
+
+
+if __name__ == '__main__':
+    unittest.main()
