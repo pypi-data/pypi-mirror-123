8398d3cb26708e471b87375aebc9b3e8bd33ab1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:59 +0000

    Add package ndd-tools/1.2.0/ndd_tools-1.2.0-py2.py3-none-any.whl

---
+++ packages/ndd-tools/ndd_tools-1.2.0-py2.py3-none-any.whl/ndd_tools/schemas.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel
+import enum
+from typing import List, Dict, Optional, Any
+
+# -------------------------------------------------
+#   For Boring Regex
+#
+class EnumStrategy(enum.Enum):
+    MATCH = 'match'
+    SEARCH = 'search'
+    FINDALL = 'findall'
+    FINDITER = 'finditer'
+    GETALL = 'getall'
+
+
+class EnumGroupType(enum.Enum):
+    GROUP = "group"
+    GROUPS = "groups"
+    GROUPDICT = "groupdict"
+
+class ObjFieldPatternRegexConfig(BaseModel):
+    value: str
+    group_type: EnumGroupType
+    group_indexes: Optional[List[int]]
+
+
+class ObjFieldRegexConfig(BaseModel):
+    active: bool = True
+    field: List[Any]
+    key: str
+    strategy: EnumStrategy
+    patterns: List[ObjFieldPatternRegexConfig]
+
+
+class ObjectRegexConfig(BaseModel):
+    result_keys: List[str]
+    regex_fields: List[ObjFieldRegexConfig]
+
+
+class RegexExecutorConfig(BaseModel):
+    version: str
+    name: str
+    description: str
+    author: Optional[str]
+    config: Dict[str, ObjectRegexConfig]
