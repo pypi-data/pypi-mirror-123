0da4a64a2283c7508f318b52a7fbc15e60cc4a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:42 +0000

    Add package GDBKokkos/0.1.1/GDBKokkos-0.1.1.tar.gz

---
+++ packages/GDBKokkos/GDBKokkos-0.1.1.tar.gz/GDBKokkos-0.1.1/GDBKokkos/__init__.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright 2020 Char Aznable <aznable.char.0083@gmail.com>
+# Description: Utilities for pretty-printing Kokkos::View
+#
+# Distributed under terms of the 3-clause BSD license.
+import gdb
+from GDBKokkos.printView import(
+    printView
+    )
+from GDBKokkos.printUnorderedMap import(
+    printUnorderedMap
+    )
+
+# This registers our class to the gdb runtime at "source" time.
+printView()
+printUnorderedMap()
