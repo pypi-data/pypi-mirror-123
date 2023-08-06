32c343eeb0f1119417b2b673130369d98001db9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:17 +0000

    Add package anananananas/0.0.0/anananananas-0.0.0.tar.gz

---
+++ packages/anananananas/anananananas-0.0.0.tar.gz/anananananas-0.0.0/anananananas/__init__.py
@@ -0,0 +1,2 @@
+def print_me_some_anas(number_of_anas):
+    return "ana" * number_of_anas
---
+++ packages/anananananas/anananananas-0.0.0.tar.gz/anananananas-0.0.0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="anananananas", packages=find_packages())
