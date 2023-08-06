e08514c90d3b65d80860f80040b3dff2538f583e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:55 +0000

    Add package py-rans/1.0.0/py_rans-1.0.0.tar.gz

---
+++ packages/py-rans/py_rans-1.0.0.tar.gz/py_rans-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_rans",
+    version="1.0.0",
+    author="Fedor Glazov",
+    author_email="fedorglazov@gmail.com",
+    description="A lightweight, pure python implementation of a rANS coder.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ],
+)
