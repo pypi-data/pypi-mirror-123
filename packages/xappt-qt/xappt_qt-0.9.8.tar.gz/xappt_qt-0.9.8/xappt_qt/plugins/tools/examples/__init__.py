cc749a1d06d11c79b783157d577fbf8b2b26a099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:20 +0000

    Add package xappt-qt/0.9.8/xappt_qt-0.9.8.tar.gz

---
+++ packages/xappt-qt/xappt_qt-0.9.8.tar.gz/xappt_qt-0.9.8/xappt_qt/plugins/tools/examples/__init__.py
@@ -0,0 +1,5 @@
+from xappt_qt.plugins.tools.examples.video_convert import ConvertX265
+from xappt_qt.plugins.tools.examples.headless import HeadlessExecution
+from xappt_qt.plugins.tools.examples.custom_icon import *
+from xappt_qt.plugins.tools.examples.auto_advance import AutoAdvance
+from xappt_qt.plugins.tools.examples.string_ui import StringUi
