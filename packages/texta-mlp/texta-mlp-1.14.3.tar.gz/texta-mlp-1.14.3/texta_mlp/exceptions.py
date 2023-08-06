42da304eed3f94e9ab6be44117d58900be522d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:49 +0000

    Add package texta-mlp/1.14.3/texta-mlp-1.14.3.tar.gz

---
+++ packages/texta-mlp/texta-mlp-1.14.3.tar.gz/texta-mlp-1.14.3/texta_mlp/exceptions.py
@@ -0,0 +1,11 @@
+class LanguageNotSupported(Exception):
+    """Raised when MLP fails to detect language or language is not supported.""" 
+    pass
+
+class BoundedListEmpty(Exception):
+    """Raised when in Concatenator class the BOUNDS are not yet loaded, but concatenate() is tried""" 
+    pass
+
+class StanzaPipelineFail(Exception):
+    """Raised when Stanza pipelines fail to load."""
+    pass
