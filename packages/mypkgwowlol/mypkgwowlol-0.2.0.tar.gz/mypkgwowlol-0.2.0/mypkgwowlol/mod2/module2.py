cdf4fc6dc9c6a632ec1ba1c1c598f9945747b1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:38 +0000

    Add package mypkgwowlol/0.2.0/mypkgwowlol-0.2.0.tar.gz

---
+++ packages/mypkgwowlol/mypkgwowlol-0.2.0.tar.gz/mypkgwowlol-0.2.0/mypkgwowlol/mod2/module2.py
@@ -0,0 +1,13 @@
+from mypkgwowlol import module1
+
+class module_2():
+
+    def __init__(self):
+        pass
+
+    def module_print():
+        modx = module1.module_1()
+        x = 73
+        y = 35
+        z = modx.add_two(x,y)
+        print(z)
