6da554428f0724630c3d0949ea5ec1c8c94b217b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:19 +0000

    Add package bayeso/0.5.1/bayeso-0.5.1.tar.gz

---
+++ packages/bayeso/bayeso-0.5.1.tar.gz/bayeso-0.5.1/bayeso/__init__.py
@@ -0,0 +1,8 @@
+#
+# author: Jungtaek Kim (jtkim@postech.ac.kr)
+# last updated: June 24, 2021
+#
+"""BayesO is a simple, but essential Bayesian optimization
+package, implemented in Python."""
+
+__version__ = '0.5.1'
