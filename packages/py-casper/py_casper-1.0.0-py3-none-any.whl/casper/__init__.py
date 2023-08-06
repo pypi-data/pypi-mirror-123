567a355462a45e89e66b4c5dc86c8220cc8cf8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:40 +0000

    Add package py-casper/1.0.0/py_casper-1.0.0-py3-none-any.whl

---
+++ packages/py-casper/py_casper-1.0.0-py3-none-any.whl/casper/__init__.py
@@ -0,0 +1,8 @@
+
+__all__ =  ['Critical_density' , 'Einasto_density' , 'Einasto_mass' , 'casper', 'density_rms', 'peak_height', 
+            'smooth_kspace' , 'spherical_top_hat' , 'spherical_top_hat_generalised']
+from .casper import *
+
+
+
+
