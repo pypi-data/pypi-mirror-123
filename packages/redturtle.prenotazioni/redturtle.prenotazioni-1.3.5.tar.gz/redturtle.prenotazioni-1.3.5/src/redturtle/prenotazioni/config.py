b70eb1d5462b736b9b1c3b591763c374ad572e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:01 +0000

    Add package redturtle.prenotazioni/1.3.5/redturtle.prenotazioni-1.3.5.tar.gz

---
+++ packages/redturtle.prenotazioni/redturtle.prenotazioni-1.3.5.tar.gz/redturtle.prenotazioni-1.3.5/src/redturtle/prenotazioni/config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+PROJECTNAME = "redturtle.prenotazioni"
+
+MIN_IN_DAY = 24 * 60
+PAUSE_SLOT = "pause_slot"
+PAUSE_PORTAL_TYPE = "DayPause"
+DELETE_TOKEN_KEY = "delete_token"
