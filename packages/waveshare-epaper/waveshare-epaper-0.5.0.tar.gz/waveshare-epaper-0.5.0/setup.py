45279d4569462c7afd7d4823a3b60d61929e7687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:54 +0000

    Add package waveshare-epaper/0.5.0/waveshare-epaper-0.5.0.tar.gz

---
+++ packages/waveshare-epaper/waveshare-epaper-0.5.0.tar.gz/waveshare-epaper-0.5.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['epaper', 'epaper.e-Paper.RaspberryPi_JetsonNano.python.lib.waveshare_epd']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'waveshare-epaper',
+    'version': '0.5.0',
+    'description': '',
+    'long_description': None,
+    'author': 'yskoht',
+    'author_email': 'ysk.oht@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+}
+
+
+setup(**setup_kwargs)
