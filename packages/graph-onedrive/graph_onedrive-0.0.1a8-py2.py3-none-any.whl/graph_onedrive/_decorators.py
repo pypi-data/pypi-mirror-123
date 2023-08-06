5bcb7523d798455cc32460fc36f2b64fdf4cb72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:23 +0000

    Add package graph-onedrive/0.0.1a8/graph_onedrive-0.0.1a8-py2.py3-none-any.whl

---
+++ packages/graph-onedrive/graph_onedrive-0.0.1a8-py2.py3-none-any.whl/graph_onedrive/_decorators.py
@@ -0,0 +1,27 @@
+"""Decorators to wrap Graph-OneDrive package methods and functions.
+"""
+from datetime import datetime
+from functools import wraps
+
+
+def token_required(func):
+    """INTERNAL: Graph-OneDrive decorator to check for and refresh the access token when calling methods."""
+
+    @wraps(func)
+    def wrapper_token(*args, **kwargs):
+        # Set self from args to get instance expires attribute
+        onedrive_instance = args[0]
+        expires = onedrive_instance._access_expires
+        # Get the current timestamp for comparison
+        now = datetime.timestamp(datetime.now())
+        # Refresh access token if expires does not exist or has expired
+        if expires <= now:
+            onedrive_instance._get_token()
+            onedrive_instance._create_headers()
+        # Fucnction that is wrapped runs here
+        wrapped_func = func(*args, **kwargs)
+        # After run do nothing
+        # Return the wrapped function
+        return wrapped_func
+
+    return wrapper_token
