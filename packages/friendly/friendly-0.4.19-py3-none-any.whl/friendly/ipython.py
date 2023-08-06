7484efa0137354bafd680eb732fcb6650fa27cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:05 +0000

    Add package friendly/0.4.19/friendly-0.4.19-py3-none-any.whl

---
+++ packages/friendly/friendly-0.4.19-py3-none-any.whl/friendly/ipython.py
@@ -0,0 +1,8 @@
+from ._ipython import *  # noqa
+from friendly_traceback import __version__
+
+# By default, we assume a terminal with a dark background.
+set_formatter("dark")  # noqa
+print(
+    f"friendly_traceback {__version__}; friendly {version}.\nType 'Friendly' for information."
+)
