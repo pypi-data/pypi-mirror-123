9eb99c9833b4ff2415abccfb99adb2fe072ba4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package helloworld-yaroslav2131234/0.1.0/helloworld_yaroslav2131234-0.1.0.tar.gz

---
+++ packages/helloworld-yaroslav2131234/helloworld_yaroslav2131234-0.1.0.tar.gz/helloworld_yaroslav2131234-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='helloworld_yaroslav2131234',
+    version='0.1.0',
+    description='A example Python package',
+    author='Yaroslav Yaroslav',
+    packages=['source'],
+    install_requires=['numpy<1.20.0']
+)
