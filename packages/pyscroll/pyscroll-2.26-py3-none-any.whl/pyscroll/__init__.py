14d022a2368c5125595e8d42ae48e4b09a3a4d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:05 +0000

    Add package pyscroll/2.26/pyscroll-2.26-py3-none-any.whl

---
+++ packages/pyscroll/pyscroll-2.26-py3-none-any.whl/pyscroll/__init__.py
@@ -0,0 +1,10 @@
+from .orthographic import BufferedRenderer
+from .isometric import IsometricBufferedRenderer
+from .data import PyscrollDataAdapter, TiledMapData
+from .group import PyscrollGroup
+
+
+__version__ = 2, 26
+__author__ = "bitcraft"
+__author_email__ = "leif.theden@gmail.com"
+__description__ = "Pygame Scrolling"
