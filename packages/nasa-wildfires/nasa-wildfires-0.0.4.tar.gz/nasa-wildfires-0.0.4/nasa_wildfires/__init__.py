0bb5f269110d9e0c4ec9182109caa881bb441260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:41 +0000

    Add package nasa-wildfires/0.0.4/nasa-wildfires-0.0.4.tar.gz

---
+++ packages/nasa-wildfires/nasa-wildfires-0.0.4.tar.gz/nasa-wildfires-0.0.4/nasa_wildfires/__init__.py
@@ -0,0 +1,52 @@
+import csv
+import geojson
+import requests
+import itertools
+
+
+def get_modis():
+    """
+    Hotspots detected by the MODIS satellite in a recent 24-hour period.
+
+    Returns GeoJSON.
+    """
+    base_url = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/c6/csv/{}'
+    url_list = [
+        base_url.format('MODIS_C6_USA_contiguous_and_Hawaii_24h.csv'),
+        base_url.format('MODIS_C6_Alaska_24h.csv')
+    ]
+    features = [_get_features(u) for u in url_list]
+    return geojson.FeatureCollection(list(itertools.chain(features)))
+
+
+def get_viirs():
+    """
+    Hotspots detected by the VIIRS satellite in a recent 24-hour period.
+
+    Returns GeoJSON.
+    """
+    base_url = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/viirs/csv/VNP14IMGTDL_NRT_{}'
+    url_list = [
+        base_url.format('USA_contiguous_and_Hawaii_24h.csv'),
+        base_url.format('Alaska_24h.csv')
+    ]
+    features = [_get_features(u) for u in url_list]
+    return geojson.FeatureCollection(list(itertools.chain(features)))
+
+
+def _get_features(url):
+    """
+    Generic function for downloading data from NASA and formatting to geojson
+    """
+    download = requests.get(url)
+    decoded_content = download.content.decode('utf-8')
+    reader = csv.DictReader(decoded_content.splitlines(), delimiter=',')
+    features = []
+    for r in reader:
+        coords = map(float, [r['longitude'], r['latitude']])
+        f = geojson.Feature(
+            geometry=geojson.Point(coords),
+            properties=r
+        )
+        features.append(f)
+    return features
