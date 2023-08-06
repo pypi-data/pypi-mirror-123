31ffd8afb35cc79e62fee49d22520404be2acc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:57 +0000

    Add package MLVisualizationTools/0.0.21/MLVisualizationTools-0.0.21-py3-none-any.whl

---
+++ packages/MLVisualizationTools/MLVisualizationTools-0.0.21-py3-none-any.whl/MLVisualizationTools/backend.py
@@ -0,0 +1,21 @@
+from typing import List, Dict
+import pandas as pd
+
+#Backend functions and classes used by the other scripts
+
+def colinfo(data: pd.DataFrame, exclude:List[str] = None) -> List[Dict]:
+    """
+    Helper function for generating column info dict for a datframe
+
+    :param data: A pandas Dataframe
+    :param exclude: A list of data items to exclude
+    """
+    if exclude is None:
+        exclude = []
+
+    coldata = []
+    for item in data.columns:
+        if item not in exclude:
+            coldata.append({'name': item, 'mean': data[item].mean(),
+                            'min': data[item].min(), 'max': data[item].max()})
+    return coldata
