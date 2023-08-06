24df14c32ea2464a8996d04e1b0922dc852852db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:03 +0000

    Add package typety/0.0.1/typety-0.0.1.tar.gz

---
+++ packages/typety/typety-0.0.1.tar.gz/typety-0.0.1/typety/__init__.py
@@ -0,0 +1,11 @@
+# This is a typing effect
+import time
+import sys
+
+
+def typingprint(text):
+    """Typingprint types out stuff"""
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
