c8f73fb48af93276cf61621e94a61717440b0ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:18 +0000

    Add package bemplt/0.0.4/bemplt-0.0.4.tar.gz

---
+++ packages/bemplt/bemplt-0.0.4.tar.gz/bemplt-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bemplt",
+    version="0.0.004",
+    author="dy1901",
+    author_email="gaetan.desrues@inria.fr",
+    license="MIT License",
+    description="Plot standard ECG chart from data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GaetanDesrues/ecg_plot",  # forked from https://github.com/dy1901/ecg_plot
+    packages=["ecg_plot"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
