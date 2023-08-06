314d5036f9c24704bc43779831b9a8430602d7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:04 +0000

    Add package pyautoeasy/0.0.5/pyautoeasy-0.0.5-py3-none-any.whl

---
+++ packages/pyautoeasy/pyautoeasy-0.0.5-py3-none-any.whl/pyautoeasy/sample.py
@@ -0,0 +1,17 @@
+
+"""
+_________________________________________________________________________________________
+ScreenPoint refers to a point in the screen. All the recorded points are generated here.
+You may use these objects and its methods to quickly create an automation script.
+_________________________________________________________________________________________
+"""
+
+from pyautoeasy import ScreenPoint
+
+sd = ScreenPoint(pos=(1140, 669))
+sd.click_here()
+
+
+asd = ScreenPoint(pos=(1089, 230))
+asd.click_here()
+
