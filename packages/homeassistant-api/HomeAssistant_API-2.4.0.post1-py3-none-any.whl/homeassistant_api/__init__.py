3a28d987a47d5e84eeff8b1d3e97091c6dae69d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:51 +0000

    Add package homeassistant-api/2.4.0.post1/HomeAssistant_API-2.4.0.post1-py3-none-any.whl

---
+++ packages/homeassistant-api/HomeAssistant_API-2.4.0.post1-py3-none-any.whl/homeassistant_api/__init__.py
@@ -0,0 +1,7 @@
+from .client import Client, RawClient
+from .models import Group, Entity, Domain, Service, State, Event
+from .processing import Processing
+
+
+__version__ = '2.4.0.post1'
+__name__ = 'Homeassistant API'
