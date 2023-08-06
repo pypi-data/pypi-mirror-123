44cf3916f09a2239f9f3f41106f31320a1397c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:20 +0000

    Add package DataVisual/0.1.1/DataVisual-0.1.1-py3-none-any.whl

---
+++ packages/DataVisual/DataVisual-0.1.1-py3-none-any.whl/DataVisual/Tools/utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+import re
+
+
+def _ToList(arg):
+
+    if isinstance(arg, list):
+        return np.asarray(arg)
+
+    if not isinstance(arg, UlistLike):
+        return np.asarray([arg])
+
+    else:
+        return arg
+
+
+def ToList(*args):
+    out = []
+    for arg in args:
+        if not isinstance(arg, UlistLike):
+            out.append( [arg] )
+        else:
+            out.append(arg)
+
+    if len(out) == 1: return out[0]
+
+    return np.asarray(out)
+
+
+def FormatStr(function):
+    def wrapped(*args, **kwargs):
+        args = (re.sub(r"\s+", "", arg.lower() ) if isinstance(arg, str) else arg for arg in args)
+
+        kwargs = {k: re.sub(r"\s+", "", v.lower() ) if isinstance(v, str) else v for k, v in kwargs.items()}
+
+        return function(*args, **kwargs)
+    return wrapped
+
+
+def FormatString(string):
+    return re.sub(r"\s+", "", string.lower() )
+
+
+def ConvertPolarization(Polarization):
+    if Polarization is None:
+        return 4242
+
+    if isinstance(Polarization, np.ndarray):
+        Polarization[Polarization==None] = 4242
+        return Polarization
+    else:
+        return Polarization
+
+
+
+# -
