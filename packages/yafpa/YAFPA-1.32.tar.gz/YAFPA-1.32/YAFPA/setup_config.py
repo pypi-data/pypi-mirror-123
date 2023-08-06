9f2aca3c5507630554421c51b6b044fe7326e5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:11 +0000

    Add package yafpa/1.32/YAFPA-1.32.tar.gz

---
+++ packages/yafpa/YAFPA-1.32.tar.gz/YAFPA-1.32/YAFPA/setup_config.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from pathlib import Path
+
+BASEDIR = os.getcwd()
+BASEDIR= Path(BASEDIR)
+env_path = Path(f"{BASEDIR}/.env")
+
+
+def create_env():
+    env = open(env_path, "w", encoding="utf-8")
+    vault = str(input("Please provide your obsidian vault path : "))
+    blog = str(input("Please provide the blog repository path : "))
+    blog_link = str(
+        input("Please provide the blog link (as https://yourblog.netlify.app) : ")
+    )
+    env.write(f"vault={vault}\n")
+    env.write(f"blog_path={blog}\n")
+    env.write(f"blog={blog_link}\n")
+    env.close()
