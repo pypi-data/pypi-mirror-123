1bb2100ef828527a0188108757e65effc7758070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:14 +0000

    Add package validations-libs/1.4.0/validations_libs-1.4.0-py3-none-any.whl

---
+++ packages/validations-libs/validations_libs-1.4.0-py3-none-any.whl/validations_libs/constants.py
@@ -0,0 +1,44 @@
+#   Copyright 2020 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+"""Default paths for validation playbook directory,
+validation groups definitions and validation logs
+are defined here.
+
+These paths are used in an absence of user defined overrides,
+or as a fallback, when custom locations fail.
+"""
+
+import os
+
+DEFAULT_VALIDATIONS_BASEDIR = '/usr/share/ansible'
+
+ANSIBLE_VALIDATION_DIR = os.path.join(
+    DEFAULT_VALIDATIONS_BASEDIR,
+    'validation-playbooks')
+
+VALIDATION_GROUPS_INFO = os.path.join(
+    DEFAULT_VALIDATIONS_BASEDIR,
+    'groups.yaml')
+
+# NOTE(fressi) The HOME folder environment variable may be undefined.
+VALIDATIONS_LOG_BASEDIR = os.path.expanduser('~/validations')
+
+VALIDATION_ANSIBLE_ARTIFACT_PATH = os.path.join(
+    VALIDATIONS_LOG_BASEDIR,
+    'artifacts')
+
+ANSIBLE_RUNNER_CONFIG_PARAMETERS = ['verbosity', 'extravars', 'fact_cache',
+                                    'fact_cache_type', 'inventory', 'playbook',
+                                    'project_dir', 'quiet', 'rotate_artifacts']
