e39be10f0d31cb9a39d884ea2dd3dd1f87c7e152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:28 +0000

    Add package dagster-graphql/0.12.15rc2/dagster_graphql-0.12.15rc2-py3-none-any.whl

---
+++ packages/dagster-graphql/dagster_graphql-0.12.15rc2-py3-none-any.whl/dagster_graphql/schema/pipelines/subscription.py
@@ -0,0 +1,31 @@
+import graphene
+
+from ..logs.events import GraphenePipelineRunEvent
+from ..util import non_null_list
+from .pipeline import GraphenePipelineRun
+
+
+class GraphenePipelineRunLogsSubscriptionSuccess(graphene.ObjectType):
+    run = graphene.NonNull(GraphenePipelineRun)
+    messages = non_null_list(GraphenePipelineRunEvent)
+    hasMorePastEvents = graphene.NonNull(graphene.Boolean)
+
+    class Meta:
+        name = "PipelineRunLogsSubscriptionSuccess"
+
+
+class GraphenePipelineRunLogsSubscriptionFailure(graphene.ObjectType):
+    message = graphene.NonNull(graphene.String)
+    missingRunId = graphene.Field(graphene.String)
+
+    class Meta:
+        name = "PipelineRunLogsSubscriptionFailure"
+
+
+class GraphenePipelineRunLogsSubscriptionPayload(graphene.Union):
+    class Meta:
+        types = (
+            GraphenePipelineRunLogsSubscriptionSuccess,
+            GraphenePipelineRunLogsSubscriptionFailure,
+        )
+        name = "PipelineRunLogsSubscriptionPayload"
