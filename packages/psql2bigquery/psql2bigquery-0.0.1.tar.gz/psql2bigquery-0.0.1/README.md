12a2d610c13c2c7d885ba31cb0546811e8d25971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:32 +0000

    Add package psql2bigquery/0.0.1/psql2bigquery-0.0.1.tar.gz

---
+++ packages/psql2bigquery/psql2bigquery-0.0.1.tar.gz/psql2bigquery-0.0.1/README.md
@@ -0,0 +1,5 @@
+# PostgreSQL to BigQuery
+
+Install with: `pip install psql2bigquery`
+
+Get instruction with: `psql2bigquery --help`
---
+++ packages/psql2bigquery/psql2bigquery-0.0.1.tar.gz/psql2bigquery-0.0.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['psql2bigquery', 'psql2bigquery.tools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click', 'google-cloud-bigquery', 'psycopg2-binary']
+
+extras_require = \
+{'sentry': ['sentry-sdk']}
+
+entry_points = \
+{'console_scripts': ['psql2bigquery = psql2bigquery.main:cli']}
+
+setup_kwargs = {
+    'name': 'psql2bigquery',
+    'version': '0.0.1',
+    'description': 'Export PostgreSQL databases to Google Cloud Platform BigQuery',
+    'long_description': '# PostgreSQL to BigQuery\n\nInstall with: `pip install psql2bigquery`\n\nGet instruction with: `psql2bigquery --help`\n',
+    'author': 'Joao Daher',
+    'author_email': 'joao@daher.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/CraveFood/psql2bigquery',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<3.10',
+}
+
+
+setup(**setup_kwargs)
