88150d1a955531c1c383be608f921f9673eb0fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:57 +0000

    Add package UW-RestClients-SWS/2.3.16/UW_RestClients_SWS-2.3.16-py3-none-any.whl

---
+++ packages/UW-RestClients-SWS/UW_RestClients_SWS-2.3.16-py3-none-any.whl/uw_sws/__init__.py
@@ -0,0 +1,48 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+from urllib.parse import quote
+from restclients_core.exceptions import DataFailureException
+from uw_pws import PWS
+from uw_sws.dao import SWS_DAO, SWS_TIMEZONE, sws_now
+
+QUARTER_SEQ = ["winter", "spring", "summer", "autumn"]
+DAO = SWS_DAO()
+UWPWS = PWS()
+
+
+def use_v5_resources():
+    return True
+
+
+def encode_section_label(label):
+    return quote(label, safe="/,")
+
+
+def get_resource(url):
+    """
+    Issue a GET request to SWS with the given url
+    and return a response in json format.
+    :returns: http response with content in json
+    """
+    response = DAO.getURL(url, {'Accept': 'application/json',
+                                'Connection': 'keep-alive'})
+    if response.status != 200:
+        raise DataFailureException(url, response.status, response.data)
+    return json.loads(response.data)
+
+
+def put_resource(url, headers={}, body={}):
+    """
+    Issue a PUT request to SWS with the given url
+    and return a response in json format.
+    :returns: http response with content in json
+    """
+    headers['Accept'] = 'application/json'
+
+    response = DAO.putURL(url, headers, json.dumps(body))
+
+    if response.status != 200:
+        raise DataFailureException(url, response.status, response.data)
+    return json.loads(response.data)
