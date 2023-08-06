3e2eccaf35c8847a20e5f510d16a55902f7317a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:28 +0000

    Add package mess_client_serzh/0.0.3/mess_client_serzh-0.0.3.tar.gz

---
+++ packages/mess_client_serzh/mess_client_serzh-0.0.3.tar.gz/mess_client_serzh-0.0.3/setup_client.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_client_serzh",
+      version="0.0.3",
+      description="mess_client_proj",
+      author="Ivan Ivanov",
+      author_email="iv.iv@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
