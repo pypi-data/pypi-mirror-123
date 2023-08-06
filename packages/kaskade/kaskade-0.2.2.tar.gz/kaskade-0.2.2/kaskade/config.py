0f3c3156953f3aa76497dca42c7fec254c0cf6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:56 +0000

    Add package kaskade/0.2.2/kaskade-0.2.2.tar.gz

---
+++ packages/kaskade/kaskade-0.2.2.tar.gz/kaskade-0.2.2/kaskade/config.py
@@ -0,0 +1,38 @@
+import logging
+from pathlib import Path
+
+import yaml
+
+
+class Config:
+    def __init__(self, path: str) -> None:
+        self.path: str = "" if path is None else path
+
+        config_files = ["kaskade.yml", "kaskade.yaml", "config.yml", "config.yaml"]
+
+        if len(self.path) > 0:
+            if not Path(self.path).exists():
+                raise Exception(f"Config file {path} not found")
+        else:
+            default_config_file = next(
+                iter([path for path in config_files if Path(path).exists()]), None
+            )
+            if default_config_file is None:
+                raise Exception(
+                    "Default config file kaskade.yml, kaskade.yaml, "
+                    "config.yml or config.yaml not found"
+                )
+
+            self.path = default_config_file
+
+        with open(self.path, "r") as file:
+            self.text = file.read()
+            self.yaml = yaml.safe_load(self.text)
+            self.kafka = self.yaml.get("kafka")
+            self.kaskade = self.yaml.get("kaskade")
+            self.schema_registry = self.yaml.get("schema-registry")
+
+            logger = logging.getLogger()
+            logger.addHandler(logging.FileHandler("kaskade.log"))
+
+            self.kafka["logger"] = logger
