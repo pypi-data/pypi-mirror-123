8f03128d5c1365bfc043fd823b3d4fa277c7eb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:27 +0000

    Add package oaloader/0.4.3/oaloader-0.4.3.tar.gz

---
+++ packages/oaloader/oaloader-0.4.3.tar.gz/oaloader-0.4.3/oaloader/const.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import click
+
+version = "0.4.3"
+
+SUBKEY_CATALOG = r"Software\Microsoft\Office\16.0\WEF\TrustedCatalogs"
+SUBKEY_PROVIDER = r"Software\Microsoft\Office\16.0\WEF\Providers"
+APP_NAME = "office-addin-sideloader"
+APP_DIR = Path(click.get_app_dir(APP_NAME))
+MANIFEST_ENCODING = "utf-8"
+DEFAULT_PATH = APP_DIR.joinpath("addins")
+DEFAULT_NETNAME = "office-addins"
