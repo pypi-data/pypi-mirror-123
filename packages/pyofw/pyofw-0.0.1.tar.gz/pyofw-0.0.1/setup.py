014db2027593053264a6ede5c1df5134ca15c5b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:20 +0000

    Add package pyofw/0.0.1/pyofw-0.0.1.tar.gz

---
+++ packages/pyofw/pyofw-0.0.1.tar.gz/pyofw-0.0.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyofw",
+    version="0.0.1",
+    author="Akshaya Niraula",
+    author_email="Akshaya.Niraula@gmail.com",
+    description="Bentley OpenFlows API stub files plus a few py files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/worthapenny/pyofw",
+    project_urls={
+        "Bug Tracker": "https://github.com/worthapenny/pyofw/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 4 - Beta",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas",
+        "pythonnet"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3.7",
+        ],
+    },
+)
