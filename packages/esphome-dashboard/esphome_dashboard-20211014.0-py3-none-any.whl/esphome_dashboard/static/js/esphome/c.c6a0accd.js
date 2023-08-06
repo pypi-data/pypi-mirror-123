57e68e11ab4a15f9b4287dcf2be16f44743bf6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:47 +0000

    Add package esphome-dashboard/20211014.0/esphome_dashboard-20211014.0-py3-none-any.whl

---
+++ packages/esphome-dashboard/esphome_dashboard-20211014.0-py3-none-any.whl/esphome_dashboard/static/js/esphome/c.c6a0accd.js
@@ -0,0 +1,7 @@
+import{_ as e,n as s,a,y as l}from"./index-3596f402.js";import"./c.e7601da3.js";let d=class extends a{render(){return l`
+      <esphome-process-dialog
+        heading="Update All"
+        .type=${"update-all"}
+        @closed=${this._handleClose}
+      ></esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};d=e([s("esphome-update-all-dialog")],d);
---
+++ packages/esphome-dashboard/esphome_dashboard-20211014.0-py3-none-any.whl/esphome_dashboard/static/js/esphome/c.c95adf65.js
@@ -0,0 +1,29 @@
+import{r as o,_ as e,e as t,t as n,n as i,a as s,y as a}from"./index-3596f402.js";import"./c.e7601da3.js";import{a as c}from"./c.ccab1153.js";import"./c.02180161.js";import"./c.0c7292d4.js";import"./c.7f9fedec.js";let r=class extends s{render(){return a`
+      <esphome-process-dialog
+        .heading=${`Download ${this.configuration}`}
+        .type=${"compile"}
+        .spawnParams=${{configuration:this.configuration}}
+        @closed=${this._handleClose}
+        @process-done=${this._handleProcessDone}
+      >
+        ${void 0===this._result?"":0===this._result?a`
+              <a
+                slot="secondaryAction"
+                href="${`./download.bin?configuration=${encodeURIComponent(this.configuration)}`}"
+              >
+                <mwc-button label="Download"></mwc-button>
+              </a>
+            `:a`
+              <mwc-button
+                slot="secondaryAction"
+                dialogAction="close"
+                label="Retry"
+                @click=${this._handleRetry}
+              ></mwc-button>
+            `}
+      </esphome-process-dialog>
+    `}_handleProcessDone(o){if(this._result=o.detail,0!==o.detail)return;const e=document.createElement("a");e.download=this.configuration+".bin",e.href=`./download.bin?configuration=${encodeURIComponent(this.configuration)}`,document.body.appendChild(e),e.click(),e.remove()}_handleRetry(){c(this.configuration)}_handleClose(){this.parentNode.removeChild(this)}};r.styles=o`
+    a {
+      text-decoration: none;
+    }
+  `,e([t()],r.prototype,"configuration",void 0),e([n()],r.prototype,"_result",void 0),r=e([i("esphome-compile-dialog")],r);
