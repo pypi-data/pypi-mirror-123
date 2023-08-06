5553c0a49d6b1eff0378b6c4619a81f879b22bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:56 +0000

    Add package MetricTool/0.1.4/MetricTool-0.1.4-py2.py3-none-any.whl

---
+++ packages/MetricTool/MetricTool-0.1.4-py2.py3-none-any.whl/MetricTools/test_Parallel_cyclomatic.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+
+import unittest
+
+import sys
+import os
+
+sys.path.append(os.path.split(os.path.realpath(__file__))[0])
+import Parallel_cyclomatic as cv
+
+
+class CyclomaticTest(unittest.TestCase):
+    # 测试并行圈复杂度计算的正确性
+    def test_Cyclomatic(self):
+        files = [ os.path.split(os.path.realpath(__file__))[0] + '/test/test.c', os.path.split(os.path.realpath(__file__))[0] + '/test/test.java']
+        results = cv.para_cyclomatic(files)
+
+
+        result = results[os.path.split(os.path.realpath(__file__))[0] + '/test/test.java']
+        
+        # test1圈复杂度为9
+        self.assertEqual(result['functions']['test1-4-4-35-5'], 9)
+        # test2圈复杂度为3
+        self.assertEqual(result['functions']['test2-38-4-55-5'], 4)
+        # 文件平均圈复杂度为6
+        self.assertEqual(result['Avg'], 6.5)
+        # 文件最大圈复杂度为9
+        self.assertEqual(result['Max'], 9)
+        # 文件累积圈复杂度为12
+        self.assertEqual(result['Sum'], 13)
+
+
+        result = results[os.path.split(os.path.realpath(__file__))[0] + '/test/test.c']
+
+        # test1圈复杂度为9
+        self.assertEqual(result['functions']['test1-2-0-33-1'], 9)
+        # test2圈复杂度为3
+        self.assertEqual(result['functions']['test2-36-0-47-1'], 3)
+        # 文件平均圈复杂度为6
+        self.assertEqual(result['Avg'], 6)
+        # 文件最大圈复杂度为9
+        self.assertEqual(result['Max'], 9)
+        # 文件累积圈复杂度为12
+        self.assertEqual(result['Sum'], 12)
+    
+
+unittest.main()
