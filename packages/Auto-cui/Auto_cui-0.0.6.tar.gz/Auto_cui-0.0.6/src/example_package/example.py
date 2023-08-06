30d7dcb9a2139cdd544fb30db51c36dd902e2147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:31 +0000

    Add package Auto-cui/0.0.6/Auto_cui-0.0.6.tar.gz

---
+++ packages/Auto-cui/Auto_cui-0.0.6.tar.gz/Auto_cui-0.0.6/src/example_package/example.py
@@ -0,0 +1,6 @@
+import numpy as np
+def cal_add_1(num1,num2):
+    result =  np.mean(num1 +num2)
+    return result
+
+print(cal_add_1(1,1))
---
+++ packages/Auto-cui/Auto_cui-0.0.6.tar.gz/Auto_cui-0.0.6/test/test_example.py
@@ -0,0 +1,3 @@
+from example_package import example
+def test_cal_add_1():
+    assert example.cal_add_1(1,1) == 2
