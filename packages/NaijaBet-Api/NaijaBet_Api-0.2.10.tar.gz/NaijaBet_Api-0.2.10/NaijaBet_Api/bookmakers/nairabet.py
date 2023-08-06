4255e9ebe3dbf041cd909e44446f557bef23d710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:53 +0000

    Add package NaijaBet-Api/0.2.10/NaijaBet_Api-0.2.10.tar.gz

---
+++ packages/NaijaBet-Api/NaijaBet_Api-0.2.10.tar.gz/NaijaBet_Api-0.2.10/NaijaBet_Api/bookmakers/nairabet.py
@@ -0,0 +1,25 @@
+from NaijaBet_Api.utils.normalizer import nairabet_match_normalizer
+from NaijaBet_Api.utils import jsonpaths
+from NaijaBet_Api.bookmakers.BaseClass import BookmakerBaseClass
+
+"""
+[summary]
+"""
+
+
+class Nairabet(BookmakerBaseClass):
+    """
+     This class provides access to https://nairabet.com 's odds data.
+
+     it provides a variety of methods to query the endpoints and obtain
+     odds data at a competiton and match level.
+
+    Attributes:
+        session: holds a requests session object for the class as a static variable.
+    """
+    _site = 'nairabet'
+    _url = "https://nairabet.com"
+    _headers = {}
+
+    def normalizer(self, args):
+        return nairabet_match_normalizer(jsonpaths.nairabet_validator(args))
