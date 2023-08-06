0f9a3ebf91decd6d0eeef12d23e2a84f6f9e89b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:24 +0000

    Add package tramway/0.6.3/tramway-0.6.3.tar.gz

---
+++ packages/tramway/tramway-0.6.3.tar.gz/tramway-0.6.3/tramway/analyzer/images/allsymbols.py
@@ -0,0 +1,7 @@
+
+from . import *
+from ..attribute import initializer_method
+
+from_tiff_file  = initializer_method( ImagesInitializer.from_tiff_file  )
+from_tiff_files = initializer_method( ImagesInitializer.from_tiff_files )
+
---
+++ packages/tramway/tramway-0.6.3.tar.gz/tramway-0.6.3/tramway/analyzer/mapper/allsymbols.py
@@ -0,0 +1,7 @@
+
+from . import *
+from ..attribute import initializer_method
+
+from_plugin   = initializer_method( MapperInitializer.from_plugin   )
+from_callable = initializer_method( MapperInitializer.from_callable )
+
