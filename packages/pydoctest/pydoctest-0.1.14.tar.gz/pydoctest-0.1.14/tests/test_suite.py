612454d140685efc34c1eca6b6309b4de362927b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:37 +0000

    Add package pydoctest/0.1.14/pydoctest-0.1.14.tar.gz

---
+++ packages/pydoctest/pydoctest-0.1.14.tar.gz/pydoctest-0.1.14/tests/test_suite.py
@@ -0,0 +1,29 @@
+import subprocess
+
+from typing import Tuple
+from pydoctest.configuration import Configuration
+from pydoctest.main import PyDoctestService
+
+
+class TestPydoctest():
+    """
+    Uses the library to test its own docstrings - how awesome is that?
+    """
+    def test_pydoctest(self) -> None:
+        config = Configuration.get_configuration_from_path("pydoctest.json")
+        ds = PyDoctestService(config)
+        result = ds.validate()
+        counts = result.get_counts()
+        assert counts.functions_failed == 0, "Run pydoctest on project and fix docstrings."
+        # TODO: This doesn't artificially bring up our code-coverage, right?
+
+
+class TestCase():
+    def execute_command(self, command: str) -> Tuple[str, str]:
+        try:
+            p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={'LC_ALL': 'C'})
+            p.wait()
+            stdout, stderr = p.communicate()
+            return str(stdout.decode("utf-8")), str(stderr.decode("utf-8"))
+        except Exception as e:
+            return "", str(e)
