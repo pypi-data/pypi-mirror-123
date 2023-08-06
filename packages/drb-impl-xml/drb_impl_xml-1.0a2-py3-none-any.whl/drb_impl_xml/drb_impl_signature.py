637cc0d7f7f054cc62e38d8b0ba860073719ebe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:18 +0000

    Add package drb-impl-xml/1.0a2/drb_impl_xml-1.0a2-py3-none-any.whl

---
+++ packages/drb-impl-xml/drb_impl_xml-1.0a2-py3-none-any.whl/drb_impl_xml/drb_impl_signature.py
@@ -0,0 +1,35 @@
+import os
+
+from drb.factory import DrbSignature, DrbSignatureType, DrbFactory
+
+from drb import DrbNode
+from .xml_node_factory import XmlNodeFactory
+
+
+class DrbXmlSignature(DrbSignature):
+    def __init__(self):
+        self._factory = XmlNodeFactory()
+
+    @property
+    def uuid(self) -> str:
+        return '40123218-2b5e-11ec-8d3d-0242ac130003'
+
+    @property
+    def label(self) -> str:
+        return 'xml'
+
+    @property
+    def category(self) -> DrbSignatureType:
+        return DrbSignatureType.FORMATTING
+
+    @property
+    def factory(self) -> DrbFactory:
+        return self._factory
+
+    def match(self, node: DrbNode) -> bool:
+        supported_ext = {
+            '.xml'
+        }
+
+        filename, file_extension = os.path.splitext(node.path.filename)
+        return file_extension and file_extension.lower() in supported_ext
