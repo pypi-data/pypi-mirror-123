938b4e22be598b0817419bc58d5279fd3d274816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:42 +0000

    Add package sectoolkit/0.1.4/sectoolkit-0.1.4.tar.gz

---
+++ packages/sectoolkit/sectoolkit-0.1.4.tar.gz/sectoolkit-0.1.4/sectoolkit/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.4"
+
+# Import items from sub-modules into sectoolkit namespace
+from .secmeta import (meta, headerfile)
+from .secfiling import (filingDocument, filingArchive, ticker_dict, name_dict)
