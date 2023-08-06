76a4b350804d35b2917b07964346a3ba89e5914d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:05 +0000

    Add package drb-impl-tar/1.0a2/drb-impl-tar-1.0a2.tar.gz

---
+++ packages/drb-impl-tar/drb-impl-tar-1.0a2.tar.gz/drb-impl-tar-1.0a2/drb_impl_tar/drb_impl_signature.py
@@ -0,0 +1,35 @@
+import os
+
+from drb.factory import DrbSignature, DrbSignatureType, DrbFactory
+from drb import DrbNode
+from .file_tar_node import DrbTarFactory
+
+
+class DrbTarSignature(DrbSignature):
+    def __init__(self):
+
+        self._factory = DrbTarFactory()
+
+    @property
+    def uuid(self) -> str:
+        return '60e58ee2-2b5c-11ec-8d3d-0242ac130003'
+
+    @property
+    def label(self) -> str:
+        return 'tar'
+
+    @property
+    def category(self) -> DrbSignatureType:
+        return DrbSignatureType.CONTAINER
+
+    @property
+    def factory(self) -> DrbFactory:
+        return self._factory
+
+    def match(self, node: DrbNode) -> bool:
+        supported_ext = {
+            '.tar'
+        }
+
+        filename, file_extension = os.path.splitext(node.path.filename)
+        return file_extension and file_extension.lower() in supported_ext
