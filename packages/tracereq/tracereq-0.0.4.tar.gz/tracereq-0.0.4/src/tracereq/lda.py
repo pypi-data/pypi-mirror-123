50ec77c694dab926cb0771de0aed1f7ebec8229b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:24 +0000

    Add package tracereq/0.0.4/tracereq-0.0.4.tar.gz

---
+++ packages/tracereq/tracereq-0.0.4.tar.gz/tracereq-0.0.4/src/tracereq/lda.py
@@ -0,0 +1,112 @@
+import pandas as pd
+from scipy.sparse import data
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from tabulate import tabulate
+
+from tracereq.preprocessing_evaluation import pengukuranEvaluasi, cleaned_text, id_req
+
+
+class latentDirichlet:
+  def __init__(self, cleaned_text):
+      self.data = cleaned_text
+      self.n_features = len(self.data)
+
+  def ukur_tfidf_vectorizer(self):
+      tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2,
+                                        max_features=self.n_features,
+                                        stop_words='english')
+      tfidf = tfidf_vectorizer.fit_transform(self.data)
+      return tfidf_vectorizer, tfidf
+
+  def ukur_tf(self):
+      tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
+                                      max_features=self.n_features,
+                                      stop_words='english')
+      tf = tf_vectorizer.fit_transform(self.data)
+      return tf_vectorizer, tf
+
+  def Frobenius_norm_feature(self, req):
+      nmf = NMF(n_components=len(self.data), random_state=1, alpha=.1, l1_ratio=.5).fit(latentDirichlet.ukur_tfidf_vectorizer(self)[1])
+      nmf_tfidf = latentDirichlet.ukur_tfidf_vectorizer(self)[0].get_feature_names()
+      fitur_frb_tfidf = (nmf_tfidf)
+      data_frb_tfidf = (nmf.components_)
+      dt_df =  pd.DataFrame(data_frb_tfidf, index= req, columns= fitur_frb_tfidf)
+      return dt_df
+
+  def Kullback_feature(self, req):
+      nmf = NMF(n_components=len(self.data), random_state=1, beta_loss='kullback-leibler', solver='mu', max_iter=1000, alpha=.1, l1_ratio=.5).fit(latentDirichlet.ukur_tfidf_vectorizer(self)[1])
+      tfidf_feature_names = latentDirichlet.ukur_tfidf_vectorizer(self)[0].get_feature_names()
+      fitur_kll_tfidfi = (tfidf_feature_names)
+      data_kll_tfidf = (nmf.components_)
+      dt_df =  pd.DataFrame(data_kll_tfidf, index= req, columns= fitur_kll_tfidfi)
+      return dt_df
+
+  def lda_feature(self, req):
+      lda = LatentDirichletAllocation(n_components=len(self.data), max_iter=5, learning_method='online', learning_offset=50., random_state=0)
+      lda.fit(latentDirichlet.ukur_tf(self)[1])
+      tf_feature_names = latentDirichlet.ukur_tf(self)[0].get_feature_names()
+      fitur_lda = (tf_feature_names)
+      nmf = NMF(n_components=len(self.data), random_state=1, beta_loss='kullback-leibler', solver='mu', max_iter=1000, alpha=.1, l1_ratio=.5).fit(latentDirichlet.ukur_tfidf_vectorizer(self)[1])
+      data_lda = (nmf.components_)
+      dt_df =  pd.DataFrame(data_lda, index= req, columns= fitur_lda)
+      return dt_df
+
+  def threshold_value(self, threshold, data):
+      dt = data.values >= threshold
+      dt1 = pd.DataFrame(dt, index= data.index, columns= data.columns)
+      mask = dt1.isin([True])
+      dt3 = dt1.where(mask, other= 0)
+      mask2 = dt3.isin([False])
+      th_cosine1 = dt3.where(mask2, other= 1)
+      return th_cosine1
+
+  def tabulasi_fr(self, th, id_req, param= ['fr', 'th', 'ukur']):
+      dt_fr = latentDirichlet.Frobenius_norm_feature(self, id_req)
+      th_fr = pengukuranEvaluasi.threshold_value(self, th, dt_fr)
+      myUkur = pengukuranEvaluasi.ukur_evaluasi(self, dt_fr, th_fr)
+      if 'fr' in param:
+          return dt_fr
+      elif 'th' in param:
+          return th_fr
+      elif 'ukur' in param:
+          return myUkur
+      
+  def tabulasi_kl(self, th, id_req, param= ['kl', 'th', 'ukur']):
+      dt_kl = latentDirichlet.Kullback_feature(self, id_req)
+      th_kl = pengukuranEvaluasi.threshold_value(self, th, dt_kl)
+      myUkur = pengukuranEvaluasi.ukur_evaluasi(self, dt_kl, th_kl)
+      if 'kl' in param:
+          return dt_kl
+      elif 'th' in param:
+          return th_kl
+      elif 'ukur' in param:
+          return myUkur
+
+  def tabulasi_lda(self, th, id_req, param= ['lda', 'th', 'ukur']):
+      dt_lda = latentDirichlet.lda_feature(id_req)
+      th_lda = pengukuranEvaluasi.threshold_value(th, dt_lda)
+      myUkur = pengukuranEvaluasi.ukur_evaluasi(self, dt_lda, th_lda)
+      if 'lda' in param:
+          return dt_lda
+      elif 'th' in param:
+          return th_lda
+      elif 'ukur' in param:
+          return myUkur
+
+  def main(self, th, id_req, output= ['fr', 'kl', 'lda']):
+      if 'fr'in output:
+        return latentDirichlet.tabulasi_fr(self, th, id_req,'fr')
+      elif 'kl'in output:
+        return latentDirichlet.tabulasi_kl(self, th, id_req, 'kl')
+      elif 'lda'in output:
+        return latentDirichlet.tabulasi_lda(self, th, id_req, 'lda')
+
+
+if __name__ == "__main__":
+  try:
+    a = latentDirichlet(cleaned_text).main(0.2, id_req, 'lda')
+    print(tabulate(a, headers = 'keys', tablefmt = 'psql'))
+
+  except OSError as err:
+    print("OS error: {0}".format(err))
---
+++ packages/tracereq/tracereq-0.0.4.tar.gz/tracereq-0.0.4/src/tracereq/preprocessing_evaluation.py
@@ -0,0 +1,118 @@
+import numpy as np, pandas as pd
+from pyAutoML.ml import ML, EncodeCategorical, ml
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from spacy.lang.en import English
+from spacy.tokenizer import Tokenizer
+from tabulate import tabulate
+
+
+class prosesData:  
+  def __init__(self, namaFile):
+      '''fungi ini digunakan untuk menginisialisasi data
+      sehingga perlu dilakukan proses construct data.
+      '''
+      self.dataFile = namaFile
+
+  def fulldataset(self, inputSRS= 'SRS1'):
+      '''fungi ini digunakan untuk menampilkan data secara spesifik,
+      sehingga menunjukkan data yang diinginkan.
+      syntax yang digunakan adalah sebagai berikut:
+      prosesData(data).fulldataset(inputSRS)
+      '''
+      xl = pd.ExcelFile(self.dataFile)
+      dfs = {sh:xl.parse(sh) for sh in xl.sheet_names}[inputSRS]
+      return dfs
+
+  def preprocessing(self):
+      '''fungsi ini digunakan untuk mengecek data yang digunakan 
+      untuk melihat data yang digunakan. sehingga syntax yang digunakan
+      yaitu adalah sebagai berikut:
+      prosesData(data).preprocessing()
+      '''
+      xl = pd.ExcelFile(self.dataFile)
+      for sh in xl.sheet_names:
+        df = xl.parse(sh)
+        print('Processing: [{}] ...'.format(sh))
+        print(df.head())
+
+  def apply_cleaning_function_to_list(self, X):
+      '''fungsi ini digunakan untuk mengaplikasikan fungsi pembersihan data, 
+      sesuai dengan list yang dipilih. Sehingga maasuk ke tahapan selanjutnya.
+      prosesData(data).apply_cleaning_function_to_list(X)
+      '''
+      cleaned_X = [prosesData.clean_text(self, raw_text= element)for element in X]
+      return cleaned_X
+
+  def clean_text(self, raw_text):
+      '''fungsi ini digunakan untuk mengaplikasikan fungsi pembersihan data, 
+      sesuai dengan list yang dipilih. Sehingga maasuk ke tahapan selanjutnya.
+      prosesData(data).clean_text(raw_text)
+      '''
+      nlp = English()
+      tokenizer = Tokenizer(nlp.vocab)
+      tokens = tokenizer(raw_text)
+      lemma_list = [token.lemma_.lower() for token in tokens if token.is_stop is False 
+      and token.is_punct is False and token.is_alpha is True]
+      joined_words = ( " ".join(lemma_list))
+      return joined_words  
+
+class pengukuranEvaluasi:
+  def __init__(self):
+      '''fungsi ini digunakan untuk menginisialisasi data
+      sehingga perlu dilakukan proses construct data.
+      '''
+      pass
+
+  def threshold_value(self, threshold, data):
+        dt = data.values >= threshold
+        dt1 = pd.DataFrame(dt, index= data.index, columns= data.columns)
+        mask = dt1.isin([True])
+        dt3 = dt1.where(mask, other= 0)
+        mask2 = dt3.isin([False])
+        th_cosine1 = dt3.where(mask2, other= 1)
+        return th_cosine1
+
+  def kmeans_cluster(self, data1, data2, nilai_cluster= 3):
+      '''fungsi ini digunakan untuk mengklaster data menggunakan metode kmeans.
+      Dari fungsi ini dapat menciptakan data korelasi yang dimuat hasil klaster.
+      pengukuranEvaluasi()
+      '''
+      XVSM = np.array(data1)
+      yVSM = np.array(data2)
+      kmeans = KMeans(n_clusters=nilai_cluster) # You want cluster the passenger records into 2: Survived or Not survived
+      kmeans.fit(XVSM)
+      correct = 0
+      for i in range(len(XVSM)):
+          predict_me = np.array(XVSM[i].astype(float))
+          predict_me = predict_me.reshape(-1, len(predict_me))
+          prediction = kmeans.predict(predict_me)
+          if prediction[0] == yVSM.all():
+              correct += 1
+      scaler = MinMaxScaler()
+      XVSM_scaled = scaler.fit_transform(yVSM)
+      print("data_correction {}".format(correct/len(XVSM)))
+      return (XVSM_scaled)
+
+
+  def ukur_evaluasi(self, data1, data2, nilai_cluster=3, test_size= 0.3, random_state= 100, size= 0.4, max_iter= 7000):
+      X_train, X_test, y_train, y_test = \
+      train_test_split(pengukuranEvaluasi.kmeans_cluster(self, data1, data2, nilai_cluster), 
+      data2, test_size) # 70% training and 30% test
+      y_train = y_train.argmax(axis= 1)
+      y_train = EncodeCategorical(y_train)
+      return ML(X_train, y_train, size, SVC(), RandomForestClassifier(), 
+      DecisionTreeClassifier(), KNeighborsClassifier(), LogisticRegression(max_iter))      
+
+if __name__ == "__main__":
+    myData = prosesData() # myData.preprocessing()
+    req = myData.fulldataset() # myData.fulldataset(inputSRS)
+    id_req  = req['ID']
+    text_to_clean = list(req['Requirement Statement'])
+    cleaned_text = myData.apply_cleaning_function_to_list(text_to_clean)
