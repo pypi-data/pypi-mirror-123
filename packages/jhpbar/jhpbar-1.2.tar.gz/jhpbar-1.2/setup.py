0a127424fa5b253aa89e5d94344203771f3abc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:01 +0000

    Add package jhpbar/1.2/jhpbar-1.2.tar.gz

---
+++ packages/jhpbar/jhpbar-1.2.tar.gz/jhpbar-1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+setup(
+	name                = 'jhpbar',
+	version             = '1.2',
+	description         = 'custom progress bar bug fix',
+    author              = 'jh.kim',
+	author_email        = 'biz.jh.kim@gmail.com',
+    url                 = 'https://github.com/jhkim31/jh-pbar',
+	download_url        = '',
+	install_requires    =  [],
+    packages            = find_packages(exclude = []),
+	keywords            = ['progress_bar'],
+	python_requires     = '>=3',
+	package_data        = {},
+	zip_safe            = False,
+	classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
