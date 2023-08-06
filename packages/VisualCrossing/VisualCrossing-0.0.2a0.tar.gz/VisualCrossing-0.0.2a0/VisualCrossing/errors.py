5e1b777cad0d3ed85e5217c10739b3068b2d662e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:22 +0000

    Add package VisualCrossing/0.0.2a0/VisualCrossing-0.0.2a0.tar.gz

---
+++ packages/VisualCrossing/VisualCrossing-0.0.2a0.tar.gz/VisualCrossing-0.0.2a0/VisualCrossing/errors.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+class WrongDateFormat(Exception):
+    """Exception is raised when the passed date is not in `YYYY-MM-DD` format"""
+
+
+class VerificationWarning(Warning):
+    """Warning is raised when fetching data with flag `verify = False`"""
+
+
+class NoDataFetched(Exception):
+    """Exception is raised when data is not fetched, i.e. received `status_code != 200`"""
