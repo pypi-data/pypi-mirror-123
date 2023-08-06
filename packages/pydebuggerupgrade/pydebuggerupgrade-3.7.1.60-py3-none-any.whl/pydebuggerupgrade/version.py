8baee35df7cc6016c5594a3370782491e92032d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:28 +0000

    Add package pydebuggerupgrade/3.7.1.60/pydebuggerupgrade-3.7.1.60-py3-none-any.whl

---
+++ packages/pydebuggerupgrade/pydebuggerupgrade-3.7.1.60-py3-none-any.whl/pydebuggerupgrade/version.py
@@ -0,0 +1,4 @@
+""" This file was generated when pydebuggerupgrade was built """
+VERSION = '3.7.1.60'
+COMMIT_ID = '9ef03c2c1682dc14d378fd36e1b35e8bfd9decf9'
+BUILD_DATE = '2021-10-11 11:11:45 +0000'
