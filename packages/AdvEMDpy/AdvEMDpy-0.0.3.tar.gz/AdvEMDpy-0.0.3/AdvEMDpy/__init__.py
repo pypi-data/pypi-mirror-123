f091cd157c005244a7d62b2d736c3f5d428f0394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:10 +0000

    Add package AdvEMDpy/0.0.3/AdvEMDpy-0.0.3.tar.gz

---
+++ packages/AdvEMDpy/AdvEMDpy-0.0.3.tar.gz/AdvEMDpy-0.0.3/AdvEMDpy/__init__.py
@@ -0,0 +1,13 @@
+
+#     ________
+#            /
+#      \    /
+#       \  /
+#        \/
+
+from AdvEMDpy import emd_utils
+from AdvEMDpy import emd_preprocess
+from AdvEMDpy import emd_basis
+from AdvEMDpy import emd_hilbert
+from AdvEMDpy import emd_mean
+from AdvEMDpy import AdvEMDpy
