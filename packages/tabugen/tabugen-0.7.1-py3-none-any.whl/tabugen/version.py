1622f65a90ba971ed2314eb66dfd2517cee102e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:28 +0000

    Add package tabugen/0.7.1/tabugen-0.7.1-py3-none-any.whl

---
+++ packages/tabugen/tabugen-0.7.1-py3-none-any.whl/tabugen/version.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2018-present ichenq@outlook.com. All rights reserved.
+# Distributed under the terms and conditions of the Apache License.
+# See accompanying files LICENSE.
+
+_major = 0
+_minor = 7
+_patch = 1
+
+VER_STRING = '%d.%d.%d' % (_major, _minor, _patch)
