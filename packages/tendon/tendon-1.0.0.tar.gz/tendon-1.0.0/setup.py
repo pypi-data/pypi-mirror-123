1695743f444fc377b9aadf0424e7ace394761be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:55 +0000

    Add package tendon/1.0.0/tendon-1.0.0.tar.gz

---
+++ packages/tendon/tendon-1.0.0.tar.gz/tendon-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tendon', 'tendon.api', 'tendon.api.nodes', 'tendon.gql']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graphql-core>=3.0.3,<4.0.0', 'httpx>=0.12.1,<0.13.0']
+
+setup_kwargs = {
+    'name': 'tendon',
+    'version': '1.0.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Tendon',
+    'author_email': 'alastair@tendon.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
