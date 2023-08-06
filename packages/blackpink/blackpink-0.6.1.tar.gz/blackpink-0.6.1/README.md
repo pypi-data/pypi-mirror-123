66857f220dfb47f8a2b6eef36f8d1a610a4847bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:31 +0000

    Add package blackpink/0.6.1/blackpink-0.6.1.tar.gz

---
+++ packages/blackpink/blackpink-0.6.1.tar.gz/blackpink-0.6.1/README.md
@@ -0,0 +1,20 @@
+[![Downloads](https://pepy.tech/badge/blackpink)](https://pepy.tech/project/blackpink)
+# Install Library
+```
+$ pip install blackpink
+```
+# Cara Pakai
+
+```bash
+> git clone https://github.com/krypton-byte/blackpink
+> python3
+```
+```python
+>>> from blackpink import blackpink
+>>> blackpink("krypton").save("out.png")
+```
+# Hasil
+<img src="out.png" >
+
+# Donasi
+<ul><li><a href="https://saweria.co/kryptonbyte">Saweria</a><li><a href="https://wa.me/6283172366463">Whatsapp</a></li></ul>
---
+++ packages/blackpink/blackpink-0.6.1.tar.gz/blackpink-0.6.1/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup, find_packages
+from os import path
+base_dir = path.abspath(path.dirname(__file__))
+setup(
+  name = 'blackpink',        
+  packages = ['blackpink'],
+  include_package_data=True,
+  long_description=open(path.join(base_dir, "README.md"), encoding="utf-8").read(),
+  long_description_content_type='text/markdown',
+  version = '0.6.1',    
+  license='MIT',     
+  description = 'Blackpink Logo Generator', 
+  author = 'Krypton Byte',                  
+  author_email = 'galaxyvplus6434@gmail.com',     
+  url = 'https://github.com/krypton-byte/blackpink',   
+  download_url = 'https://github.com/krypton-byte/blackpink/archive/0.1.tar.gz',    
+  keywords = ['blackpink', 'logo', 'generator'], 
+  install_requires=[           
+          'pillow',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
