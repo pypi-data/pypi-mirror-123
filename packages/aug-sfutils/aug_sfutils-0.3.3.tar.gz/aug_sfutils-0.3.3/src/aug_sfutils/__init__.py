935de77c6f51815a8296137cc0014b9ea31bb860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:49 +0000

    Add package aug-sfutils/0.3.3/aug_sfutils-0.3.3.tar.gz

---
+++ packages/aug-sfutils/aug_sfutils-0.3.3.tar.gz/aug_sfutils-0.3.3/src/aug_sfutils/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# sfutils
+
+"""Shotfile reading with pure python
+
+https://www.aug.ipp.mpg.de/~git/pyaug/sfread.html
+
+"""
+__author__  = 'Giovanni Tardini (Tel. 1898)'
+__version__ = '0.3.3'
+__date__    = '14.10.2021'
+
+import sys, logging
+
+fmt = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s: %(message)s', '%H:%M:%S')
+hnd = logging.StreamHandler()
+hnd.setFormatter(fmt)
+logger = logging.getLogger('aug_sfutils')
+logger.addHandler(hnd)
+logger.setLevel(logging.INFO)
+
+try:
+    from .ww import *
+    from .sfh import *
+except:
+    logger.warning('ww and sfh not loaded, SFREAD and EQU available')
+    pass
+
+from .sfread import *
+from .sf2equ import *
+from .libddc import ddcshotnr, previousshot
+from .mapeq import *
+
+import encodings.utf_8
