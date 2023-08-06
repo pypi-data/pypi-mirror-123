af1c91075f2204e1ce12cfbbc152cb5b20fc463b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:38 +0000

    Add package PySXO/0.0.10/PySXO-0.0.10.tar.gz

---
+++ packages/PySXO/PySXO-0.0.10.tar.gz/PySXO-0.0.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="PySXO",
+    install_requires=[
+        "requests >= 2.19.0",
+        "attrdict >= 2.0.1"
+    ]
+)
