4c55dbce67fce4df8afe0d0b52ed91a15afc8cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:07 +0000

    Add package distributions-uda-ex/0.1/distributions_uda_ex-0.1.tar.gz

---
+++ packages/distributions-uda-ex/distributions_uda_ex-0.1.tar.gz/distributions_uda_ex-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_uda_ex',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_uda_ex'],
+      zip_safe=False)
