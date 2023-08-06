bcd9cf34ba192640d0a25b8753d3f49f628fcada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:10 +0000

    Add package angrivhid97/1.0/angrivhid97-1.0-py3-none-any.whl

---
+++ packages/angrivhid97/angrivhid97-1.0-py3-none-any.whl/angrivhid97/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 16:15:00 2021
+
+@author: lito
+"""
+
+def saludoEIP(nombre):
+	return print(f'Hola {nombre}, mi nombre es Angel Rivas Hidalgo, soy alumno de FPv4 de EIP y esto es la tarea 4')
