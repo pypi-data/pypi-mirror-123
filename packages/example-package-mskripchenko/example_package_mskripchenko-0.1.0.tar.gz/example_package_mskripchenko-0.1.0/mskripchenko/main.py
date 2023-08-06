6c4bca1c6834c5210bbc9086480be9b983d7e0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package example-package-mskripchenko/0.1.0/example_package_mskripchenko-0.1.0.tar.gz

---
+++ packages/example-package-mskripchenko/example_package_mskripchenko-0.1.0.tar.gz/example_package_mskripchenko-0.1.0/mskripchenko/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random
+
+def hello_world():
+    print("Hello World")
+    
+def hello_human(name: str):
+    print("Hello, " + name)
+
+def numpy_array():
+    print("Array:\n" + np.array([16, 4, 2]))
