96dffe9cd564b36e02b8105747403a501a438e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:55 +0000

    Add package test-jrubics-poetry-publish/0.2.0/test-jrubics-poetry-publish-0.2.0.tar.gz

---
+++ packages/test-jrubics-poetry-publish/test-jrubics-poetry-publish-0.2.0.tar.gz/test-jrubics-poetry-publish-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test_jrubics_poetry_publish']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'test-jrubics-poetry-publish',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jelena Dokic',
+    'author_email': 'jelena.dpk@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
