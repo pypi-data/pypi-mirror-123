4233ebdee957723c2d19e0eb18178cea96fd3834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:55 +0000

    Add package cord-client-python/0.1.18/cord_client_python-0.1.18-py3-none-any.whl

---
+++ packages/cord-client-python/cord_client_python-0.1.18-py3-none-any.whl/tests/test_data/test_blurb2.py
@@ -0,0 +1,114 @@
+[
+  {
+    "name": "Polyp polygon",
+    "range": [
+      [
+        0,
+        0
+      ],
+      [
+        7,
+        12
+      ]
+    ],
+    "shape": "polygon",
+    "objectID": 0,
+    "indexHash": "cb3dc473-91e1-4216-820f-301e33e83488",
+    "objectHash": "4706dda6-4986-4955-8343-67c45844699b",
+    "featureHash": "0308ff48-906a-43eb-bf0d-ae5216fde93f",
+    "classifications": [],
+    "manual_annotation": true
+  },
+  {
+    "name": "Polyp polygon",
+    "range": [
+      [
+        86,
+        92
+      ]
+    ],
+    "shape": "polygon",
+    "objectID": 1,
+    "indexHash": "658dc5c8-620a-418f-a73f-97ef4649f3ca",
+    "objectHash": "d392992c-9861-4a56-b8d3-66003de6efbd",
+    "featureHash": "0308ff48-906a-43eb-bf0d-ae5216fde93f",
+    "classifications": [],
+    "manual_annotation": true
+  },
+  {
+    "name": "Polyp",
+    "range": [
+      [
+        0,
+        0
+      ],
+      [
+        152,
+        197
+      ],
+      [
+        260,
+        289
+      ],
+      [
+        300,
+        329
+      ],
+      [
+        636,
+        677
+      ],
+      [
+        683,
+        712
+      ],
+      [
+        873,
+        873
+      ],
+      [
+        1465,
+        1491
+      ]
+    ],
+    "shape": "bounding_box",
+    "objectID": 0,
+    "indexHash": "b23f1717-4f24-489d-9c24-b3298818b7eb",
+    "objectHash": "bc6165a7-05bb-4073-969f-c615ad66f752",
+    "featureHash": "8ec525ed-a0df-42b2-bb00-0a2e52c57cd5",
+    "classifications": [],
+    "manual_annotation": true
+  },
+  {
+    "name": "Polyp",
+    "range": [
+      [
+        0,
+        0
+      ]
+    ],
+    "shape": "bounding_box",
+    "objectID": 1,
+    "indexHash": "be52e87c-b3d6-4297-baf5-2b2d657f40c6",
+    "objectHash": "b3886f53-b790-42c7-a56d-0f51617986d8",
+    "featureHash": "8ec525ed-a0df-42b2-bb00-0a2e52c57cd5",
+    "classifications": [],
+    "manual_annotation": true
+  },
+  {
+    "name": "Polyp",
+    "range": [
+      [
+        0,
+        18
+      ]
+    ],
+    "shape": "bounding_box",
+    "objectID": 2,
+    "indexHash": "5e88e3ef-1971-4e99-a84e-cc5e439ddc73",
+    "objectHash": "d281cddb-ebd7-48f9-9cee-fa887b4529b9",
+    "featureHash": "8ec525ed-a0df-42b2-bb00-0a2e52c57cd5",
+    "classifications": [],
+    "manual_annotation": true
+  }
+]
