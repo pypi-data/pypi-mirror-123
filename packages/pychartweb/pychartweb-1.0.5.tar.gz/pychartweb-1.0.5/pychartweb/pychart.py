4d1fa22a0343a729236a80ea6ce599aeffa008bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:23 +0000

    Add package pychartweb/1.0.5/pychartweb-1.0.5.tar.gz

---
+++ packages/pychartweb/pychartweb-1.0.5.tar.gz/pychartweb-1.0.5/pychartweb/pychart.py
@@ -0,0 +1,33 @@
+from bottle import route, run, view, static_file
+
+graph_lst = []
+
+class graph:
+
+    def __init__(self,name, xaxis):
+        self.xaxis = xaxis
+        self.name = name
+        self.data = {'base' : {'name' : name, 'xaxis' : xaxis}, 'data' : [] } 
+    
+    def adddata(self,ctype, label, value, backcol = ['grey']):
+        dic = {'type' : ctype,'label' : label, 'data' : value, 'backgroundColor' : backcol}
+        self.data['data'].append(dic)
+
+    def getgraph(self):
+        return self.data
+ 
+@route('/<filename:path>')
+def js(filename):
+    return static_file('chart2.js', root="")
+
+def show(graphs):
+    graph_lst = []
+    for i in graphs:
+        graph_lst.append(i.getgraph())
+    @route('/')
+    @view('pychartweb/base2')
+    def hello():
+        return dict(graph = graph_lst)
+    graphs = graphs
+    run(host='localhost', port=8080, debug=True)
+
