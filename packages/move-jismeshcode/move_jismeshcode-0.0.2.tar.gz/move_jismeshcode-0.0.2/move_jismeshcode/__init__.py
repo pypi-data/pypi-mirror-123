8e3c1120a33e273b2961412a3863f89729e6523b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:10 +0000

    Add package move-jismeshcode/0.0.2/move_jismeshcode-0.0.2.tar.gz

---
+++ packages/move-jismeshcode/move_jismeshcode-0.0.2.tar.gz/move_jismeshcode-0.0.2/move_jismeshcode/__init__.py
@@ -0,0 +1,10 @@
+from .move import *
+
+__copyright__    = 'Copyright (C) 2021 igtm'
+__version__      = '0.0.2'
+__license__      = 'MIT'
+__author__       = 'igtm'
+__author_email__ = 'tomo14916253@yahoo.co.jp'
+__url__          = 'http://github.com/igtm/move_jismeshcode'
+
+__all__ = ['move']
