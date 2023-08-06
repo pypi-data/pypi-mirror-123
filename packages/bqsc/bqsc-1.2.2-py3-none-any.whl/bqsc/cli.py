581c19a4cacd1ad6cb1781e34d838c3e1e33a07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:01 +0000

    Add package bqsc/1.2.2/bqsc-1.2.2-py3-none-any.whl

---
+++ packages/bqsc/bqsc-1.2.2-py3-none-any.whl/bqsc/cli.py
@@ -0,0 +1,28 @@
+from typing import Optional, Sequence, Union
+import glob
+import os
+
+
+import click
+
+from .load import load
+
+
+@click.command()
+@click.option("--pyi_path", type=click.Path(file_okay=False, writable=True))
+@click.argument("json_dir", type=click.Path(file_okay=False, readable=True))
+def main(pyi_path: Optional[str], json_dir: str):
+    ret = """from typing import Sequence, Union
+from datetime import date, datetime, time
+
+"""
+    for js in glob.glob(os.path.join(json_dir, "**.json")):
+        Table = load(js)
+        table = Table()
+        ret += table._typehint() + "\n" * 2
+
+    print(ret)
+
+
+if __name__ == "__main__":
+    main()
