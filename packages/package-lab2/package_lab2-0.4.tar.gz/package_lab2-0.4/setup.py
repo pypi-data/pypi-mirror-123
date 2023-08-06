21c1b44607268e9e1ac1246b3dfab0b4385d8248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:42 +0000

    Add package package-lab2/0.4/package_lab2-0.4.tar.gz

---
+++ packages/package-lab2/package_lab2-0.4.tar.gz/package_lab2-0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    author='me me me',
+    author_email='wooooah@gmail.com',
+    name='package_lab2',                    # package name
+    version='0.4',                          # version
+    description='Package for Lab 2...',      # short description
+    url='http://example.com',               # package URL
+    install_requires=['matplotlib<1.2', 'matplotlib_iterm2'],                    # list of packages this package depends
+                                            # on.
+    packages=['package_lab2'],              # List of module names that installing
+                                            # this package will provide.
+)
