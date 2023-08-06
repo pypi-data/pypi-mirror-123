a592ccc926b33dd7396155f6cc0dc3b91e0f9bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:01 +0000

    Add package oddsapi/1.2.3/oddsapi-1.2.3.tar.gz

---
+++ packages/oddsapi/oddsapi-1.2.3.tar.gz/oddsapi-1.2.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oddsapi", 
+    version="1.2.3",
+    author="Artur Saradzhyan",
+    author_email="saradzhyanartur@gmail.com",
+    description="Python wrapper for The Odds-Api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sarartur/oddsapi",
+    packages=setuptools.find_packages(),
+    install_requires=['aiohttp==3.7.4.post0', ],
+    setup_requires=['aiohttp==3.7.4.post0', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
