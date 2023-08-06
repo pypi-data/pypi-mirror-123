cd71fa07ce95c8d29d299dafce9135828724085c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:14 +0000

    Add package hyperant/1.0/hyperant-1.0.tar.gz

---
+++ packages/hyperant/hyperant-1.0.tar.gz/hyperant-1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='hyperant',
+   version='1.0',
+   python_requires='>3.5.2',
+   description='BMC Research Programmer Technical Test - Ant on Cube',
+   license="MIT",
+   long_description=long_description,
+   long_description_content_type='text/markdown',
+   author='Victor Arsenescu',
+   url="https://github.com/v0rtex20k",
+   author_email='victor.arsenescu@tufts.edu',
+   packages=['hyperant'],
+   scripts=[
+            'scripts/hypercube.h',
+            'scripts/hypercube.cpp',
+            # 'scripts/run-hypercube', // Bash build script for backend
+           ]
+)
