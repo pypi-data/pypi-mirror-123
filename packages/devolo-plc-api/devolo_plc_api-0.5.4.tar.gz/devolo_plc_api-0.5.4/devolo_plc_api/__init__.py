3a8fc3bcb534e8d3e9c9b0d8d3ea7dd7d307e493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:21 +0000

    Add package devolo-plc-api/0.5.4/devolo_plc_api-0.5.4.tar.gz

---
+++ packages/devolo-plc-api/devolo_plc_api-0.5.4.tar.gz/devolo_plc_api-0.5.4/devolo_plc_api/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version("devolo_plc_api")
+except PackageNotFoundError:
+    # package is not installed - e.g. pulled and run locally
+    __version__ = "0.0.0"
