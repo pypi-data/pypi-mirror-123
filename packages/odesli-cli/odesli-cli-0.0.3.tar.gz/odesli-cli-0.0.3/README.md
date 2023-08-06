1359c0296b1a5bbe1655cfc495b8e14a73d9b161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:05 +0000

    Add package odesli-cli/0.0.3/odesli-cli-0.0.3.tar.gz

---
+++ packages/odesli-cli/odesli-cli-0.0.3.tar.gz/odesli-cli-0.0.3/README.md
@@ -0,0 +1,43 @@
+# odesli-cli
+
+CLI client for the Odesli/Songlink API.
+
+## Installation
+
+```bash
+pip install odesli-cli
+```
+
+### From source
+
+Install the PyPI package `build`:
+```bash
+pip install build
+```
+
+Then (from the root of the repo):
+```bash
+pip install dist/*.whl
+```
+
+## Sample usages:
+
+### Get information on a song by it's url
+```bash
+odesli-cli '{URL}' all
+```
+
+### Convert current Spotify track to Tidal link and copy it to the X clipboard (Linux only)
+```bash
+current_spotify="$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep xesam:url |  awk '{print $2}')"
+odesli-cli "$current_spotify" --provider tidal link | xclip -selection clipboard
+```
+
+### Open Tidal, Spotify, Deezer, etc. links directly in the Spotify app (The coolest one, Linux only)
+For this functionality one needs to hack a bit more. I have it all set up with my Linux dofiles, so trust me, it works.
+Just some key points to make it work:
+- Create a shell script that can handle all sorts of http(s) links and set it as the mime default.
+- Match Tidal, etc. links, convert them with this tool and play them with `qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.OpenUri "{URI}"`.
+- Match Spotify links and play them directly with above command (note that you need to convert the url to a uri in both cases).
+
+If you need additional information don't hesitate to contact me via mail. I haven't tried the dbus command for playing with the Tidal or Deezer apps, but it may work for them in the same fashion.
---
+++ packages/odesli-cli/odesli-cli-0.0.3.tar.gz/odesli-cli-0.0.3/odesli_cli/odesli_cli.py
@@ -0,0 +1,71 @@
+import argparse
+
+from odesli.Odesli import Odesli
+from odesli.entity.song.SongResult import SongResult
+from odesli.entity.album.AlbumResult import AlbumResult
+
+INDENTATION_WIDTH = 4
+PROPERTIES = ['all', 'artistName', 'id', 'link', 'thumbnailUrl', 'thumbnailWidth', 'thumbnailHeight', 'title', 'type', 'songLink', 'albumLink']
+
+def indentString(level, s):
+    return ' '*INDENTATION_WIDTH*level+s
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', metavar='url', type=str,
+                        help='an url to some music content')
+    parser.add_argument('--provider', metavar='provider', type=str,
+                        help='the api provider to use data of (default: provider used for the query)')
+    parser.add_argument('property', metavar='property', type=str,
+                        choices=PROPERTIES,
+                        help='the property to output. Must be one of: '+', '.join(PROPERTIES))
+    parser.add_argument('--platform', metavar='platform', type=str,
+                        help='platform to print the link of (default: first link in list)')
+    args = parser.parse_args()
+
+    odesli = Odesli()
+    result = odesli.getByUrl(args.url)
+
+    # use specified api provider for printing the data
+    if isinstance(result, SongResult):
+        if args.provider == None:
+            entity = result.song
+        else:
+            entity = result.songsByProvider[args.provider]
+    elif isinstance(result, AlbumResult):
+        if args.provider == None:
+            entity = result.album
+        else:
+            entity = result.albumsByProvider[args.provider]
+    else:
+        raise NotImplementedError()
+
+
+    # print out requested properties
+    if args.property == 'link':
+        if args.platform == None:
+            # get first link if no platform specified
+            print(next(iter(entity.linksByPlatform.values())))
+        else:
+            print(entity.linksByPlatform[args.platform])
+    elif args.property == 'type':
+        print(entity.getType())
+    elif args.property == 'all':
+        formatString = "{:<17}"*2
+        if entity.getType() == 'song':
+            print(formatString.format('songLink:', result.songLink))
+        elif entity.getType() == 'album':
+            print(formatString.format('albumLink:', result.albumLink))
+        print(formatString.format('id:', entity.id))
+        print(formatString.format('type:', entity.getType()))
+        print(formatString.format('title:', entity.title))
+        print(formatString.format('artistName:', entity.artistName))
+        print(formatString.format('thumbnailUrl:', entity.thumbnailUrl))
+        print(formatString.format('thumbnailWidth:', entity.thumbnailWidth))
+        print(formatString.format('thumbnailHeight:', entity.thumbnailHeight))
+        print(formatString.format('provider:', entity.provider))
+        print('links:')
+        for platform in entity.linksByPlatform:
+            print(formatString.format(indentString(1, platform)+':', entity.linksByPlatform[platform]))
+    else:
+        print(getattr(entity, args.property))
