73f13d0442992919b2a6f9276aa2273f06096693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:17 +0000

    Add package cobaltstrike/0.0.2/cobaltstrike-0.0.2-py3-none-any.whl

---
+++ packages/cobaltstrike/cobaltstrike-0.0.2-py3-none-any.whl/cobaltstrike/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+
+def bridge():
+    print("bridge")
+
+def sleep_python_bridge():
+    print("sleep_python_bridge")
+
+def aggressor():
+    print("aggressor")
+
+def aggressor_script():
+    print("aggressor_script")
