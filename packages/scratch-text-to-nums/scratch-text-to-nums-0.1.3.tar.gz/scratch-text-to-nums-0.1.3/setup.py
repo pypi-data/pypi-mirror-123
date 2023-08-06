f050241fef5d681d6a19f50df5699da9349420b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:40 +0000

    Add package scratch-text-to-nums/0.1.3/scratch-text-to-nums-0.1.3.tar.gz

---
+++ packages/scratch-text-to-nums/scratch-text-to-nums-0.1.3.tar.gz/scratch-text-to-nums-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['scratch_text_to_nums']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'scratch-text-to-nums',
+    'version': '0.1.3',
+    'description': 'Encodes strings to send to the Scratch cloud',
+    'long_description': None,
+    'author': 'joecooldoo',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
