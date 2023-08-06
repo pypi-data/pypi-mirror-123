18c994037e9fada290238b94e2752f0852a167b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:31 +0000

    Add package jitsuin-archivist/0.7.1/jitsuin_archivist-0.7.1-py3-none-any.whl

---
+++ packages/jitsuin-archivist/jitsuin_archivist-0.7.1-py3-none-any.whl/archivist/constants.py
@@ -0,0 +1,34 @@
+"""Archivist constants
+
+"""
+
+# these are in separate file to stop import loops
+ROOT = "archivist"
+SEP = "/"
+
+# define in MIME canonical form
+HEADERS_REQUEST_TOTAL_COUNT = "X-Request-Total-Count"
+HEADERS_TOTAL_COUNT = "X-Total-Count"
+HEADERS_RETRY_AFTER = "Archivist-Rate-Limit-Reset"
+
+CONFIRMATION_STATUS = "confirmation_status"
+CONFIRMATION_PENDING = "PENDING"
+CONFIRMATION_FAILED = "FAILED"
+CONFIRMATION_CONFIRMED = "CONFIRMED"
+
+ASSETS_SUBPATH = "v2"
+ASSETS_LABEL = "assets"
+ASSETS_WILDCARD = "assets/-"
+EVENTS_LABEL = "events"
+
+LOCATIONS_SUBPATH = "v2"
+LOCATIONS_LABEL = "locations"
+
+ATTACHMENTS_SUBPATH = "v1"
+ATTACHMENTS_LABEL = "blobs"
+
+ACCESS_POLICIES_SUBPATH = "iam/v1"
+ACCESS_POLICIES_LABEL = "access_policies"
+
+SUBJECTS_SUBPATH = "iam/v1"
+SUBJECTS_LABEL = "subjects"
