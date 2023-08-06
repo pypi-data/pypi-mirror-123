bffdc3f2f2f46d1706354f7e2e8614f712820c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:30 +0000

    Add package MixacLib-tumuidle/0.2.0/MixacLib-tumuidle-0.2.0.tar.gz

---
+++ packages/MixacLib-tumuidle/MixacLib-tumuidle-0.2.0.tar.gz/MixacLib-tumuidle-0.2.0/src/MixacLib/example.py
@@ -0,0 +1,59 @@
+import console.utils
+from colorama import Fore
+from console import *
+from arcade import *
+
+class color():
+    def inf(information):
+        green = Fore.GREEN
+        reset = Fore.RESET
+        print('[' + green + '信息' +reset + ']' + information)
+        pass
+
+    def debug(debuginf):
+        cyan = Fore.CYAN
+        reset = Fore.RESET
+        print('[' + cyan + '调试' + reset + ']' + debuginf)
+        pass
+
+    def warn(warning):
+        yellow = Fore.YELLOW
+        reset = Fore.RESET
+        print('[' + yellow + '警告' + reset + ']' + warning)
+        pass
+
+    def error(errorinf):
+        red = Fore.RED
+        reset = Fore.RESET
+        print('[' + red + '错误' + reset + ']' + errorinf)
+        pass
+
+    def title(titlestr):
+        magenta = Fore.MAGENTA
+        reset = Fore.RESET
+        print('[' + magenta + reset + ']' + titlestr)
+
+    pass
+
+class cons():
+    def title(name):
+        console.utils.set_title(name)
+        pass
+    pass
+
+class arcade_mixed():
+    def render():
+        start_render()
+        pass
+
+    def set_bc(color):
+        set_background_color(color)
+        pass
+
+    def d_circle_o(x,y,radius,color,b_width=1,ta=0,ns=-1):
+        draw_circle_outline(x,y,radius,color,b_width,ta,ns)
+    pass
+
+if __name__ == '__main__':
+    color.inf('test')
+    pass
