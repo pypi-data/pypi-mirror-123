2c11904ae767cd7794dad96cbea444bccc86de2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:47 +0000

    Add package prototxt-parser/1.0/prototxt_parser-1.0.tar.gz

---
+++ packages/prototxt-parser/prototxt_parser-1.0.tar.gz/prototxt_parser-1.0/README.md
@@ -0,0 +1,26 @@
+# prototxt-parser
+
+prototxt-parser allows to parse `*.prototxt` files to python dict objects
+
+## Usage:
+
+From command line from this repo:
+
+```commandline
+$ python3 prototxt_parser_main.py prototxt_file
+```
+
+Alternatively can use pip as well to get the package:
+
+```commandline
+pip3 install prototxt-parser
+```
+
+To run, from python:
+
+```python
+input_string = ... # the prototxt string to be parsed e.g., open("some_prototxt_file").read()
+from prototxt_parser.prototxt import parse
+parsed_dict = parse(input_string)
+```
+
---
+++ packages/prototxt-parser/prototxt_parser-1.0.tar.gz/prototxt_parser-1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="prototxt_parser",
+    version="1.0",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['parsy'],
+
+    package_data={},
+
+    # metadata for upload to PyPI
+    author="Yogin Patel",
+    author_email="yogin.daiict@gmail.com",
+    description="prototxt-parser allows to parse *.prototxt files to python dict objects",
+
+    license="MIT",
+    keywords="prototxt, dict",
+    url="https://github.com/yogin16/prototxt_parser",  # project home page
+
+    # could also include long_description, download_url, classifiers, etc.
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
