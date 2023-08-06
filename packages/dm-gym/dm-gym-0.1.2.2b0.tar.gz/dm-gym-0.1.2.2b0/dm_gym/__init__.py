f8b6671a25dac5d9ad1b1ebc2e1929fd5c896c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:03 +0000

    Add package dm-gym/0.1.2.2b0/dm-gym-0.1.2.2b0.tar.gz

---
+++ packages/dm-gym/dm-gym-0.1.2.2b0.tar.gz/dm-gym-0.1.2.2b0/dm_gym/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(id='clustering-v0', entry_point='dm_gym.envs:ClusteringEnv_0',)
+register(id='clustering-v1', entry_point='dm_gym.envs:ClusteringEnv_1',)
+
+#register(id='basic-v2', entry_point='gym_basic.envs:BasicEnv2',)
