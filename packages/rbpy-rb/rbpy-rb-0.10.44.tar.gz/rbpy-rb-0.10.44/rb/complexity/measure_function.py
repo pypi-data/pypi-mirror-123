5a1fdcd5b891dd52e8e38c703246dfcc56dd8802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:13 +0000

    Add package rbpy-rb/0.10.44/rbpy-rb-0.10.44.tar.gz

---
+++ packages/rbpy-rb/rbpy-rb-0.10.44.tar.gz/rbpy-rb-0.10.44/rb/complexity/measure_function.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from functools import partial
+from statistics import mean, stdev
+from typing import List
+
+def average(elements: List[float]) -> float:
+    if len(elements) == 0:
+        return 0
+    return mean(elements)
+
+def standard_deviation(elements: List[float]) -> float:
+    if len(elements) == 0:
+        return 0
+    elif len(elements) == 1:
+        return 0
+    return stdev(elements)
+
+def maximum(elements: List[float]) -> float:
+    if len(elements) == 0:
+        return 0
+    return max(elements)
+
+
+class MeasureFunction(Enum):
+    AVG = average
+    STDEV = standard_deviation
+    MAX = maximum
