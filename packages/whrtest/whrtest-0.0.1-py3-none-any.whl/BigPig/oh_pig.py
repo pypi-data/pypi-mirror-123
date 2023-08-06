249f61ec7b1d46a31ce4f20e595bed1a91db3d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:32 +0000

    Add package whrtest/0.0.1/whrtest-0.0.1-py3-none-any.whl

---
+++ packages/whrtest/whrtest-0.0.1-py3-none-any.whl/BigPig/oh_pig.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/10/12 5:44 下午
+# @Author : A Big Boby
+# @File : oh_pig.py 
+# @Software: PyCharm
+
+def output():
+    print('今天又是从满希望的一天！')
