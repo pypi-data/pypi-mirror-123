06674cff005de3e51714fa018fe88f956607af93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:22 +0000

    Add package drb-impl-netcdf/1.0a2/drb_impl_netcdf-1.0a2-py3-none-any.whl

---
+++ packages/drb-impl-netcdf/drb_impl_netcdf-1.0a2-py3-none-any.whl/drb_impl_netcdf/netcdf_list_node.py
@@ -0,0 +1,32 @@
+from typing import Any, List, Dict, Optional, Tuple
+
+import drb
+
+from drb import DrbNode
+from drb.exceptions import DrbNotImplementationException
+from drb.utils.logical_node import DrbLogicalNode
+
+from drb_impl_netcdf.netcdf_common import DrbNetcdfAbstractNode
+
+
+class DrbNetcdfListNode(DrbNetcdfAbstractNode):
+
+    def __init__(self, parent: DrbNode, name: str):
+        path = parent.path / name
+        DrbLogicalNode.__init__(self, path, parent=parent)
+        self._children: List[DrbNode] = []
+
+    @property
+    def attributes(self) -> Dict[Tuple[str, str], Any]:
+        return {}
+
+    @property
+    @drb.resolve_children
+    def children(self) -> List[DrbNode]:
+        return self._children
+
+    def has_impl(self, impl: type) -> bool:
+        return False
+
+    def get_impl(self, impl: type) -> Any:
+        raise DrbNotImplementationException(f'no {impl} implementation found')
---
+++ packages/drb-impl-netcdf/drb_impl_netcdf-1.0a2-py3-none-any.whl/drb_impl_netcdf/netcdf_node_factory.py
@@ -0,0 +1,54 @@
+import io
+from typing import Any, List
+
+import netCDF4
+
+from drb import DrbNode
+from drb.factory import DrbFactory
+from drb.utils.logical_node import DrbLogicalNode
+
+from drb_impl_netcdf.execptions import DrbNetcdfNodeException
+from drb_impl_netcdf.netcdf_group_node import DrbNetcdfGroupNode
+
+
+class DrbNetcdfNode(DrbLogicalNode):
+
+    def __init__(self, base_node: DrbNode):
+        self._netcdf_file_source = None
+        self._root_dataset = None
+        DrbLogicalNode.__init__(self, base_node)
+
+        if self._wrapped_node.has_impl(io.BufferedIOBase):
+            self._netcdf_file_source = self._wrapped_node \
+                         .get_impl(io.BufferedIOBase)
+            self._root_dataset = netCDF4.Dataset(filename=self
+                                                 ._netcdf_file_source.name)
+            self.root_node = DrbNetcdfGroupNode(self, self._root_dataset)
+
+        else:
+            raise DrbNetcdfNodeException(f'Unsupported parent '
+                                         f'{type(self.parent).__name__} '
+                                         f'for DrbNetcdfRootNode')
+
+    @property
+    def children(self) -> List[DrbNode]:
+        return [self.root_node]
+
+    def has_impl(self, impl: type) -> bool:
+        return self._wrapped_node.has_impl(impl)
+
+    def get_impl(self, impl: type) -> Any:
+        return self._wrapped_node.get_impl(impl)
+
+    def close(self):
+        if self._root_dataset is not None:
+            self._root_dataset.close()
+        if self._netcdf_file_source is not None:
+            self._netcdf_file_source.close()
+        self._wrapped_node.close()
+
+
+class DrbNetcdfFactory(DrbFactory):
+
+    def _create(self, node: DrbNode) -> DrbNode:
+        return DrbNetcdfNode(base_node=node)
