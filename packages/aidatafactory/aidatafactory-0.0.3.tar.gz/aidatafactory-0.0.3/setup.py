bf4e8dc13400b6739ee4c3b50b81e61459b300e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:01 +0000

    Add package aidatafactory/0.0.3/aidatafactory-0.0.3.tar.gz

---
+++ packages/aidatafactory/aidatafactory-0.0.3.tar.gz/aidatafactory-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Data factory for producing synthetic data.'
+LONG_DESCRIPTION = 'Variantional Autoencoder used as a generative model for generating synthetic data.'
+
+# Setting up
+setup(
+    name="aidatafactory",
+    version=VERSION,
+    author="Ngo Cuong",
+    author_email="ngo.cuong.work@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'seaborn', 'tensorflow', \
+        'tensorflow_probability', 'matplotlib', 'scipy'],
+    keywords=['python', 'synthetic', 'data'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
