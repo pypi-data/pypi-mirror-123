36811676d6f990c22c7be932b258ff0a70239657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:24 +0000

    Add package tramway/0.6.3/tramway-0.6.3-py3-none-any.whl

---
+++ packages/tramway/tramway-0.6.3-py3-none-any.whl/tramway/analyzer/time/allsymbols.py
@@ -0,0 +1,7 @@
+
+from . import *
+from ..attribute import initializer_method
+
+from_sliding_window = initializer_method( TimeInitializer.from_sliding_window )
+from_sampling       = initializer_method( TimeInitializer.from_sampling       )
+
