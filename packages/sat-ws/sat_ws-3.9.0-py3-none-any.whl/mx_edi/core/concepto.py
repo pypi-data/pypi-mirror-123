ff7a54e266ccf677d48ca73d8217150d0db53e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package sat-ws/3.9.0/sat_ws-3.9.0-py3-none-any.whl

---
+++ packages/sat-ws/sat_ws-3.9.0-py3-none-any.whl/mx_edi/core/concepto.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Concepto:
+    Descripcion: str
+    Cantidad: float
+    ValorUnitario: float
+    Importe: float
+    Impuestos: dict[str, float]
