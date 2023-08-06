92bf6d3e9bc2704c8357eddb8f5ce9b7612ee946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:25 +0000

    Add package insolver/0.4.15/insolver-0.4.15-py3-none-any.whl

---
+++ packages/insolver/insolver-0.4.15-py3-none-any.whl/insolver/model_tools/model_comparison.py
@@ -0,0 +1,142 @@
+import os
+import traceback
+from glob import glob
+
+from numpy import min, max, mean, var, std, quantile, median
+from pandas import DataFrame
+
+from insolver.wrappers import InsolverGLMWrapper, InsolverGBMWrapper, InsolverRFWrapper, InsolverTrivialWrapper
+
+
+class ModelMetricsCompare:
+    """Class for model comparison.
+
+    Parameters:
+        X (:obj:`pd.DataFrame`, :obj:`pd.Series`): Data for making predictions.
+        y (:obj:`pd.DataFrame`, :obj:`pd.Series`): Actual target values for X.
+        source (:obj:`str`, :obj:`list`, :obj:`tuple`, :obj:`None`): List or tuple of insolver wrappers or path to the
+        metrics (:obj:`list`, :obj:`tuple`, :obj:`callable`, optional): Metrics or list of metrics to compute.
+        stats (:obj:`list`, :obj:`tuple`, :obj:`callable`, optional): Statistics or list of statistics to compute.
+        folder with models. If `None`, taking current working directory as source.
+        h2o_init_params (:obj:`dict`, optional): Parameters passed to `h2o.init()`, when `backend` == 'h2o'.
+        predict_params (:obj:`list`, optional): List of dictionaries containing parameters passed to predict methods
+         for each model.
+        features (:obj:`list`, optional): List of lists containing features for predict method for each model.
+        names (:obj:`list`, optional): List of model names.
+    """
+
+    def __init__(self, X, y, source=None, metrics=None, stats=None, h2o_init_params=None, predict_params=None,
+                 features=None, names=None):
+        assert True if names is None else len(names) == len(source), 'Check length of list containing model names.'
+        wrappers = {'glm': InsolverGLMWrapper, 'gbm': InsolverGBMWrapper, 'rf': InsolverRFWrapper}
+        self.stats, self.metrics = None, None
+        if (source is None) or isinstance(source, str):
+            source = os.getcwd() if source is None else source
+            files = glob(os.path.join(source, '*'))
+            files = [file for file in files if os.path.basename(file).split('_')[0] == 'insolver']
+            if files:
+                model_list = []
+                for file in files:
+                    algo, backend = os.path.basename(file).split('_')[1:3]
+                    model_list.append(wrappers[algo](backend=backend, load_path=file) if backend != 'h2o' else
+                                      wrappers[algo](backend=backend, load_path=file, h2o_init_params=h2o_init_params))
+                self.models = model_list
+            else:
+                raise Exception('No models with appropriate name format found.')
+        elif isinstance(source, (list, tuple)):
+            self.models = source
+        else:
+            raise TypeError(f'Source of type {type(source)} is not supported.')
+
+        self._calc_metrics(X=X, y=y, metrics=metrics, stats=stats, predict_params=predict_params, features=features,
+                           names=names)
+
+    def __repr__(self):
+        stk = traceback.extract_stack()
+        if not ('IPython' in stk[-2][0] and 'info' == stk[-2][2]):
+            import IPython.display
+            print('Model comparison statistics:')
+            IPython.display.display(self.stats)
+            print('\nModels comparison metrics:')
+            IPython.display.display(self.metrics)
+        else:
+            print('Model comparison statistics:')
+            print(self.stats)
+            print('\nModels comparison metrics:')
+            print(self.metrics)
+        return ''
+
+    def _calc_metrics(self, X, y, metrics=None, stats=None, predict_params=None, features=None, names=None):
+        """Computing metrics and statistics for models.
+
+        Args:
+            X (:obj:`pd.DataFrame`, :obj:`pd.Series`): Data for making predictions.
+            y (:obj:`pd.DataFrame`, :obj:`pd.Series`): Actual target values for X.
+            metrics (:obj:`list`, :obj:`tuple`, :obj:`callable`, optional): Metrics or list of metrics to compute.
+            stats (:obj:`list`, :obj:`tuple`, :obj:`callable`, optional): Statistics or list of statistics to compute.
+            predict_params (:obj:`list`, optional): List of dictionaries containing parameters passed to predict methods
+         for each model.
+            features (:obj:`list`, optional): List of lists containing features for predict method for each model.
+            names (:obj:`list`, optional): List of model names.
+
+        Returns:
+            Returns `None`, but results available in `self.stats`, `self.metrics`.
+        """
+        stats_df, model_metrics, algos, backend = DataFrame(), DataFrame(), [], []
+        trivial = InsolverTrivialWrapper(task='reg', agg=lambda x: x)
+        trivial.fit(X, y)
+        models = [trivial] + self.models
+        features = [None] + features if features is not None else None
+        for model in models:
+            algos.append(model.algo.upper()) if hasattr(model, 'algo') else algos.append('-')
+            backend.append(model.backend.capitalize()) if hasattr(model, 'backend') else backend.append(
+                model.__class__.__name__)
+            p = model.predict(X if (features is None) or (features[models.index(model)] is None)
+                              else X[features[models.index(model)]],
+                              **({} if (predict_params is None) or (predict_params[models.index(model)] is None)
+                                 else predict_params[models.index(model)]))
+            stats_val = [mean(p), var(p), std(p), min(p), quantile(p, 0.25), median(p), quantile(p, 0.75), max(p)]
+            name_stats = ['Mean', 'Variance', 'St. Dev.', 'Min', 'Q1', 'Median', 'Q3', 'Max']
+            if stats is not None:
+                if isinstance(stats, (list, tuple)):
+                    for stat in stats:
+                        if callable(stat):
+                            stats_val.append(stat(p))
+                            name_stats.append(stat.__name__.replace('_', ' '))
+                        else:
+                            raise TypeError(f'Statistics with type {type(stat)} are not supported.')
+                elif callable(stats):
+                    stats_val.append(stats(p))
+                    name_stats.append(stats.__name__.replace('_', ' '))
+                else:
+                    raise TypeError(f'Statistics with type {type(stats)} are not supported.')
+            stats_df = stats_df.append(DataFrame([stats_val], columns=name_stats))
+
+            if (metrics is not None) and not models.index(model) == 0:
+                if isinstance(metrics, (list, tuple)):
+                    m_metrics = []
+                    for metric in metrics:
+                        if callable(metric):
+                            m_metrics.append(metric(y, p))
+                        else:
+                            raise TypeError(f'Metrics with type {type(metric)} are not supported.')
+                    metrics_names = [m.__name__.replace('_', ' ') for m in metrics]
+                    model_metrics = model_metrics.append(DataFrame(dict(zip(metrics_names, m_metrics), index=[0])))
+                elif callable(metrics):
+                    name = [metrics.__name__.replace('_', ' ')]
+                    model_metrics = model_metrics.append(DataFrame(dict(zip(name, [metrics(y, p)])), index=[0]))
+                else:
+                    raise TypeError(f'Metrics with type {type(metrics)} are not supported.')
+                model_metrics = model_metrics.reset_index(drop=True)
+                model_metrics = (model_metrics if 'index' not in model_metrics.columns
+                                 else model_metrics.drop(['index'], axis=1))
+
+        model_metrics.index = (list(range(len(model_metrics))) if names is None else names)
+        stats_df.index = ['Actual'] + model_metrics.index.tolist()
+        stats_df[['Algo', 'Backend']] = DataFrame({'Algo': ['-'] + algos[1:], 'Backend': ['-'] + backend[1:]},
+                                                  index=stats_df.index)
+        model_metrics[['Algo', 'Backend']] = DataFrame({'Algo': algos[1:], 'Backend': backend[1:]},
+                                                       index=model_metrics.index)
+        stats_df = stats_df[list(stats_df.columns[-2:]) + list(stats_df.columns[:-2])]
+        model_metrics = model_metrics[list(model_metrics.columns[-2:]) + list(model_metrics.columns[:-2])]
+        self.stats, self.metrics = stats_df, model_metrics
---
+++ packages/insolver/insolver-0.4.15-py3-none-any.whl/insolver/wrappers/base.py
@@ -0,0 +1,150 @@
+import os
+import copy
+import time
+import pickle
+import joblib
+from numpy import array, mean, broadcast_to, where
+from pandas import concat, merge
+
+
+class InsolverBaseWrapper:
+    def __init__(self, backend):
+        """Base wrapper serving as a building block for other wrappers.
+
+        Parameters:
+            backend (str): Name of the backend to built the model.
+        """
+        self.algo, self.backend, self._backends = None, backend, None
+        self._back_load_dict, self._back_save_dict = None, None
+        self.meta, self.object, self.model = None, None, None
+        self.best_params, self.trials = None, None
+
+    def __call__(self):
+        return self.model
+
+    @staticmethod
+    def get_init_args(vars_):
+        c_vars = copy.deepcopy(vars_)
+        for key in ['__class__', 'self']:
+            del c_vars[key]
+        return c_vars
+
+    def load_model(self, load_path):
+        """Loading a model to the wrapper.
+
+        Args:
+            load_path (:obj:`str`): Path to the model that will be loaded to wrapper.
+        """
+        load_path = os.path.normpath(load_path)
+        if self.backend in self._back_load_dict.keys():
+            self._back_load_dict[self.backend](load_path)
+        else:
+            raise NotImplementedError(f'Error with the backend choice. Supported backends: {self._backends}')
+
+    def save_model(self, path=None, name=None, suffix=None, **kwargs):
+        """Saving the model contained in wrapper.
+
+        Args:
+            path (:obj:`str`, optional): Path to save the model. Using current working directory by default.
+            name (:obj:`str`, optional): Optional, name of the model.
+            suffix (:obj:`str`, optional): Optional, suffix in the name of the model.
+            **kwargs: Other parameters passed to, e.g. h2o.save_model().
+        """
+        path = os.getcwd() if path is None else os.path.normpath(path)
+        def_name = f"insolver_{self.algo}_{self.backend}_{round(time.time() * 1000)}"
+        name = name if name is not None else def_name
+        name = name if suffix is None else f'{name}_{suffix}'
+
+        self.model.insolver_meta = self.meta
+        self.model.algo = self.algo
+        self.model.backend = self.backend
+
+        if self.backend in self._back_save_dict.keys():
+            self._back_save_dict[self.backend](path, name, **kwargs)
+        else:
+            raise NotImplementedError(f'Error with the backend choice. Supported backends: {self._backends}')
+
+    def _pickle_load(self, load_path):
+        with open(load_path, 'rb') as _model:
+            self.model = pickle.load(_model)
+
+    def _pickle_save(self, path, name):
+        with open(os.path.join(path, f'{name}.pickle'), 'wb') as _model:
+            pickle.dump(self.model, _model, pickle.HIGHEST_PROTOCOL)
+
+    def _joblib_load(self, load_path):
+        self.model = joblib.load(load_path)
+
+    def _joblib_save(self, path, name):
+        joblib.dump(self.model, os.path.join(path, f'{name}.joblib'))
+
+    def _update_meta(self):
+        self.meta = self.__dict__.copy()
+        for key in ['_backends', '_back_load_dict', '_back_save_dict', 'object', 'model', 'meta']:
+            self.meta.pop(key)
+
+
+class InsolverTrivialWrapper(InsolverBaseWrapper):
+    """Dummy wrapper for returning trivial "predictions" for metric comparison and statistics.
+
+    Parameters:
+        col_name (:obj:`str` or :obj:`list`, optional): String or list of strings containing column name(s) to perform
+         groupby operation.
+        agg (:obj:`callable`, optional): Aggregation function.
+        thresh (:obj:`float`, optional): Threshold for continuous prediction in dummy classification.
+        **kwargs: Other arguments.
+    """
+    def __init__(self, task=None, col_name=None, agg=None, thresh=0.5, **kwargs):
+        super(InsolverTrivialWrapper, self).__init__(backend='trivial')
+        self._tasks = ['class', 'reg']
+        self.init_args = self.get_init_args(vars())
+        self._backends, self.x_train, self.y_train = ['trivial'], None, None
+        self._back_load_dict, self._back_save_dict = {'trivial': self._pickle_load}, {'trivial': self._pickle_save}
+        if task in self._tasks:
+            self.task = task
+            self.thresh = thresh if task == 'class' else None
+        else:
+            raise NotImplementedError(f'Task parameter supports values in {self._tasks}.')
+
+        if (isinstance(col_name, (str, list, tuple)) or col_name is None or
+                (isinstance(col_name, (list, tuple)) and all([isinstance(element, str) for element in col_name]))):
+            self.col_name = col_name
+        else:
+            raise TypeError(f'Column of type {type(self.col_name)} is not supported.')
+        self.fitted, self.agg, self.kwargs = None, agg, kwargs
+        self.agg = mean if self.agg is None else self.agg
+
+        if self.col_name is None:
+            self.algo = self.agg.__name__.replace('_', ' ')
+        else:
+            self.algo = f"{self.agg.__name__} target: {self.col_name}"
+
+    def fit(self, X, y):
+        """Fitting dummy model.
+
+        Args:
+            X (:obj:`pd.DataFrame`): Data.
+            y (:obj:`pd.Series`): Target values.
+        """
+        self.x_train, self.y_train = X, y
+        if self.col_name is None:
+            self.fitted = self.agg(self.y_train)
+        else:
+            _df = concat([self.y_train, self.x_train[self.col_name]], axis=1)
+            self.fitted = _df.groupby(self.col_name).aggregate(self.agg).reset_index()
+
+    def predict(self, X):
+        """Making dummy predictions.
+
+        Args:
+            X (:obj:`pd.DataFrame` or :obj:`pd.Series`): Data.
+
+        Returns:
+            array: Trivial model "prediction".
+        """
+        if self.col_name is None:
+            output = broadcast_to(self.fitted, X.shape[0])
+        else:
+            output = merge(X[self.col_name], self.fitted, how='left', on=self.col_name)[self.y_train.name].fillna(
+                self.agg(self.y_train))
+        return array(output) if self.task != 'class' else where(array(output) >= self.thresh, 1, 0)
