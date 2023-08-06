5a2ca39e509f0862c14b9d04854098937aedd53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:43 +0000

    Add package pychron-cm/0.1.1/pychron-cm-0.1.1.tar.gz

---
+++ packages/pychron-cm/pychron-cm-0.1.1.tar.gz/pychron-cm-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# ===============================================================================
+# Copyright 2021 ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pychron-cm',
+    version='0.1.1',
+    py_modules=['cli'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cli = pcm.cli:cli',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=['pcm'],
+    python_requires=">=3.6",
+)
+# ============= EOF =============================================
