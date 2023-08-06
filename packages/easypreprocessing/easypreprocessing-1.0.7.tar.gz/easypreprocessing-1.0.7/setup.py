03db23924331c8dba91d3ddad8f9f54849f6fefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:17 +0000

    Add package easypreprocessing/1.0.7/easypreprocessing-1.0.7.tar.gz

---
+++ packages/easypreprocessing/easypreprocessing-1.0.7.tar.gz/easypreprocessing-1.0.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Developers',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='easypreprocessing',
+  version='1.0.7',
+  description='An easy to use pre-processing utility for machine learning.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='',  
+  author='Shreyas Kudav',
+  author_email='shreyaskudav@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='machine learning', 
+  packages=find_packages(),
+  install_requires=['numpy ','pandas','seaborn ','sklearn','kneed','imbalanced-learn'],
+  py_modules=['easypreprocessing'],
+  scripts=['easypreprocessing.py'],
+  entry_points={
+      'console_scripts': ['easypreprocessing=easypreprocessing:main']
+  }
+)
