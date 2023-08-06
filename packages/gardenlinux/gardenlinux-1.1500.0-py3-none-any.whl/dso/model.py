82b00995ee39d5f6e0358050f5219f0a01065f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:05 +0000

    Add package gardenlinux/1.1500.0/gardenlinux-1.1500.0-py3-none-any.whl

---
+++ packages/gardenlinux/gardenlinux-1.1500.0-py3-none-any.whl/dso/model.py
@@ -0,0 +1,50 @@
+import dataclasses
+import datetime
+from enum import Enum
+import typing
+
+import gci.componentmodel as cm
+
+import dso.labels
+
+
+# abstraction of component model v2 source and resource
+@dataclasses.dataclass
+class ScanArtifact:
+    name: str
+    access: typing.Union[
+        cm.OciAccess,
+        cm.GithubAccess,
+        cm.HttpAccess,
+        cm.ResourceAccess,
+    ]
+    label: dso.labels.ScanningHint
+
+
+class Datasource(Enum):
+    WHITESOURCE = 'whitesource'
+    PROTECODE = 'protecode'
+    CHECKMARX = 'checkmarx'
+    CLAMAV = 'clamav'
+
+
+@dataclasses.dataclass
+class ComplianceIssueId:
+    componentName: str
+    componentVersion: str
+    ctxRepo: str
+    artifact: cm.Artifact
+
+
+@dataclasses.dataclass
+class ComplianceIssueMeta:
+    datasource: Datasource
+    creationDate: datetime.datetime
+    uuid: str
+
+
+@dataclasses.dataclass
+class ComplianceIssue:
+    id: ComplianceIssueId
+    meta: ComplianceIssueMeta
+    data: dict
