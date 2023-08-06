fbb5ca5a5b9621c09dcaa4f29bd9b61062de85f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:48 +0000

    Add package biobox/1.0.1/biobox-1.0.1.tar.gz

---
+++ packages/biobox/biobox-1.0.1.tar.gz/biobox-1.0.1/README.md
@@ -0,0 +1,5 @@
+Biobox provides a collection of data structures and methods for loading, manipulating and analyzing atomistic and pseudoatomistic structures.
+
+See more at https://github.com/degiacom/biobox
+With the documentation at https://degiacom.github.io/biobox/
+And a tutorial at https://github.com/degiacom/biobox_notebook
---
+++ packages/biobox/biobox-1.0.1.tar.gz/biobox-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension, find_packages
+from setuptools.command.install import install
+import subprocess
+import os
+
+extensions = [Extension("*", ["./biobox/lib/*.pyx"])]
+from Cython.Build import cythonize
+extensions = cythonize(extensions)
+
+setup(
+  name = 'biobox',
+  packages = ['biobox',
+              'biobox.classes',
+              'biobox.lib',
+              'biobox.measures',
+              'biobox.test'
+      ],
+  version = '1.0.1',
+  license='GPL3',
+  description = 'Biobox provides a collection of data structures and methods for loading, manipulating and analyzing atomistic and pseudoatomistic structures.',
+  author = 'Matteo T. Degiacomi, Lucas S.P. Rudden, Samuel C. Musson',
+  author_email = 'lucas.s.p.rudden@gmail.com',
+  url = 'https://github.com/degiacom/biobox',
+  download_url = 'https://github.com/degiacom/biobox/archive/v1.0.0.tar.gz',
+  keywords = ['biobox'],
+  install_requires=[
+          'numpy',
+          'scipy',
+          'scikit-learn',
+          'pandas'
+      ],
+  package_data={'biobox' : ['./lib/*.pyx', './classes/*.sh', './classes/*.dat, ./*.cfg', './test/*.pdb', './test/*.mrc']},
+  ext_modules = extensions
+)
