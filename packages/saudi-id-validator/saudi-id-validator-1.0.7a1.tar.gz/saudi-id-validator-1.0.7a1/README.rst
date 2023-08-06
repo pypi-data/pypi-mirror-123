30bb7d3cb833e86537f66ab714e26b7b3ab53c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:21 +0000

    Add package saudi-id-validator/1.0.7a1/saudi-id-validator-1.0.7a1.tar.gz

---
+++ packages/saudi-id-validator/saudi-id-validator-1.0.7a1.tar.gz/saudi-id-validator-1.0.7a1/README.rst
@@ -0,0 +1,12 @@
+Saudi ID Validator
+==================
+
+This package has been **deprecated** and should be replaced by
+`luhn-validator <https://pypi.org/project/luhn-validator/>`_.
+
+To get the same result with luhn-validator, see this example:
+
+.. code:: python
+
+    from luhn_validator import validate
+    validate(10717243691, 10, [1, 2])
---
+++ packages/saudi-id-validator/saudi-id-validator-1.0.7a1.tar.gz/saudi-id-validator-1.0.7a1/setup.py
@@ -0,0 +1,24 @@
+import pathlib
+
+from setuptools import setup
+
+here = pathlib.Path(__file__).parent
+readme = (here / "README.rst").read_text(encoding="utf-8")
+
+setup(
+    name="saudi-id-validator",
+    version="1.0.7.a1",
+    description="Validate ID numbers of Saudi Arabian identity cards",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    url="",
+    author="Mohd Alshehri",
+    author_email="",
+    license="MIT",
+    classifiers=["Development Status :: 7 - Inactive"],
+    keywords="saudi government validator identity number",
+    py_modules=[module.stem for module in here.glob("src/*.py")],
+    package_dir={"": "src"},
+    include_package_data=True,
+    python_requires=">=3.5",
+)
