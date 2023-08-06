a8209f261f36ac28010f6c37ecc488be3e33b918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:21 +0000

    Add package downloadpdf/1.0.8/downloadpdf-1.0.8.tar.gz

---
+++ packages/downloadpdf/downloadpdf-1.0.8.tar.gz/downloadpdf-1.0.8/util/url.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import re
+
+
+def validateURL(url):
+    regex = re.compile(
+        r'^https?://'  # http:// or https://
+        # domain...
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    if (regex.search(url) is None or url is None):
+        raise Exception('Invalid URL!')
