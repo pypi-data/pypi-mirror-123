b13312f3fe8211862630f6a2008cae9e35b80ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:02 +0000

    Add package django-international-sites/0.1.4/django-international-sites-0.1.4.tar.gz

---
+++ packages/django-international-sites/django-international-sites-0.1.4.tar.gz/django-international-sites-0.1.4/international/localize.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.contrib.gis.geoip2 import GeoIP2
+from django.conf import settings 
+
+def visitor_ip_address(request):
+
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
+
+
+def get_country_from_ip(request):
+
+	if not getattr(settings, "GEOIP_PATH", False):
+		return None
+
+	IP = visitor_ip_address(request)
+
+	# Example
+	# IP = "143.177.174.48"
+	
+	g = GeoIP2()
+
+	try:
+		country = g.country(IP)
+	except:
+		return None
+    
+	if country.get("country_code"):
+		return country["country_code"]
