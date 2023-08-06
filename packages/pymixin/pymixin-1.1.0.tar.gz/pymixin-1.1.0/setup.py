6216c6958bd74f3c67ead7a51af949f1a40ea848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:13 +0000

    Add package pymixin/1.1.0/pymixin-1.1.0.tar.gz

---
+++ packages/pymixin/pymixin-1.1.0.tar.gz/pymixin-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pymixin",
+    version="1.1.0",
+    author="Martmists",
+    author_email="martmists@gmail.com",
+    description="A high-level library for manipulating Python Bytecode in an easy way.",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Code Generators",
+        "License :: OSI Approved :: MIT License"
+    ],
+    packages=["mixin"],
+    requires=["pyasm"],
+    python_requires=">=3.5",
+)
