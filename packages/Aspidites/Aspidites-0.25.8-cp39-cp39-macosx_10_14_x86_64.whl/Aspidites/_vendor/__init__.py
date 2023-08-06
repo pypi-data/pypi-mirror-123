0f3c45aa258efdc7322185ecb6c22415f5410fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:28 +0000

    Add package Aspidites/0.25.8/Aspidites-0.25.8-cp39-cp39-macosx_10_14_x86_64.whl

---
+++ packages/Aspidites/Aspidites-0.25.8-cp39-cp39-macosx_10_14_x86_64.whl/Aspidites/_vendor/__init__.py
@@ -0,0 +1,43 @@
+from pyrsistent import v
+from .fn import *
+
+__all__ = v("F",  # BEGIN func.py
+            "curried",  # END func.py
+            "take",  # BEGIN iters.py
+            "drop",
+            "takelast",
+            "droplast",
+            "consume",
+            "nth",
+            "first_true",
+            "iterate",
+            "padnone",
+            "ncycles",
+            "repeatfunc",
+            "grouper",
+            "group_by",
+            "roundrobin",
+            "partition",
+            "splitat",
+            "splitby",
+            "powerset",
+            "pairwise",
+            "iter_suppress",
+            "flatten",
+            "accumulate",  # END iters.py
+            "call",  # BEGIN op.py
+            "apply",
+            "flip",
+            "curry",
+            "zipwith",
+            "foldl",
+            "foldr",
+            "unfold",  # END op.py
+            "tco",
+            "stackless",
+            "Stream",
+            "_",
+            "reduce",
+            "filterfalse",
+            "zip_longest"
+            )
