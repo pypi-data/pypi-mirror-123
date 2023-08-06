a0d749a49c1aa3cf01fb1b12f8caf6556ce70cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:23 +0000

    Add package transform/0.0.48/transform-0.0.48.tar.gz

---
+++ packages/transform/transform-0.0.48.tar.gz/transform-0.0.48/transform/constants.py
@@ -0,0 +1,7 @@
+DEFAULT_QUERY_TIMEOUT = 180
+
+API_KEY_CONFIG_KEY = "api_key"
+BEARER_TOKEN_CONFIG_KEY = "bearer_token"
+BEARER_TOKEN_EXPIRES_AT_CONFIG_KEY = "bearer_token_expires_at"
+MQL_OVERRIDE_CONFIG_KEY = "mql_server_url_override"
+PINNED_MODEL_ID_CONFIG_KEY = "pinned_model_id"
