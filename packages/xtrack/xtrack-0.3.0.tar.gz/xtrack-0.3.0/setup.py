17c95c633c7b53e492690c5cf9c74c274b5089cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:10 +0000

    Add package xtrack/0.3.0/xtrack-0.3.0.tar.gz

---
+++ packages/xtrack/xtrack-0.3.0.tar.gz/xtrack-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages, Extension
+
+#######################################
+# Prepare list of compiled extensions #
+#######################################
+
+extensions = []
+
+
+#########
+# Setup #
+#########
+
+setup(
+    name='xtrack',
+    version='0.3.0',
+    description='Tracking library for particle accelerators',
+    url='https://github.com/xsuite/xtrack',
+    author='Riccard De Maria, Giovanni Iadarola',
+    packages=find_packages(),
+    ext_modules = extensions,
+    install_requires=[
+        'numpy>=1.0',
+        'xobjects>=0.0.8',
+        'xline'
+        ]
+    )
