b73f8cc4cb615f3fbdedfc3dc3aed56bc2f31767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:25 +0000

    Add package py-configuration-Orinnass/2.0.2/py_configuration_Orinnass-2.0.2-py3-none-any.whl

---
+++ packages/py-configuration-Orinnass/py_configuration_Orinnass-2.0.2-py3-none-any.whl/config/__init__.py
@@ -0,0 +1,8 @@
+"""
+Модуль с описанием класса Configuration
+"""
+# from .handlers import DBHandler
+from .configuration import Configuration
+
+__version__ = '2.0.2'
+__name__ = 'py-configuration-Orinnass'
