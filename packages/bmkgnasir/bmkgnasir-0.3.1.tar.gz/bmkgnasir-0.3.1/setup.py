e2ea5ac570a7007c93dfcafed46271b43995274f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:45 +0000

    Add package bmkgnasir/0.3.1/bmkgnasir-0.3.1.tar.gz

---
+++ packages/bmkgnasir/bmkgnasir-0.3.1.tar.gz/bmkgnasir-0.3.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bmkgnasir",
+    version="0.3.1",
+    author="Nasir Nooruddin",
+    author_email="nasir@nasir.id",
+    description="retrieve latest earthquake information in Indonesia",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nasirnooruddin/latest-indonesia-earthquake",
+    project_urls={
+        "Website": "http://nasir.id",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    #package_dir={"": "src"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
