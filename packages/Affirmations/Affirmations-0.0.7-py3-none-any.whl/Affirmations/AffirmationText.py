192200c4b8b9d78249cdc7be873fab79a161e367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package Affirmations/0.0.7/Affirmations-0.0.7-py3-none-any.whl

---
+++ packages/Affirmations/Affirmations-0.0.7-py3-none-any.whl/Affirmations/AffirmationText.py
@@ -0,0 +1,33 @@
+
+affirmations = [
+    "You're Great!",
+    "Way to Go!",
+    "Woo hoo!",
+    "You got out of bed this morning!",
+    "Keep going!",
+    "What nimble fingers you have!",
+    "Awesome!",
+    "Did you ever know that you're my hero?",
+    "Lookin' good.",
+    "You're on your way to glory",
+    "You can do it!",
+    "Coding is hard! And you did great!",
+    "''You miss 100\% of the shots you don't take' - Wayne Gretzky' - Michael Scott (And you took your shot! Way to go!)",
+    "You rock my socks off",
+    "You're too cool for school!",
+    "Daaammmmnnnn, that's some sweet code",
+    "Five! Six! Seven! Eight! What coder does this code appreciate!? You! You! Gooooo you!",
+    "I think you're neat!",
+    "High five!!!",
+    "If you were a president, you'd be Code-braham Lincoln",
+    "Great job! Go get a coffee",
+    "\U0001F44D\U0001F44D\U0001F44D\U0001F44D\U0001F44D\U0001F44D",
+    "If I had hands, you'd be getting a high five",
+    "If I could rearrange the alphabet I would put 'U' 'R' 'GR8' together. Then I would learn what an alphabet is.",
+    "\U0001F525\U0001F525\U0001F525 Your Code \U0001F525\U0001F525\U0001F525",
+    "Woo hoo! Time to celebrate!"
+]
+
+
+if __name__ == "__main__":
+    print(affirmations[-1])
