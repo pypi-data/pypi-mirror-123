29483a008f33e2504dbd07428fc333a0a2a8c7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:33 +0000

    Add package lolpros-parser/0.2/lolpros_parser-0.2.tar.gz

---
+++ packages/lolpros-parser/lolpros_parser-0.2.tar.gz/lolpros_parser-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='lolpros_parser',
+      version='0.2',
+      description='Python Parser for lolpros.gg',
+      url='http://github.com/samhine/lolpros-parser',
+      download_url="https://github.com/samhine/lolpros_parser/archive/refs/tags/0.0.2.zip",
+      author='Samuel Hine',
+      author_email='sam.hine27@gmail.com',
+      keywords=['PYTHON', 'LOLPROS', 'API'],
+      license='MIT',
+      install_requires=['requests'],
+      long_description_content_type='text/markdown',
+      long_description=long_description)
