c0e485ecd2d488452560313c8af0bb99d768066b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:25 +0000

    Add package kbpdf/2.0/kbpdf-2.0-py3-none-any.whl

---
+++ packages/kbpdf/kbpdf-2.0-py3-none-any.whl/kbpdf/pdf2text.py
@@ -0,0 +1,17 @@
+""" This modeule provides functions to convert a PDF to the string. """
+
+
+class Converter:
+    """ A simple converter for converting PDFs to text."""
+
+    def convert(self, path):
+        """ 
+        Convert the given PDF to text.
+
+        Parameters:
+        path (str): The path to aPDF file.
+
+        Returns:
+        str: The content of the PDF file as text.
+        """
+        print('pdf2text')
