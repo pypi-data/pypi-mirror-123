1e62ccdc12af18804444770f6ec4d917daa9c006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:00 +0000

    Add package sdo-hmi-rvs/0.0.2/sdo_hmi_rvs-0.0.2.tar.gz

---
+++ packages/sdo-hmi-rvs/sdo_hmi_rvs-0.0.2.tar.gz/sdo_hmi_rvs-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='sdo_hmi_rvs',
+    packages=['sdo_hmi_rvs'],
+    version='0.0.2',  # Ideally should be same as your GitHub release tag varsion
+    description="Python package to independently derive 'sun-as-a-star' radial velocity variations.",
+    author='Tamar Ervin',
+    author_email='tamarervin@gmail.com',
+    url='https://github.com/tamarervin/sdo_hmi_rvs',
+    download_url='https://github.com/tamarervin/sdo_hmi_rvs/archive/refs/tags/v0.0.2.tar.gz',
+    keywords=['RVs', 'Solar'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
