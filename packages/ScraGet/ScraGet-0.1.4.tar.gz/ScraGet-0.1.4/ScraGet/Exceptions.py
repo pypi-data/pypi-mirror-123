d45aeb453e2024c051abe9a5778fb2af57212d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:08 +0000

    Add package ScraGet/0.1.4/ScraGet-0.1.4.tar.gz

---
+++ packages/ScraGet/ScraGet-0.1.4.tar.gz/ScraGet-0.1.4/ScraGet/Exceptions.py
@@ -0,0 +1,20 @@
+class UserNotFound(Exception):
+  pass
+
+class ProjectNotFound(Exception):
+  pass
+
+class StudioNotFound(Exception):
+  pass
+
+class PostNotFound(Exception):
+  pass
+
+class TopicNotFound(Exception):
+  pass
+
+class PageNotFound(Exception):
+  pass
+
+class InvalidValue(Exception):
+  pass
---
+++ packages/ScraGet/ScraGet-0.1.4.tar.gz/ScraGet-0.1.4/ScraGet/__init__.py
@@ -0,0 +1,9 @@
+from ScraGet.forum import get_post, get_topic
+from ScraGet.project import get_project
+from ScraGet.studios import get_studio
+from ScraGet.user import get_user, get_user_extra
+from ScraGet.frontpage import get_frontpage
+from ScraGet.encoder import encoder
+from ScraGet import ScraGet
+
+print("ScraGet: https://github.com/Quantum-Codes/ScraGet")
