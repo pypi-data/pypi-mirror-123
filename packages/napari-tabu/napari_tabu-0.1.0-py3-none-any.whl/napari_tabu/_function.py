06e9f0255c2ff48634ae8ef4908c9025da79cd95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:31 +0000

    Add package napari-tabu/0.1.0/napari_tabu-0.1.0-py3-none-any.whl

---
+++ packages/napari-tabu/napari_tabu-0.1.0-py3-none-any.whl/napari_tabu/_function.py
@@ -0,0 +1,33 @@
+"""
+This module is an example of a barebones function plugin for napari
+
+It implements the ``napari_experimental_provide_function`` hook specification.
+see: https://napari.org/docs/dev/plugins/hook_specifications.html
+
+Replace code below according to your needs.
+"""
+from typing import TYPE_CHECKING
+
+from enum import Enum
+import numpy as np
+from napari_plugin_engine import napari_hook_implementation
+
+import napari
+
+@napari_hook_implementation
+def napari_experimental_provide_function():
+    return [open_in_new_window]
+
+
+# 1.  First example, a simple function that thresholds an image and creates a labels layer
+def open_in_new_window(layer : napari.layers.Layer, napari_viewer:napari.Viewer):
+    new_viewer = napari.Viewer()
+    new_viewer.add_layer(layer)
+
+    # add back button to new viewer
+    from ._dock_widget import SendBackWidget
+    sbw = SendBackWidget(napari_viewer, new_viewer)
+
+    new_viewer.window.add_dock_widget(sbw, area='right',
+                                         name="Return")
+
