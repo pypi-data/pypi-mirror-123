f0eabdaa2bd7d886e575d888a55b05f6455d89bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:00 +0000

    Add package lightkube/0.8.1/lightkube-0.8.1.tar.gz

---
+++ packages/lightkube/lightkube-0.8.1.tar.gz/lightkube-0.8.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from pathlib import Path
+
+setup(
+    name='lightkube',
+    version="0.8.1",
+    description='Lightweight kubernetes client library',
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author='Giuseppe Tribulato',
+    author_email='gtsystem@gmail.com',
+    license='MIT',
+    url='https://github.com/gtsystem/lightkube',
+    packages=['lightkube', 'lightkube.config', 'lightkube.core'],
+    install_requires=[
+        'lightkube-models >= 1.15.12.0',
+        'httpx >= 0.20.0',
+        'PyYAML',
+        'backports-datetime-fromisoformat;python_version<"3.7"',
+        'dataclasses;python_version<"3.7"'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
+)
