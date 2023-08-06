d60643ca9206e39ce51a8650c615947b1936150f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:24 +0000

    Add package mskit/0.4.0/mskit-0.4.0-py2.py3-none-any.whl

---
+++ packages/mskit/mskit-0.4.0-py2.py3-none-any.whl/mskit/plot_kit/colors/__init__.py
@@ -0,0 +1,5 @@
+from . import precolors_grad
+from . import precolors_single
+from . import rgb_hsl
+from .precolors_grad import PreColorDict
+from .rgb_hex import hex_to_rgb
---
+++ packages/mskit/mskit-0.4.0-py2.py3-none-any.whl/mskit/plot_kit/colors/trans_color_code.py
@@ -0,0 +1,29 @@
+
+def hex_to_rgb():
+    pass
+
+
+def rgb_to_hex():
+    pass
+
+
+def hsl_to_rgb():
+    pass
+
+
+def rgb_to_hsl():
+    pass
+
+
+def hue_to_rgb():
+    pass
+
+
+def rgb_to_hue():
+    pass
+
+
+
+
+def trans_color_code(input_=None, from_='', to_='', *args):
+    pass
