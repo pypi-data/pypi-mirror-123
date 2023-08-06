39f3fa1c57506c2cb58ebdcf4f6b7773ef8da99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:35 +0000

    Add package so3/1.3.5/so3-1.3.5.tar.gz

---
+++ packages/so3/so3-1.3.5.tar.gz/so3-1.3.5/setup.py
@@ -0,0 +1,34 @@
+from skbuild import setup
+
+cmake_args = [
+    "-DBUILD_TESTING:BOOL=OFF",
+    "-Dconan_deps=ON",
+    "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+]
+
+setup(
+    name="so3",
+    version="1.3.5",
+    author="Jason McEwen",
+    install_requires=["numpy", "scipy"],
+    extras_require={
+        "dev": [
+            "setuptools",
+            "wheel",
+            "scikit-build",
+            "cmake",
+            "ninja",
+            "cython",
+            "conan",
+            "black",
+            "pytest",
+        ]
+    },
+    description="Fast and exact Wigner Transforms",
+    url="http://astro-informatics.github.io/so3/",
+    package_dir={"so3": "src/so3"},
+    cmake_args=cmake_args,
+    cmake_languages=("C",),
+    license="GPL-3",
+    packages=["so3"],
+)
