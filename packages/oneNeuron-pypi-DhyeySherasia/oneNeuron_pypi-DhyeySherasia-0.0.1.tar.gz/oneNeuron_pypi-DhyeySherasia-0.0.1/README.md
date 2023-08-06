e7c5875dd9e22c82fc4bc341424736e67e550b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:46 +0000

    Add package oneNeuron-pypi-DhyeySherasia/0.0.1/oneNeuron_pypi-DhyeySherasia-0.0.1.tar.gz

---
+++ packages/oneNeuron-pypi-DhyeySherasia/oneNeuron_pypi-DhyeySherasia-0.0.1.tar.gz/oneNeuron_pypi-DhyeySherasia-0.0.1/README.md
@@ -0,0 +1,9 @@
+# oneNeuron_pypi
+
+oneNeuron_pypi
+
+## References
+
+[Official python docs](https://packaging.python.org/tutorials/packaging-projects)
+
+[Other references](https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-python#publishing-to-package-registries)
---
+++ packages/oneNeuron-pypi-DhyeySherasia/oneNeuron_pypi-DhyeySherasia-0.0.1.tar.gz/oneNeuron_pypi-DhyeySherasia-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+username = "DhyeySherasia"
+project_name = "oneNeuron_pypi"
+
+setuptools.setup(
+    name=f"{project_name}-{username}",
+    version="0.0.1",
+    author=username,
+    author_email="dhyeysherasia2002@gmail.com",
+    description="An implementation of Perceptron in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{username}/{project_name}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{username}/{project_name}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy"
+    ]
+)
