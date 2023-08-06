c00489ebd8af32510fb91ddd6f41970c10f13c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:42 +0000

    Add package mangatools/0.1.0/mangatools-0.1.0.tar.gz

---
+++ packages/mangatools/mangatools-0.1.0.tar.gz/mangatools-0.1.0/mangatools/commands/manga_archive.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+
+def archive(extension):
+    cwd = os.getcwd()
+    chapters = []
+    print(f"\nFOLDER:\n{os.path.basename(cwd)} [{cwd}]\n")
+
+    for path in os.listdir(cwd):
+        full_path = os.path.join(cwd, path)
+        if os.path.isdir(full_path):
+            chapters.append(full_path)
+
+    print("\nCHAPTERS:")
+    for chapter in chapters:
+        print(f"{os.path.basename(chapter)}")
+        subprocess.call(f'7z a -tzip "{chapter}.{extension}" "{chapter}\\"')
+                
