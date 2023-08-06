8e3f132b07b3bb49c23e0af3bf644b9c7094d04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:44 +0000

    Add package pygenricher/0.0.8/pygenricher-0.0.8.tar.gz

---
+++ packages/pygenricher/pygenricher-0.0.8.tar.gz/pygenricher-0.0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='pygenricher',  # Required
+    version='0.0.8',
+    url='https://github.com/bammor/pygenricher',
+    author='Bashar Morouj',
+    author_email='bashar.morouj@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.6, <4',
+    install_requires=['numpy', 'gseapy', 'pandas', 'plotly'],
+    entry_points={"console_scripts": ["pygenricher=pygenricher.GeneEnrichment:main"]}
+)
