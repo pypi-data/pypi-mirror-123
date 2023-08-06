04f260689edc47a5cd751829506d352b9592c4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:52 +0000

    Add package instatool/0.0.1/instatool-0.0.1-py3-none-any.whl

---
+++ packages/instatool/instatool-0.0.1-py3-none-any.whl/instatool/__init__.py
@@ -0,0 +1,138 @@
+import requests
+
+s = requests.session()
+Headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'cookie': 'mid=YSma9gALAAHwbHKIhuux1KzBdiZB; ig_did=56438543-E7CE-4F94-BF2F-775E3EFF14A5; ig_nrcb=1; fbm_124024574287414=base_domain=.instagram.com; csrftoken=kGyX3X9BbID7RKGpXixQG3NgGWexLy97; ds_user_id=46630507089; sessionid=46630507089%3AuN9rMi3KO0OOKm%3A11; shbid="11806\05446630507089\0541665950374:01f7ba3214773a068855d3127d477b577f1d65ee7700c3dea4a54f2cebb5417067c6fce6"; shbts="1634414374\05446630507089\0541665950374:01f70d00fd5524f918666abd31eae55de19df3e0c0fc6634e692c44ba0cb62e84dde2a40"; fbsr_124024574287414=Qyk_U8vq0Zt0baGTARQKfbd9winOk46sSvOmsUhWk70.eyJ1c2VyX2lkIjoiMTAwMDAyNzMwODc3MjYyIiwiY29kZSI6IkFRQlNmbEVyQmd5S2tiWlRCMmRYemdkZi15X2NxLU05TlhlVVVfbGppUjJSTjZVYndTUERpTDNsNlpvMURmQ3FLNVVaQjZ5Qms2WXdHNHp1RUFTSWljMFNyZzBPbS1KNDN2NzFZRUFfQ2U1cEJuaHN2amdpLW1lX1Vtck9HVWRyRFlJOFE4MTVqRmdZVjRYbTlLQXNUVzVjZTZVb0JScTRUMEwxTm9EaUJUd0E4RHprQnh4NENGQlpVUUJFcURyMndPcUwwVHh5UjdsX09Sb3liVmVPZW1aeDJfSlNiQTFYUzlQZ0lzWXRrdHpXOUF4NHJrOGpXb2p6LUNraHFNZXdhNmhwSF9aQzVRcWVGLU9wRzF1bm1icnRTd2JuSmdPdEZDNzJJQ05lNEtoWlh5WDVFSXR4clN1RkFFSk8wblZ5TmZzbXdxVU1TZmtkaUUyRVo0aW01RHd1Iiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUZKME5zcHZPeFRzdWo2aTNBbzMzcHFUZWJtZ0h3OTdSbFpDMVpDNVZXbk1sOFE3VFpCV3pUd3BiRFVEMVFsUDN3WkFqOXE2UklkM2NoMEg0Z1ViMDB6R3ZDUDk4ZkFYeXZLRDVrU2dNU0tsUzlCWkNXUkVsV2dySVhaQ3EwTlNaQVJHMUpOYk53emxRR3FqTUpTb0hxVUFRbkhqSHRsb3JmZUp2bFBPekZneGpYMWNaQ2o1SkhJY1RQeEl5emE4RHdaRFpEIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE2MzQ0MTQzOTJ9; fbsr_124024574287414=6AvQ0BWcLYZ40FnvJItrZfwa16smuI6FTjiPQ6LP3D0.eyJ1c2VyX2lkIjoiMTAwMDAyNzMwODc3MjYyIiwiY29kZSI6IkFRQnBDQU5xWVpUN19JazZXMzhXSFpsTnNvNG1JSzloeW41LVpzNjA1T0M4YjFQT21fWEFHSWtiRmtsbmFsMUhtLWdNcU1ZR1RPMUo0UDBfenB0Z2hDaHZ1SUlGenRfMEZkZndhelRpQVZxOGg0SGphWVUxV0tna3NmX1ZpNWl1TXRFU1NWaHhteldjY1lhTV9tR19wemxNeUM5MlA3bEF6X21RTjZScklOWE5OZWw0STBQU3pkN0lqMzRVeEhvbnMtREVYNEFDOThlNm9GcEN3S1NXbnFURWxGazBvbExvY0tmeFVVR21ac256Nk1CNVI4TzBCeHlJRUpNeVMwUUp1b09pdjNlOFBoR29fekZxSFVVZWR6YWZNNXFBTHd4MmVaX29SQ0FLdDdwUlVJR2lEaWtQUlpVSW9rUmJRTjJqLXFvSmJZNDRHeFptYUowb09RTjEtVl84Iiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQU55b2NMc2ZaQkFERmVZTU4xVEdJU2k3Y0Z5QkE2VXVpWkNBUGpnY0JoM3h1SlZ3UmY3MVY2eHN4eTEwRkhqamRDaXNUQkloaUhHRXVEckM5cDlvblpCTXJhVGdibW83OHZ6OGJuWE9DQzZsSUM4Q2NBSXNiZ3c3NEc3eUthWkNndVNGWkNBdWo0TmRIMGZXMjZuM05tZnhsUkY4eXB5SHNFdHNpVDNzT1RVUVhvZjlRWkNHZ0RJb0liSVBCN3pRWkRaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjM0NDE0MzExfQ; rur="RVA\05446630507089\0541665950423:01f7132f93320743f44f844ce0e8229b9d856d033d22095595c4d03db2e80834babd9fc3"',
+    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+}
+login_Headers = {
+    'accept': '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-length': '318',
+    'content-type': 'application/x-www-form-urlencoded',
+    'cookie': 'mid=YSma9gALAAHwbHKIhuux1KzBdiZB; ig_did=56438543-E7CE-4F94-BF2F-775E3EFF14A5; ig_nrcb=1; fbm_124024574287414=base_domain=.instagram.com; shbid="11806\05446630507089\0541665950374:01f7ba3214773a068855d3127d477b577f1d65ee7700c3dea4a54f2cebb5417067c6fce6"; shbts="1634414374\05446630507089\0541665950374:01f70d00fd5524f918666abd31eae55de19df3e0c0fc6634e692c44ba0cb62e84dde2a40"; csrftoken=p9lrKwGNFBtZGPlazVEzhyNaewxxZeDS; fbsr_124024574287414=tQs_sqNzLEYWXBcgYFHahtra3ZuS8CNz-L-O-Hi2o9Q.eyJ1c2VyX2lkIjoiMTAwMDAyNzMwODc3MjYyIiwiY29kZSI6IkFRQTFHTWJ0RGVqeFhPRDFsbVptSURFR09PTG9wNTQzMHhVMWZJTFRqTjhPbnZuRDJVRzdNY1ZFQUhhSFNCUUN0R3NYaDNrdVl6QTFUbWtRWEotdXI2NWdOd3Y0RlYxVWs1VlBFS0lNV2FQcWRpNm40dkJkMERCbGZwaXBTbTZYMEJMUUxwcTJMRlF5TkZhR0NxM2MxQkRlOFFaZXdjZXQ2NTRNZVJOTjVCTEVqRXJWUTRqd3Q3OURTR1FMUURDLXVJWXoxNmxIclphRklwUUVhT2U4a2ZFc0VrWmFjeHBhTjJoZGFEVWRxYVJHUzNtNmRGUTRaNjNCNjRHS2Zuam4xYk1YeEQtVmVHN0dLa0gzLVBhaTFlc3JsQUZjQzZISldNNkczUjB0Y0FYR3RHdXpRUFREYndZWDdoR04xWkdMZTVubDVFRjdJQXJLRnhxTjNobW9wZldGIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUlmRVFyY2NGTzA5UUlqM2g2T1pCcTVHZnJudnR0UzZFZVVwejlNY1ZmUm9aQms3NUN0WkJxMjRXVGxodm5YV01pc2t0dktId2d2QWZrcnRkaTdwWkFRV0tsSXJYTHQ4dEh2UkxoYm12R2E1SE5zcTRSeXl1MlpBckFKWG9ic0NWTXhCYXFiWWpUZ0JuN1pBV1VoTnJsT1BDWEdhNUJYU2l5SDY5QmJvNXpnZjdxTmx4ekdaQVM4Sk5oaThlUFU5d1pEWkQiLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTYzNDQ3Mzk1NX0',
+    'origin': 'https://www.instagram.com',
+    'referer': 'https://www.instagram.com/accounts/emailsignup/',
+    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'x-asbd-id': '198387',
+    'x-csrftoken': 'p9lrKwGNFBtZGPlazVEzhyNaewxxZeDS',
+    'x-ig-app-id': '936619743392459',
+    'x-ig-www-claim': '0',
+    'x-instagram-ajax': 'fded3d1a393b',
+    'x-requested-with': 'XMLHttpRequest',
+}
+
+class grab:
+    def name(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['full_name'])
+        except:
+            return ("Error account is banded")
+
+    def bio(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['biography'])
+        except:
+            return ("Error account is banded")
+
+    def followers(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['edge_followed_by']['count'])
+        except:
+            return ("Error account is banded")
+
+    def following(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['edge_follow']['count'])
+        except:
+            return ("Error account is banded")
+
+    def posts(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['edge_owner_to_timeline_media']['count'])
+        except:
+            return ("Error account is banded")
+
+    def id(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['id'])
+        except:
+            return ("Error account is banded")
+
+    def is_business_account(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['is_business_account'])
+        except:
+            return ("Error account is banded")
+
+    def is_new_account(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['is_joined_recently'])
+        except:
+            return ("Error account is banded")
+
+    def is_verified(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['is_verified'])
+        except:
+            return ("Error account is banded")
+
+    def is_private(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['is_private'])
+        except:
+            return ("Error account is banded")
+
+    def get_pic(user):
+        try:
+            r = s.get(f'https://www.instagram.com/{user}/?__a=1', headers=Headers).json()
+            return (r['graphql']['user']['profile_pic_url_hd'])
+        except:
+            return ("Error account is banded")
+
+    def login(user,password):
+        login_data = {
+            'username': user,
+            'enc_password': '#PWD_INSTAGRAM_BROWSER:0:&:' + password
+        }
+        r = requests.post('https://www.instagram.com/accounts/login/ajax/', headers=login_Headers , data=login_data).text
+        if ('"authenticated":false') in r:
+            return False
+        else:
+            return True
+
+def coder():
+    return {
+        'name':'yazan alqasem',
+        'telegram':'@Plugin',
+        'channel':'https://t.me/CodeLeak'
+    }
