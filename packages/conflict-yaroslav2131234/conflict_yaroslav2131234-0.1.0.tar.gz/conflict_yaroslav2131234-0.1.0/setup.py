051764d9ab40aa06e44fb2c9aa2cc0cdc4772f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:57 +0000

    Add package conflict-yaroslav2131234/0.1.0/conflict_yaroslav2131234-0.1.0.tar.gz

---
+++ packages/conflict-yaroslav2131234/conflict_yaroslav2131234-0.1.0.tar.gz/conflict_yaroslav2131234-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='conflict_yaroslav2131234',
+    version='0.1.0',
+    description='A conflict Python package',
+    author='Yaroslav Yaroslav',
+    packages=['source'],
+    install_requires=['numpy>=1.21.0']
+)
---
+++ packages/conflict-yaroslav2131234/conflict_yaroslav2131234-0.1.0.tar.gz/conflict_yaroslav2131234-0.1.0/source/module_print.py
@@ -0,0 +1,1 @@
+print("Conflict(")
