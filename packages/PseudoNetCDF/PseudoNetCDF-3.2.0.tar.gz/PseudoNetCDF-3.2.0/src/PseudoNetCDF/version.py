7166509ad97a4982e0d006f0236a6716540e7dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:39 +0000

    Add package PseudoNetCDF/3.2.0/PseudoNetCDF-3.2.0.tar.gz

---
+++ packages/PseudoNetCDF/PseudoNetCDF-3.2.0.tar.gz/PseudoNetCDF-3.2.0/src/PseudoNetCDF/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM PseudoNetCDF SETUP.PY
+short_version = '3.2.0'
+version = '3.2.0'
+full_version = '3.2.0.dev0+c990327'
+git_revision = 'c990327ecc68f975848e3b219602d10bf6366ac4'
+release = False
+if not release:
+    version = full_version
