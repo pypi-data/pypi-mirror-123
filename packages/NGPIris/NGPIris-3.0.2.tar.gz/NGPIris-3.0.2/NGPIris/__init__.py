462f3d5ef9ac740752c768e838aabe3b90b89683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:43 +0000

    Add package NGPIris/3.0.2/NGPIris-3.0.2.tar.gz

---
+++ packages/NGPIris/NGPIris-3.0.2.tar.gz/NGPIris-3.0.2/NGPIris/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from datetime import datetime
+
+logformat =  logging.Formatter("[%(asctime)s] - %(levelname)s: %(message)s")
+
+# File work directory
+WD = os.path.dirname(os.path.realpath(__file__))
+TIMESTAMP = datetime.now().strftime("%y%m%d-%H%M%S")
+
+# Initialize log
+log = logging.getLogger("main_log")
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(logformat)
+log.addHandler(ch)
