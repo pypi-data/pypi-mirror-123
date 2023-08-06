5aa6b9ee6aad3926f0e1e0e6489a756b22143000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:12 +0000

    Add package minet/0.53.10/minet-0.53.10-py3-none-any.whl

---
+++ packages/minet/minet-0.53.10-py3-none-any.whl/minet/cli/twitter/utils.py
@@ -0,0 +1,96 @@
+# =============================================================================
+# Minet Twitter CLI Utils
+# =============================================================================
+#
+# Miscellaneous generic functions used throughout the twitter actions.
+#
+import casanova
+from twitter import TwitterHTTPError
+
+from minet.cli.utils import LoadingBar
+from minet.twitter import TwitterAPIClient
+
+
+def make_twitter_action(method_name, csv_headers):
+
+    def action(cli_args):
+        enricher = casanova.batch_enricher(
+            cli_args.file,
+            cli_args.output,
+            keep=cli_args.select,
+            add=csv_headers
+        )
+
+        loading_bar = LoadingBar(
+            desc='Retrieving ids',
+            unit=method_name[:-1],
+            stats={
+                'users': 0
+            }
+        )
+
+        client = TwitterAPIClient(
+            cli_args.access_token,
+            cli_args.access_token_secret,
+            cli_args.api_key,
+            cli_args.api_secret_key
+        )
+
+        resuming_state = None
+
+        if cli_args.resume:
+            resuming_state = cli_args.output.pop_state()
+
+        for row, user in enricher.cells(cli_args.column, with_rows=True):
+            loading_bar.update_stats(user=user)
+
+            all_ids = []
+            next_cursor = -1
+            result = None
+
+            if resuming_state is not None and resuming_state.last_cursor:
+                next_cursor = int(resuming_state.last_cursor)
+
+            if cli_args.ids:
+                client_kwargs = {'user_id': user}
+            else:
+                client_kwargs = {'screen_name': user}
+
+            while next_cursor != 0:
+                client_kwargs['cursor'] = next_cursor
+
+                skip_in_output = None
+
+                if resuming_state:
+                    skip_in_output = resuming_state.values_to_skip
+                    resuming_state = None
+
+                try:
+                    result = client.call([method_name, 'ids'], **client_kwargs)
+                except TwitterHTTPError as e:
+
+                    # The user does not exist
+                    loading_bar.inc('users_not_found')
+                    break
+
+                if result is not None:
+                    all_ids = result.get('ids', [])
+                    next_cursor = result.get('next_cursor', 0)
+
+                    loading_bar.update(len(all_ids))
+
+                    batch = []
+
+                    for user_id in all_ids:
+                        if skip_in_output and user_id in skip_in_output:
+                            continue
+
+                        batch.append([user_id])
+
+                    enricher.writebatch(row, batch, next_cursor or None)
+                else:
+                    next_cursor = 0
+
+            loading_bar.inc('users')
+
+    return action
