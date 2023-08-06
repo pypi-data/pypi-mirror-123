77dc1847c4cf1f28687046f93c34c34f231b4ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:29 +0000

    Add package cubicweb-i18nfield/0.2.0/cubicweb-i18nfield-0.2.0.tar.gz

---
+++ packages/cubicweb-i18nfield/cubicweb-i18nfield-0.2.0.tar.gz/cubicweb-i18nfield-0.2.0/cubicweb_i18nfield/__pkginfo__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=W0622
+"""cubicweb-i18nfield application packaging information"""
+
+modname = 'i18nfield'
+distname = 'cubicweb-i18nfield'
+
+numversion = (0, 2, 0)
+version = '.'.join(str(num) for num in numversion)
+
+license = 'LGPL'
+author = 'Florent Cayré (Villejuif, FRANCE)'
+author_email = 'Florent Cayré <florent.cayre@gmail.com>'
+description = 'Provides a way to translate entity fields individually.'
+web = 'http://www.cubicweb.org/project/%s' % distname
+
+__depends__ = {
+    'cubicweb': '>= 3.24',
+    'cubicweb-card': '>= 0.5',
+}
+
+__recommends__ = {}
+classifiers = [
+    'Environment :: Web Environment',
+    'Framework :: CubicWeb',
+    'Programming Language :: Python',
+    'Programming Language :: JavaScript',
+]
