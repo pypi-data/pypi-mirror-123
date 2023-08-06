d10461aca6593e475f843a270ac3fbb0efee21d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:36 +0000

    Add package django-denorm-iplweb/0.5.1/django-denorm-iplweb-0.5.1.tar.gz

---
+++ packages/django-denorm-iplweb/django-denorm-iplweb-0.5.1.tar.gz/django-denorm-iplweb-0.5.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name="django-denorm-iplweb",
+    version="0.5.1",
+    description="Denormalization magic for Django",
+    long_description="django-denorm-iplweb is a Django application to provide "
+    "automatic management of denormalized database fields.",
+    author_email="michal.dtz@gmail.com",
+    url="http://github.com/mpasternak/django-denorm-iplweb/",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+    ],
+    packages=[
+        "denorm",
+        "denorm.db",
+        "denorm.management",
+        "denorm.management.commands",
+        "denorm.migrations",
+    ],
+)
