13a3f1ec916bfd0c78ad434dbfeaead5cb59ea57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:25 +0000

    Add package authx/0.0.7/AuthX-0.0.7-py3-none-any.whl

---
+++ packages/authx/AuthX-0.0.7-py3-none-any.whl/AuthX/__init__.py
@@ -0,0 +1,11 @@
+"""Ready to use and customizable Authentications and Oauth2 management for FastAPI"""
+
+__version__ = "0.0.7"
+
+__author__ = "Yezz123"
+
+__author_email__ = "yasserth19@gmail.com"
+
+from AuthX.main import Authentication, AuthX, User
+
+__all__ = ["AuthX", "Authentication", "User"]
