743a5fb91c21a9988fc85a675d572fe6db1d5c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:52 +0000

    Add package MetricTool/0.1.3/MetricTool-0.1.3-py2.py3-none-any.whl

---
+++ packages/MetricTool/MetricTool-0.1.3-py2.py3-none-any.whl/MetricTools/PyCyclomatic.py
@@ -0,0 +1,31 @@
+
+import sys
+import os
+
+sys.path.append(os.path.split(os.path.realpath(__file__))[0])
+import File_visit as f_v
+import Parallel_cyclomatic as p_c
+
+# 圈复杂度计算模块对外接口
+# 输入： 文件或目录
+# 输出： 圈复杂度计算结果
+def CyclomaticAnalysis(file_s):
+    # 对文件、目录进行有效源码遍历
+    files = f_v.visit_files(file_s)
+    if len(files) == 0:
+        print("没有发现有效的源码文件！")
+        return None
+    
+    # 对文件列表进行并行化圈复杂度计算
+    results = p_c.para_cyclomatic(files)
+    return results
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python PyCyclomatic.py file_or_dir or python -m XX file_or_dir ")
+        exit(-1)
+    
+    file_s = sys.argv[1]
+    result = CyclomaticAnalysis(file_s)
+    print(result)
---
+++ packages/MetricTool/MetricTool-0.1.3-py2.py3-none-any.whl/MetricTools/test/test.c
@@ -0,0 +1,48 @@
+
+// Cyclomatic ： 9
+void test1() {
+    int a = 20;
+
+    if (a>10) {
+        a = 5;
+    }
+
+    while (a>30) {
+        a = 20;
+    }
+
+    for (a=20; a>30; a++) {
+        a = 20;
+    }
+
+    do {
+        a = 20;
+    } while (a>30);
+
+    a = a>30?20:40;
+
+    switch(a) {
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4: 
+            break;
+        default:
+            break;
+    }
+}
+
+// Cyclomatic: 3
+void test2() {
+    int a = 20;
+
+    if (a>10) {
+        a = 5;
+    }
+
+    while (a>30) {
+        a = 20;
+    }
+
+}
