7f548df37c27d309df011af042e16ab6bcac40d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:18 +0000

    Add package muswmm/0.0.0/muswmm-0.0.0.tar.gz

---
+++ packages/muswmm/muswmm-0.0.0.tar.gz/muswmm-0.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 14:12:41 2021
+
+@author: mumuz
+"""
+
+import setuptools
+
+setuptools.setup(name='muswmm',
+                 version='0.0.0',
+                 description='Muswmm is a library that allows users to modify SWMM project and get output results.',
+                 url='',
+                 author='Lei Zhang',
+                 author_email='gemini.zhang@qq.com',
+                 license='MIT',
+                 packages=setuptools.find_packages())
