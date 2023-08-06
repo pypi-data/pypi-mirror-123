d786ce0f1d953e8e8bad9f66451dd4ea03012f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:03 +0000

    Add package notifierbot/0.1.1/notifierbot-0.1.1-py3-none-any.whl

---
+++ packages/notifierbot/notifierbot-0.1.1-py3-none-any.whl/notifierbot/telegram_bot.py
@@ -0,0 +1,59 @@
+"""
+Telegram Notification Bot
+"""
+
+import requests
+import logging
+
+logger = logging.getLogger("telegram-bot")
+
+class TelegramConfig:
+    """
+    Configuration Variable for Telegram Bot
+
+    Parameters
+    ---
+    bot_token: BOT TOKEN recieved from @botfather
+
+    chat_id: CHAT ID received from https://api.telegram.org/bot{BOT_TOKEN}/getUpdates
+    """
+    def __init__(self, bot_token: str, chat_id: str):
+        self.bot_token = bot_token
+        self.chat_id = chat_id
+
+class TelegramBot:
+    """
+    API for Telegram Bot
+
+    Parameters
+    ---
+    config: TelegramConfig object
+    """
+    def __init__(self, config: TelegramConfig):
+        self.bot_token = config.bot_token
+        self.chat_id = config.chat_id
+
+    def add_message(self, message):
+        """
+        Function to add a message to notification
+
+        Parameters
+        ---
+        message: Message to be passed in notification
+        """
+        self.message = message
+
+    def notify(self):
+        """
+        Function to send a notification
+
+        Parameters
+        ---
+        None
+        """
+        url = f'https://api.telegram.org/bot{self.bot_token}/sendMessage?chat_id={self.chat_id}&text="{self.message}"'
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            logger.error(err)
