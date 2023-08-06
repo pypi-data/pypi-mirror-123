a7bed404e1eba3b81f4c3b09b7081db172410751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:29 +0000

    Add package simplearraysumVT/0.1.0/simplearraysumVT-0.1.0.tar.gz

---
+++ packages/simplearraysumVT/simplearraysumVT-0.1.0.tar.gz/simplearraysumVT-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='simplearraysumVT',
+    version='0.1.0',    
+    description='Calculate the sum of array',
+    url='https://github.com/VankaTaganai/simplearraysumVT',
+    author='Ivan Panov',
+    author_email='panovivan01@yandex.ru',
+    packages=['simplearraysumVT'],
+)
---
+++ packages/simplearraysumVT/simplearraysumVT-0.1.0.tar.gz/simplearraysumVT-0.1.0/simplearraysumVT/__init__.py
@@ -0,0 +1,2 @@
+def array_sum(arr):
+    return sum(arr)
