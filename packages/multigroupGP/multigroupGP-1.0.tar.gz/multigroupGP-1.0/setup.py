884e7c341e56a70e754b15be1a70bcf4390c248c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:05 +0000

    Add package multigroupGP/1.0/multigroupGP-1.0.tar.gz

---
+++ packages/multigroupGP/multigroupGP-1.0.tar.gz/multigroupGP-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multigroupGP",
+    version="1.0",
+    description="Multi-group Gaussian processes",
+    url="https://github.com/andrewcharlesjones/multi-group-GP",
+    download_url="https://github.com/andrewcharlesjones/multi-group-GP/archive/refs/tags/v_1.tar.gz",
+    author="Andy Jones",
+    author_email="ajones788@gmail.com",
+    packages=find_packages(),
+)
