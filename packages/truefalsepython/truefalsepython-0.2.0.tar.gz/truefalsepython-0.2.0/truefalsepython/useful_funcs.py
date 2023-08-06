066856b81d1b54388f5295bf62c5afa09c082ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:11 +0000

    Add package truefalsepython/0.2.0/truefalsepython-0.2.0.tar.gz

---
+++ packages/truefalsepython/truefalsepython-0.2.0.tar.gz/truefalsepython-0.2.0/truefalsepython/useful_funcs.py
@@ -0,0 +1,35 @@
+
+from typing import Sequence, Any
+
+import sys
+import random
+
+
+def randomTrue(prob: float = 0.5):
+    return True if random.random() < prob else False
+
+
+def fast_sample(objects: Sequence[Any], probs: Sequence[float]):
+    """
+    returns 1 random object from objects with probs probabilities 
+    """
+    x = random.random()
+    cum = 0
+    for i, p in enumerate(probs):
+        cum += p
+        if x < cum:
+            return objects[i]
+    return objects[-1]
+
+
+
+
+def set_trace():
+    from IPython.core.debugger import Pdb
+    Pdb(color_scheme = 'Linux').set_trace(sys._getframe().f_back)
+
+
+def debug(f, *args, **kwargs):
+    from IPython.core.debugger import Pdb
+    pdb = Pdb(color_scheme = 'Linux')
+    return pdb.runcall(f, *args, **kwargs)
