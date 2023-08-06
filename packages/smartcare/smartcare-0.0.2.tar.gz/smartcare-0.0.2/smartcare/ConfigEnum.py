4e8b7216cffab9964a5a64ccb35bda2e34f8fd96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:23 +0000

    Add package smartcare/0.0.2/smartcare-0.0.2.tar.gz

---
+++ packages/smartcare/smartcare-0.0.2.tar.gz/smartcare-0.0.2/smartcare/ConfigEnum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+from pkg_resources import resource_filename, Requirement
+
+class ConfigEnum(Enum):
+    IMAGE_HEIGHT = 128
+    IMAGE_WIDTH = 128
+    CNN_30MIN_MODEL = resource_filename(__name__, 'model/modelCNN-1630841987.h5')
+    CNN_DEFAULT_MODEL = resource_filename(__name__, 'model/modelCNN-1605022224.h5')
