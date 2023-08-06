bba758ddfb85eb44b0440c470878cf9b2721baed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:40 +0000

    Add package tkitTorchEx/0.0.0.116340442/tkitTorchEx-0.0.0.116340442.tar.gz

---
+++ packages/tkitTorchEx/tkitTorchEx-0.0.0.116340442.tar.gz/tkitTorchEx-0.0.0.116340442/tkitTorchEx/shift_tokens_right.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def shift_tokens_right(input_ids, pad_token_id):
+  """ Shift input ids one token to the right, and wrap the last non pad token (usually <eos>).
+      This is taken directly from modeling_bart.py
+  """
+  prev_output_tokens = input_ids.clone()
+  index_of_eos = (input_ids.ne(pad_token_id).sum(dim=1) - 1).unsqueeze(-1)
+  prev_output_tokens[:, 0] = input_ids.gather(1, index_of_eos).squeeze()
+  prev_output_tokens[:, 1:] = input_ids[:, :-1]
+  return prev_output_tokens
