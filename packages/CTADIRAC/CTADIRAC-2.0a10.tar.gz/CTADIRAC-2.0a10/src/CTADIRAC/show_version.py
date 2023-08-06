d8fbb3312d4ea5720235094eeb9ff88761a8793b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:19 +0000

    Add package CTADIRAC/2.0a10/CTADIRAC-2.0a10.tar.gz

---
+++ packages/CTADIRAC/CTADIRAC-2.0a10.tar.gz/CTADIRAC-2.0a10/src/CTADIRAC/show_version.py
@@ -0,0 +1,12 @@
+
+
+
+
+import DIRAC as dirac
+import CTADIRAC as ctadirac
+
+
+dirac_version = dirac.__version__
+ctadirac_version = ctadirac.__version__
+print(dirac_version + " " + ctadirac_version)
+
