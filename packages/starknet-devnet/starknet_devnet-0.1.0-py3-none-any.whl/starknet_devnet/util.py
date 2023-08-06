c524dd1ba295c461e5dd783e178019409248d1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:14 +0000

    Add package starknet-devnet/0.1.0/starknet_devnet-0.1.0-py3-none-any.whl

---
+++ packages/starknet-devnet/starknet_devnet-0.1.0-py3-none-any.whl/starknet_devnet/util.py
@@ -0,0 +1,25 @@
+from enum import Enum, auto
+import argparse
+
+class TxStatus(Enum):
+    PENDING = auto()
+    NOT_RECEIVED = auto()
+
+
+DEFAULT_HOST = "0.0.0.0"
+DEFAULT_PORT = 5000
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run a local instance of Starknet devnet")
+    parser.add_argument(
+        "--host",
+        help=f"the address to listen at; defaults to {DEFAULT_HOST} (use the address the program outputs on start)",
+        default=DEFAULT_HOST
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        help=f"the port to listen at; defaults to {DEFAULT_PORT}",
+        default=DEFAULT_PORT
+    )
+
+    return parser.parse_args()
