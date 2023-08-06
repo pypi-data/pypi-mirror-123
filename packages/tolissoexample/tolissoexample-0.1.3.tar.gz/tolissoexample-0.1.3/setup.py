757a2a3d3f7a151842f9d94729cbbd55c06d77c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:55 +0000

    Add package tolissoexample/0.1.3/tolissoexample-0.1.3.tar.gz

---
+++ packages/tolissoexample/tolissoexample-0.1.3.tar.gz/tolissoexample-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='tolissoexample',
+    version='0.1.3',    
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Stephen Hudson',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    packages=['tolissoexample'],
+    install_requires=["seaborn==0.11.2", "matplotlib==2.1"],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
---
+++ packages/tolissoexample/tolissoexample-0.1.3.tar.gz/tolissoexample-0.1.3/tolissoexample/module1.py
@@ -0,0 +1,2 @@
+def tolisso():
+    print("working")
