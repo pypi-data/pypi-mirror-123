5121c4f0694c67d5c0984c014a27bb88fa8ffb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:30 +0000

    Add package atomic-loop-pkg-test/0.1/atomic_loop_pkg_test-0.1.tar.gz

---
+++ packages/atomic-loop-pkg-test/atomic_loop_pkg_test-0.1.tar.gz/atomic_loop_pkg_test-0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup , find_packages
+
+setup(name='atomic_loop_pkg_test',
+      version='0.1',
+      description='This is a test Package',
+      long_description="This is just for a testing purpose",
+      author='Mangesh',
+      packages=['atomic_loop_pkg_test'],
+      requires=[])
