b57d50e4e64bede8e125c01bf872ed78908e82c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:23 +0000

    Add package test-cd/0.5.0/test_cd-0.5.0.tar.gz

---
+++ packages/test-cd/test_cd-0.5.0.tar.gz/test_cd-0.5.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test_cd']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'test-cd',
+    'version': '0.5.0',
+    'description': '',
+    'long_description': None,
+    'author': 'dylandoamaral',
+    'author_email': 'do.amaral.dylan@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
