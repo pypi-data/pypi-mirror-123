bc55b71c1a3fd5a45b379ea43c20de403246b161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:09 +0000

    Add package automaster/0.0.7/automaster-0.0.7.tar.gz

---
+++ packages/automaster/automaster-0.0.7.tar.gz/automaster-0.0.7/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+package_data={
+    'autotk': ['*.pyd', '*.dll'],
+}
+setuptools.setup(include_package_data=True,packages=setuptools.find_packages("whl"),package_data=package_data,install_requires=[
+    'requests','Pillow',
+])
