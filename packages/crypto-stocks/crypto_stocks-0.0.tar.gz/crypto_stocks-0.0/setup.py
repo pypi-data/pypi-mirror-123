f7257b8c6862e20011a6c5a94678bee52b599ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:53 +0000

    Add package crypto-stocks/0.0/crypto_stocks-0.0.tar.gz

---
+++ packages/crypto-stocks/crypto_stocks-0.0.tar.gz/crypto_stocks-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='crypto_stocks',
+      version='0.0',
+      description='Get current price of crypto from most popular stock exchanges',
+      packages=['crypto_stocks'],
+      author_email='1mgavura@gmail.com',
+      zip_safe=False)
