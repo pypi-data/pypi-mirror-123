1cdfcbf3c30b52893a6b3b9ba03a303e05b1ae3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:34 +0000

    Add package dsocli/1.0.177.dev3/dsocli-1.0.177.dev3.tar.gz

---
+++ packages/dsocli/dsocli-1.0.177.dev3.tar.gz/dsocli-1.0.177.dev3/src/dsocli/provider/package/aws/cfn/v1/main.py
@@ -0,0 +1,34 @@
+from dsocli.exceptions import DSOException
+from dsocli.logger import Logger
+from dsocli.providers import Providers
+from dsocli.artifacts import ArtifactStore
+from dsocli.packages import PackageProvider
+from dsocli.stages import Stages
+from dsocli.constants import *
+from dsocli.contexts import Contexts
+from dsocli.aws_s3_utils import *
+from dsocli.settings import *
+
+
+__default_spec = {
+    'foo': 'bar',
+}
+
+
+def get_default_spec():
+    return __default_spec.copy()
+
+
+class AwsCfnPackageProvider(PackageProvider):
+
+    def __init__(self):
+        super().__init__('package/aws/cfn/v1')
+
+    def build(self, config):
+        self.config = config
+        Logger.debug(f"Building CFN package: namespace:{config.namespace}, project={config.project}, application={config.application}, stage={config.stage}")
+        return 'package1.zip'
+
+
+def register():
+    Providers.register(AwsCfnPackageProvider())
