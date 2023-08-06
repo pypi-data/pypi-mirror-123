29e3293cbd92fc49d7c120ddd1e7ccb72ace5669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:17 +0000

    Add package django-lightweight-queue/4.2.0/django-lightweight-queue-4.2.0.tar.gz

---
+++ packages/django-lightweight-queue/django-lightweight-queue-4.2.0.tar.gz/django-lightweight-queue-4.2.0/django_lightweight_queue/backends/redis.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+import redis
+
+from .. import app_settings
+from ..job import Job
+from .base import BaseBackend
+from ..types import QueueName, WorkerNumber
+
+
+class RedisBackend(BaseBackend):
+    """
+    This backend has at-most-once semantics.
+    """
+
+    def __init__(self) -> None:
+        self.client = redis.StrictRedis(
+            host=app_settings.REDIS_HOST,
+            port=app_settings.REDIS_PORT,
+            password=app_settings.REDIS_PASSWORD,
+        )
+
+    def enqueue(self, job: Job, queue: QueueName) -> None:
+        self.client.lpush(self._key(queue), job.to_json().encode('utf-8'))
+
+    def dequeue(self, queue: QueueName, worker_num: WorkerNumber, timeout: int) -> Optional[Job]:
+        raw = self.client.brpop(self._key(queue), timeout)
+        if raw is None:
+            return None
+
+        _, data = raw
+        return Job.from_json(data.decode('utf-8'))
+
+    def length(self, queue: QueueName) -> int:
+        return self.client.llen(self._key(queue))
+
+    def _key(self, queue: QueueName) -> str:
+        if app_settings.REDIS_PREFIX:
+            return '{}:django_lightweight_queue:{}'.format(
+                app_settings.REDIS_PREFIX,
+                queue,
+            )
+
+        return 'django_lightweight_queue:{}'.format(queue)
