cb4d1e4f8eef4841b05b1734f285eae82bc3d9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:28 +0000

    Add package trainml/0.3.1/trainml-0.3.1-py3-none-any.whl

---
+++ packages/trainml/trainml-0.3.1-py3-none-any.whl/trainml/cli/gpu.py
@@ -0,0 +1,41 @@
+import asyncio
+import click
+from trainml.cli import cli, pass_config
+from trainml.trainml import TrainML
+
+
+@cli.group()
+@pass_config
+def gpu(config):
+    """TrainML GPU commands."""
+    pass
+
+
+@gpu.command()
+@pass_config
+def list(config):
+    """List GPUs."""
+    data = [
+        ["ID", "NAME", "ABBRV", "PROVIDER", "AVAILABLE", "CREDITS/HR"],
+        ["-" * 80, "-" * 80, "-" * 80, "-" * 80, "-" * 80, "-" * 80],
+    ]
+
+    gpus = config.trainml.run(config.trainml.client.gpu_types.list())
+
+    for gpu in gpus:
+        data.append(
+            [
+                gpu.id,
+                gpu.name,
+                gpu.abbrv,
+                gpu.provider,
+                f"{gpu.available}",
+                f"{gpu.credits_per_hour:.2f}",
+            ]
+        )
+    for row in data:
+        click.echo(
+            "{: >36.34} {: >16.14} {: >10.8} {: >10.8} {: >11.9} {: >12.10}"
+            "".format(*row),
+            file=config.stdout,
+        )
