90cbf272cb18b07980463f374d7e224054603bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:43 +0000

    Add package pyaffine/0.1.6/pyaffine-0.1.6.tar.gz

---
+++ packages/pyaffine/pyaffine-0.1.6.tar.gz/pyaffine-0.1.6/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyaffine']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-datamuse==1.3.0']
+
+entry_points = \
+{'console_scripts': ['affine = pyaffine.affine:main']}
+
+setup_kwargs = {
+    'name': 'pyaffine',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': None,
+    'author': 'davidshivaji',
+    'author_email': 'davidshivaji@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
