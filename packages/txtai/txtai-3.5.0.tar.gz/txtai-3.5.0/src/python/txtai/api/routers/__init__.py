7c065987184d7f26868702fc61416bd3e4253851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:59 +0000

    Add package txtai/3.5.0/txtai-3.5.0.tar.gz

---
+++ packages/txtai/txtai-3.5.0.tar.gz/txtai-3.5.0/src/python/txtai/api/routers/__init__.py
@@ -0,0 +1,13 @@
+"""
+Router imports
+"""
+
+from . import embeddings
+from . import extractor
+from . import segmentation
+from . import similarity
+from . import summary
+from . import textractor
+from . import transcription
+from . import translation
+from . import workflow
