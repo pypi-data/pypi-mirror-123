11e0d60b11766dee86d7234e86c2d2cd2bb976b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:20 +0000

    Add package tomsk-energosbyt-python/0.0.23a1/tomsk-energosbyt-python-0.0.23a1.tar.gz

---
+++ packages/tomsk-energosbyt-python/tomsk-energosbyt-python-0.0.23a1.tar.gz/tomsk-energosbyt-python-0.0.23a1/inter_rao_energosbyt/const.py
@@ -0,0 +1,11 @@
+CURRENCY_DEFAULT = "руб."
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55"
+).strip()
+
+FIELD_PROVIDER_TYPE = "kd_provider"
+FIELD_SERVICE_TYPE = "kd_service_type"
+
+META_DISPLAY_NAME = "display_name"
