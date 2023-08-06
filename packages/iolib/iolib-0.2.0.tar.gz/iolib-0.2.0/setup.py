31f9eb48a9e6043e8331c30cf1985d2570811d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:22 +0000

    Add package iolib/0.2.0/iolib-0.2.0.tar.gz

---
+++ packages/iolib/iolib-0.2.0.tar.gz/iolib-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name="iolib",
+    version="0.2.0",
+    description="IO library inspired by the builtin function 'input'",
+    author="FloatingInt",
+)
