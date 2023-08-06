fe1ee57716977842f23a7d3e543b7e7de2cd8421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:43 +0000

    Add package interpretdl/0.2.4/interpretdl-0.2.4.tar.gz

---
+++ packages/interpretdl/interpretdl-0.2.4.tar.gz/interpretdl-0.2.4/interpretdl/interpreter/__init__.py
@@ -0,0 +1,31 @@
+from .lime import LIMECVInterpreter, LIMENLPInterpreter
+from .gradient_cam import GradCAMInterpreter
+from .integrated_gradients import IntGradCVInterpreter, IntGradNLPInterpreter
+from .smooth_grad import SmoothGradInterpreter
+from .occlusion import OcclusionInterpreter
+from .gradient_shap import GradShapCVInterpreter, GradShapNLPInterpreter
+from ._normlime_base import NormLIMECVInterpreter, NormLIMENLPInterpreter
+from .score_cam import ScoreCAMInterpreter
+from .lrp import LRPCVInterpreter
+from .rollout import RolloutInterpreter
+from .transition_attention_maps import TAMInterpreter
+
+__all__ = [
+    "LIMECVInterpreter", "LIMENLPInterpreter", "NormLIMECVInterpreter",
+    "NormLIMENLPInterpreter", "GradCAMInterpreter", "IntGradCVInterpreter",
+    "IntGradNLPInterpreter", "SmoothGradInterpreter", "OcclusionInterpreter",
+    "GradShapCVInterpreter", "GradShapNLPInterpreter", "ScoreCAMInterpreter",
+    "LRPCVInterpreter", "RolloutInterpreter", "TAMInterpreter"
+]
+
+try:
+    import paddle
+    from . import lime_prior
+    from .lime_prior import LIMEPriorInterpreter
+    from .forgetting_events import ForgettingEventsInterpreter
+    __all__.append("LIMEPriorInterpreter")
+    __all__.append("ForgettingEventsInterpreter")
+except ModuleNotFoundError:
+    print(
+        "Warning: Paddle should be installed before using LIMEPriorInterpreter or ForgettingEventsInterpreter."
+    )
