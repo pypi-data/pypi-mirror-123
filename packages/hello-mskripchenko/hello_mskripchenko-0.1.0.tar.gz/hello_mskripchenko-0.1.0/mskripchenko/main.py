2299f0efbd4d91186cbb4ec29ebba5e1c3565498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package hello-mskripchenko/0.1.0/hello_mskripchenko-0.1.0.tar.gz

---
+++ packages/hello-mskripchenko/hello_mskripchenko-0.1.0.tar.gz/hello_mskripchenko-0.1.0/mskripchenko/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def hello_world():
+    print("Hello World")
+    
+def hello_human(name: str):
+    print("Hello, " + name)
+
+def numpy_array():
+    print(np.array([16, 4, 2]))
---
+++ packages/hello-mskripchenko/hello_mskripchenko-0.1.0.tar.gz/hello_mskripchenko-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='hello_mskripchenko',
+    version='0.1.0',    
+    description='A example Python package. Very welcoming',
+    author='Maria Skripchenko',
+    author_email="idontwanttowritemyrealemail@gmail.com",
+    url="https://random-url.com",
+    packages=['mskripchenko'],
+    install_requires=['numpy',
+                      ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
