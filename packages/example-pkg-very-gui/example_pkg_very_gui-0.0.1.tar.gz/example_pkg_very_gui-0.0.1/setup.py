c19fe3753d89151eb20ad72958dbefdac49bdd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:10 +0000

    Add package example-pkg-very-gui/0.0.1/example_pkg_very_gui-0.0.1.tar.gz

---
+++ packages/example-pkg-very-gui/example_pkg_very_gui-0.0.1.tar.gz/example_pkg_very_gui-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="example_pkg_very_gui",
+  version="0.0.1",
+  author="xxx",
+  author_email="e2662020@outlook.com",
+  description="This is a great GUI library for beginners",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/ZhengHaotian2010/gui.py",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
