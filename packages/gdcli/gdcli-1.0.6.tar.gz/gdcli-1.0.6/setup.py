8f313769d386cbea265a3d84ae48d80639503168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:48 +0000

    Add package gdcli/1.0.6/gdcli-1.0.6.tar.gz

---
+++ packages/gdcli/gdcli-1.0.6.tar.gz/gdcli-1.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="gdcli",
+    version="1.0.6",
+    author="Subrahmanya s hegade",
+    author_email="hegadedevelopment@gmail.com",
+    description='''
+    Command line too used for simple google dorks
+    ''',
+
+    packages=find_packages(include=['gdcli', 'gdcli.*']),
+    entry_points='''
+    [console_scripts]
+    gdcli=gdcli.gd:start
+    '''
+
+)
