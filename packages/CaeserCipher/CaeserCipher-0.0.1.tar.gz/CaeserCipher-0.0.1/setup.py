55fc98c483143f20781aecb5ea87fe9cdad73df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:41 +0000

    Add package CaeserCipher/0.0.1/CaeserCipher-0.0.1.tar.gz

---
+++ packages/CaeserCipher/CaeserCipher-0.0.1.tar.gz/CaeserCipher-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='CaeserCipher',
+    version='0.0.1',
+    packages=['CaeserCipher'],
+    install_requires=[
+        "fire",
+        "pyperclip"
+    ],
+)
