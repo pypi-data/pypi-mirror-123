985914775fae518cc06938ff6964530b4cbc5906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:48 +0000

    Add package zerohash-python/0.0.4/zerohash-python-0.0.4.tar.gz

---
+++ packages/zerohash-python/zerohash-python-0.0.4.tar.gz/zerohash-python-0.0.4/zerohash/object_classes.py
@@ -0,0 +1,12 @@
+from zerohash import resources
+
+OBJECT_CLASSES = {
+    resources.DigitalAssetAddresses.OBJECT_NAME: resources.DigitalAssetAddresses,
+    resources.LiquidityRfq.OBJECT_NAME: resources.LiquidityRfq,
+    resources.ParticipantCustomerNew.OBJECT_NAME: resources.ParticipantCustomerNew,
+    resources.LiquidityExecute.OBJECT_NAME: resources.LiquidityExecute,
+    resources.TradesBatch.OBJECT_NAME: resources.TradesBatch,
+    resources.Transfers.OBJECT_NAME: resources.Transfers,
+    resources.Participants.OBJECT_NAME: resources.Participants,
+    resources.Time.OBJECT_NAME: resources.Time,
+}
