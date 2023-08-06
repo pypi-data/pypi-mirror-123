b6402dd3d892ba0125cc6551d657a82a5b63acfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:07 +0000

    Add package david-home-automation/0.1.15/david_home_automation-0.1.15-py3-none-any.whl

---
+++ packages/david-home-automation/david_home_automation-0.1.15-py3-none-any.whl/david_home_automation/static/script.js
@@ -0,0 +1,45 @@
+(function() {
+    var app = new Vue({
+        el: '#app',
+        data(){
+        return {
+                config: {},
+                defaultTemperatures: [5, 20, 25]
+            }
+        },
+        async mounted() {
+            this.config = (await axios.get('/api/config')).data;
+            // https://stackoverflow.com/a/55379279
+            this.config.thermostats.forEach(t => this.$set(t, 'temperature', 22));
+         },
+        methods: {
+            wakeupHost: (name) => {
+                axios
+                  .post('/api/wake-on-lan', {
+                    name: name
+                  })
+            },
+            changeToAutomatic: (name) => {
+                axios
+                  .post('/api/thermostats/change-to-automatic', {
+                    name: name
+                  })
+            },
+            changeTemperatureTo: (name, temperature) => {
+                changeTemperatureTo(name, temperature)
+            },
+            changeTemperature: (thermostat) => {
+                changeTemperatureTo(thermostat.name, thermostat.temperature)
+            }
+        }
+    });
+
+
+    function changeTemperatureTo(name, temperature) {
+                axios
+                  .post('/api/thermostats/change-temperature', {
+                    name: name,
+                    temperature: temperature
+                  })
+            }
+})();
