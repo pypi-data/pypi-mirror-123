cf2f89b8badf9574010d492b3b85cbdb1dab5c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:52 +0000

    Add package ec2objects/0.0.10/ec2objects-0.0.10.tar.gz

---
+++ packages/ec2objects/ec2objects-0.0.10.tar.gz/ec2objects-0.0.10/ec2objects/ec2object/image.py
@@ -0,0 +1,180 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from ..ec2api.images import Images
+
+from ..ec2common.ec2exceptions import *
+
+import json
+import threading
+import time
+import re
+
+
+@dataclass
+class ImageAttributes:
+    Architecture: str = None
+    CreationDate: str = None
+    ImageId: str = None
+    ImageLocation: str = None
+    ImageType: str = None
+    Public: bool = None
+    KernelId: str = None
+    OwnerId: str = None
+    Platform: str = None
+    PlatformDetails: str = None
+    UsageOperation: str = None
+    RamdiskId: str = None
+    State: str = None
+    Description: str = None
+    EnaSupport: bool = None
+    Hypervisor: str = None
+    ImageOwnerAlias: str = None
+    Name: str = None
+    RootDeviceName: str = None
+    RootDeviceType: str = None
+    SriovNetSupport: str = None
+    VirtualizationType: str = None
+    BootMode: str = None
+    DepricationTime: str = None
+    # ProductCodes: object = None
+    ##BlockDeviceMappings: object = None
+    # StateReason: object = None
+    # Tags: object = None
+
+
+@dataclass
+class BlockDeviceMappingEBS:
+    # deviceType: str = None
+    DeviceName: str = None
+    SnapshotId: str = None
+    VolumeSize: str = None
+    DeleteOnTermination: bool = None
+    VolumeType: str = None
+    Encrypted: bool = None
+
+
+@dataclass
+class BlockDeviceMappingVirtual:
+    # deviceType: str = None
+    DeviceName: str = None
+    VirtualName: str = None
+
+
+class ImageManager:
+    def __init__(self):
+        self.imageapi = Images()
+
+    def retrieve_all_ubuntu_x86_64_machine_images_hvm_ssd(self, name=""):
+        # https://wiki.ubuntu.com/Releases
+        # uset the first string of the code name e.g. Focal, Bionic, Trusty
+        image_objects = []
+        json_results = self.imageapi.list_all_ubuntu_x86_64_machine_images_hvm_ssd(
+            name=name
+        )
+        image_datas = json_results["Images"]
+        for image_data in image_datas:
+            ebsblockdevices = []
+            virtualblockdevices = []
+            if "BlockDeviceMappings" in image_data:
+                BlockDeviceMappings = image_data.pop("BlockDeviceMappings")
+                (
+                    ebsblockdevices,
+                    virtualblockdevices,
+                ) = self._extract_block_device_mapping(BlockDeviceMappings)
+            newimage = Image()
+            newimage.attributes = ImageAttributes(**image_data)
+            newimage.ebsblockdevices = ebsblockdevices
+            newimage.virtualblockdevices = virtualblockdevices
+            image_objects.append(newimage)
+        if len(image_objects) > 1:
+            image_objects.sort(key=lambda x: x.attributes.Name, reverse=True)
+        return image_objects
+
+    def retrieve_all_ubuntu_arm64_machine_images_hvm_ssd(self, name=""):
+        # https://wiki.ubuntu.com/Releases
+        # uset the first string of the code name e.g. Focal, Bionic, Trusty
+        image_objects = []
+        json_results = self.imageapi.list_all_ubuntu_arm64_machine_images_hvm_ssd(
+            name=name
+        )
+        image_datas = json_results["Images"]
+        for image_data in image_datas:
+            ebsblockdevices = []
+            virtualblockdevices = []
+            if "BlockDeviceMappings" in image_data:
+                BlockDeviceMappings = image_data.pop("BlockDeviceMappings")
+                (
+                    ebsblockdevices,
+                    virtualblockdevices,
+                ) = self._extract_block_device_mapping(BlockDeviceMappings)
+            newimage = Image()
+            newimage.attributes = ImageAttributes(**image_data)
+            newimage.ebsblockdevices = ebsblockdevices
+            newimage.virtualblockdevices = virtualblockdevices
+            image_objects.append(newimage)
+        if len(image_objects) > 1:
+            image_objects.sort(key=lambda x: x.attributes.Name, reverse=True)
+        return image_objects
+
+    def retrieve_image(self, imageId):
+        json_results = self.imageapi.retrieve_image(imageId)
+        image_datas = json_results["Images"]
+        image_data = image_datas[0]
+        # print(type(image_data))
+        # print(image_data)
+        ebsblockdevices = []
+        virtualblockdevices = []
+        if "BlockDeviceMappings" in image_data:
+            BlockDeviceMappings = image_data.pop("BlockDeviceMappings")
+            ebsblockdevices, virtualblockdevices = self._extract_block_device_mapping(
+                BlockDeviceMappings
+            )
+        # if "ProductCodes" in image_data:
+        #    ProductCodes = image_data.pop("ProductCodes")
+        #    print(ProductCodes)
+        # if "StateReason" in image_data:
+        #    StateReason = image_data.pop("StateReason")
+        #    print(StateReason)
+        # if "Tags" in image_data:
+        #    Tags = image_data.pop("Tags")
+        #    print(Tags)
+        newimage = Image()
+        newimage.attributes = ImageAttributes(**image_data)
+        newimage.ebsblockdevices = ebsblockdevices
+        newimage.virtualblockdevices = virtualblockdevices
+        return newimage
+
+    def _extract_block_device_mapping(self, BlockDeviceMappings):
+        ebsblockdevices = []
+        virtualblockdevices = []
+        for BlockDeviceMapping in BlockDeviceMappings:
+            if "Ebs" in BlockDeviceMapping:
+                newebsbd = BlockDeviceMappingEBS()
+                newebsbd.DeviceName = BlockDeviceMapping["DeviceName"]
+                if "SnapshotId" in BlockDeviceMapping["Ebs"]:
+                    newebsbd.SnapshotId = BlockDeviceMapping["Ebs"]["SnapshotId"]
+                if "DeleteOnTermination" in BlockDeviceMapping["Ebs"]:
+                    newebsbd.DeleteOnTermination = BlockDeviceMapping["Ebs"][
+                        "DeleteOnTermination"
+                    ]
+                if "VolumeSize" in BlockDeviceMapping["Ebs"]:
+                    newebsbd.VolumeSize = BlockDeviceMapping["Ebs"]["VolumeSize"]
+                if "VolumeType" in BlockDeviceMapping["Ebs"]:
+                    newebsbd.VolumeType = BlockDeviceMapping["Ebs"]["VolumeType"]
+                if "Encrypted" in BlockDeviceMapping["Ebs"]:
+                    newebsbd.Encrypted = BlockDeviceMapping["Ebs"]["Encrypted"]
+                ebsblockdevices.append(newebsbd)
+            if "VirtualName" in BlockDeviceMapping:
+                newvirtual = BlockDeviceMappingVirtual()
+                newvirtual.DeviceName = BlockDeviceMapping["DeviceName"]
+                newvirtual.VirtualName = BlockDeviceMapping["VirtualName"]
+                virtualblockdevices.append(newvirtual)
+        return ebsblockdevices, virtualblockdevices
+
+
+class Image:
+    def __init__(self):
+        self.attributes = ImageAttributes()
+        self.ebsblockdevices = []
+        self.virtualblockdevices = []
---
+++ packages/ec2objects/ec2objects-0.0.10.tar.gz/ec2objects-0.0.10/ec2objects/ec2object/keypair.py
@@ -0,0 +1,137 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from ..ec2api.keypairs import KeyPairs
+
+from ..ec2common.ec2exceptions import *
+
+import json
+import threading
+import time
+import re
+
+
+@dataclass
+class KeyPairAttributes:
+    KeyPairId: str = None
+    KeyFingerprint: str = None
+    KeyName: str = None
+    KeyType: str = None
+    # Tags: list = field(default_factory=list)
+
+
+@dataclass
+class Tags:
+    Key: str = None
+    Value: str = None
+
+
+class KeyPairManager:
+    def __init__(self):
+        self.keypairapi = KeyPairs()
+        self.valid_aws_sshkey_types = ["ssh-rsa", "ssh-ed25519"]
+
+    def retrieve_keypairs(self):
+        response = self.keypairapi.list_keypairs()
+        keypair_objects = self._build_keypair_objects_from_response(response)
+        return keypair_objects
+
+    def retrieve_keypair_by_name(self, name):
+        response = self.keypairapi.get_keypair_by_name(name)
+        keypair_objects = self._build_keypair_objects_from_response(response)
+        return keypair_objects
+
+    def retrieve_keypair_by_id(self, keypairid):
+        response = self.keypairapi.get_keypair_by_id(keypairid)
+        keypair_objects = self._build_keypair_objects_from_response(response)
+        return keypair_objects
+
+    def retrieve_keypair_by_fingerprint(self, fingerprint):
+        response = self.keypairapi.get_keypair_by_fingerprint(fingerprint)
+        keypair_objects = self._build_keypair_objects_from_response(response)
+        return keypair_objects
+
+    def retrieve_keypair_by_tag_keyname(self, tagkeyname):
+        response = self.keypairapi.get_keypair_by_tag_keyname(tagkeyname)
+        keypair_objects = self._build_keypair_objects_from_response(response)
+        return keypair_objects
+
+    def retrieve_keypair_by_tag(self, key, value):
+        response = self.keypairapi.get_keypair_by_tag(key, value)
+        keypair_objects = self._build_keypair_objects_from_response(response)
+        return keypair_objects
+
+    def upload_keypair(self, name, publickey, tag_dict):
+        self._check_if_valid_publickey(publickey)
+        if self._does_keypair_name_exist(name) == True:
+            raise KeyPairNameAlreadyExists(f"Key pair name {name} already exists  ")
+        if tag_dict:
+            tag_specification = self._build_tag_specification_list(tag_dict)
+            # print(tag_specification)
+            self.keypairapi.import_keypair(name, publickey, tag_specification)
+        else:
+            self.keypairapi.import_keypair(name, publickey)
+        response = self.retrieve_keypair_by_name(name)
+        # print("RESPONSE", response)
+        # print(response)
+        # keypair_objects = self._build_keypair_objects_from_response(response)
+        return response[0]
+
+    def _check_if_valid_publickey(self, publickey):
+        publickeytype = publickey.split(" ")[0]
+        if publickeytype in self.valid_aws_sshkey_types:
+            return True
+        else:
+            raise KeyPairTypeNotSupportedByAWS(
+                f"AWS does not allow ssh key type {publickeytype}"
+            )
+
+    def _build_keypair_objects_from_response(self, response):
+        keypair_objects = []
+        keypairs = response["KeyPairs"]
+        for keypair in keypairs:
+            tag_objects = []
+            if "Tags" in keypair:
+                tags = keypair.pop("Tags")
+                # print(tags)
+                for tag in tags:
+                    # print(tag)
+                    newtag = Tags(**tag)
+                    tag_objects.append(newtag)
+            newkeypair = KeyPair()
+            newkeypair.attributes = KeyPairAttributes(**keypair)
+            newkeypair.tags = tag_objects
+            keypair_objects.append(newkeypair)
+        return keypair_objects
+
+    def _build_tag_specification_list(self, tag_dict):
+        tag_specifications = []
+        tag_specification = {}
+        tag_specification["ResourceType"] = "key-pair"
+        tag_list = []
+        for key, value in tag_dict.items():
+            tag = {}
+            tag["Key"] = key
+            tag["Value"] = value
+
+            tag_list.append(tag)
+        tag_specification["Tags"] = tag_list
+        tag_specifications.append(tag_specification)
+        return tag_specifications
+
+    def _does_keypair_name_exist(self, KeyName):
+        if len(self.retrieve_keypair_by_name(KeyName)) > 0:
+            return True
+        else:
+            return False
+
+
+class KeyPair:
+    def __init__(self):
+        self.keypairapi = KeyPairs()
+        self.attributes = KeyPairAttributes()
+        self.tags = []
+
+    def delete(self):
+        response = self.keypairapi.delete_keypair(self.attributes.KeyName)
+        # return response
