8da568d2596004b9966186bf9fc339b44987e4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:41 +0000

    Add package mabelbeta/0.5.0a55/mabelbeta-0.5.0a55-py3-none-any.whl

---
+++ packages/mabelbeta/mabelbeta-0.5.0a55-py3-none-any.whl/mabel/data/internals/storage_classes/__init__.py
@@ -0,0 +1,5 @@
+from .base_storage_class import BaseStorageClass
+from .storage_class_compressed_memory import StorageClassCompressedMemory
+from .storage_class_disk import StorageClassDisk
+from .storage_class_memory import StorageClassMemory
+from .storage_class_msgpack import StorageClassMsgPackMemory
