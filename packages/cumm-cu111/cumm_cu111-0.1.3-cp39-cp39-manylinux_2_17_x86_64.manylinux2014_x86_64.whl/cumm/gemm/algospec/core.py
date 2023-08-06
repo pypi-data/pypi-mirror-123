d21c200edbab811cc49766aaf34714937d19dcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:39 +0000

    Add package cumm-cu111/0.1.3/cumm_cu111-0.1.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cumm-cu111/cumm_cu111-0.1.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cumm/gemm/algospec/core.py
@@ -0,0 +1,58 @@
+import enum
+from typing import Dict, Tuple, List
+from cumm.gemm.core import MetaArray, metaseq, seq
+
+
+class GemmAlgo(enum.Enum):
+    Simt = "Simt"
+    SimtDP4A = "SimtDP4A"
+    SimtDP2A = "SimtDP2A"
+    Volta = "Volta"
+    Turing = "Turing"
+    Ampere = "Ampere"
+
+_GEMM_MIN_ARCH_TO_ALGO : List[Tuple[Tuple[int, int], List[str]]] = [
+    ((5, 2), [GemmAlgo.Simt.value]),
+    ((6, 1), [GemmAlgo.SimtDP4A.value, GemmAlgo.SimtDP2A.value]),
+    ((7, 0), [GemmAlgo.Volta.value]),
+    ((7, 5), [GemmAlgo.Turing.value]),
+    ((8, 0), [GemmAlgo.Ampere.value]),
+]
+
+_GEMM_ALGO_TO_MIN_ARCH: Dict[str, Tuple[int, int]] = {}
+
+for min_arch, algos in _GEMM_MIN_ARCH_TO_ALGO:
+    for algo in algos:
+        _GEMM_ALGO_TO_MIN_ARCH[algo] = min_arch
+
+def get_min_arch_of_algo(algo: GemmAlgo):
+    return _GEMM_ALGO_TO_MIN_ARCH[algo.value]
+
+def get_min_arch_of_algo_str(algo_str: str):
+    return _GEMM_ALGO_TO_MIN_ARCH[algo_str]
+
+def get_available_algo_str_from_arch(arch: Tuple[int, int]):
+    res: List[str] = []
+    for i in range(len(_GEMM_MIN_ARCH_TO_ALGO) - 1, -1, -1):
+        arch_cur, algos = _GEMM_MIN_ARCH_TO_ALGO[i]
+        if arch >= arch_cur:
+            res.extend(algos)
+    return res
+
+class ShuffleStrideType(enum.Enum):
+    NoShuffle = "NS"
+    # A and C have indices, for spatial spconv forward and backward input
+    ShuffleAC = "SAC"
+    # A and B have indices, for spatial spconv backward weight
+    ShuffleAB = "SAB"
+
+
+class TensorOpParams(object):
+    def __init__(self, shape: Tuple[int, int, int]):
+        self.shape = seq(*shape)
+
+    def to_string(self):
+        return f"{self.shape[0]}{self.shape[1]}{self.shape[2]}"
+
+    def __getitem__(self, val: int):
+        return self.shape[val]
