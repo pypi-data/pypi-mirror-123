7e0d674ff1a63b9c27fb15b38fd28b442efab8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:15 +0000

    Add package actionpack/1.4.0/actionpack-1.4.0.tar.gz

---
+++ packages/actionpack/actionpack-1.4.0.tar.gz/actionpack-1.4.0/actionpack/actions/__init__.py
@@ -0,0 +1,20 @@
+from actionpack.actions.call import Call
+from actionpack.actions.pipeline import Pipeline
+from actionpack.actions.make_request import MakeRequest
+from actionpack.actions.read_bytes import ReadBytes
+from actionpack.actions.read_input import ReadInput
+from actionpack.actions.retry_policy import RetryPolicy
+from actionpack.actions.serialization import Serialization
+from actionpack.actions.write_bytes import WriteBytes
+
+
+__all__ = [
+    'Call',
+    'MakeRequest',
+    'Pipeline',
+    'ReadBytes',
+    'ReadInput',
+    'RetryPolicy',
+    'Serialization',
+    'WriteBytes'
+]
---
+++ packages/actionpack/actionpack-1.4.0.tar.gz/actionpack-1.4.0/tests/actionpack/__init__.py
@@ -0,0 +1,48 @@
+from io import BytesIO
+from io import StringIO
+
+from actionpack import Action
+from actionpack.action import Name
+from actionpack.action import Outcome
+
+
+class FakeAction(Action[Name, Outcome]):
+
+    result = 'Performing Action.'
+
+    def __init__(self, name: Name = None, exception=None):
+        self.name = name
+        self.exception = exception
+        self.state = {'this': 'state'}
+
+    def instruction(self) -> str:
+        return self.result
+
+    def validate(self):
+        if self.exception:
+            raise self.exception
+        return super().validate()
+
+
+class FakeFile:
+    def __init__(self, contents: bytes = bytes(), mode: str = None):
+        self.buffer = BytesIO(contents)
+        self.buffer.read()
+        self.mode = mode
+
+    def read(self):
+        self.buffer.seek(0)
+        return self.buffer.read()
+
+    def write(self, data: bytes):
+        if self.mode == 'wb':
+            self.buffer.seek(0)
+            self.buffer.truncate()
+        self.buffer.write(data)
+        return len(data)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.closed = True
