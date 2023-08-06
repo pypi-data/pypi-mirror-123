7a4b4483489f0d5d13cf083b3844a0edebb52190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:49 +0000

    Add package notegame/0.3.11/notegame-0.3.11-py3-none-any.whl

---
+++ packages/notegame/notegame-0.3.11-py3-none-any.whl/notegame/shumo/load_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def load_tag_info(path):
+    tag_info = open(path).read()
+    tag_info = tag_info.replace('  ', ' ')
+    tag_info = tag_info.replace('  ', ' ')
+    tag_info = tag_info.replace('  ', ' ')
+    tag_info = tag_info.replace(':', '')
+    tag_info = tag_info.replace(' \n', '\n')
+    tag_info_list = tag_info.split('\n')
+
+    tmp = [[i for i in line.split(' ')] for line in tag_info_list[2:] if ' ' in line]
+
+    tag_df = pd.DataFrame(tmp)
+
+    tag_df.columns = ['anchor_id', 'x', 'y', 'z']
+    tag_df['anchor_id'] = tag_df['anchor_id'].astype('int')
+    return tag_df
