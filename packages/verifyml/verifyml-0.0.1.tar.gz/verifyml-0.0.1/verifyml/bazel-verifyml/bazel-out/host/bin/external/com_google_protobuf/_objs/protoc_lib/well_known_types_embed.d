29d65a9d94f7f30d2b3de6cd5abd69e7a2032df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:13 +0000

    Add package verifyml/0.0.1/verifyml-0.0.1.tar.gz

---
+++ packages/verifyml/verifyml-0.0.1.tar.gz/verifyml-0.0.1/verifyml/bazel-verifyml/bazel-out/host/bin/external/com_google_protobuf/_objs/protoc_lib/well_known_types_embed.d
@@ -0,0 +1,5 @@
+bazel-out/host/bin/external/com_google_protobuf/_objs/protoc_lib/well_known_types_embed.o: \
+ external/com_google_protobuf/src/google/protobuf/compiler/js/well_known_types_embed.cc \
+ /usr/include/stdc-predef.h \
+ external/com_google_protobuf/src/google/protobuf/compiler/js/well_known_types_embed.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h
