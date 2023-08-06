837f7406c5a046bbdc0fea1d81515757b7ed57c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:11 +0000

    Add package aesim.simba/2021.10.13/aesim.simba-2021.10.13.tar.gz

---
+++ packages/aesim.simba/aesim.simba-2021.10.13.tar.gz/aesim.simba-2021.10.13/aesim/simba/__init__.py
@@ -0,0 +1,21 @@
+#%% Load modules...
+import clr, sys, os
+
+foldername = os.path.join(os.path.dirname(os.path.abspath(__file__)),'Resources')
+sys.path.append(foldername)
+simba_dll_filepath = os.path.join(foldername,'Simba.Data.dll')
+clr.AddReference(simba_dll_filepath)
+
+from Simba.Data.Repository import ProjectRepository
+from Simba.Data import License, Design, Circuit, DesignExamples, ACSweep
+import Simba.Data
+Simba.Data.FunctionsAssemblyResolver.RedirectAssembly()
+Simba.Data.DoubleArrayPythonEncoder.Register()
+Simba.Data.Double2DArrayPythonEncoder.Register()
+Simba.Data.EnumPythonEncoder.Register()
+Simba.Data.ParameterToPythonEncoder.Register()
+
+Simba.Data.PythonToParameterDecoder.Register()
+
+if os.environ.get('SIMBA_DEPLOYMENT_KEY') is not None:
+    License.Activate(os.environ.get('SIMBA_DEPLOYMENT_KEY'))
