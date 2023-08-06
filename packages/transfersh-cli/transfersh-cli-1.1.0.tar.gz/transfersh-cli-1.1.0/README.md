00a491f95a2dc90646b49b43f047ba6ae6ca0ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:49 +0000

    Add package transfersh-cli/1.1.0/transfersh-cli-1.1.0.tar.gz

---
+++ packages/transfersh-cli/transfersh-cli-1.1.0.tar.gz/transfersh-cli-1.1.0/README.md
@@ -0,0 +1,61 @@
+# transfersh-cli
+
+![Demo](media/demo.svg)
+
+## Install
+
+``` bash
+pip3 install transfersh-cli
+```
+
+## Use
+
+``` bash
+trasnfersh [file]
+```
+
+Example
+
+``` bash
+trasnfersh photos.zip
+```
+
+Output
+
+``` bash
+Uploading file
+Download from here: https://transfer.sh/uDRFQ/upload_file
+It has also been copied to the clipboard!
+```
+
+In addition, the route will be saved in your clipboard. Ready to paste it where you want :wink:
+
+## Options
+
+### Maximum number of days to keep file
+
+`-a` or `--max-days`.
+
+``` bash
+trasnfersh [file] --max-days [number]
+```
+
+Example
+
+``` bash
+trasnfersh photos.zip --max-days 7
+```
+
+### Maximum number of times that file can be downloaded
+
+`-d` or `--max-downloads`.
+
+``` bash
+trasnfersh [file] --max-downloads [number]
+```
+
+Example
+
+``` bash
+trasnfersh photos.zip --max-downloads 2
+```
---
+++ packages/transfersh-cli/transfersh-cli-1.1.0.tar.gz/transfersh-cli-1.1.0/transfersh.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Libraries
+import os
+import requests
+import click
+import pyperclip
+
+# Variables
+URL_TRANSFERSH = 'https://transfer.sh'
+
+@click.command()
+@click.argument('filename')
+@click.option('-a', '--max-days', type=int, help='Maximum number of days to keep file')
+@click.option('-d', '--max-downloads', type=int, help='Maximum number of times that file can be downloaded')
+def transfersh_cli(filename, max_days, max_downloads):
+    """ Program that uploads a file to Transfer.sh """
+    try:
+        # Open file
+        with open(filename, 'rb') as data:
+            click.echo('Uploading file')
+            # Upload file
+            conf_file = {filename: data}
+            headers = {}
+            # Option to indicate the maximum number of days
+            if max_days is not None:
+                headers['Max-Days'] = str(max_days)
+            # Option to indicate the maximum number of downloads
+            if max_downloads is not None:
+                headers['Max-Downloads'] = str(max_downloads)
+            r = requests.post(URL_TRANSFERSH, files=conf_file, headers=headers)
+            # Shows route to download
+            download_url = r.text
+            click.echo(f'Download from here: {download_url}')
+            click.echo(f'It has also been copied to the clipboard!')
+            # Copy route to clipboard
+            pyperclip.copy(download_url)
+    except Exception:
+        click.echo('Something has failed. The file could not be uploaded.')
+
+if __name__ == '__main__':
+    transfersh_cli()
