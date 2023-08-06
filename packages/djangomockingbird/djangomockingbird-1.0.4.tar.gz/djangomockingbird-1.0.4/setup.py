55f98482e5c7ad5e1cc381ffe4df14a66ff9b4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:15 +0000

    Add package djangomockingbird/1.0.4/djangomockingbird-1.0.4.tar.gz

---
+++ packages/djangomockingbird/djangomockingbird-1.0.4.tar.gz/djangomockingbird-1.0.4/setup.py
@@ -0,0 +1,25 @@
+
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="djangomockingbird",
+    version="1.0.4",
+    description="The fastest way to write the fastest Django unit tests",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/larsvonschaff/Django-mockingbird",
+    author_email="larsvonschaff@gmail.com",
+    license="MIT",
+    install_requires=["django"],
+    packages=["djangomockingbird"],
+
+)
+
