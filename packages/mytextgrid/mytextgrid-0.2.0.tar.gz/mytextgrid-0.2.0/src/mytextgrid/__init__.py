f6cfc76699d336eb6b7a9f531db38f170de18b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:00 +0000

    Add package mytextgrid/0.2.0/mytextgrid-0.2.0.tar.gz

---
+++ packages/mytextgrid/mytextgrid-0.2.0.tar.gz/mytextgrid-0.2.0/src/mytextgrid/__init__.py
@@ -0,0 +1,5 @@
+"""A tiny python project for creating, reading, writing and querying Praat annotation files
+(TextGrid).
+"""
+from mytextgrid.textgrid.textgrid import create_textgrid
+from mytextgrid.parser.full_text_format import read_from_file
