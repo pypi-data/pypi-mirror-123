bb67a2b2736f8be3803614366e197812a0e6ad69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:49 +0000

    Add package thanker/0.0.5/thanker-0.0.5-py3-none-any.whl

---
+++ packages/thanker/thanker-0.0.5-py3-none-any.whl/thanker/__init__.py
@@ -0,0 +1,16 @@
+from ._thanker import Thanker
+from ._group_by import GroupBy
+
+
+__version__ = "0.0.5"
+__url__ = "https://github.com/WardPearce/thanker"
+__description__ = "Don't be a wanker, be a thanker!"
+__author__ = "WardPearce"
+__author_email__ = "wardpearce@protonmail.com"
+__license__ = " Apache-2.0"
+
+
+__all__ = [
+    "Thanker",
+    "GroupBy"
+]
