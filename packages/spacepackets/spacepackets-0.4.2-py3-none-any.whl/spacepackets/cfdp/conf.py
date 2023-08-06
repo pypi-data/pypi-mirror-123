fd55daad7021bf4f290b1c11d2c3586dbb13e139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:23 +0000

    Add package spacepackets/0.4.2/spacepackets-0.4.2-py3-none-any.whl

---
+++ packages/spacepackets/spacepackets-0.4.2-py3-none-any.whl/spacepackets/cfdp/conf.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import TypedDict, Tuple
+
+from spacepackets.cfdp.definitions import FileSize, TransmissionModes, CrcFlag, \
+    SegmentationControl, Direction
+from spacepackets.log import get_console_logger
+
+
+@dataclass
+class PduConfig:
+    """Common configuration fields for a PDU.
+
+    The default values for source entity ID and destination entity ID are the the GLOBAL_CONFIG
+    equivalent which should cause the header implementation to assume a globally configured
+    default value.
+    """
+    transaction_seq_num: bytes
+    trans_mode: TransmissionModes
+    file_size: FileSize = FileSize.GLOBAL_CONFIG
+    crc_flag: CrcFlag = CrcFlag.GLOBAL_CONFIG
+    direction: Direction = Direction.TOWARDS_RECEIVER
+    seg_ctrl: SegmentationControl = SegmentationControl.NO_RECORD_BOUNDARIES_PRESERVATION
+    source_entity_id: bytes = bytes()
+    dest_entity_id: bytes = bytes()
+
+    @classmethod
+    def empty(cls) -> PduConfig:
+        return PduConfig(
+            transaction_seq_num=bytes([0]),
+            trans_mode=TransmissionModes.ACKNOWLEDGED,
+            source_entity_id=bytes([0]),
+            dest_entity_id=bytes([0])
+        )
+
+
+class CfdpDict(TypedDict):
+    source_dest_entity_ids: Tuple[bytes, bytes]
+    with_crc: bool
+    file_size: FileSize
+
+
+# TODO: Protect dict access with a dedicated lock for thread-safety
+__CFDP_DICT: CfdpDict = {
+    'source_dest_entity_ids': (bytes(), bytes()),
+    'with_crc': False,
+    'file_size': FileSize.NORMAL,
+}
+
+
+def set_default_pdu_crc_mode(with_crc: bool):
+    __CFDP_DICT['with_crc'] = with_crc
+
+
+def get_default_pdu_crc_mode() -> bool:
+    return __CFDP_DICT['with_crc']
+
+
+def set_entity_ids(source_entity_id: bytes, dest_entity_id: bytes):
+    __CFDP_DICT['source_dest_entity_ids'] = (source_entity_id, dest_entity_id)
+
+
+def get_entity_ids() -> Tuple[bytes, bytes]:
+    """Return a tuple where the first entry is the source entity ID"""
+    return __CFDP_DICT['source_dest_entity_ids']
+
+
+def set_default_file_size(file_size: FileSize):
+    __CFDP_DICT['file_size'] = file_size
+
+
+def get_default_file_size() -> FileSize:
+    return __CFDP_DICT['file_size']
+
+
+def check_packet_length(raw_packet_len: int, min_len: int, warn_on_fail: bool = True) -> bool:
+    """Check whether the length of a raw packet is shorter than a specified expected minimum length.
+    By defaults, prints a warning if this is the case
+    :param raw_packet_len:
+    :param min_len:
+    :param warn_on_fail:
+    :return: Returns True if the raw packet is larger than the specified minimum length, False
+    otherwise
+    """
+    if raw_packet_len < min_len:
+        if warn_on_fail:
+            logger = get_console_logger()
+            logger.warning(
+                f'Detected packet length {raw_packet_len}, smaller than expected {min_len}'
+            )
+        return False
+    return True
---
+++ packages/spacepackets/spacepackets-0.4.2-py3-none-any.whl/spacepackets/cfdp/pdu/eof.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+import struct
+
+
+from spacepackets.cfdp.pdu.file_directive import FileDirectivePduBase, DirectiveCodes, \
+    ConditionCode
+from spacepackets.cfdp.conf import PduConfig
+from spacepackets.cfdp.tlv import CfdpTlv
+from spacepackets.cfdp.conf import check_packet_length
+
+
+class EofPdu:
+    """Encapsulates the EOF file directive PDU, see CCSDS 727.0-B-5 p.79"""
+    MINIMAL_LENGTH = FileDirectivePduBase.FILE_DIRECTIVE_PDU_LEN + 1 + 4 + 4
+
+    def __init__(
+        self,
+        file_checksum: int,
+        file_size: int,
+        pdu_conf: PduConfig,
+        fault_location: CfdpTlv = None,
+        condition_code: ConditionCode = ConditionCode.NO_ERROR,
+    ):
+        self.pdu_file_directive = FileDirectivePduBase(
+            directive_code=DirectiveCodes.EOF_PDU,
+            pdu_conf=pdu_conf,
+            directive_param_field_len=9
+        )
+        self.condition_code = condition_code
+        self.file_checksum = file_checksum
+        self.file_size = file_size
+        self.fault_location = fault_location
+
+    @property
+    def packet_len(self) -> int:
+        return self.pdu_file_directive.packet_len
+
+    @classmethod
+    def __empty(cls) -> EofPdu:
+        empty_conf = PduConfig.empty()
+        return cls(
+            file_checksum=0,
+            file_size=0,
+            pdu_conf=empty_conf
+        )
+
+    def pack(self) -> bytearray:
+        eof_pdu = bytearray()
+        eof_pdu.extend(self.pdu_file_directive.pack())
+        eof_pdu.append(self.condition_code << 4)
+        eof_pdu.extend(struct.pack('!I', self.file_checksum))
+        if self.pdu_file_directive.pdu_header.large_file:
+            eof_pdu.extend(struct.pack('!Q', self.file_size))
+        else:
+            eof_pdu.extend(struct.pack('!I', self.file_size))
+        if self.fault_location is not None:
+            eof_pdu.extend(self.fault_location.pack())
+        return eof_pdu
+
+    @classmethod
+    def unpack(cls, raw_packet: bytearray) -> EofPdu:
+        """Deserialize raw EOF PDU packet
+        :param raw_packet:
+        :raise ValueError: If raw packet is too short
+        :return:
+        """
+        eof_pdu = cls.__empty()
+        eof_pdu.pdu_file_directive = FileDirectivePduBase.unpack(raw_packet=raw_packet)
+        expected_min_len = cls.MINIMAL_LENGTH
+        if not check_packet_length(raw_packet_len=len(raw_packet), min_len=expected_min_len):
+            raise ValueError
+        current_idx = eof_pdu.pdu_file_directive.packet_len
+        eof_pdu.condition_code = raw_packet[current_idx] & 0xf0
+        expected_min_len = current_idx + 5
+        current_idx += 1
+        checksum_raw = raw_packet[current_idx: current_idx + 4]
+        eof_pdu.file_checksum = struct.unpack('!I', checksum_raw)[0]
+        current_idx += 4
+        current_idx, eof_pdu.file_size = eof_pdu.pdu_file_directive.parse_fss_field(
+            raw_packet=raw_packet, current_idx=current_idx
+        )
+        if len(raw_packet) > current_idx:
+            eof_pdu.fault_location = CfdpTlv.unpack(raw_bytes=raw_packet[current_idx:])
+        return eof_pdu
