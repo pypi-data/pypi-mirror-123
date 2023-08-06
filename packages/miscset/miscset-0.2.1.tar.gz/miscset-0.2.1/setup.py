c45eafa2cc67ffc6d98ce6fd2f2cf75b8267cc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:12 +0000

    Add package miscset/0.2.1/miscset-0.2.1.tar.gz

---
+++ packages/miscset/miscset-0.2.1.tar.gz/miscset-0.2.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+import miscset
+with open("README.md", "r", encoding = "utf-8") as fs:
+    long_description = fs.read()
+
+
+setuptools.setup(
+    name = "miscset",
+    version = miscset.version,
+    author = "Sven Templer",
+    author_email = "sven.templer@gmail.com",
+    description = "Miscellaneous set of helpful methods.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/setempler/miscset.py",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Shells",
+        "Topic :: Utilities",
+        "Topic :: Text Processing :: Markup :: HTML"
+    ],
+    install_requires = [
+        "pyyaml"
+    ],
+    python_requires = ">=3.6",
+)
