e2d08ab56e75ae8a2eacd76870cade5c4b7873e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:06 +0000

    Add package abacusai/0.31.0/abacusai-0.31.0.tar.gz

---
+++ packages/abacusai/abacusai-0.31.0.tar.gz/abacusai-0.31.0/abacusai/__init__.py
@@ -0,0 +1,3 @@
+from .client import ApiClient, ApiException, ClientOptions
+
+__version__ = "0.31.0"
---
+++ packages/abacusai/abacusai-0.31.0.tar.gz/abacusai-0.31.0/abacusai/schema.py
@@ -0,0 +1,27 @@
+from .nested_column import NestedColumn
+from .point_in_time_feature import PointInTimeFeature
+
+
+class Schema():
+    '''
+        A schema description for a column
+    '''
+
+    def __init__(self, client, name=None, columnMapping=None, columnDataType=None, custom=None, sql=None, selectExpression=None):
+        self.client = client
+        self.id = None
+        self.name = name
+        self.column_mapping = columnMapping
+        self.column_data_type = columnDataType
+        self.custom = custom
+        self.sql = sql
+        self.select_expression = selectExpression
+
+    def __repr__(self):
+        return f"Schema(name={repr(self.name)}, column_mapping={repr(self.column_mapping)}, column_data_type={repr(self.column_data_type)}, custom={repr(self.custom)}, sql={repr(self.sql)}, select_expression={repr(self.select_expression)})"
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self.id == other.id
+
+    def to_dict(self):
+        return {'name': self.name, 'column_mapping': self.column_mapping, 'column_data_type': self.column_data_type, 'custom': self.custom, 'sql': self.sql, 'select_expression': self.select_expression}
