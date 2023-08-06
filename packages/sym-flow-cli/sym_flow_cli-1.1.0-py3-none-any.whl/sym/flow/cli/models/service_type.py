1e8c1ee70af735fa29f8c65e19d86f752c6239ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:35 +0000

    Add package sym-flow-cli/1.1.0/sym_flow_cli-1.1.0-py3-none-any.whl

---
+++ packages/sym-flow-cli/sym_flow_cli-1.1.0-py3-none-any.whl/sym/flow/cli/models/service_type.py
@@ -0,0 +1,56 @@
+from enum import Enum
+from typing import Callable, List, NamedTuple, Optional
+
+
+class ServiceTypeMeta(NamedTuple):
+    type_name: str
+    external_id_name: str
+    validator: Optional[Callable[[str], bool]] = None
+    help_str: Optional[str] = None
+
+
+class ServiceType(ServiceTypeMeta, Enum):
+    """All Service Types recognized by the Sym API"""
+
+    @classmethod
+    def public(cls) -> List["ServiceType"]:
+        """Returns all ServiceTypes that can be edited by external users"""
+        return [s for s in cls if s != cls.SYM]
+
+    @classmethod
+    def all_public_names(cls) -> List[str]:
+        """Returns all service types names that can be edited by external users"""
+        return [str(s) for s in cls.public()]
+
+    @classmethod
+    def all_names(cls) -> List[str]:
+        """Returns all service type names"""
+        return [str(s) for s in ServiceType]
+
+    def __str__(self):
+        return self.value.type_name
+
+    SYM = ServiceTypeMeta("sym", "Org Slug")
+    SLACK = ServiceTypeMeta(
+        "slack",
+        "Workspace ID",
+        lambda id: id.startswith("T") and id.isupper(),
+        "Must start with 'T' and be all upper case",
+    )
+    AWS_SSO = ServiceTypeMeta(
+        "aws_sso",
+        "Instance ARN",
+        lambda arn: arn.startswith("arn:aws:sso:::instance/"),
+        "Must start with 'arn:aws:sso:::instance/'",
+    )
+    PAGERDUTY = ServiceTypeMeta("pagerduty", "Account Number")
+    AWS_IAM = ServiceTypeMeta(
+        "aws_iam",
+        "Account #",
+        lambda acct_num: acct_num.isdigit() and len(acct_num) == 12,
+        "Must be a 12 digit account number",
+    )
+    APTIBLE = ServiceTypeMeta("aptible", "Organization ID")
+    GOOGLE = ServiceTypeMeta("google", "Email Domain")
+    AUTH0 = ServiceTypeMeta("auth0", "External ID")
+    OKTA = ServiceTypeMeta("okta", "Okta Domain")
