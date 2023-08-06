04ccafe664dae511b0b1d00945d1472b41cf4ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:05 +0000

    Add package torchsolver/1.4/torchsolver-1.4-py3-none-any.whl

---
+++ packages/torchsolver/torchsolver-1.4-py3-none-any.whl/torchsolver/__init__.py
@@ -0,0 +1,7 @@
+from .config import Config
+from .solver import Solver, GANSolver
+from .module import Module, GANModule
+
+import torchsolver.nn as nn
+import torchsolver.nets as nets
+import torchsolver.datasets as datasets
