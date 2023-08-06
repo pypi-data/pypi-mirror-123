f3f81d1133f5562bcdea9a513673b1a92f597126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:51 +0000

    Add package AppZoo/2021.10.12.21.28.14/appzoo-2021.10.12.21.28.14.tar.gz

---
+++ packages/AppZoo/appzoo-2021.10.12.21.28.14.tar.gz/appzoo-2021.10.12.21.28.14/appzoo/apps_streamlit/st_pandas_profiling.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AppZoo.
+# @File         : st_pandas_profiling
+# @Time         : 2021/10/12 下午8:39
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+# from sklearn.datasets import load_iris
+# import streamlit as st
+
+
+def app():
+    import streamlit as st
+    import pandas_profiling
+
+    from sklearn.datasets import load_iris
+    from streamlit_pandas_profiling import st_profile_report
+
+    st.markdown(f"""
+        # 一键生成数据报告
+        """)
+
+    process_func = eval(st.text_input('数据预处理，支持 lambda', value="""pd.read_csv"""))  # pd.read_excel
+    uploaded_file = st.file_uploader('File uploader')
+
+    if uploaded_file is not None:
+        df = process_func(uploaded_file)
+
+    if st.sidebar.checkbox('Demo', value=False):
+        df = pd.concat(load_iris(1, 1), 1)
+
+
+
+    pr = df.profile_report()
+    st_profile_report(pr)
