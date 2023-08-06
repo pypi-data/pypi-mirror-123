2d5bd089f5d81c51cd8a1a59f39bae95850942a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:21 +0000

    Add package drb-impl-image/1.0a2/drb_impl_image-1.0a2-py3-none-any.whl

---
+++ packages/drb-impl-image/drb_impl_image-1.0a2-py3-none-any.whl/drb_impl_image/image_list_node.py
@@ -0,0 +1,27 @@
+from typing import Any, List, Dict, Optional, Tuple
+
+from drb import DrbNode
+from drb.exceptions import DrbNotImplementationException
+from drb.utils.logical_node import DrbLogicalNode
+
+
+class DrbImageListNode(DrbLogicalNode):
+
+    def __init__(self, parent: DrbNode, name: str):
+        path = parent.path / name
+        DrbLogicalNode.__init__(self, path, parent=parent)
+        self._children: List[DrbNode] = []
+
+    @property
+    def attributes(self) -> Dict[Tuple[str, str], Any]:
+        return {}
+
+    @property
+    def children(self) -> List[DrbNode]:
+        return self._children
+
+    def has_impl(self, impl: type) -> bool:
+        return False
+
+    def get_impl(self, impl: type) -> Any:
+        raise DrbNotImplementationException(f'no {impl} implementation found')
