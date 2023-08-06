7e6f6017465aaca7e95176507128926c8fcc2e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:39 +0000

    Add package iar-wiki-parser/1.0.0/iar_wiki_parser-1.0.0.tar.gz

---
+++ packages/iar-wiki-parser/iar_wiki_parser-1.0.0.tar.gz/iar_wiki_parser-1.0.0/iar_wiki_parser/wikipedia_parser_consts.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+u"""
+"""
+
+HEADER = '<mediawiki version="0.10" xml:lang="es">\n'
+FOOTER = "</mediawiki>"
+
+DEBUG = False
+DIVISIONES = 100
+
+PALABRAS_MINIMAS_SNIPPET = 100
+VERSION = u''
+MARCA_FIN_ARTICULO = u'~~~~ Fin artículo ~~~~\n'
