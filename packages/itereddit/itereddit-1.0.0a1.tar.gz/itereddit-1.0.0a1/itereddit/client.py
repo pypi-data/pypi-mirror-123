438515e371d230dd0ed4377b936398edeb1a71f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:15 +0000

    Add package itereddit/1.0.0a1/itereddit-1.0.0a1.tar.gz

---
+++ packages/itereddit/itereddit-1.0.0a1.tar.gz/itereddit-1.0.0a1/itereddit/client.py
@@ -0,0 +1,56 @@
+from asyncio import Queue
+from typing import AsyncIterator
+
+from httpx import AsyncClient
+
+from itereddit.models.subreddit_posts import SubredditPiece, SubredditPost
+
+
+class Itereddit:
+    """
+    A handy tool for parsing posts in subreddit
+
+    :param subreddit: name of subreddit
+    :type subreddit: str
+    """
+
+    def __init__(self, subreddit: str = "rate_my_dick"):
+        self.subreddit = subreddit
+        self._client = None
+        self.__last_post = None
+        self.__posts_queue = Queue()
+
+    @property
+    def client(self):
+        if not self._client:
+            self._client = AsyncClient()
+        return self._client
+
+    @property
+    def url(self):
+        return f"https://gateway.reddit.com/desktopapi/v1/subreddits/{self.subreddit}"
+
+    def params(self, after: str = None):
+        return {
+            "rtj": "only",
+            "allow_over18": 1,
+            "include": "prefsSubreddit",
+            "after": after,
+            "forceGeopopular": False,
+            "sort": "new",
+        }
+
+    def __aiter__(self) -> AsyncIterator[SubredditPost]:
+        return self
+
+    async def __anext__(self) -> SubredditPost:
+        if not self.__posts_queue.qsize():
+            response = await self.client.get(
+                self.url, params=self.params(self.__last_post)
+            )
+            if response.status_code == 200:
+                piece = SubredditPiece(**response.json())
+                for post_id in piece.posts:
+                    await self.__posts_queue.put(piece.posts[post_id])
+                self.__last_post = piece.post_ids[-1]
+        return await self.__posts_queue.get()
