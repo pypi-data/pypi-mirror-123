b9eea06e895658130e2c415323674d01961c9778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:37 +0000

    Add package cumm-cu102/0.1.3/cumm_cu102-0.1.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cumm-cu102/cumm_cu102-0.1.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cumm/__init__.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import pccm
+from pccm.utils import project_is_editable, project_is_installed
+
+from .constants import PACKAGE_NAME
+
+if project_is_installed(PACKAGE_NAME) and project_is_editable(PACKAGE_NAME):
+    from cumm.constants import PACKAGE_ROOT
+    from cumm.csrc.arrayref import ArrayPtr
+    from cumm.tensorview_bind import TensorViewBind
+
+    pccm.builder.build_pybind([ArrayPtr(), TensorViewBind()],
+                              PACKAGE_ROOT / "core_cc",
+                              namespace_root=PACKAGE_ROOT,
+                              load_library=False)
