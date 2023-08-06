1d27ab2af468c9d6213a99430c3466088d68d1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:46 +0000

    Add package apysc/0.54.2/apysc-0.54.2.tar.gz

---
+++ packages/apysc/apysc-0.54.2.tar.gz/apysc-0.54.2/apysc/_animation/animation_interface_base.py
@@ -0,0 +1,15 @@
+"""Base class for each animation interface.
+"""
+
+from apysc._animation.animation_finish_interface import \
+    AnimationFinishInterface
+from apysc._animation.animation_pause_interface import AnimationPauseInterface
+from apysc._animation.animation_play_interface import AnimationPlayInterface
+from apysc._animation.animation_reset_interface import AnimationResetInterface
+
+
+class AnimationInterfaceBase(
+        AnimationPauseInterface, AnimationPlayInterface,
+        AnimationResetInterface, AnimationFinishInterface):
+    """Base class for each animation interface.
+    """
