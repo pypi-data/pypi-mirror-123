64ac4279f5b46371a11d13255507f71459482742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:48 +0000

    Add package torchsolver/1.0/torchsolver-1.0.tar.gz

---
+++ packages/torchsolver/torchsolver-1.0.tar.gz/torchsolver-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="torchsolver",
+    version="1.0",
+    author="killf",
+    author_email="killf@foxmail.com",
+    description="A pytorch based deep learning solver framework.",
+    url="https://github.com/killf/torchsolver",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
