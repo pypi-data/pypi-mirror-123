4e885abb3c66a2c4550a21f0657f69ed239b1c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:11 +0000

    Add package taktile-cli/0.8.2b0/taktile_cli-0.8.2b0-py2.py3-none-any.whl

---
+++ packages/taktile-cli/taktile_cli-0.8.2b0-py2.py3-none-any.whl/tktl/core/future/t.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import typing as t
+
+import numpy as np  # type: ignore
+import pandas as pd  # type: ignore
+
+from tktl.core import ExtendedEnum
+
+
+class EndpointKinds(str, ExtendedEnum):
+    GENERIC = "generic"
+    TYPED = "typed"
+    ARROW = "arrow"
+    PROFILED = "profiled"
+
+
+class ProfileKinds(str, ExtendedEnum):
+    BINARY = "binary"
+    REGRESSION = "regression"
+
+
+class ArrowFormatKinds(str, ExtendedEnum):
+    DATAFRAME = "dataframe"
+    SERIES = "series"
+    ARRAY = "array"
+
+    @staticmethod
+    def from_type(t: t.Union[pd.DataFrame, pd.Series, np.ndarray]) -> ArrowFormatKinds:
+        if isinstance(t, pd.DataFrame):
+            return ArrowFormatKinds.DATAFRAME
+        if isinstance(t, pd.Series):
+            return ArrowFormatKinds.SERIES
+        if isinstance(t, np.ndarray):
+            return ArrowFormatKinds.ARRAY
+        raise ValueError(f"{type(t)} is not an arrow communication type")
---
+++ packages/taktile-cli/taktile_cli-0.8.2b0-py2.py3-none-any.whl/tktl/future/settings.py
@@ -0,0 +1,8 @@
+"""Module for user exposed env settings"""
+
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    TAKTILE_GIT_SHA: str = "unknown"
