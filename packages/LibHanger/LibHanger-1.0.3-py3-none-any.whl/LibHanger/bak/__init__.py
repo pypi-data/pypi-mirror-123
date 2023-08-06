ba5c4db586a6d646942d9ad15ed9a163c64d1bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:11 +0000

    Add package LibHanger/1.0.3/LibHanger-1.0.3-py3-none-any.whl

---
+++ packages/LibHanger/LibHanger-1.0.3-py3-none-any.whl/LibHanger/bak/__init__.py
@@ -0,0 +1,10 @@
+from LibHanger.Library.uwConfig import cmnConfig
+import LibHanger.Library.uwGetter as uwGetter
+import LibHanger.Library.uwLogger as uwLogger
+
+__copyright__    = 'Copyright (C) 2021 Unchain Works'
+__version__      = '1.0.1'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Unchain Works'
+__author_email__ = 'kajin0318@gmail.com'
+__url__          = 'https://github.com/kaioman/LibHanger.git'
