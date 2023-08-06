ec91ce8f459c95eea357af3a56aba1c3920e4165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:54 +0000

    Add package gurunudi/2.0.2/gurunudi-2.0.2-py2.py3-none-any.whl

---
+++ packages/gurunudi/gurunudi-2.0.2-py2.py3-none-any.whl/gurunudi/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Generic URI of the Gurunudi AI API"""
+API_URL = "https://api.gurunudi.com/{}/{}"	
+
+"""Gurunudi AI API accepts and sents JSON content only"""
+HEADERS = {'Content-type': 'application/json; charset=utf-8', 'Accept': 'application/json','User-Agent':'Gurunudi Python Client 2.0.2','gnapi':'demo'}
+
+DEBUG = False
