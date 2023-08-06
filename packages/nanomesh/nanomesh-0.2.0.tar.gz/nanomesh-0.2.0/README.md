f83027621e153f6431e6b5cc98d653309f0f3548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:31 +0000

    Add package nanomesh/0.2.0/nanomesh-0.2.0.tar.gz

---
+++ packages/nanomesh/nanomesh-0.2.0.tar.gz/nanomesh-0.2.0/README.md
@@ -0,0 +1,101 @@
+[![Documentation Status](https://readthedocs.org/projects/nanomesh/badge/?version=latest)](https://nanomesh.readthedocs.io/en/latest/?badge=latest)
+[![Linux](https://github.com/hpgem/nanomesh/actions/workflows/test_on_linux.yml/badge.svg)](https://github.com/hpgem/nanomesh/actions/workflows/test_on_linux.yml)
+[![MacOS](https://github.com/hpgem/nanomesh/actions/workflows/test_on_macos.yaml/badge.svg)](https://github.com/hpgem/nanomesh/actions/workflows/test_on_macos.yaml)
+[![Windows](https://github.com/hpgem/nanomesh/actions/workflows/test_on_windows.yaml/badge.svg)](https://github.com/hpgem/nanomesh/actions/workflows/test_on_windows.yaml)
+
+![Nanomesh banner](./notebooks/banner/banner.png)
+
+# nanomesh
+
+Creates 3d meshes from microscopy experimental data.
+
+Documentation: https://nanomesh.readthedocs.io/en/latest/
+
+## Installation
+
+If you use conda, create a new environment:
+
+```
+conda create -n nanomesh python=3.8
+conda activate nanomesh
+```
+
+Install nanomesh:
+
+```
+git clone https://github.com/hpgem/nanomesh.git
+cd nanomesh
+pip install .
+```
+
+Note, [to enable the IPython widgets](https://ipywidgets.readthedocs.io/en/latest/user_install.html#installation):
+
+```
+jupyter nbextension enable --py widgetsnbextension
+```
+
+Note, [if you are using Jupyter lab](https://github.com/InsightSoftwareConsortium/itkwidgets#installation):
+
+```
+jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-matplotlib jupyterlab-datawidgets itkwidgets
+```
+
+### With pygalmesh (linux only)
+
+To use some of the additional functionality depending on pygalmesh,
+first install the dependencies for pygalmesh (CGAL and EIGEN3)
+
+```
+sudo apt-get install libcgal-dev libeigen3-dev
+```
+
+The instructions are the same as above, except specify `with_pygalmesh` as option
+to trigger the installation of pygalmesh. Currently this installs
+[our fork](https://github.com/hpgem/pygalmesh).
+
+```
+pip install .[with_pygalmesh]
+```
+
+### Development
+
+Install `nanomesh` using the development dependencies:
+
+`pip install -e .[develop] -c constraints.txt`
+
+Running the tests:
+
+`pytest`
+
+Linting/checks:
+
+`pre-commit`
+
+Building the docs:
+
+```
+make html --directory docs
+```
+
+
+### License
+
+Copyright (c) 2020,
+
+Licensed under the Apache License, Version 2.0 (the \"License\"); you
+may not use this file except in compliance with the License. You may
+obtain a copy of the License at
+
+<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an \"AS IS\" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+### Credits
+
+This package was created with
+[Cookiecutter](https://github.com/audreyr/cookiecutter) and the
+[NLeSC/python-template](https://github.com/NLeSC/python-template).
---
+++ packages/nanomesh/nanomesh-0.2.0.tar.gz/nanomesh-0.2.0/nanomesh/mesh3d.py
@@ -0,0 +1,344 @@
+import logging
+from dataclasses import dataclass
+from typing import List, Tuple, Union
+
+import matplotlib.pyplot as plt
+import meshio
+import numpy as np
+from skimage import measure, morphology
+
+from nanomesh import Volume
+from nanomesh.mesh_utils import simple_triangulate
+
+from ._mesh_shared import BaseMesher
+from .mesh_container import TriangleMesh
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class BoundingBox:
+    """Container for bounding box coordinates."""
+    xmin: float
+    xmax: float
+    ymin: float
+    ymax: float
+    zmin: float
+    zmax: float
+
+    @classmethod
+    def from_shape(cls, shape):
+        xmin, ymin, zmin = 0, 0, 0
+        xmax, ymax, zmax = np.array(shape) - 1
+        return cls(xmin=xmin,
+                   ymin=ymin,
+                   zmin=zmin,
+                   xmax=xmax,
+                   ymax=ymax,
+                   zmax=zmax)
+
+
+def get_point_in_prop(
+        prop: measure._regionprops.RegionProperties) -> np.ndarray:
+    """Uses `skeletonize` to find a point in the center of the regionprop.
+
+    Parameters
+    ----------
+    prop : RegionProperties
+        RegionProp from skimage.measure.regionproperties.
+
+    Returns
+    -------
+    point : (3,) np.array
+        Returns 3 indices describing a pixel in the labeled region.
+    """
+    skeleton = morphology.skeletonize(prop.image)
+    coords = np.argwhere(skeleton)
+    middle = len(coords) // 2
+    try:
+        point = coords[middle]
+        point += np.array(prop.bbox[0:3])  # Add prop offset
+    except IndexError:
+        point = np.array(prop.centroid)
+    return point
+
+
+def get_region_markers(
+        vol: Union[Volume, np.ndarray]) -> List[Tuple[int, np.ndarray]]:
+    """Get region markers describing the featuers in the volume.
+
+    The array will be labeled, and points inside the labeled region
+    will be obtained using the `skeletonize` function. The region
+    markers can be used to flood the connected regions in the
+    tetrahedralization step.
+
+    Parameters
+    ----------
+    vol : Union[Volume, np.array]
+        Segmented integer volume.
+
+    Returns
+    -------
+    region_markers : List[tuple]
+        List of tuples. The first element is the label in the source image,
+        and the second the pixel coordinates somewhere in the center of the
+        corresponding region.
+    """
+    region_markers = []
+
+    if isinstance(vol, Volume):
+        image = vol.image
+    else:
+        image = vol
+
+    labels = measure.label(image, background=-1, connectivity=1)
+
+    props = measure.regionprops(labels, intensity_image=image)
+
+    for prop in props:
+        point = get_point_in_prop(prop)
+        i, j, k = point.astype(int)
+        label = image[i, j, k]
+        region_markers.append((label, point))
+
+    return region_markers
+
+
+def add_corner_points(mesh: TriangleMesh, bbox: BoundingBox) -> None:
+    """Add corner points from bounding box to mesh vertices.
+
+    Parameters
+    ----------
+    mesh : TriangleMesh
+        Mesh to add corner points to.
+    bbox : BoundingBox
+        Container for the bounding box coordinates.
+    """
+    corners = np.array([
+        [bbox.xmin, bbox.ymin, bbox.zmin],
+        [bbox.xmin, bbox.ymin, bbox.zmax],
+        [bbox.xmin, bbox.ymax, bbox.zmin],
+        [bbox.xmin, bbox.ymax, bbox.zmax],
+        [bbox.xmax, bbox.ymin, bbox.zmin],
+        [bbox.xmax, bbox.ymin, bbox.zmax],
+        [bbox.xmax, bbox.ymax, bbox.zmin],
+        [bbox.xmax, bbox.ymax, bbox.zmax],
+    ])
+
+    mesh.vertices = np.vstack([mesh.vertices, corners])
+
+
+def close_side(mesh, *, side: str, bbox: BoundingBox, ax: plt.Axes = None):
+    """Fill a side of the bounding box with triangles.
+
+    Parameters
+    ----------
+    mesh : TriangleMesh
+        Input contour mesh.
+    side : str
+        Side of the volume to close. Must be one of
+        `left`, `right`, `top`, `bottom`, `front`, `back`.
+    bbox : BoundingBox
+        Coordinates of the bounding box.
+    ax : plt.Axes, optional
+        Plot the generated side on a matplotlib axis.
+
+    Returns
+    -------
+    mesh : TriangleMesh
+        Triangle mesh with the given side closed.
+
+    Raises
+    ------
+    ValueError
+        When the value of `side` is invalid.
+    """
+    all_verts = mesh.vertices
+
+    if side == 'top':
+        edge_col = 2
+        edge_value = bbox.zmin
+    elif side == 'bottom':
+        edge_col = 2
+        edge_value = bbox.zmax
+    elif side == 'left':
+        edge_col = 1
+        edge_value = bbox.ymin
+    elif side == 'right':
+        edge_col = 1
+        edge_value = bbox.ymax
+    elif side == 'front':
+        edge_col = 0
+        edge_value = bbox.xmin
+    elif side == 'back':
+        edge_col = 0
+        edge_value = bbox.xmax
+    else:
+        raise ValueError('Side must be one of `right`, `left`, `bottom`'
+                         f'`top`, `front`, `back`. Got {side=}')
+
+    keep_cols = [col for col in (0, 1, 2) if col != edge_col]
+    is_edge = all_verts[:, edge_col] == edge_value
+
+    coords = all_verts[is_edge][:, keep_cols]
+
+    edge_mesh = simple_triangulate(vertices=coords, opts='')
+
+    mesh_edge_index = np.argwhere(is_edge).flatten()
+    new_edge_index = np.arange(len(mesh_edge_index))
+    mapping = np.vstack([new_edge_index, mesh_edge_index])
+
+    shape = edge_mesh.faces.shape
+    new_faces = edge_mesh.faces.copy().ravel()
+
+    mask = np.in1d(new_faces, mapping[0, :])
+    new_faces[mask] = mapping[1,
+                              np.searchsorted(mapping[0, :], new_faces[mask])]
+    new_faces = new_faces.reshape(shape)
+
+    new_labels = np.ones(len(new_faces)) * 123
+
+    vertices = all_verts
+    faces = np.vstack([mesh.faces, new_faces])
+    labels = np.hstack([mesh.labels, new_labels])
+
+    mesh = TriangleMesh(vertices=vertices, faces=faces, labels=labels)
+
+    if ax:
+        edge_mesh.plot(ax=ax)
+        ax.set_title(side)
+
+    return mesh
+
+
+def generate_envelope(mesh: TriangleMesh,
+                      *,
+                      bbox: BoundingBox,
+                      plot: bool = False) -> TriangleMesh:
+    """Wrap the surface mesh and close any open contours along the bounding
+    box.
+
+    Parameters
+    ----------
+    mesh : TriangleMesh
+        Input mesh.
+    bbox : BoundingBox
+        Coordinates of the bounding box.
+
+    Returns
+    -------
+    TriangleMesh
+        Ouput mesh.
+    """
+    add_corner_points(mesh, bbox=bbox)
+
+    sides = 'top', 'bottom', 'left', 'right', 'front', 'back'
+
+    for i, side in enumerate(sides):
+        mesh = close_side(mesh, side=side, bbox=bbox)
+
+    return mesh
+
+
+class Mesher3D(BaseMesher):
+    def __init__(self, image: np.ndarray):
+        super().__init__(image)
+        self.contour: TriangleMesh
+        self.pad_width = 0
+
+    def generate_contour(
+        self,
+        level: float = None,
+    ):
+        """Generate contours using marching cubes algorithm.
+
+        Also generates an envelope around the entire data volume
+        corresponding to the bounding box.
+
+        The bounding box equals the dimensions of the data volume.
+
+        Parameters
+        ----------
+        level : float, optional
+            Contour value to search for isosurfaces (i.e. the threshold value).
+            By default takes the average of the min and max value. Can be
+            ignored if a binary image is passed to `Mesher3D`.
+        """
+        verts, faces, *_ = measure.marching_cubes(
+            self.image,
+            level=level,
+            allow_degenerate=False,
+        )
+
+        mesh = TriangleMesh(vertices=verts, faces=faces)
+
+        bbox = BoundingBox.from_shape(self.image.shape)
+        mesh = generate_envelope(mesh, bbox=bbox)
+
+        logger.info(f'Generated contour with {len(mesh.faces)} faces')
+
+        self.contour = mesh
+
+    def tetrahedralize(self, generate_region_markers: bool = False, **kwargs):
+        """Tetrahedralize a surface contour mesh.
+
+        Parameters
+        ----------
+        generate_region_markers : bool, optional
+            Attempt to automatically generate region markers.
+        **kwargs
+            Keyword arguments passed to
+            `nanomesh.mesh_container.TriangleMesh.tetrahedralize`
+
+        Returns
+        -------
+        TetraMesh
+
+        Raises
+        ------
+        ValueError
+            Description
+        """
+        if not self.contour:
+            raise ValueError('No contour mesh available.'
+                             'Run `Mesher3D.generate_contour()` first.')
+
+        if generate_region_markers:
+            region_markers = get_region_markers(self.image)
+            kwargs['region_markers'] = region_markers
+
+        contour = self.contour
+        volume_mesh = contour.tetrahedralize(**kwargs)
+        return volume_mesh
+
+
+def generate_3d_mesh(
+    image: np.ndarray,
+    *,
+    level: float = None,
+    **kwargs,
+) -> 'meshio.Mesh':
+    """Generate mesh from binary (segmented) image.
+
+    Parameters
+    ----------
+    image : 3D np.ndarray
+        Input image to mesh.
+    level : float, optional
+        Contour value to search for isosurfaces (i.e. the threshold value).
+        By default takes the average of the min and max value. Can be
+        ignored if a binary image is passed as `image`.
+    **kwargs
+        Optional keyword arguments passed to
+        `nanomesh.mesh_container.TriangleMesh.tetrahedralize`
+
+    Returns
+    -------
+    volume_mesh : TetraMesh
+        Description of the mesh.
+    """
+    mesher = Mesher3D(image)
+    mesher.generate_contour(level=level)
+
+    volume_mesh = mesher.tetrahedralize(**kwargs)
+    return volume_mesh
