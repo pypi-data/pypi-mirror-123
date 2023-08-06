866f920d4254cfd3c2b95fe7f7e9138283264065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:36 +0000

    Add package emailservice/0.0.4/emailservice-0.0.4.tar.gz

---
+++ packages/emailservice/emailservice-0.0.4.tar.gz/emailservice-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Email Service Package'
+LONG_DESCRIPTION = 'A package that allows to send emails with ease.'
+
+# Setting up package settings
+setup(
+    name="emailservice",
+    version=VERSION,
+    author="Chaitanya Patil",
+    author_email="<cdpgdg@gmail.com>",
+    url="https://github.com/Chaitanyadp/EmailService.git",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    keywords=['email', 'smtp'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
---
+++ packages/emailservice/emailservice-0.0.4.tar.gz/emailservice-0.0.4/src/__init__.py
@@ -0,0 +1,5 @@
+from emailservice import EmailService
+
+def setup():
+    print("Welcome to EmailService, To get started, please turn on less secure apps in gmail settings.")
+    return "Done"
