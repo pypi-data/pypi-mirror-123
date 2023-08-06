40840a22a76da575539cbe130b35b405ef67dd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:26 +0000

    Add package ttpypi/0.0.1/ttpypi-0.0.1-py3-none-any.whl

---
+++ packages/ttpypi/ttpypi-0.0.1-py3-none-any.whl/tpypi/oh.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/10/15 4:43 下午 
+# @Author : A Big Boby
+# @File : oh.py 
+# @Software: PyCharm
+
+
+def output():
+    print('今天也是充满希望的一天')
+
