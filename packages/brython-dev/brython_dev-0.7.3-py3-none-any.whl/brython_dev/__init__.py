2dbb6329586b2e9845de3bcecc9abbc76a7ad666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:37 +0000

    Add package brython-dev/0.7.3/brython_dev-0.7.3-py3-none-any.whl

---
+++ packages/brython-dev/brython_dev-0.7.3-py3-none-any.whl/brython_dev/__init__.py
@@ -0,0 +1,91 @@
+import sysconfig
+from pathlib import Path
+
+import pkg_resources
+import yaml
+from flask import Flask, escape, render_template_string, send_from_directory
+
+try:
+    __version__ = pkg_resources.get_distribution("brython-dev").version
+except pkg_resources.DistributionNotFound:  # pragma: no cover
+    __version__ = "unknown"
+
+    
+INDEX_TEMPLATE = """
+<!doctype html>
+<html>
+<head>
+    <title>{{ config.get("NAME") or "Unnamed" }}</title>
+    <meta charset="utf-8">
+    {% for stylesheet in config.get("STYLESHEETS") or [] %}
+    <link rel="stylesheet" href="{{ stylesheet }}">{% endfor %}
+    {% if config.get("EXTENSIONS", {}).get("brython", True) %}
+    <script type="text/javascript" src="/brython.js"></script>{% endif %}{% if config.get("EXTENSIONS", {}).get("brython_stdlib", False) %}
+    <script type="text/javascript" src="/brython_stdlib.js"></script>{% endif %}{% for script in config.get("SCRIPTS") or [] %}
+    <script type="text/javascript" src="{{ script }}"></script>{% endfor %}
+</head>
+<body onload="brython({{ config.get('BRYTHON_OPTIONS', {'debug': 1})|pretty_dict }})">
+    <script id="load.py" type="text/python3">
+    from browser import document, html
+    document.select("body")[0] <= html.TEMPLATE(open("{{ config.get("TEMPLATE", "app.html") }}").read()).content
+    </script>
+    {% if config.get("CONSOLE", True) and config.get("EXTENSIONS", {}).get("brython_stdlib", False) %}
+    <script id="console.py" type="text/python3">
+    from interpreter import Interpreter
+    import sys
+    sys.stdout = sys.stderr = Interpreter()
+    print("\\n")
+    </script>
+    {% endif %}
+    <script id="app.py" type="text/python3" src="{{ config.get('APP', 'app.py') }}"></script>
+</body>
+</html>
+"""
+
+def create_app(config: dict = {}) -> Flask:
+    config_file = Path(config.get("CONFIG_FILE", "brython.yml")).resolve()
+
+    config_yaml = (
+        {k.upper(): v for k, v in yaml.safe_load(config_file.read_text()).items()}
+        if config_file.exists()
+        else {"NAME": "Unnamed"}
+    )
+
+    proyect = Path.cwd()
+    if Path(config_yaml["NAME"].lower().replace("-", "_")).is_dir():  # pragma: no cover
+        proyect = Path(config_yaml["NAME"].lower().replace("-", "_"))
+
+    app = Flask(
+        __name__,
+        static_folder=str(proyect.resolve()),
+        static_url_path=config_yaml.get("STATIC_URL", "/"),
+    )
+
+    app.config.from_mapping(config_yaml)
+    app.config.from_mapping(config)
+    
+    @app.template_filter()
+    def pretty_dict(_dict):
+        return f"{{{', '.join(f'{k}: {v}' for k, v in _dict.items())}}}"
+
+    @app.route("/")
+    def index():
+        return render_template_string(INDEX_TEMPLATE)
+
+    @app.route("/brython.js")
+    def brythonjs():
+        return send_from_directory(
+            sysconfig.get_path("purelib"), "brython/data/brython.js"
+        )
+
+    @app.route("/brython_stdlib.js")
+    def brythonstdlibjs():
+        return send_from_directory(
+            sysconfig.get_path("purelib"), "brython/data/brython_stdlib.js"
+        )
+
+    @app.route("/Lib/site-packages/<path:filename>")
+    def site_packages(filename: str):
+        return send_from_directory(sysconfig.get_path("purelib"), filename)
+
+    return app
---
+++ packages/brython-dev/brython_dev-0.7.3-py3-none-any.whl/brython_dev/__main__.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+import click
+import yaml
+from flask import Flask, render_template_string, current_app
+from flask.cli import FlaskGroup
+from flask.cli import with_appcontext
+from flask.globals import _app_ctx_stack
+
+from brython_dev import __version__, create_app, INDEX_TEMPLATE
+
+MAIN_TEMPLATE = """
+import argparse
+
+def install(args):
+    print(args)
+
+parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers()
+
+install = subparser.add_parser('install', help='Install {name} in an empty directory')
+install.set_defaults(func=install)
+
+"""
+
+PYTHON_TEMPLATE = """from browser import document, html
+
+document['app'] <= html.SPAN('Hello Brython!')
+"""
+HTML_TEMPLATE = """<div id="app"></div>"""
+
+
+@click.group(cls=FlaskGroup, create_app=create_app, add_version_option=False)
+@click.version_option(version=__version__)
+def cli():
+    """Management script for brython developers."""
+
+
+@cli.command()
+@click.option("--name", prompt=True, help="Proyect name")
+@click.option("--app", prompt=True, help="Proyect app", default="app.py")
+@click.option("--template", prompt=True, help="Proyect template", default="app.html")
+def init(name, app, template):
+    """Creates a basic brython.yml file in the current directory."""
+
+    safe_name = Path(name.lower().replace("-", "_"))
+    root = safe_name if safe_name.is_dir() else Path.cwd()
+
+    with Path("brython.yml") as file:
+        file.write_text(f"name: {name}\napp: {app}\ntemplate: {template}")
+    with Path(root / app) as file:
+        file.write_text(PYTHON_TEMPLATE)
+    with Path(root / template) as file:
+        file.write_text(HTML_TEMPLATE)
+
+
+@cli.command()
+def build():
+    """Build the proyect."""
+
+    safe_name = Path(current_app.config["NAME"].lower().replace("-", "_"))
+    root = safe_name if safe_name.is_dir() else Path.cwd()
+
+
+    with Path(root / "__main__.py") as file:
+        file.write_text(MAIN_TEMPLATE)
+        
+    with Path(root / "index.html") as file:
+        file.write_text(render_template_string(INDEX_TEMPLATE))
+        
+
+
+if __name__ == "__main__":  # pragma: no cover
+    cli(prog_name="bython-dev")
