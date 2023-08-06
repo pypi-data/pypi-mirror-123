3664d631de896c5af17bd9b57ef15d023d2f459d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:49 +0000

    Add package pyIFD/0.0.1/pyIFD-0.0.1.tar.gz

---
+++ packages/pyIFD/pyIFD-0.0.1.tar.gz/pyIFD-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyIFD',
+    version='0.0.1',
+    extras_require=dict(tests=['pytest']),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    setup_requires=[
+        'cython','numpy'],
+    install_requires=[
+        'cython',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'scikit-image',
+        'pillow',
+        'opencv-python',
+        'PyWavelets',]
+)
