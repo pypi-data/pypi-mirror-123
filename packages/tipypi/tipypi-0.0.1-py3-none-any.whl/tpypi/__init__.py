f47ea2ab7b9427e9f27c4af06acc5bbeeba364ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:35 +0000

    Add package tipypi/0.0.1/tipypi-0.0.1-py3-none-any.whl

---
+++ packages/tipypi/tipypi-0.0.1-py3-none-any.whl/tpypi/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/10/15 4:42 下午 
+# @Author : A Big Boby
+# @File : __init__.py.py 
+# @Software: PyCharm
+
+from __future__ import absolute_import
+from tpypi.oh import *
+
+name = "Oh Pig"
