61e35e108007d234078616181bdc209825a30fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:30 +0000

    Add package proyectoFinal/1.1/proyectoFinal-1.1.tar.gz

---
+++ packages/proyectoFinal/proyectoFinal-1.1.tar.gz/proyectoFinal-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='proyectoFinal',
+    version='1.1',
+    author='iduarte',
+    description='pg2 TTS  Irving Duarte',
+    author_email='irving.duarte@outlook.com',
+    packages=find_packages(),
+)
