bb49f1230e5a3a68aa27e2c7f14287d8a88d16b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:18 +0000

    Add package rdbl/0.1.5/rdbl-0.1.5.tar.gz

---
+++ packages/rdbl/rdbl-0.1.5.tar.gz/rdbl-0.1.5/README.md
@@ -0,0 +1,26 @@
+# rdbl
+
+This is a small package made for convenience in handling currencies and other figures. It's installable via:
+
+```console
+pip install git+http://github.com/nikhilwoodruff/rdbl.git
+```
+
+## Usage
+
+Usage is simple enough, just import a specific number function in a Python file and pass to the function:
+
+```
+from rdbl import gbp
+
+gbp(22231155.22)
+
+# out: '£22.2m'
+
+gbp(0.229)
+
+# out: '23p'
+
+gbp(-22333933932)
+
+# out: '-£22.3bn'
---
+++ packages/rdbl/rdbl-0.1.5.tar.gz/rdbl-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+long_description = """This package is a small utility for handling currency amounts and filesizes and converting to human-readable figures."""
+
+setup(
+    name="rdbl",
+    version="0.1.5",
+    author="Nikhil Woodruff",
+    author_email="nikhil.woodruff@outlook.com",
+    packages=["rdbl"],
+    scripts=[],
+    url="https://github.com/PolicyEngine/rdbl",
+    license="",
+    description="A lightweight package to format numbers for readability.",
+    long_description=long_description,
+    install_requires=[],
+)
