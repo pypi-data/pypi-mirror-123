dd8daae546df23aaffee90e95b7c38ac198b2674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:33 +0000

    Add package solana-nfts/0.0.3/solana_nfts-0.0.3-py3-none-any.whl

---
+++ packages/solana-nfts/solana_nfts-0.0.3-py3-none-any.whl/solana_nfts/src/connection.py
@@ -0,0 +1,23 @@
+import requests
+
+from solana_nfts.src.constants import (
+    TOKEN_VIEWER_BASE_URL
+)
+
+class SplTokenViewerApiClient(object):
+    def __init__(self):
+        self.base_url = TOKEN_VIEWER_BASE_URL
+
+    def fetch_nfts_from_wallet_addresses(self, addresses):
+        responses = []
+        for address in addresses:
+            responses.extend(self.fetch_nfts_from_wallet_adddress(address))
+        return responses
+
+    def fetch_nfts_from_wallet_address(self, address):
+        params = {'address': address}
+        response = requests.get(
+            '{}/fetch_nft_data'.format(self.base_url),
+            data=params)
+        return response.json()
+
