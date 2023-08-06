e61c781bc64bee0e80364b98e066972f44e7ffb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:19 +0000

    Add package pyquickhelper/1.10.3653/pyquickhelper-1.10.3653-py3-none-any.whl

---
+++ packages/pyquickhelper/pyquickhelper-1.10.3653-py3-none-any.whl/pyquickhelper/helpgen/process_notebooks_cmd.py
@@ -0,0 +1,28 @@
+"""
+@file
+@brief Calls :epkg:`nbconvert` in command line for latex and pdf.
+"""
+import sys
+import warnings
+
+try:
+    from nbconvert.nbconvertapp import main as nbconvert_main
+except AttributeError as e:
+    raise ImportError("Unable to import nbconvert") from e
+
+
+def run_nbconvert(argv):
+    try:
+        nbconvert_main(argv=argv)
+    except Exception as ee:
+        warnings.warn(
+            "[run_nbconvert-ERROR] Unable to to convert a notebook with "
+            "args=%r due to %r." % (argv, ee), RuntimeWarning)
+
+
+def main():
+    run_nbconvert(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    main()
