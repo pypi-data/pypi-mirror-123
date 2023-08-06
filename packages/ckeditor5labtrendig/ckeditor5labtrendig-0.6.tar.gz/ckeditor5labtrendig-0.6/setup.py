23fef7d1ce8a396b589c0d77f8f5feb4ae645d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:44 +0000

    Add package ckeditor5labtrendig/0.6/ckeditor5labtrendig-0.6.tar.gz

---
+++ packages/ckeditor5labtrendig/ckeditor5labtrendig-0.6.tar.gz/ckeditor5labtrendig-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ckeditor5labtrendig',
+    version='0.6',
+    description='CKEditor 5 for django labtrendig to s3.',
+    url='https://github.com/LabTrendig/django_ckeditor_5',
+    author='josuedjh',
+    author_email='josuedjhcayola@outlook.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["*example*"]),
+    license='BSD 2-clause',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ],
+)
