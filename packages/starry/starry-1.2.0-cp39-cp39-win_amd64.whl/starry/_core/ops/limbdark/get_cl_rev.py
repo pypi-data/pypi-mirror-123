f39b1a5a21231543be5c9b6d2e2325c0a2cd246d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:00 +0000

    Add package starry/1.2.0/starry-1.2.0-cp39-cp39-win_amd64.whl

---
+++ packages/starry/starry-1.2.0-cp39-cp39-win_amd64.whl/starry/_core/ops/limbdark/get_cl_rev.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from ....compat import Apply, tt
+from .base_op import LimbDarkBaseOp
+
+
+__all__ = ["GetClRevOp"]
+
+
+class GetClRevOp(LimbDarkBaseOp):
+
+    __props__ = ()
+    func_file = "./get_cl_rev.cc"
+    func_name = "APPLY_SPECIFIC(get_cl_rev)"
+
+    def make_node(self, bc):
+        return Apply(self, [tt.as_tensor_variable(bc)], [bc.type()])
+
+    def infer_shape(self, *args):
+        shapes = args[-1]
+        return (shapes[0],)
