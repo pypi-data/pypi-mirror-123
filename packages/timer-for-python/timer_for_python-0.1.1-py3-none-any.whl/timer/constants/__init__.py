dfef3c4ebba9804c8058a931d0e96cd30fbba51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:40 +0000

    Add package timer-for-python/0.1.1/timer_for_python-0.1.1-py3-none-any.whl

---
+++ packages/timer-for-python/timer_for_python-0.1.1-py3-none-any.whl/timer/constants/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["decimals", "list_key"]
+
+def none_value() -> str:
+    return "NONE" # NB: Has to be string and upppercase.
---
+++ packages/timer-for-python/timer_for_python-0.1.1-py3-none-any.whl/timer/constants/list_key.py
@@ -0,0 +1,8 @@
+def decimals() -> str:
+    return "decimals"
+
+def start_time() -> str:
+    return "start_time"
+
+def thread() -> str:
+    return "thread"
