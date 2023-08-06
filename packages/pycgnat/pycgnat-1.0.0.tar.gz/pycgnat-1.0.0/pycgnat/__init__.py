11d7d3ff66437649a74787c86772041e8bff194c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:53 +0000

    Add package pycgnat/1.0.0/pycgnat-1.0.0.tar.gz

---
+++ packages/pycgnat/pycgnat-1.0.0.tar.gz/pycgnat-1.0.0/pycgnat/__init__.py
@@ -0,0 +1,4 @@
+# Define the package's version.
+# - Is used for indexing and distributing the package
+# - Follow the conventions defined by PEP440
+__version__ = "1.0.0"
---
+++ packages/pycgnat/pycgnat-1.0.0.tar.gz/pycgnat-1.0.0/pycgnat/settings.py
@@ -0,0 +1,4 @@
+# Define the supported platforms.
+# - Uses the same name defined by the file inside 'generator' package
+# - Is used for auto calling the 'generate' function at runtime execution
+SUPPORTED_PLATFORMS = ("routeros",)
