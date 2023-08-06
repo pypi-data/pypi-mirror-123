3beb2fdfb74ef7419989d3f2e63ef2af8c929b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:06 +0000

    Add package gcoinbuild/0.0.22/gcoinbuild-0.0.22.tar.gz

---
+++ packages/gcoinbuild/gcoinbuild-0.0.22.tar.gz/gcoinbuild-0.0.22/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gcoinbuild",
+    version="0.0.22",
+    author="Yek",
+    author_email="gwojtysiak34@gmail.com",
+    description="Official GCoin Library",
+    long_description=long_description, # don't touch this, this is your README.md
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
