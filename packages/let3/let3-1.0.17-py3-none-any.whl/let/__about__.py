2e87c8e39d37d1a7d5b9eebdf847b0d46a5cc353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:19 +0000

    Add package let3/1.0.17/let3-1.0.17-py3-none-any.whl

---
+++ packages/let3/let3-1.0.17-py3-none-any.whl/let/__about__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2016-2021 Adam Karpierz
+# Licensed under the MIT License
+# https://opensource.org/licenses/MIT
+
+__import__("pkg_about").about("let3")
+__copyright__ = f"Copyright (c) 2016-2021 {__author__}"  # noqa
