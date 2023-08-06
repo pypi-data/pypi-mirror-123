ee816e8dbabd481657c404500216704f3eb53ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:36 +0000

    Add package my-awesome-package-woong/0.1/my-awesome-package-woong-0.1.tar.gz

---
+++ packages/my-awesome-package-woong/my-awesome-package-woong-0.1.tar.gz/my-awesome-package-woong-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='my-awesome-package-woong',
+      version='0.1',
+      description='Test',
+      author='Me',
+      author_email='me@example.com',
+      license='MIT',
+      packages=['helloworld'],
+      zip_safe=False)
