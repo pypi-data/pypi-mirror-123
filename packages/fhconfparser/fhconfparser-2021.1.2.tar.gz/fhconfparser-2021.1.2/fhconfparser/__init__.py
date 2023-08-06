6c82cdd41095331d8ea9858a3ca921566642f879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:56 +0000

    Add package fhconfparser/2021.1.2/fhconfparser-2021.1.2.tar.gz

---
+++ packages/fhconfparser/fhconfparser-2021.1.2.tar.gz/fhconfparser-2021.1.2/fhconfparser/__init__.py
@@ -0,0 +1,16 @@
+"""Provides a config language independent way to read a config file.
+
+## Rationale for project
+For instance toml and ini syntax is very similar but not identical. Currently, tools such as
+pylint must implement custom ways to deal with this. Hopefully this code
+streamlines that a bit.
+
+## Currently supports
+
+- Ini
+- Toml
+- Json
+"""
+
+from .fhconfparser import FHConfParser
+from .simpleconf import SimpleConf
