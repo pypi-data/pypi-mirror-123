0602e1d2e6b8386a02dc6dfb8561da4ad684c494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:18 +0000

    Add package colt/0.7.2/colt-0.7.2-py3-none-any.whl

---
+++ packages/colt/colt-0.7.2-py3-none-any.whl/colt/__init__.py
@@ -0,0 +1,31 @@
+from typing import Any, Callable, Optional, Type, TypeVar, Union
+
+from colt.builder import ColtBuilder
+from colt.default_registry import DefaultRegistry
+from colt.registrable import Registrable  # noqa: F401
+from colt.utils import import_modules  # noqa: F401
+from colt.version import VERSION as __version__  # noqa: F401
+
+T = TypeVar("T")
+
+
+def register(
+    name: str,
+    constructor: Optional[str] = None,
+    exist_ok: bool = False,
+) -> Callable[[Type[T]], Type[T]]:
+    def decorator(cls: Type[T]) -> Type[T]:
+        DefaultRegistry.register(name, constructor, exist_ok)(cls)
+        return cls
+
+    return decorator
+
+
+def build(
+    config: Any,
+    cls: Optional[Type[T]] = None,
+    typekey: Optional[str] = None,
+    argskey: Optional[str] = None,
+) -> Union[T, Any]:
+    builder = ColtBuilder(typekey, argskey)
+    return builder(config, cls)
