e579f1677ab4a35b0be427968a7a7c63a8b7adfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:42 +0000

    Add package fakts/0.1.4/fakts-0.1.4-py3-none-any.whl

---
+++ packages/fakts/fakts-0.1.4-py3-none-any.whl/fakts/qt.py
@@ -0,0 +1,25 @@
+from fakts import Fakts
+from qtpy import QtCore, QtWidgets
+
+
+
+class QtKonfik(Fakts, QtWidgets.QWidget):
+    loaded_signal = QtCore.Signal(bool)
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args,  **kwargs)
+
+    
+    async def aload(self):
+        nana = await super().aload()
+        self.loaded_signal.emit(True)
+        return nana
+
+
+    async def adelete(self):
+        nana = await super().adelete()
+        self.loaded_signal.emit(False)
+        return nana
+
+
+    
