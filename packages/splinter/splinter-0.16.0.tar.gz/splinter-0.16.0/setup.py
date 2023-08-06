cef24ca183e8ad62d68f481c4ef68da45e4f0f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:38 +0000

    Add package splinter/0.16.0/splinter-0.16.0.tar.gz

---
+++ packages/splinter/splinter-0.16.0.tar.gz/splinter-0.16.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2012 splinter authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+from setuptools import setup, find_packages
+import codecs
+
+
+README = codecs.open("README.rst", encoding="utf-8").read()
+
+setup(
+    name="splinter",
+    version="0.16.0",
+    url="https://github.com/cobrateam/splinter",
+    description="browser abstraction for web acceptance testing",
+    long_description=README,
+    author="CobraTeam",
+    author_email="andrewsmedina@gmail.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ]
+    + [("Programming Language :: Python :: %s" % x) for x in "2.7 3.6 3.7 3.8 3.9".split()],
+    packages=find_packages(exclude=["docs", "tests", "samples"]),
+    include_package_data=True,
+    install_requires=["selenium>=3.141.0,<4.0", "six"],
+    extras_require={
+        "zope.testbrowser": ["zope.testbrowser>=5.2.4", "lxml>=4.2.4", "cssselect"],
+        "django": ["Django>=1.7.11", "lxml>=2.3.6", "cssselect", "six"],
+        "flask": ["Flask>=1.0.2", "lxml>=2.3.6", "cssselect"],
+        "edge": ["msedge-selenium-tools"],
+    },
+    tests_require=["coverage", "flask"],
+)
