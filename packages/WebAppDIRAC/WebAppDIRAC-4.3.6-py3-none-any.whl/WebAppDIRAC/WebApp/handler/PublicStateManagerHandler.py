38eeac6e68af06c609c34afd70a6742ce6e71415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:27 +0000

    Add package WebAppDIRAC/4.3.6/WebAppDIRAC-4.3.6-py3-none-any.whl

---
+++ packages/WebAppDIRAC/WebAppDIRAC-4.3.6-py3-none-any.whl/WebAppDIRAC/WebApp/handler/PublicStateManagerHandler.py
@@ -0,0 +1,13 @@
+from WebAppDIRAC.Lib.WebHandler import WebHandler, asyncGen
+
+
+class PublicStateManagerHandler(WebHandler):
+
+  AUTH_PROPS = "authenticated"
+
+  @asyncGen
+  def web_getTreeMenuItems(self):
+    applications = {}
+    desktops = {}
+    tree = {'text': '.',
+            "children": [applications, desktops]}
