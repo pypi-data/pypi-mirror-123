34b0317573fcf251b449b212a2ca1c438aff7c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:01 +0000

    Add package cpgzh/0.1.18/cpgzh-0.1.18.tar.gz

---
+++ packages/cpgzh/cpgzh-0.1.18.tar.gz/cpgzh-0.1.18/cpgzh/__init__.py
@@ -0,0 +1,18 @@
+
+from pgzero import clock, loaders, music,  rect, runner, constants, keyboard
+from pgzero.clock import clock, tick, schedule
+from pgzero.loaders import fonts, sounds, root, images
+from pgzero.rect import Rect
+from pgzero.constants import mouse
+from pgzero.keyboard import keys, keyboard
+
+
+
+from .pen import Pen
+from .pen import Font
+from .data import Data
+from .master import Master
+from .actor import Actor
+from .animation import animate
+from .runner import go
+from .runner import get_screen
