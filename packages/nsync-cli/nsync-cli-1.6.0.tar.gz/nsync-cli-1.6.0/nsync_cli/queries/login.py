aa066548d86eefd5208930f03fa58f87e1b71f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:49 +0000

    Add package nsync-cli/1.6.0/nsync-cli-1.6.0.tar.gz

---
+++ packages/nsync-cli/nsync-cli-1.6.0.tar.gz/nsync-cli-1.6.0/nsync_cli/queries/login.py
@@ -0,0 +1,18 @@
+login_query = """
+mutation {
+  login(
+    input: {
+      username: $username
+      password: $password
+    }
+  ) {
+    user{
+      username
+      sessionExpiration
+    }
+    errors{
+      messages
+    }
+  }
+}
+"""
