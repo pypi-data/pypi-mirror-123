14bcc7505a2d495d591c0be0ee597837cf8cffbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:10 +0000

    Add package entityrdfizer/1.0.3/entityrdfizer-1.0.3-py3-none-any.whl

---
+++ packages/entityrdfizer/entityrdfizer-1.0.3-py3-none-any.whl/entityrdfizer/driver.py
@@ -0,0 +1,28 @@
+from docopt import docopt, DocoptExit
+import entityrdfizer.app as app
+
+
+__doc__ = """csv2rdf
+
+Usage:
+    csv2rdf <csvFileOrDirPath> --csvType=<type> [--outDir=<dir>]
+
+Options:
+--csvType=<type>  Type of the csv file.
+                  Choose between abox/tbox   [default: abox]
+--outDir=<dir>    Output directory path
+"""
+
+def main():
+    try:
+        args = docopt(__doc__)
+    except DocoptExit:
+        raise DocoptExit('Error: parser called with wrong arguments.')
+
+    app.csv2rdf_wrapper(
+        csvFileOrDirPath = args['<csvFileOrDirPath>'],
+        csvType = args['--csvType'],
+        outDir = args['--outDir'])
+
+if __name__ == '__main__':
+    main()
