baf3df1ec91bc69b922444d9b1537e9860d2709d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:41 +0000

    Add package stabilaapi/1.0.3/stabilaapi-1.0.3.tar.gz

---
+++ packages/stabilaapi/stabilaapi-1.0.3.tar.gz/stabilaapi-1.0.3/stabilaapi/constants.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------------------
+# Copyright (c) stabilaclick. All rights reserved.
+# Licensed under the MIT License.
+# See License.txt in the project root for license information.
+# --------------------------------------------------------------------
+
+# Here you can specify links to sites of different types.
+# The default is verified links.
+# If you do not know what links are used for, it is recommended not to change
+DEFAULT_NODES = {
+    'full_node': 'https://api.stabilaapi.org',
+    'solidity_node': 'https://api.stablilascan.org',
+    'event_server': 'https://api.stablilascan.org'
+}
