0ab5879451d5e96c8830adf0a56354d15a170ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:41 +0000

    Add package xsuite/0.1.1/xsuite-0.1.1.tar.gz

---
+++ packages/xsuite/xsuite-0.1.1.tar.gz/xsuite-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages, Extension
+
+#######################################
+# Prepare list of compiled extensions #
+#######################################
+
+extensions = []
+
+
+#########
+# Setup #
+#########
+
+setup(
+    name='xsuite',
+    version='0.1.1',
+    description='Integrated suite for particle acclerator simulation',
+    url='https://github.com/xsuite/xsuite',
+    author='Giovanni Iadarola, Riccardo De Maria',
+    packages=find_packages(),
+    ext_modules = extensions,
+    install_requires=[
+        'numpy>=1.0',
+        'xobjects',
+        'xline',
+        'xtrack',
+        'xfields',
+        'xpart'
+        ]
+    )
