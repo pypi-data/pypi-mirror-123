10e7faaca1d56d526c2095ee8016ff3ddc28c6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:43 +0000

    Add package quart-admin/2.0.5/quart-admin-2.0.5.tar.gz

---
+++ packages/quart-admin/quart-admin-2.0.5.tar.gz/quart-admin-2.0.5/quart_admin/__init__.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+
+from .cli import cli
+
+__version__ = "2.0.5"
+VersionInfo = namedtuple("VersionInfo", "major minor macro release")
+
+version_info = VersionInfo(2, 0, 5, "stable")
