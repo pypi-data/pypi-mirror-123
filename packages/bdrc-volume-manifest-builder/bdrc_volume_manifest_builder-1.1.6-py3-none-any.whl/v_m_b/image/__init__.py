0c26f799e8056b8f59ac214cfcbba3040d2379ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:01 +0000

    Add package bdrc-volume-manifest-builder/1.1.6/bdrc_volume_manifest_builder-1.1.6-py3-none-any.whl

---
+++ packages/bdrc-volume-manifest-builder/bdrc_volume_manifest_builder-1.1.6-py3-none-any.whl/v_m_b/image/__init__.py
@@ -0,0 +1,2 @@
+from v_m_b.image.generateManifest import generateManifest_a, generateManifest_s, fillDataWithBlobImage
+__all__ = ['fillDataWithBlobImage', 'generateManifest_a', 'generateManifest_s']
