1a9a93304b78607dede578e3e7b33e66fb571037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:21 +0000

    Add package sharedlogic/0.1.0/sharedlogic-0.1.0.tar.gz

---
+++ packages/sharedlogic/sharedlogic-0.1.0.tar.gz/sharedlogic-0.1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="sharedlogic",
+    version="0.1.0",
+    description="Repository for shared functions",
+    packages=setuptools.find_packages(),
+)
