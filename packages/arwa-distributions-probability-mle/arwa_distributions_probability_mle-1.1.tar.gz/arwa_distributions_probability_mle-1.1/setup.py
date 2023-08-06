0b5093044f767d10f750738240bf075111256174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:52 +0000

    Add package arwa-distributions-probability-mle/1.1/arwa_distributions_probability_mle-1.1.tar.gz

---
+++ packages/arwa-distributions-probability-mle/arwa_distributions_probability_mle-1.1.tar.gz/arwa_distributions_probability_mle-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='arwa_distributions_probability_mle',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['arwa_distributions_probability_mle'],
+      zip_safe=False)
