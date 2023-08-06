b748a47e02fede905bd84b23d6f4ca040b7c17d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:11 +0000

    Add package redcap-client/0.1.1/redcap_client-0.1.1.tar.gz

---
+++ packages/redcap-client/redcap_client-0.1.1.tar.gz/redcap_client-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['redcap_client']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'redcap-client',
+    'version': '0.1.1',
+    'description': 'A fork of seattleflu/id3c containing only the redcap Project client',
+    'long_description': None,
+    'author': 'Tom Thorogood',
+    'author_email': 'tom@tomthorogood.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6.2,<3.10',
+}
+
+
+setup(**setup_kwargs)
