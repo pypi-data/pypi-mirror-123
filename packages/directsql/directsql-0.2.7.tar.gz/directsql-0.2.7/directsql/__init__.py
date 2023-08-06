d543cc48ccf3b6f0a340a3d0d451dd10f046d86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:37 +0000

    Add package directsql/0.2.7/directsql-0.2.7.tar.gz

---
+++ packages/directsql/directsql-0.2.7.tar.gz/directsql-0.2.7/directsql/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.2.7"
+
+
+
+from .sqlgenerator import SqlGenerator,MysqlSqler
+from .connector import MysqlPool, MysqlConnection
+__all__=["SqlGenerator","MysqlConnection","MysqlPool",]
