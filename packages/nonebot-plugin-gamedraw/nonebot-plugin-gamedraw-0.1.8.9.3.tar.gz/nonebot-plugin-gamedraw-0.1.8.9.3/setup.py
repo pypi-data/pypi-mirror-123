edbde78dcecd2185ffb4945566c6e868825e4967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:41 +0000

    Add package nonebot-plugin-gamedraw/0.1.8.9.3/nonebot-plugin-gamedraw-0.1.8.9.3.tar.gz

---
+++ packages/nonebot-plugin-gamedraw/nonebot-plugin-gamedraw-0.1.8.9.3.tar.gz/nonebot-plugin-gamedraw-0.1.8.9.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nonebot-plugin-gamedraw',
+      version='0.1.8.9.3',
+      description='Nonebot2插件',
+      url='https://github.com/HibiKier/nonebot_plugin_gamedraw',
+      author='hibikier',
+      author_email='775757368@qq.com',
+      license='MIT',
+      packages=['nonebot_plugin_gamedraw'])
