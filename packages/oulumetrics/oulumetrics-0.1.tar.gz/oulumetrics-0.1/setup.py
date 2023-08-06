d07e6e576bdbf8f9a7b3b68c9bb016ab09c33528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:32 +0000

    Add package oulumetrics/0.1/oulumetrics-0.1.tar.gz

---
+++ packages/oulumetrics/oulumetrics-0.1.tar.gz/oulumetrics-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='oulumetrics',
+      version='0.1',
+      description='oulu-npu database metrics implementations (APCER, BPCER, ACER)',
+      packages=['oulumetrics'],
+      author='Guilherme Baldo Carlos',
+      author_email='guilhermebaldocarlos@gmail.com',
+      zip_safe=False)
