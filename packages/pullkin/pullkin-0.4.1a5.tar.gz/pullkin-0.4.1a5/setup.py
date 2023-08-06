2db26f956781423bc0ef4bd9b1f9072067bebf2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:48 +0000

    Add package pullkin/0.4.1a5/pullkin-0.4.1a5.tar.gz

---
+++ packages/pullkin/pullkin-0.4.1a5.tar.gz/pullkin-0.4.1a5/setup.py
@@ -0,0 +1,41 @@
+#!/bin/env python
+
+from setuptools import find_packages, setup
+
+push_receiver_classifiers = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries",
+]
+
+with open("README.md", "r") as f:
+    push_receiver_readme = f.read()
+
+
+def requirements():
+    with open("requirements.txt", "r") as req:
+        return [r for r in req.read().split("\n") if r]
+
+
+setup(
+    name="pullkin",
+    version="0.4.1a5",
+    author="WhiteApfel",
+    author_email="white@pfel.ru",
+    url="https://github.com/WhiteApfel/pullkin",
+    packages=find_packages("."),
+    description="Like Pushkin, but subscribe to GCM/FCM and receive notifications",
+    long_description=push_receiver_readme,
+    long_description_content_type="text/markdown",
+    license="Mozilla Public License 2.0",
+    classifiers=push_receiver_classifiers,
+    keywords="fcm gcm push notification receive firebase google",
+    install_requires=requirements(),
+    extras_require={"listen": ["http-ece"], "example": ["appdirs"]},
+)
