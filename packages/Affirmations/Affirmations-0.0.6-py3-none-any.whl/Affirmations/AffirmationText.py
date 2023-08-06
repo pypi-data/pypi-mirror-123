178e2f23bb0b992d49804de931d5e87f79525e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:52 +0000

    Add package Affirmations/0.0.6/Affirmations-0.0.6-py3-none-any.whl

---
+++ packages/Affirmations/Affirmations-0.0.6-py3-none-any.whl/Affirmations/AffirmationText.py
@@ -0,0 +1,14 @@
+
+affirmations = [
+    "You're Great!",
+    "Way to Go!",
+    "Woo hoo!",
+    "You got out of bed this morning!",
+    "Keep going!",
+    "What nimble fingers you have!",
+    "Awesome!",
+    "Did you ever know that you're my hero?",
+    "Lookin' good.",
+    "You're on your way to glory",
+    "You can do it!"
+]
