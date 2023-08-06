b7a95a2c06048fb65c8c112a98f9b52fb57754f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:04 +0000

    Add package fmyar-binom-quantile/0.1.0/fmyar%20binom%20quantile-0.1.0.tar.gz

---
+++ packages/fmyar-binom-quantile/fmyar%20binom%20quantile-0.1.0.tar.gz/fmyar binom quantile-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='fmyar binom quantile',
+    version='0.1.0',
+    description='A example Python package',
+    url='https://example.com/',
+    author='Yaroslav Ilin',
+    author_email='fmyar@vk.com',
+    license='BSD 2-clause',
+    packages=['superPackage'],
+    install_requires=[
+        'scipy',
+    ],
+)
