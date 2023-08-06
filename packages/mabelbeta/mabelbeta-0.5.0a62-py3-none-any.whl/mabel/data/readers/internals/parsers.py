28b0e3ee8dc0982d563478dc0c929e513090d58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:50 +0000

    Add package mabelbeta/0.5.0a62/mabelbeta-0.5.0a62-py3-none-any.whl

---
+++ packages/mabelbeta/mabelbeta-0.5.0a62-py3-none-any.whl/mabel/data/readers/internals/parsers.py
@@ -0,0 +1,29 @@
+from ...internals import xmler
+import simdjson
+
+
+def json(ds):
+    """parse each line in the file to a dictionary"""
+
+    try:
+        json_parser = simdjson.Parser()
+        return json_parser.parse(ds)
+    except Exception as e:
+        print(ds, e)
+        raise e
+
+
+def pass_thru(ds):
+    """just pass it through"""
+    return ds
+
+
+def pass_thru_block(ds):
+    """each blob is read as a block"""
+    if isinstance(ds, str):
+        return ds
+    return "\n".join([r for r in ds])  # pragma: no cover
+
+
+def xml(ds):
+    return xmler.parse(ds)
