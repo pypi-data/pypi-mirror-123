e7896c36091b1df0a72cee0d18ff6cb926fc9481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:48 +0000

    Add package MetricTool/0.1/MetricTool-0.1-py2.py3-none-any.whl

---
+++ packages/MetricTool/MetricTool-0.1-py2.py3-none-any.whl/MetricTools/File_visit.py
@@ -0,0 +1,32 @@
+
+
+import os
+
+import C_visitor as c_v
+import Java_visitor as j_v
+
+
+# 判定源码文件是否为有效源码文件
+def is_valid_file(f_file):
+    return c_v.is_valid_file(f_file) or j_v.is_valid_file(f_file)
+
+# 对输入文件、目录进行统一遍历
+# 返回有效的源码文件列表
+def visit_files(file_s):
+    # 记录有效源码文件
+    result_files = []
+    # 判定输入是否为文件
+    if os.path.isfile(file_s):
+        if is_valid_file(file_s):
+            result_files.append(file_s)
+        return result_files
+    
+    # 对目录进行遍历
+    for root, dirs, files in os.walk(file_s):
+        # 对目录下文件进行遍历
+        for file in files:
+            f_path = os.path.join(root, file)
+            if is_valid_file(f_path):
+                result_files.append(f_path)
+    
+    return result_files
