41ad1f3a79919650ab5dfa6be100f5c3eee34bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:42 +0000

    Add package vohlc/0.1.0/vohlc-0.1.0.tar.gz

---
+++ packages/vohlc/vohlc-0.1.0.tar.gz/vohlc-0.1.0/tests/test1.py
@@ -0,0 +1,28 @@
+"""
+1. Change the name from Configuration. Since that's the only import, make it meaningful. It's in
+a cile called client, so something like that.
+2. Change the persistence configuration to be more intuitive. I don't like having to pass everything
+in this way, especially with the chaining, that just gets too big.
+3. Test the persistence functionality, other than that it works great.
+Definitely a program that will take some time to run depending on timeframe and start date.
+"""
+import os
+from vohlc import RunBasic, RunCsv, RunSqlite
+
+
+df = RunBasic('coinbasepro', 1000).collect('BTC/USD', '1d', '2021-01-01 00:00:00') \
+    .collect_data().transform_data()
+
+db_path = os.path.join(os.getcwd(), 'data', 'btcusd.db')
+csv_path = os.path.join(os.getcwd(), 'data', 'btcusd.csv')
+
+print(db_path)
+print(csv_path)
+
+print(df)
+sym = 'BTC/USD'
+tf = '1d'
+iso = '2021-01-01 00:00:00'
+
+csv_df = RunCsv('coinbasepro', 1000).collect(sym, tf, iso, csv_path)
+sqlite_df = RunSqlite('coinbasepro', 1000).collect(sym, tf, iso, db_path, 'btc_usd_data')
