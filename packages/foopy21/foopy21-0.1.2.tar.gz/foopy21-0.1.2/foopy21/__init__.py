5dae95743a3dd1d2bcbc36801b1065f1c189041b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:21 +0000

    Add package foopy21/0.1.2/foopy21-0.1.2.tar.gz

---
+++ packages/foopy21/foopy21-0.1.2.tar.gz/foopy21-0.1.2/foopy21/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.2"
+__author__ = 'Yaroslav Zhurba'
+__credits__ = 'ITMO University'
---
+++ packages/foopy21/foopy21-0.1.2.tar.gz/foopy21-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='foopy21',
+    version='0.1.2',
+    description='Packege use numpy 1.16.0 that conflicts with pandas 1.3.3',
+    url='https://github.com/YaroslavZhurba/foopy21',
+    author='Yaroslav Zhurba',
+    author_email='zhurba_ys@mail.ru',
+    license='BSD 2-clause',
+    packages=['foopy21'],
+    install_requires=['numpy==1.16.0']
+)
