c8385c9f270dd7c84e09b2fd56d6cccbb8ac5728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:42 +0000

    Add package sectoolkit/0.1.6/sectoolkit-0.1.6-py3-none-any.whl

---
+++ packages/sectoolkit/sectoolkit-0.1.6-py3-none-any.whl/sectoolkit/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.6
+"
+
+# Import items from sub-modules into sectoolkit namespace
+from .secmeta import (meta, headerfile)
+from .secfiling import (filingDocument, filingArchive, ticker_dict, name_dict)
