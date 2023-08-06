41e6c59e9afaaada292d3d1830f8ad9a107f4e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:56 +0000

    Add package fastapi-lazy/1.2.4/fastapi_lazy-1.2.4-py3-none-any.whl

---
+++ packages/fastapi-lazy/fastapi_lazy-1.2.4-py3-none-any.whl/fastapi_lazy/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "1.2.4"
+
+"""
+    fastapi-lazy - A Lazy package-starter for FastAPI applications.
+"""
+
+import fastapi_lazy.auth
+import fastapi_lazy.database
+import fastapi_lazy.generator
+import fastapi_lazy.models
+import fastapi_lazy.utils
+
+__all__ = [
+    "auth",
+    "database",
+    "generator",
+    "models",
+    "utils",
+]
