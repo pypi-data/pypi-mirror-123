b1ab4ba388fdb8a5463b5499fbde104dd3fb9950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:04 +0000

    Add package MLVisualizationTools/0.0.12/MLVisualizationTools-0.0.12.tar.gz

---
+++ packages/MLVisualizationTools/MLVisualizationTools-0.0.12.tar.gz/MLVisualizationTools-0.0.12/MLVisualizationTools/examples/Demo.py
@@ -0,0 +1,28 @@
+from MLVisualizationTools import Analytics, Interfaces, Graphs
+import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #stops agressive error message printing
+from tensorflow import keras
+
+try:
+    import plotly
+except:
+    raise ImportError("Plotly is required to run this demo. If you don't have plotly installed, install it with"
+                      "`pip install plotly' or run the matplotlib demo instead.")
+
+def main():
+    model = keras.models.load_model('Models/titanicmodel')
+    df = pd.read_csv('Datasets/Titanic/train.csv')
+
+    AR = Analytics.Tensorflow(model, df, ["Survived"])
+    maxvar = AR.maxVariance()
+
+    grid = Interfaces.TensorflowGrid(model, maxvar[0].name, maxvar[1].name, df, ["Survived"])
+    fig = Graphs.PlotlyGrid(grid, maxvar[0].name, maxvar[1].name)
+    fig.show()
+
+    grid = Interfaces.TensorflowGrid(model, 'Parch', 'SibSp', df, ["Survived"])
+    fig = Graphs.PlotlyGrid(grid, 'Parch', 'SibSp')
+    fig.show()
+
+main()
---
+++ packages/MLVisualizationTools/MLVisualizationTools-0.0.12.tar.gz/MLVisualizationTools-0.0.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+#TODO - fix hardcoded versions
+
+setup(
+    name='MLVisualizationTools',
+    url='https://github.com/RobertJN64/MLVisualizationTools',
+    author='Robert Nies',
+    author_email='robertjnies@gamil.com',
+    # Needed to actually package something
+    packages=['MLVisualizationTools'],
+    include_package_data=True,
+    install_requires=['pandas'],
+    extras_require={'dash': ['dash', 'plotly', 'dash-bootstrap-components==1.0.0rc1'],
+                    'dash-notebook': ['MLVisualizationTools[dash]', 'jupyter-dash']},
+    # *strongly* suggested for sharing
+    version='0.0.12',
+    # The license can be anything you like
+    license='MIT',
+    description='A set of functions and demos to make machine learning projects easier to understand.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
