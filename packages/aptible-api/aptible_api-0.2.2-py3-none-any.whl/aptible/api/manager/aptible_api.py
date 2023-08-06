ab1649f392acdf803af5c394f27ad0039d59a672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:06 +0000

    Add package aptible-api/0.2.2/aptible_api-0.2.2-py3-none-any.whl

---
+++ packages/aptible-api/aptible_api-0.2.2-py3-none-any.whl/aptible/api/manager/aptible_api.py
@@ -0,0 +1,42 @@
+import requests
+from typing import Dict
+from .api_manager import ApiManager
+from .aptible_auth_api import AptibleAuthApi
+from ..model.empty_resource import EmptyResource
+
+class AptibleApi(ApiManager):
+    # pylint: disable=inconsistent-return-statements
+
+    def __init__(self, api_base: str = "https://api.aptible.com"):
+        self.bearer_token = None
+        super().__init__(api_base=api_base)
+
+    def authorize(self, token: str = None, email: str = None, password: str = None, scope: str = "manage", expires_in: int = 86400, otp_token: str = None) -> bool:
+        if token:
+            self.bearer_token = token
+
+        else:
+            auth_api = AptibleAuthApi()
+            self.token = auth_api.create_token(
+                grant_type="password",
+                username=email,
+                password=password,
+                otp_token=otp_token,
+                scope=scope,
+                expires_in=expires_in
+            )
+            self.bearer_token = self.token.access_token
+
+        return self.authorized
+
+    @property
+    def request_headers(self) -> Dict:
+        return {
+            'Accepts': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.bearer_token}',
+        }
+
+    @property
+    def authorized(self) -> bool:
+        return bool(self.bearer_token)
