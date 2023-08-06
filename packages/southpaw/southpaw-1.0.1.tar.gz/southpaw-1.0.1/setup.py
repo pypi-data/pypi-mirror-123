3c2ed5bb3d7c7469c14599376a3dcf4b7c1f8507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:12 +0000

    Add package southpaw/1.0.1/southpaw-1.0.1.tar.gz

---
+++ packages/southpaw/southpaw-1.0.1.tar.gz/southpaw-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+DESCRIPTION = 'MMA DFS Toolkit'
+LONG_DESCRIPTION = 'MMA DFS Toolkit written in Python'
+
+# Setting up
+setup(
+    name="southpaw",
+    version=VERSION,
+    author="Brandin Canfield",
+    author_email="<brandincanfield@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['PuLP==2.4'],
+    keywords=['fanduel', 'api', 'mma', 'dfs',
+              'lineup', 'optimizer', 'generator'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
