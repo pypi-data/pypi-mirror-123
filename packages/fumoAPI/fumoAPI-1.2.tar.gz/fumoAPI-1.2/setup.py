60953a88f60a38c4f285967f8ef3eda67cd838ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:38 +0000

    Add package fumoAPI/1.2/fumoAPI-1.2.tar.gz

---
+++ packages/fumoAPI/fumoAPI-1.2.tar.gz/fumoAPI-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name='fumoAPI',
+    version='1.2',
+    description='Simple API wrapper for https://fumos.live',
+    long_description_content_type="text/markdown",
+    long_description="Simple API wrapper for https://fumos.live",
+    license='MIT',
+    packages=find_packages(),
+    author='Adobee',
+    author_email='Adobe@katsumi.cf',
+    keywords=['fumoAPI'],
+    url='https://github.com/Airiuwu/fumoAPI',
+    download_url='https://pypi.org/project/fumoAPI/'
+)
+
+install_requires = [
+    'aiohttp',
+    'requests'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
