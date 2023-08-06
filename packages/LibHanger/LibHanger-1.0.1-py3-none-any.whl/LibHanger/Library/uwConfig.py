fdc4b54c062715ff2823928a50e4c35561291dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:14 +0000

    Add package LibHanger/1.0.1/LibHanger-1.0.1-py3-none-any.whl

---
+++ packages/LibHanger/LibHanger-1.0.1-py3-none-any.whl/LibHanger/Library/uwConfig.py
@@ -0,0 +1,62 @@
+import configparser
+import os
+import pathlib
+import logging
+
+class cmnConfig:
+
+    """
+    共通設定クラス
+    """ 
+
+    def __init__(self):
+
+        """
+        コンストラクタ
+        """
+
+        self.LogFileName = 'default.log'
+        """ ログファイル名 """
+
+        self.LogFolderName = 'log'
+        """ ログ出力先フォルダ名 """
+
+        self.LogFormat = '%(levelname)s : %(asctime)s : %(message)s'
+        """ ログフォーマット定義 """
+
+        self.LogLevel = logging.DEBUG
+        """ ログレベル """
+
+        self.config_ini = None
+        """ iniファイル読込内容 """
+
+    def getConfig(self, scriptPath):
+
+        """ 
+        設定ファイルを読み込む 
+        
+        Parameters
+        ----------
+        self : LibHanger.cmnConfig
+            共通設定クラス
+        scriptPath : string
+            スクリプトファイルパス
+
+        """
+        
+        # configparser宣言
+        config_ini = configparser.ConfigParser()
+
+        # iniファイル読み込み
+        iniDirPath = pathlib.Path(os.path.abspath(os.path.dirname(scriptPath)))
+        iniFilePath = os.path.join(iniDirPath,'config.ini')
+        config_ini.read(iniFilePath, encoding='utf-8')
+
+        # 各設定値をメンバ変数にセット
+        self.LogFileName = config_ini['DEFAULT']['LOGFILE_NAME']
+        self.LogFolderName = config_ini['DEFAULT']['LOGFOLDER_NAME']
+        self.LogFormat = config_ini['DEFAULT']['LOGFORMAT']
+        self.LogLevel = int(config_ini['DEFAULT']['LOGLEVEL'])
+        
+        # 読込内容をメンバ変数に設定する
+        self.config_ini = config_ini
