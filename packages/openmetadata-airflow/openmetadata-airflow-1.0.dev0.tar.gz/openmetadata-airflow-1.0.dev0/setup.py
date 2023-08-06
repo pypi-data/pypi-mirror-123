cc8827ef762de89ea673372c8fbb931cb68ac95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:00 +0000

    Add package openmetadata-airflow/1.0.dev0/openmetadata-airflow-1.0.dev0.tar.gz

---
+++ packages/openmetadata-airflow/openmetadata-airflow-1.0.dev0.tar.gz/openmetadata-airflow-1.0.dev0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='openmetadata-airflow',
+      version='1.0.dev0',
+      description='Python Distribution Utilities',
+        packages=find_namespace_packages(where='./apache_airflow_provider', exclude=['tests*']),
+      entry_points={
+        "apache_airflow_provider": ["provider_info = airflow_provider_openmetadata:get_provider_config"],
+        }
+    )
