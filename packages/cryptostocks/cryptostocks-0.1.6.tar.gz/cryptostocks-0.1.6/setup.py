47f1ab26e4ba3eefa142b2f41119f0670891181f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:35 +0000

    Add package cryptostocks/0.1.6/cryptostocks-0.1.6.tar.gz

---
+++ packages/cryptostocks/cryptostocks-0.1.6.tar.gz/cryptostocks-0.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from pathlib import Path
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(name='cryptostocks',
+      version='0.1.6',
+      description='Get current price of crypto from most popular stock exchanges',
+      packages=['cryptostocks'],
+      #author_email='1mgavura@gmail.com',
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
+      install_requires=[            # I get to this in a second
+          'requests',
+      ],
+      #url = 'https://github.com/mishagavura',
+      zip_safe=False)
