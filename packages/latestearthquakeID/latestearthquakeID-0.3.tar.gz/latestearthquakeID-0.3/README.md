ae74178fc0fb829109b740e2448df9ad7feb6970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:03 +0000

    Add package latestearthquakeID/0.3/latestearthquakeID-0.3.tar.gz

---
+++ packages/latestearthquakeID/latestearthquakeID-0.3.tar.gz/latestearthquakeID-0.3/README.md
@@ -0,0 +1,6 @@
+# latest Indonesia Earthquake
+this package will get the latest earthquake from BMKG | Meteorological, Climatological, and Geophysical Agency
+
+## How it work?
+This package will scrape from [BMKG](https://bmkg.go.id) to get latest quake happened in Indonesia.
+This package will use BeautifulSoup4 and Requests, to produce output in the form of JSON that is ready to be used in web or mobile applications. 
---
+++ packages/latestearthquakeID/latestearthquakeID-0.3.tar.gz/latestearthquakeID-0.3/setup.py
@@ -0,0 +1,32 @@
+"""
+https://packaging.python.org/tutorials/packaging-projects/
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latestearthquakeID",
+    version="0.3",
+    author="Dinda Wahyu Candradewa",
+    author_email="candradewa1981@gmail.com",
+    description="this package will get the latest earthquake from BMKG | Meteorological, Climatological, and Geophysical Agency",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Candyy1st/latest-indonesia-earthquake",
+    project_urls={
+        "Bug Tracker": "https://github.com/Candyy1st/latest-indonesia-earthquake/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
