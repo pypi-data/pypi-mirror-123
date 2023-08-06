442a8c3362df8f891c195ede0a7d7efaa34974a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:54 +0000

    Add package meshkernel/1.0.0/meshkernel-1.0.0.tar.gz

---
+++ packages/meshkernel/meshkernel-1.0.0.tar.gz/meshkernel-1.0.0/meshkernel/__init__.py
@@ -0,0 +1,19 @@
+# If you change these imports,
+# do not forget to sync the docs at "docs/api"
+from meshkernel.errors import InputError, MeshKernelError
+from meshkernel.factories import Mesh2dFactory
+from meshkernel.meshkernel import MeshKernel
+from meshkernel.py_structures import (
+    AveragingMethod,
+    Contacts,
+    DeleteMeshOption,
+    GeometryList,
+    Mesh1d,
+    Mesh2d,
+    Mesh2dLocation,
+    MeshRefinementParameters,
+    OrthogonalizationParameters,
+    ProjectToLandBoundaryOption,
+    RefinementType,
+)
+from meshkernel.version import __version__
