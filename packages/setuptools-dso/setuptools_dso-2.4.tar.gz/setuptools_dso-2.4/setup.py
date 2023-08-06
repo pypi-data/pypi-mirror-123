5a723d89d32c05e609d66f58060c65de89cfd982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:06 +0000

    Add package setuptools-dso/2.4/setuptools_dso-2.4.tar.gz

---
+++ packages/setuptools-dso/setuptools_dso-2.4.tar.gz/setuptools_dso-2.4/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md', 'r') as F:
+    long_description = F.read()
+
+setup(
+    name='setuptools_dso',
+    version="2.4",
+    description="setuptools extension to build non-python shared libraries",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/mdavidsaver/setuptools_dso',
+    project_urls={
+        'Documentation':'https://mdavidsaver.github.io/setuptools_dso',
+        'Release Notes':'https://mdavidsaver.github.io/setuptools_dso/releasenotes.html',
+    },
+
+    author='Michael Davidsaver',
+    author_email='mdavidsaver@gmail.com',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Archiving :: Packaging',
+        'Framework :: Setuptools Plugin',
+        'License :: OSI Approved :: BSD License',
+    ],
+    python_requires='>=2.7',
+    install_requires = ['setuptools'],
+
+    packages=['setuptools_dso'],
+    package_dir={'':'src'},
+)
