f446913acbc8c6b222af3e78affc4cac1461d19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:30 +0000

    Add package checker21/0.2a0/checker21-0.2a0-py3-none-any.whl

---
+++ packages/checker21/checker21-0.2a0-py3-none-any.whl/checker21/checkers/norminette.py
@@ -0,0 +1,15 @@
+from checker21.core import Checker
+
+__all__ = ('NorminetteChecker',)
+
+
+class NorminetteChecker(Checker):
+	name = 'norminette'
+	verbose_name = 'Norminette'
+	description = 'Runs installed norminette to check for files matching Norm'
+
+	def run(self, project, subject):
+		from checker21.management.commands.norminette import Command
+		cmd = Command(stdout=self.stdout, stderr=self.stderr)
+		cmd.set_style(self.style)
+		cmd.handle_check(project)
