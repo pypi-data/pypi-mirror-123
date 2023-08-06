d8664eeab4946e7e9b70ac92da474bd0308d3d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:33 +0000

    Add package smartnoise-sql/0.2.1.1/smartnoise-sql-0.2.1.1.tar.gz

---
+++ packages/smartnoise-sql/smartnoise-sql-0.2.1.1.tar.gz/smartnoise-sql-0.2.1.1/snsql/metadata/__init__.py
@@ -0,0 +1,11 @@
+from .collection import (CollectionMetadata, Table, String, Float, Boolean,
+                         DateTime, Int)
+
+
+__all__ = ["CollectionMetadata",
+           "Table",
+           "String",
+           "Boolean",
+           "DateTime",
+           "Int",
+           "Float"]
