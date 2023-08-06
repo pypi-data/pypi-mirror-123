b131854317de40601a9e5bee3e853c3959708a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:08 +0000

    Add package phevaluator/0.4.0.3/phevaluator-0.4.0.3-py3-none-any.whl

---
+++ packages/phevaluator/phevaluator-0.4.0.3-py3-none-any.whl/tables/__init__.py
@@ -0,0 +1,18 @@
+from .dptables import CHOOSE, DP, SUITS
+from .hashtable import FLUSH
+from .hashtable5 import NO_FLUSH_5
+from .hashtable6 import NO_FLUSH_6
+from .hashtable7 import NO_FLUSH_7
+from .hashtable_omaha import FLUSH_OMAHA, NO_FLUSH_OMAHA
+
+__all__ = [
+    CHOOSE,
+    DP,
+    SUITS,
+    FLUSH,
+    NO_FLUSH_5,
+    NO_FLUSH_6,
+    NO_FLUSH_7,
+    FLUSH_OMAHA,
+    NO_FLUSH_OMAHA,
+]
