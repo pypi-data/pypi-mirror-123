df52c639fb9fcc34d4f60f888926b237e20edcbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:27 +0000

    Add package pincer/0.9.2/pincer-0.9.2.tar.gz

---
+++ packages/pincer/pincer-0.9.2.tar.gz/pincer-0.9.2/pincer/middleware/message_delete.py
@@ -0,0 +1,28 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+"""sent when a message is deleted in a subscribed text channel"""
+
+from ..core.dispatch import GatewayDispatch
+from ..objects.events.message import MessageDeleteEvent
+
+
+async def on_message_delete_middleware(self, payload: GatewayDispatch):
+    """
+    Middleware for ``on_message_delete`` event.
+
+    :param self:
+        The current client.
+
+    :param payload:
+        The data received from the delete message event.
+    """
+    return "on_message_delete", [
+        MessageDeleteEvent.from_dict(
+            {"_client": self, "_http": self.http, **payload.data}
+        )
+    ]
+
+
+def export():
+    return on_message_delete_middleware
---
+++ packages/pincer/pincer-0.9.2.tar.gz/pincer-0.9.2/pincer/middleware/message_update.py
@@ -0,0 +1,30 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+"""sent when a message is updated in a subscribed text channel"""
+
+from ..core.dispatch import GatewayDispatch
+from ..objects import UserMessage
+
+
+async def message_update_middleware(self, payload: GatewayDispatch):
+    """
+    Middleware for ``on_message_update`` event,
+        generate a class for the message that has been updated.
+
+    :param self:
+        The current client.
+
+    :param payload:
+        The data received from the message update event.
+
+    """
+    return "on_message_update", [
+        UserMessage.from_dict(
+            {"_client": self, "_http": self.http, **payload.data}
+        )
+    ]
+
+
+def export():
+    return message_update_middleware
