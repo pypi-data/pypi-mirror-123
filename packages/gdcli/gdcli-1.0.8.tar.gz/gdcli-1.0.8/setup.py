b986c9172abd5f6d55e0ba56520c15953e6c8c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:18 +0000

    Add package gdcli/1.0.8/gdcli-1.0.8.tar.gz

---
+++ packages/gdcli/gdcli-1.0.8.tar.gz/gdcli-1.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name="gdcli",
+    version="1.0.8",
+    author="Subrahmanya s hegade",
+    author_email="hegadedevelopment@gmail.com",
+    description='''
+    Command line too used for simple google dorks
+    ''',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/hegade",
+    keywords=["google dorks","cli","advanced google search"],
+    packages=find_packages(include=['gdcli', 'gdcli.*']),
+    entry_points='''
+    [console_scripts]
+    gdcli=gdcli.gd:start
+    '''
+
+)
