e67d0c33725da2a857c5f2e3ca18c3e54394b0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:28 +0000

    Add package freighter/0.1.2/freighter-0.1.2-py2.py3-none-any.whl

---
+++ packages/freighter/freighter-0.1.2-py2.py3-none-any.whl/freighter/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.2"
+__author__ = "Kai (@kai13xd)"
+__credits__ = "Yoshi2 (RenolY2) & Minty Meeo"
+
+from freighter.constants import*
+from freighter.doltools import *
+from freighter.hooks import *
+from freighter.devkit_tools import Project
+
