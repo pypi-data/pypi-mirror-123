479180f980e98177710e41c014034943fb2159ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:30 +0000

    Add package snap2py/0.1.0/Snap2Py-0.1.0-py3-none-any.whl

---
+++ packages/snap2py/Snap2Py-0.1.0-py3-none-any.whl/snap2py/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '0.1.0'
+
+def say(text):
+  print(text)
+
+def write(text)size(size):
+  print(text)
+
+def if(tf, code):
+  if tf:
+    code
+
+def report(thing):
+  return thing
+
+def (thing1)+(thing2):
+  return thing1 + thing2
+
