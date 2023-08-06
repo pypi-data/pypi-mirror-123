fe11fcfeec6ef1e093b54fc132be83142e9ff448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:42 +0000

    Add package uk-politics/1.0.0/uk-politics-1.0.0.tar.gz

---
+++ packages/uk-politics/uk-politics-1.0.0.tar.gz/uk-politics-1.0.0/setup.py
@@ -0,0 +1,8 @@
+"""Setup shim for local, editable installs."""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(package_data={
+        "": ["*.csv", "*.tsv", "*.license"]
+    })
